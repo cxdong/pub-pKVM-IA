c225a28b7a63937af0482580cc371df4b55b8e6d
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-05-09 18:35:27 +0800

    pkvm: x86: Support clearing BNDREGS/BNDCSR components in the fpstate
    Doing vcpu reset requires to clear the BNDREGS/BNDCSR components in the
    fpstate. Port the code fpstate_clear_xstate_component() from the linux
    kernel to locate the position in the fpstate and do the memory clearing.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/x86.c
+++ arch/x86/kvm/pkvm/x86.c
@@ -2904,14 +2904,28 @@ void kvm_vcpu_reset(struct kvm_vcpu *vcpu, bool init_event)
 	kvm_async_pf_hash_reset(vcpu);
 	vcpu->arch.apf.halted = false;
 
+#ifdef __PKVM_HYP__
 	/*
-	 * FIXME: As the guest fpu is still managed by the host and the pkvm
-	 * hypervisor doesn't have valid fpstate in its vcpu->arch.guest_fpu,
-	 * reset the fpstate for MPX is done by the host when runs its
-	 * kvm_vcpu_reset. To add fpu isolation, revisit to see how to do this
-	 * in the pkvm hypervisor.
+	 * The pkvm hypervisor does the FPU switching for the pVM and the host
+	 * does the FPU switching for the npVM, which means that the pkvm
+	 * hypervisor only needs to take care the fpstate of the pVM. So only
+	 * needs to clearing the BNDREGS/BNDCSR for the pVM.
 	 */
-#ifndef __PKVM_HYP__
+	if (pkvm_is_protected_vcpu(vcpu) &&
+	    vcpu->arch.guest_fpu.fpstate && kvm_mpx_supported()) {
+		struct fpstate *fpstate = vcpu->arch.guest_fpu.fpstate;
+		bool in_use = fpstate->in_use;
+
+		if (in_use)
+			kvm_put_guest_fpu(vcpu);
+
+		fpstate_clear_xstate_component(fpstate, XFEATURE_BNDREGS);
+		fpstate_clear_xstate_component(fpstate, XFEATURE_BNDCSR);
+
+		if (in_use)
+			kvm_load_guest_fpu(vcpu);
+	}
+#else
 	if (vcpu->arch.guest_fpu.fpstate && kvm_mpx_supported()) {
 		struct fpstate *fpstate = vcpu->arch.guest_fpu.fpstate;
 

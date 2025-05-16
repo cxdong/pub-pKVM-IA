intel-staging/pKVM-IA

commit 241210f173f057ca84cd6dcddaa8eccbaa8e52c3

pkvm: x86: Add vcpu_after_set_cpuid PV interface
Add the vcpu_after_set_cpuid PV interface to copy the cpuid entries from
the host KVM to the pkvm hypervisor.

The host KVM allocates pages for the cpuid entries in the pkvm
hypervisor. These pages will be donated to the pkvm hypervisor by this
PV interface, and undonated when VM is destroyed. The pkvm hypervisor
will use kvm_set_cpuid() to copy the cpuid entries from the host KVM to
these pages. If the pkvm hypervisor already have cpuid entries, the old
pages will be undonated and its physical address will be returned back
to the host KVM to free.

Signed-off-by: Zhiyuan Lv <zhiyuan.lv@intel.com>
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -3,6 +3,7 @@
 #include <asm/kvm_pkvm.h>
 #include "x86.h"
 #include "pkvm.h"
+#include "cpuid.h"
 #include <asm/pkvm_spinlock.h>
 //FIXME: clean up the header files
 #include <vmx/pkvm/hyp/mem_protect.h>
@@ -302,8 +303,13 @@ static void pkvm_vm_destroy(int handle)
 
 	for (i = 0; i < to_kvm(pkvm_vm)->created_vcpus; i++) {
 		struct pkvm_vcpu *pkvm_vcpu = pkvm_vm->vcpus[i];
+		struct kvm_vcpu *vcpu = to_kvm_vcpu(pkvm_vcpu);
 
 		detach_pkvm_vcpu_from_vm(pkvm_vcpu, pkvm_vm);
+		teardown_donated_memory(&shared_pkvm->teardown_mc,
+					(void *)vcpu->arch.cpuid_entries,
+					sizeof(struct kvm_cpuid_entry2) *
+					vcpu->arch.cpuid_nent);
 		teardown_donated_memory(&shared_pkvm->teardown_mc,
 					(void *)pkvm_vcpu, pkvm_vcpu->size);
 		/* TODO: unpin shared kvm_vcpu */
@@ -470,6 +476,46 @@ static fastpath_t pkvm_vcpu_run(struct pkvm_vcpu *pkvm_vcpu, bool force_immediat
 	return kvm_vcpu_enter_guest(to_kvm_vcpu(pkvm_vcpu), force_immediate_exit);
 }
 
+static unsigned long pkvm_vcpu_after_set_cpuid(struct pkvm_vcpu *pkvm_vcpu, unsigned long new_pa)
+{
+	struct kvm_cpuid_entry2 *new, *old;
+	unsigned long ret = new_pa;
+	struct kvm_vcpu *vcpu;
+	int nent;
+	u64 size;
+
+	if (WARN_ON_ONCE(!pkvm_vcpu))
+		return ret;
+
+	nent = pkvm_vcpu->shared_vcpu->arch.cpuid_nent;
+	size = PAGE_ALIGN(sizeof(struct kvm_cpuid_entry2) * nent);
+	if (__pkvm_host_donate_hyp(new_pa, size))
+		return ret;
+
+	vcpu = to_kvm_vcpu(pkvm_vcpu);
+	old = vcpu->arch.cpuid_entries;
+	new = __pkvm_va(new_pa);
+
+	if (kvm_set_cpuid(vcpu, new, nent) || vcpu->arch.cpuid_entries != new) {
+		/* New physical page is not consumed */
+		__pkvm_hyp_donate_host(new_pa, size);
+	} else if (vcpu->arch.cpuid_entries == new) {
+		/* New physical page is consumed */
+		if (old) {
+			memset(old, 0, size);
+			/* Let the host VMM to free the old physical pages */
+			ret = __pkvm_pa(old);
+			/* Before that, undonate the old physical pages */
+			__pkvm_hyp_donate_host(ret, size);
+		} else {
+			/* No physical page for the host VMM to free */
+			ret = INVALID_PAGE;
+		}
+	}
+
+	return ret;
+}
+
 static unsigned long pkvm_vcpu_handle_kvm_call(unsigned long fn,
 					       struct kvm_vcpu *shared_vcpu,
 					       unsigned long p2, unsigned  long p3)
@@ -488,6 +534,9 @@ static unsigned long pkvm_vcpu_handle_kvm_call(unsigned long fn,
 		break;
 	case __pkvm__vcpu_run:
 		ret = pkvm_vcpu_run(pkvm_vcpu, (bool)p2);
+		break;
+	case __pkvm__vcpu_after_set_cpuid:
+		ret = pkvm_vcpu_after_set_cpuid(pkvm_vcpu, p2);
 		break;
 	default:
 		ret = -EINVAL;

arch/x86/include/asm/kvm_pkvm.h
@@ -264,6 +264,7 @@ enum pkvm_fn {
 	__pkvm__vcpu_load,
 	__pkvm__vcpu_put,
 	__pkvm__vcpu_run,
+	__pkvm__vcpu_after_set_cpuid,
 };
 
 #endif

arch/x86/kvm/pkvm/cpuid.c
@@ -3,7 +3,11 @@
 #include <reverse_cpuid.h>
 #include <asm/kvm_pkvm.h>
 #include <asm/cpuid.h>
-#include <cpuid.h>
+#include <pkvm/cpuid.h>
+#include <mmu.h>
+#include <pmu.h>
+#include <hyperv.h>
+#include <xen.h>
 
 /*
  * Unlike "struct cpuinfo_x86.x86_capability", kvm_cpu_caps doesn't need to be
@@ -11,6 +15,31 @@
  */
 u32 kvm_cpu_caps[NR_KVM_CPU_CAPS] __read_mostly;
 EXPORT_SYMBOL_GPL(kvm_cpu_caps);
+
+u32 xstate_required_size(u64 xstate_bv, bool compacted)
+{
+	int feature_bit = 0;
+	u32 ret = XSAVE_HDR_SIZE + XSAVE_HDR_OFFSET;
+
+	xstate_bv &= XFEATURE_MASK_EXTEND;
+	while (xstate_bv) {
+		if (xstate_bv & 0x1) {
+			u32 eax, ebx, ecx, edx, offset;
+			cpuid_count(0xD, feature_bit, &eax, &ebx, &ecx, &edx);
+			/* ECX[1]: 64B alignment in compacted form */
+			if (compacted)
+				offset = (ecx & 0x2) ? ALIGN(ret, 64) : ret;
+			else
+				offset = ebx;
+			ret = max(ret, offset + eax);
+		}
+
+		xstate_bv >>= 1;
+		feature_bit++;
+	}
+
+	return ret;
+}
 
 #define F feature_bit
 
@@ -79,6 +108,376 @@ static inline struct kvm_cpuid_entry2 *cpuid_entry2_find(
 	}
 
 	return NULL;
+}
+
+static int kvm_check_cpuid(struct kvm_vcpu *vcpu,
+			   struct kvm_cpuid_entry2 *entries,
+			   int nent)
+{
+	struct kvm_cpuid_entry2 *best;
+	u64 xfeatures;
+
+	/*
+	 * The existing code assumes virtual address is 48-bit or 57-bit in the
+	 * canonical address checks; exit if it is ever changed.
+	 */
+	best = cpuid_entry2_find(entries, nent, 0x80000008,
+				 KVM_CPUID_INDEX_NOT_SIGNIFICANT);
+	if (best) {
+		int vaddr_bits = (best->eax & 0xff00) >> 8;
+
+		if (vaddr_bits != 48 && vaddr_bits != 57 && vaddr_bits != 0)
+			return -EINVAL;
+	}
+
+	/*
+	 * Exposing dynamic xfeatures to the guest requires additional
+	 * enabling in the FPU, e.g. to expand the guest XSAVE state size.
+	 */
+	best = cpuid_entry2_find(entries, nent, 0xd, 0);
+	if (!best)
+		return 0;
+
+	xfeatures = best->eax | ((u64)best->edx << 32);
+	xfeatures &= XFEATURE_MASK_USER_DYNAMIC;
+	if (!xfeatures)
+		return 0;
+
+#ifdef __PKVM_HYP__
+	/*
+	 * TODO: The guest fpu xfd feature is enabled by the host when the host
+	 * KVM run its kvm_check_cpuid function before calling the
+	 * vcpu_after_set_cpuid PV interface. Revisit when implements the fpu
+	 * isolation.
+	 */
+	return 0;
+#else
+	return fpu_enable_guest_xfd_features(&vcpu->arch.guest_fpu, xfeatures);
+#endif
+}
+
+/* Check whether the supplied CPUID data is equal to what is already set for the vCPU. */
+static int kvm_cpuid_check_equal(struct kvm_vcpu *vcpu, struct kvm_cpuid_entry2 *e2,
+				 int nent)
+{
+	struct kvm_cpuid_entry2 *orig;
+	int i;
+
+	if (nent != vcpu->arch.cpuid_nent)
+		return -EINVAL;
+
+	for (i = 0; i < nent; i++) {
+		orig = &vcpu->arch.cpuid_entries[i];
+		if (e2[i].function != orig->function ||
+		    e2[i].index != orig->index ||
+		    e2[i].flags != orig->flags ||
+		    e2[i].eax != orig->eax || e2[i].ebx != orig->ebx ||
+		    e2[i].ecx != orig->ecx || e2[i].edx != orig->edx)
+			return -EINVAL;
+	}
+
+	return 0;
+}
+
+static struct kvm_hypervisor_cpuid __kvm_get_hypervisor_cpuid(struct kvm_cpuid_entry2 *entries,
+							      int nent, const char *sig)
+{
+	struct kvm_hypervisor_cpuid cpuid = {};
+	struct kvm_cpuid_entry2 *entry;
+	u32 base;
+
+	for_each_possible_hypervisor_cpuid_base(base) {
+		entry = cpuid_entry2_find(entries, nent, base, KVM_CPUID_INDEX_NOT_SIGNIFICANT);
+
+		if (entry) {
+			u32 signature[3];
+
+			signature[0] = entry->ebx;
+			signature[1] = entry->ecx;
+			signature[2] = entry->edx;
+
+			if (!memcmp(signature, sig, sizeof(signature))) {
+				cpuid.base = base;
+				cpuid.limit = entry->eax;
+				break;
+			}
+		}
+	}
+
+	return cpuid;
+}
+
+static struct kvm_hypervisor_cpuid kvm_get_hypervisor_cpuid(struct kvm_vcpu *vcpu,
+							    const char *sig)
+{
+	return __kvm_get_hypervisor_cpuid(vcpu->arch.cpuid_entries,
+					  vcpu->arch.cpuid_nent, sig);
+}
+
+static struct kvm_cpuid_entry2 *__kvm_find_kvm_cpuid_features(struct kvm_cpuid_entry2 *entries,
+							      int nent, u32 kvm_cpuid_base)
+{
+	return cpuid_entry2_find(entries, nent, kvm_cpuid_base | KVM_CPUID_FEATURES,
+				 KVM_CPUID_INDEX_NOT_SIGNIFICANT);
+}
+
+static struct kvm_cpuid_entry2 *kvm_find_kvm_cpuid_features(struct kvm_vcpu *vcpu)
+{
+	u32 base = vcpu->arch.kvm_cpuid.base;
+
+	if (!base)
+		return NULL;
+
+	return __kvm_find_kvm_cpuid_features(vcpu->arch.cpuid_entries,
+					     vcpu->arch.cpuid_nent, base);
+}
+
+void kvm_update_pv_runtime(struct kvm_vcpu *vcpu)
+{
+	struct kvm_cpuid_entry2 *best = kvm_find_kvm_cpuid_features(vcpu);
+
+	/*
+	 * save the feature bitmap to avoid cpuid lookup for every PV
+	 * operation
+	 */
+	if (best)
+		vcpu->arch.pv_cpuid.features = best->eax;
+}
+
+/*
+ * Calculate guest's supported XCR0 taking into account guest CPUID data and
+ * KVM's supported XCR0 (comprised of host's XCR0 and KVM_SUPPORTED_XCR0).
+ */
+static u64 cpuid_get_supported_xcr0(struct kvm_cpuid_entry2 *entries, int nent)
+{
+	struct kvm_cpuid_entry2 *best;
+
+	best = cpuid_entry2_find(entries, nent, 0xd, 0);
+	if (!best)
+		return 0;
+
+	return (best->eax | ((u64)best->edx << 32)) & kvm_caps.supported_xcr0;
+}
+
+static void __kvm_update_cpuid_runtime(struct kvm_vcpu *vcpu, struct kvm_cpuid_entry2 *entries,
+				       int nent)
+{
+	struct kvm_cpuid_entry2 *best;
+	struct kvm_hypervisor_cpuid kvm_cpuid;
+
+	best = cpuid_entry2_find(entries, nent, 1, KVM_CPUID_INDEX_NOT_SIGNIFICANT);
+	if (best) {
+		/* Update OSXSAVE bit */
+		if (boot_cpu_has(X86_FEATURE_XSAVE))
+			cpuid_entry_change(best, X86_FEATURE_OSXSAVE,
+					   kvm_is_cr4_bit_set(vcpu, X86_CR4_OSXSAVE));
+
+		cpuid_entry_change(best, X86_FEATURE_APIC,
+			   vcpu->arch.apic_base & MSR_IA32_APICBASE_ENABLE);
+	}
+
+	best = cpuid_entry2_find(entries, nent, 7, 0);
+	if (best && boot_cpu_has(X86_FEATURE_PKU) && best->function == 0x7)
+		cpuid_entry_change(best, X86_FEATURE_OSPKE,
+				   kvm_is_cr4_bit_set(vcpu, X86_CR4_PKE));
+
+	best = cpuid_entry2_find(entries, nent, 0xD, 0);
+	if (best)
+		best->ebx = xstate_required_size(vcpu->arch.xcr0, false);
+
+	best = cpuid_entry2_find(entries, nent, 0xD, 1);
+	if (best && (cpuid_entry_has(best, X86_FEATURE_XSAVES) ||
+		     cpuid_entry_has(best, X86_FEATURE_XSAVEC)))
+		best->ebx = xstate_required_size(vcpu->arch.xcr0, true);
+
+	kvm_cpuid = __kvm_get_hypervisor_cpuid(entries, nent, KVM_SIGNATURE);
+	if (kvm_cpuid.base) {
+		best = __kvm_find_kvm_cpuid_features(entries, nent, kvm_cpuid.base);
+		if (kvm_hlt_in_guest(vcpu->kvm) && best)
+			best->eax &= ~(1 << KVM_FEATURE_PV_UNHALT);
+	}
+
+	if (!kvm_check_has_quirk(vcpu->kvm, KVM_X86_QUIRK_MISC_ENABLE_NO_MWAIT)) {
+		best = cpuid_entry2_find(entries, nent, 0x1, KVM_CPUID_INDEX_NOT_SIGNIFICANT);
+		if (best)
+			cpuid_entry_change(best, X86_FEATURE_MWAIT,
+					   vcpu->arch.ia32_misc_enable_msr &
+					   MSR_IA32_MISC_ENABLE_MWAIT);
+	}
+}
+
+static bool kvm_cpuid_has_hyperv(struct kvm_cpuid_entry2 *entries, int nent)
+{
+#if defined(CONFIG_KVM_HYPERV) && !defined(__PKVM_HYP__)
+	struct kvm_cpuid_entry2 *entry;
+
+	entry = cpuid_entry2_find(entries, nent, HYPERV_CPUID_INTERFACE,
+				  KVM_CPUID_INDEX_NOT_SIGNIFICANT);
+	return entry && entry->eax == HYPERV_CPUID_SIGNATURE_EAX;
+#else
+	return false;
+#endif
+}
+
+static bool guest_cpuid_is_amd_or_hygon(struct kvm_vcpu *vcpu)
+{
+	struct kvm_cpuid_entry2 *entry;
+
+	entry = kvm_find_cpuid_entry(vcpu, 0);
+	if (!entry)
+		return false;
+
+	return is_guest_vendor_amd(entry->ebx, entry->ecx, entry->edx) ||
+	       is_guest_vendor_hygon(entry->ebx, entry->ecx, entry->edx);
+}
+
+static void kvm_vcpu_after_set_cpuid(struct kvm_vcpu *vcpu)
+{
+#ifndef __PKVM_HYP__
+	struct kvm_lapic *apic = vcpu->arch.apic;
+	struct kvm_cpuid_entry2 *best;
+#endif
+	bool allow_gbpages;
+
+	BUILD_BUG_ON(KVM_NR_GOVERNED_FEATURES > KVM_MAX_NR_GOVERNED_FEATURES);
+	bitmap_zero(vcpu->arch.governed_features.enabled,
+		    KVM_MAX_NR_GOVERNED_FEATURES);
+
+	/*
+	 * If TDP is enabled, let the guest use GBPAGES if they're supported in
+	 * hardware.  The hardware page walker doesn't let KVM disable GBPAGES,
+	 * i.e. won't treat them as reserved, and KVM doesn't redo the GVA->GPA
+	 * walk for performance and complexity reasons.  Not to mention KVM
+	 * _can't_ solve the problem because GVA->GPA walks aren't visible to
+	 * KVM once a TDP translation is installed.  Mimic hardware behavior so
+	 * that KVM's is at least consistent, i.e. doesn't randomly inject #PF.
+	 * If TDP is disabled, honor *only* guest CPUID as KVM has full control
+	 * and can install smaller shadow pages if the host lacks 1GiB support.
+	 */
+	allow_gbpages = tdp_enabled ? boot_cpu_has(X86_FEATURE_GBPAGES) :
+				      guest_cpuid_has(vcpu, X86_FEATURE_GBPAGES);
+	if (allow_gbpages)
+		kvm_governed_feature_set(vcpu, X86_FEATURE_GBPAGES);
+
+#ifndef __PKVM_HYP__
+	best = kvm_find_cpuid_entry(vcpu, 1);
+	if (best && apic) {
+		if (cpuid_entry_has(best, X86_FEATURE_TSC_DEADLINE_TIMER))
+			apic->lapic_timer.timer_mode_mask = 3 << 17;
+		else
+			apic->lapic_timer.timer_mode_mask = 1 << 17;
+
+		kvm_apic_set_version(vcpu);
+	}
+#endif
+	vcpu->arch.guest_supported_xcr0 =
+		cpuid_get_supported_xcr0(vcpu->arch.cpuid_entries, vcpu->arch.cpuid_nent);
+
+	kvm_update_pv_runtime(vcpu);
+
+	vcpu->arch.is_amd_compatible = guest_cpuid_is_amd_or_hygon(vcpu);
+	vcpu->arch.maxphyaddr = cpuid_query_maxphyaddr(vcpu);
+	vcpu->arch.reserved_gpa_bits = kvm_vcpu_reserved_gpa_bits_raw(vcpu);
+
+#ifndef __PKVM_HYP__
+	kvm_pmu_refresh(vcpu);
+#endif
+	vcpu->arch.cr4_guest_rsvd_bits =
+	    __cr4_reserved_bits(guest_cpuid_has, vcpu);
+
+	kvm_hv_set_cpuid(vcpu, kvm_cpuid_has_hyperv(vcpu->arch.cpuid_entries,
+						    vcpu->arch.cpuid_nent));
+
+	/* Invoke the vendor callback only after the above state is updated. */
+	kvm_x86_call(vcpu_after_set_cpuid)(vcpu);
+
+	/* FIXME: How to do this with PV EPT? */
+#ifndef __PKVM_HYP__
+	/*
+	 * Except for the MMU, which needs to do its thing any vendor specific
+	 * adjustments to the reserved GPA bits.
+	 */
+	kvm_mmu_after_set_cpuid(vcpu);
+#endif
+}
+
+int cpuid_query_maxphyaddr(struct kvm_vcpu *vcpu)
+{
+	struct kvm_cpuid_entry2 *best;
+
+	best = kvm_find_cpuid_entry(vcpu, 0x80000000);
+	if (!best || best->eax < 0x80000008)
+		goto not_found;
+	best = kvm_find_cpuid_entry(vcpu, 0x80000008);
+	if (best)
+		return best->eax & 0xff;
+not_found:
+	return 36;
+}
+
+/*
+ * This "raw" version returns the reserved GPA bits without any adjustments for
+ * encryption technologies that usurp bits.  The raw mask should be used if and
+ * only if hardware does _not_ strip the usurped bits, e.g. in virtual MTRRs.
+ */
+u64 kvm_vcpu_reserved_gpa_bits_raw(struct kvm_vcpu *vcpu)
+{
+	return rsvd_bits(cpuid_maxphyaddr(vcpu), 63);
+}
+
+int kvm_set_cpuid(struct kvm_vcpu *vcpu, struct kvm_cpuid_entry2 *e2, int nent)
+{
+	int r;
+
+	__kvm_update_cpuid_runtime(vcpu, e2, nent);
+
+	/*
+	 * KVM does not correctly handle changing guest CPUID after KVM_RUN, as
+	 * MAXPHYADDR, GBPAGES support, AMD reserved bit behavior, etc.. aren't
+	 * tracked in kvm_mmu_page_role.  As a result, KVM may miss guest page
+	 * faults due to reusing SPs/SPTEs. In practice no sane VMM mucks with
+	 * the core vCPU model on the fly. It would've been better to forbid any
+	 * KVM_SET_CPUID{,2} calls after KVM_RUN altogether but unfortunately
+	 * some VMMs (e.g. QEMU) reuse vCPU fds for CPU hotplug/unplug and do
+	 * KVM_SET_CPUID{,2} again. To support this legacy behavior, check
+	 * whether the supplied CPUID data is equal to what's already set.
+	 */
+	if (kvm_vcpu_has_run(vcpu)) {
+		r = kvm_cpuid_check_equal(vcpu, e2, nent);
+		if (r)
+			return r;
+
+#ifndef __PKVM_HYP__
+		kvfree(e2);
+#endif
+		return 0;
+	}
+
+#if defined(CONFIG_KVM_HYPERV)
+	if (kvm_cpuid_has_hyperv(e2, nent)) {
+		r = kvm_hv_vcpu_init(vcpu);
+		if (r)
+			return r;
+	}
+#endif
+
+	r = kvm_check_cpuid(vcpu, e2, nent);
+	if (r)
+		return r;
+
+#ifndef __PKVM_HYP__
+	kvfree(vcpu->arch.cpuid_entries);
+#endif
+	vcpu->arch.cpuid_entries = e2;
+	vcpu->arch.cpuid_nent = nent;
+
+	vcpu->arch.kvm_cpuid = kvm_get_hypervisor_cpuid(vcpu, KVM_SIGNATURE);
+#ifdef CONFIG_KVM_XEN
+	vcpu->arch.xen.cpuid = kvm_get_hypervisor_cpuid(vcpu, XEN_SIGNATURE);
+#endif
+	kvm_vcpu_after_set_cpuid(vcpu);
+
+	return 0;
 }
 
 /* Mask kvm_cpu_caps for @leaf with the raw CPUID capabilities of this CPU. */

arch/x86/kvm/pkvm/cpuid.h
@@ -1,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __PKVM_X86_CPUID_H
+#define __PKVM_X86_CPUID_H
+
+#include <cpuid.h>
+
+int kvm_set_cpuid(struct kvm_vcpu *vcpu, struct kvm_cpuid_entry2 *e2, int nent);
+
+#endif

cxdong/pub-pKVM-IA

commit 4b1cbfe305afb9119765143e6a10d5b0aa6c63b9

pkvm: x86: Use memcache to teardown the old cpuid entries memory
The vcpu_after_set_cpuid PV interface will add a new cpuid entries to
the pkvm hypervisor if it is allowed. In this case, the previous cpuid
entries memory physical addresses will be returned back to the host. And
such memory is freed with the size of the new cpuid entries, which is
incorrect. To resolve this, the pkvm hypervisor returns such memory
physical addresses via the pkvm memcache format so that the size will
also be returned to the host.

Fixes: 241210f173f0 ("pkvm: x86: Add vcpu_after_set_cpuid PV interface")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -703,44 +703,64 @@ static unsigned long pkvm_vcpu_run(struct pkvm_vcpu *pkvm_vcpu, bool force_immed
 	return reqs;
 }
 
-static unsigned long pkvm_vcpu_after_set_cpuid(struct pkvm_vcpu *pkvm_vcpu, unsigned long new_pa)
+static unsigned long pkvm_vcpu_after_set_cpuid(struct pkvm_vcpu *pkvm_vcpu,
+					       unsigned long new_entries_gpa,
+					       unsigned long e2size)
 {
+	struct pkvm_memcache mc = {
+		.head = INVALID_PAGE,
+		.nr_pages = 0,
+	};
 	struct kvm_cpuid_entry2 *new, *old;
-	unsigned long ret = new_pa;
+	int new_nent, old_nent;
 	struct kvm_vcpu *vcpu;
-	int nent;
-	u64 size;
+	unsigned long e2pa;
 
-	if (WARN_ON_ONCE(!pkvm_vcpu))
-		return ret;
+	if (!VALID_PAGE(new_entries_gpa) ||
+	    !PAGE_ALIGNED(new_entries_gpa) ||
+	    !PAGE_ALIGNED(e2size) ||
+	    !e2size)
+		return INVALID_PAGE;
 
-	nent = pkvm_vcpu->shared_vcpu->arch.cpuid_nent;
-	size = PAGE_ALIGN(sizeof(struct kvm_cpuid_entry2) * nent);
-	if (__pkvm_host_donate_hyp(new_pa, size))
-		return ret;
+	e2pa = host_gpa2hpa(new_entries_gpa);
+	if (WARN_ON_ONCE(!pkvm_vcpu) ||
+	    __pkvm_host_donate_hyp(e2pa, e2size))
+		goto out;
 
 	vcpu = to_kvm_vcpu(pkvm_vcpu);
+	old_nent = vcpu->arch.cpuid_nent;
 	old = vcpu->arch.cpuid_entries;
-	new = __pkvm_va(new_pa);
+	new_nent = e2size / sizeof(struct kvm_cpuid_entry2);
+	new = __pkvm_va(e2pa);
 
-	if (kvm_set_cpuid(vcpu, new, nent) || vcpu->arch.cpuid_entries != new) {
-		/* New physical page is not consumed */
-		__pkvm_hyp_donate_host(new_pa, size);
-	} else if (vcpu->arch.cpuid_entries == new) {
-		/* New physical page is consumed */
+	if (!kvm_set_cpuid(vcpu, new, new_nent) && (vcpu->arch.cpuid_entries == new)) {
+		/*
+		 * New physical page is consumed. Teardown the old cpuid
+		 * entry memory pages if there is.
+		 */
 		if (old) {
-			memset(old, 0, size);
-			/* Let the host VMM to free the old physical pages */
-			ret = __pkvm_pa(old);
-			/* Before that, undonate the old physical pages */
-			__pkvm_hyp_donate_host(ret, size);
+			e2pa = __pkvm_pa(old);
+			e2size = sizeof(struct kvm_cpuid_entry2) * old_nent;
 		} else {
-			/* No physical page for the host VMM to free */
-			ret = INVALID_PAGE;
+			e2pa = INVALID_PAGE;
+			e2size = 0;
 		}
 	}
+out:
+	if (VALID_PAGE(e2pa))
+		teardown_donated_memory(&mc, (void *)__pkvm_va(e2pa), e2size);
 
-	return ret;
+	if (VALID_PAGE(mc.head)) {
+		/*
+		 * Store the nr_page in the first page of the teardowned memory
+		 * at the offset skipping sizeof(phys_addr_t) where stores the
+		 * next page physical address.
+		 */
+		unsigned long *nr_pages = __pkvm_va(mc.head) + sizeof(phys_addr_t);
+
+		*nr_pages = mc.nr_pages;
+	}
+	return mc.head;
 }
 
 static void pkvm_reset_vcpu(struct pkvm_vcpu *pkvm_vcpu, bool init_event)
@@ -1324,7 +1344,7 @@ static unsigned long pkvm_vcpu_handle_kvm_call(unsigned long fn,
 		ret = pkvm_vcpu_run(pkvm_vcpu, (bool)p2);
 		break;
 	case __pkvm__vcpu_after_set_cpuid:
-		ret = pkvm_vcpu_after_set_cpuid(pkvm_vcpu, p2);
+		ret = pkvm_vcpu_after_set_cpuid(pkvm_vcpu, p2, p3);
 		break;
 	case __pkvm__vcpu_reset:
 		pkvm_reset_vcpu(pkvm_vcpu, (bool)p2);

arch/x86/kvm/vmx/pkvm_high.c
@@ -1760,7 +1760,7 @@ static void pkvm_vcpu_after_set_cpuid(struct kvm_vcpu *vcpu)
 {
 	struct kvm_cpuid_entry2 *e2 = vcpu->arch.cpuid_entries;
 	int nent = vcpu->arch.cpuid_nent;
-	unsigned long unused_pa;
+	unsigned long old_entries_pa;
 	void *entries;
 	size_t size;
 
@@ -1776,10 +1776,16 @@ static void pkvm_vcpu_after_set_cpuid(struct kvm_vcpu *vcpu)
 
 	memcpy(entries, (void *)e2, size);
 
-	unused_pa = kvm_call_pkvm(vcpu_after_set_cpuid, vcpu, __pa(entries));
-	if (VALID_PAGE(unused_pa)) {
-		entries = __va(unused_pa);
-		free_pages_exact(entries, size);
+	old_entries_pa = kvm_call_pkvm(vcpu_after_set_cpuid, vcpu,
+				       __pa(entries), PAGE_ALIGN(size));
+	if (VALID_PAGE(old_entries_pa)) {
+		unsigned long *nr_pages = __va(old_entries_pa) + sizeof(phys_addr_t);
+		struct pkvm_memcache mc = {
+			.head = old_entries_pa,
+			.nr_pages = *nr_pages,
+		};
+
+		free_pkvm_memcache(&mc);
 	}
 }
 
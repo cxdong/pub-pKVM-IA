476ce933c13c4e2c386aaa0cbe65abce9ec8bc8e
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-08-15 14:26:57 +0800

    pkvm: x86: Move pkvm_memory and pkvm_memblock_nr to hypervisor
    The pkvm_memory[] and pkvm_memblock_nr are defined in the host which
    is accessed by the host before deprivileging, and will not be accessible
    for the pkvm hypervisor after enabling the isolation. Move these to the
    pkvm hypervisor space.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/memory.c
+++ arch/x86/kvm/pkvm/memory.c
@@ -1,8 +1,12 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <linux/types.h>
 #include <linux/cache.h>
+#include <asm/kvm_pkvm.h>
 
 #ifdef CONFIG_DYNAMIC_MEMORY_LAYOUT
 unsigned long page_offset_base __ro_after_init;
 #endif
 unsigned long phys_base;
+
+struct memblock_region pkvm_memory[PKVM_MEMBLOCK_REGIONS];
+unsigned int pkvm_memblock_nr;

cxdong/pub-pKVM-IA

commit ec228d74d937ec7a9bf889865bc04132b6df9e40

pkvm: x86: Patch pkvm text for retpoline and return thunk during boot
Kernel boot time code patching mechanism uses quite some retpoline.S
defined symbols directly. All those places should consider pkvm-IA
corresponding symbols as well. Implement the symbol patching for the
pkvm text with two steps:

  step one: Let kernel patch pkvm-IA code just like the rest of Linux
            kernel, without using pKVM specific trampoline symbols;
  step two: Patch the pkvm code by converting the kernel trampoline symbol
            to corresponding pkvm's own trampoline symbol.

Refer to the first new design choice in "CPU Vulnerability Mitigation in
pKVM-IA"[1].

[1]: https://docs.google.com/document/d/1Ashf6F3ifkBMAxs_kmMTNjKs3JEZVfeEjGOkB5Cqv5U/edit?tab=t.0

[ Chuanxiao: Add its support for the pkvm text ]

Co-developed-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
Signed-off-by: Zhiyuan Lv <zhiyuan.lv@intel.com>

arch/x86/kernel/alternative.c
@@ -34,6 +34,7 @@
 #include <asm/cfi.h>
 #include <asm/ibt.h>
 #include <asm/set_memory.h>
+#include <asm/pkvm_image.h>
 
 int __read_mostly alternatives_patched;
 
@@ -239,10 +240,18 @@ static void *its_alloc(void)
 	return no_free_ptr(page);
 }
 
-static void *its_allocate_thunk(int reg)
+static void *its_allocate_thunk(void *addr, int reg)
 {
 	int size = 3 + (reg / 8);
 	void *thunk;
+
+	/*
+	 * For the pkvm hypervisor code, fallback to the static thunks rather
+	 * than using dynamic thunks as the page holding the thunk code belongs
+	 * to the host which will be not accessible to the pkvm after deprivilege.
+	 */
+	if (!is_pkvm_text(addr))
+		return NULL;
 
 #ifdef CONFIG_FINEIBT
 	/*
@@ -805,7 +814,7 @@ static int emit_call_track_retpoline(void *addr, struct insn *insn, int reg, u8 
 static int emit_its_trampoline(void *addr, struct insn *insn, int reg, u8 *bytes)
 {
 	u8 *thunk = __x86_indirect_its_thunk_array[reg];
-	u8 *tmp = its_allocate_thunk(reg);
+	u8 *tmp = its_allocate_thunk(addr, reg);
 
 	if (tmp)
 		thunk = tmp;
@@ -939,6 +948,95 @@ static int patch_retpoline(void *addr, struct insn *insn, u8 *bytes)
 	return i;
 }
 
+#ifdef CONFIG_PKVM_X86
+
+static inline uintptr_t get_pkvm_offset(void *dest)
+{
+#define GEN(reg)								\
+	if (dest == __x86_indirect_thunk_##reg)					\
+		return (void *)&__x86_indirect_thunk_##reg##__pkvm -		\
+		       (void *)&__x86_indirect_thunk_##reg;
+#include <asm/GEN-for-each-reg.h>
+#undef GEN
+
+#ifdef CONFIG_MITIGATION_CALL_DEPTH_TRACKING
+#define GEN(reg)								\
+	if (dest == __x86_indirect_call_thunk_##reg)				\
+		return (void *)&__x86_indirect_call_thunk_##reg##__pkvm -	\
+		       (void *)&__x86_indirect_call_thunk_##reg;
+#include <asm/GEN-for-each-reg.h>
+#undef GEN
+
+#define GEN(reg)								\
+	if (dest == __x86_indirect_jump_thunk_##reg)				\
+		return (void *)&__x86_indirect_jump_thunk_##reg##__pkvm -	\
+		       (void *)&__x86_indirect_jump_thunk_##reg;
+#include <asm/GEN-for-each-reg.h>
+#undef GEN
+#endif
+
+#ifdef CONFIG_MITIGATION_RETHUNK
+#ifdef CONFIG_MITIGATION_SRSO
+	if (dest == srso_alias_return_thunk)
+		return srso_alias_return_thunk__pkvm - srso_alias_return_thunk;
+	if (dest == srso_return_thunk)
+		return srso_return_thunk__pkvm - srso_return_thunk;
+#endif
+#ifdef CONFIG_MITIGATION_UNRET_ENTRY
+	if (dest == retbleed_return_thunk)
+		return retbleed_return_thunk__pkvm - retbleed_return_thunk;
+#endif
+#ifdef CONFIG_MITIGATION_CALL_DEPTH_TRACKING
+	if (dest == call_depth_return_thunk)
+		return call_depth_return_thunk__pkvm - call_depth_return_thunk;
+#endif
+	if (dest == __x86_return_thunk)
+		return __x86_return_thunk__pkvm - __x86_return_thunk;
+#ifdef CONFIG_MITIGATION_ITS
+#define GEN(reg)								\
+	if (dest == __x86_indirect_its_thunk_##reg)				\
+		return (void *)&__x86_indirect_its_thunk_##reg##__pkvm -	\
+		       (void *)&__x86_indirect_its_thunk_##reg;
+#include <asm/GEN-for-each-reg.h>
+#undef GEN
+	if (dest == its_return_thunk)
+		return its_return_thunk__pkvm - its_return_thunk;
+#endif
+#endif
+	return 0;
+}
+
+static void patch_pkvm(void *addr)
+{
+	struct insn insn;
+	uintptr_t offset;
+	void *dest;
+	s32 imm;
+
+	if (!is_pkvm_text(addr))
+		return;
+
+	if (WARN_ON_ONCE(insn_decode_kernel(&insn, addr) < 0))
+		return;
+
+	imm = insn.immediate.value;
+	dest = addr + insn.length + imm;
+	if (is_pkvm_text(dest))
+		return;
+
+	offset = get_pkvm_offset(dest);
+
+	if (WARN_ON_ONCE(offset == 0)) {
+		pr_err_ratelimited("%pS not found in pkvm's symbols\n", dest);
+		return;
+	}
+	apply_reloc(4, &imm, offset);
+	text_poke_early(addr + insn_offset_immediate(&insn), &imm, 4);
+}
+#else
+static inline void patch_pkvm(void *addr) {}
+#endif /* CONFIG_PKVM_X86 */
+
 /*
  * Generated by 'objtool --retpoline'.
  */
@@ -997,6 +1095,15 @@ void __init_or_module noinline apply_retpolines(s32 *start, s32 *end)
 			DUMP_BYTES(RETPOLINE, ((u8*)bytes), len, "%px: repl: ", addr);
 			text_poke_early(addr, bytes, len);
 		}
+
+		/*
+		 * Patch the pkvm hypervisor text following the same check condition
+		 * as done by the patch_retpoline.
+		 */
+		if ((cpu_feature_enabled(X86_FEATURE_RETPOLINE) &&
+		     !cpu_feature_enabled(X86_FEATURE_RETPOLINE_LFENCE)) ||
+		    cpu_feature_enabled(X86_FEATURE_INDIRECT_THUNK_ITS))
+			patch_pkvm(addr);
 	}
 }
 
@@ -1072,7 +1179,7 @@ void __init_or_module noinline apply_returns(s32 *start, s32 *end)
 		    WARN_ONCE(dest != &__x86_return_thunk,
 			      "missing return thunk: %pS-%pS: %*ph",
 			      addr, dest, 5, addr))
-			continue;
+			goto try_patch_pkvm;
 
 		DPRINTK(RET, "return thunk at: %pS (%px) len: %d to: %pS",
 			addr, addr, insn.length,
@@ -1084,6 +1191,9 @@ void __init_or_module noinline apply_returns(s32 *start, s32 *end)
 			DUMP_BYTES(RET, ((u8*)bytes), len, "%px: repl: ", addr);
 			text_poke_early(addr, bytes, len);
 		}
+try_patch_pkvm:
+		if (cpu_feature_enabled(X86_FEATURE_RETHUNK))
+			patch_pkvm(addr);
 	}
 }
 #else /* !CONFIG_MITIGATION_RETHUNK: */
@@ -1681,7 +1791,7 @@ static int emit_paranoid_trampoline(void *addr, struct insn *insn, int reg, u8 *
 	u8 *thunk = (void *)__x86_indirect_its_thunk_array[reg] - 2;
 
 #ifdef CONFIG_MITIGATION_ITS
-	u8 *tmp = its_allocate_thunk(reg);
+	u8 *tmp = its_allocate_thunk(addr, reg);
 	if (tmp)
 		thunk = tmp;
 #endif

arch/x86/include/asm/nospec-branch.h
@@ -490,6 +490,57 @@ static inline void call_depth_return_thunk(void) {}
 # define THUNK_TARGET(addr) [thunk_target] "rm" (addr)
 #endif
 
+#ifdef CONFIG_PKVM_X86
+#ifdef CONFIG_MITIGATION_RETPOLINE
+#define GEN(reg)								\
+	extern retpoline_thunk_t __x86_indirect_thunk_##reg##__pkvm;
+#include <asm/GEN-for-each-reg.h>
+#undef GEN
+#endif
+
+#ifdef CONFIG_MITIGATION_RETHUNK
+extern void __x86_return_thunk__pkvm(void);
+#else
+static inline void __x86_return_thunk__pkvm(void) {}
+#endif
+
+#ifdef CONFIG_MITIGATION_UNRET_ENTRY
+extern void retbleed_return_thunk__pkvm(void);
+#else
+static inline void retbleed_return_thunk__pkvm(void) {}
+#endif
+
+#ifdef CONFIG_MITIGATION_SRSO
+extern void srso_return_thunk__pkvm(void);
+extern void srso_alias_return_thunk__pkvm(void);
+#else
+static inline void srso_return_thunk__pkvm(void) {}
+static inline void srso_alias_return_thunk__pkvm(void) {}
+#endif
+
+#ifdef CONFIG_MITIGATION_CALL_DEPTH_TRACKING
+extern void call_depth_return_thunk__pkvm(void);
+#define GEN(reg)								\
+	extern retpoline_thunk_t __x86_indirect_call_thunk_##reg##__pkvm;	\
+	extern retpoline_thunk_t __x86_indirect_jump_thunk_##reg##__pkvm;
+#include <asm/GEN-for-each-reg.h>
+#undef GEN
+#else
+static inline void call_depth_return_thunk__pkvm(void) {}
+#endif
+
+#ifdef CONFIG_MITIGATION_ITS
+extern void its_return_thunk__pkvm(void);
+#define GEN(reg)								\
+	extern its_thunk_t __x86_indirect_its_thunk_##reg;			\
+	extern its_thunk_t __x86_indirect_its_thunk_##reg##__pkvm;
+#include <asm/GEN-for-each-reg.h>
+#undef GEN
+#else
+static inline void its_return_thunk__pkvm(void) {}
+#endif
+#endif /* CONFIG_PKVM_X86 */
+
 /* The Spectre V2 mitigation variants */
 enum spectre_v2_mitigation {
 	SPECTRE_V2_NONE,

commit abbfea2e86fa971c43bcd1dd2638ec6be68e6635

Fixes: pkvm: x86: Incorrect pkvm text check in its_alloc_thunk
Fixes: ec228d7 ("pkvm: x86: Patch pkvm text for retpoline and return thunk during boot")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kernel/alternative.c
@@ -250,7 +250,7 @@ static void *its_allocate_thunk(void *addr, int reg)
 	 * than using dynamic thunks as the page holding the thunk code belongs
 	 * to the host which will be not accessible to the pkvm after deprivilege.
 	 */
-	if (!is_pkvm_text(addr))
+	if (is_pkvm_text(addr))
 		return NULL;
 
 #ifdef CONFIG_FINEIBT
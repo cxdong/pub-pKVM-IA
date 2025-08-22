1f9cc87c5701b9c65af4aeb060af5790f33674d0
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-08-22 14:48:45 +0800

    pkvm: x86: Add helper to kick vcpu
    Add helper to kick a running vcpu out of non-root mode. As the interrupt
    is passthrough to the host VM, sending IPI interrupt while a host vcpu
    is running will not make it out of non-root mode. Use INIT which can
    kick a vcpu out of non-root mode for this case. The limitation of using
    INIT is that, the CPU cannot receive this INIT signal if it is in root
    mode, until enter the guest. The INIT signal vmexit is handle by
    handling the pending events, which is already covered in the
    pkvm_host_vmexit_main().
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/pkvm.h
+++ arch/x86/kvm/pkvm/pkvm.h
@@ -10,5 +10,6 @@ DECLARE_PER_CPU(struct kvm_vcpu *, host_vcpu);
 int pkvm_handle_kvm_call(unsigned long func, unsigned long a0,
 			 unsigned long a1, unsigned long a2,
 			 unsigned long a3);
+void pkvm_kick_vcpu(struct kvm_vcpu *vcpu);
 
 #endif /* __PKVM_X86_PKVM_H */

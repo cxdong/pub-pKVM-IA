012afad0bafc23a2791aef6ac45258e1749845e2
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-08-27 10:31:54 +0800

    pkvm: vmx: Initialize the host mmu as EPT
    To achieve the isolation purpose, the EPT should be enabled for the host
    VM. Add the function pkvm_host_ept_init() to initialize the host mmu
    with the buddy allocator mm_ops and ept type pgt_ops, as an EPT
    instance.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/vmx/host_vmx.c
+++ arch/x86/kvm/pkvm/vmx/host_vmx.c
@@ -6,6 +6,7 @@
 #include "init_finalize.h"
 #include "host_vmx.h"
 #include "pkvm.h"
+#include "ept.h"
 
 #define CR4			4
 #define MOV_TO_CR		0
@@ -25,6 +26,7 @@ static int vmx_hyp_mmu_finalize(struct pkvm_pgtable *pgt)
 
 static struct pkvm_init_ops vmx_init_ops = {
 	.hyp_mmu_finalize = vmx_hyp_mmu_finalize,
+	.host_mmu_init = pkvm_host_ept_init,
 };
 
 static void skip_emulated_instruction(void)

0d1f7e4738861e9da2c2fce524bc60f23e274ce1
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-09-01 08:53:53 +0800

    pKVM: x86: Duplicate the physical_mask symbol
    Add a pKVM-local physical_mask and initialize it from the trusted host.
    The hypervisor can then mask and validate physical-address bits without
    accessing the host's symbol after isolation.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/include/asm/kvm_pkvm.h
+++ arch/x86/include/asm/kvm_pkvm.h
@@ -104,6 +104,9 @@ extern u64 pkvm_sym(x86_pred_cmd);
 extern struct memblock_region pkvm_sym(pkvm_memory)[];
 extern unsigned int pkvm_sym(pkvm_memblock_nr);
 extern struct cpuinfo_x86 pkvm_sym(boot_cpu_data);
+#ifdef CONFIG_DYNAMIC_PHYSICAL_MASK
+extern phys_addr_t pkvm_sym(physical_mask);
+#endif
 
 PKVM_DECLARE(void *, pkvm_early_alloc_page, (void));
 PKVM_DECLARE(void *, pkvm_early_alloc_contig, (unsigned int nr_pages));

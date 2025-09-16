b322cd78b6a2f1e67d207e58fb5328205dbc0ef3
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-09-16 14:56:42 +0800

    pKVM: x86: Add __pkvm_init_finalize PV interface
    Introduce the __pkvm_init_finalize PV interface for the host VM to
    finalize the deprivileging for each CPU. It will create pKVM's own mmu
    and host mmu (the 2nd-level page table for the host VM) to isolate the
    pKVM hypervisor from the host, as well as any other necessary setup to
    make the pKVM hypervisor fully functional.
    
    The initial implementation is just create this infrastructure and the
    detailed finalizing will be done by the follow patches.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/pkvm.h
+++ arch/x86/kvm/pkvm/pkvm.h
@@ -7,4 +7,8 @@
 DECLARE_PER_CPU(struct pkvm_pcpu *, phys_cpu);
 DECLARE_PER_CPU(struct kvm_vcpu *, host_vcpu);
 
+int pkvm_handle_kvm_call(unsigned long func, unsigned long a0,
+			 unsigned long a1, unsigned long a2,
+			 unsigned long a3);
+
 #endif /* __PKVM_X86_PKVM_H */

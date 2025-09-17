daffd0e703a446719fa3c65e2329f5a51a7e62af
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-09-17 12:24:51 +0800

    pKVM: x86: Setup percpu area for the pKVM hypervisor
    pKVM cannot use the host's per-CPU storage after isolation, but private
    per-CPU storage allows pKVM to reuse code that relies on the kernel
    per-CPU model.
    
    Collect pKVM per-CPU symbols in a dedicated section and reserve enough
    pages for every possible CPU. Initialize each area's __per_cpu_offset
    and this_cpu_off values at the corresponding Linux CPU index. The
    __per_cpu_offset will be used as pKVM's GS_BASE to locate pKVM's each
    per-cpu storage by the next patch.
    
    Squash changelog:
      c1a901671fde ("pKVM: x86: Index host_vcpus and pcpus via consecutive number")
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/include/asm/kvm_pkvm.h
+++ arch/x86/include/asm/kvm_pkvm.h
@@ -4,6 +4,7 @@
 
 #ifdef CONFIG_PKVM_X86
 #include <linux/bug.h>
+#include <linux/kvm_host.h>
 #include <linux/mm.h>
 #include <asm/pkvm_image.h>
 #include <asm/pkvm_redef.h>
@@ -47,12 +48,16 @@ extern unsigned long pkvm_sym(phys_base);
 PKVM_DECLARE(void *, pkvm_early_alloc_page, (void));
 PKVM_DECLARE(void *, pkvm_early_alloc_contig, (unsigned int nr_pages));
 PKVM_DECLARE(void, pkvm_early_alloc_init, (void *virt, unsigned long size));
+PKVM_DECLARE(int, pkvm_setup_per_cpu, (int cpu, unsigned long base,
+				       unsigned long pcpu_pa, unsigned long vcpu_pa));
+PKVM_DECLARE(unsigned int, pkvm_per_cpu_nr_pages, (void));
 
 static inline unsigned long pkvm_data_pages(unsigned long extra_global,
 					    unsigned long extra_percpu)
 {
 	unsigned long global_pages = PKVM_HYP_PAGES + extra_global;
-	unsigned long percpu_pages = PKVM_PCPU_PAGES + extra_percpu;
+	unsigned long percpu_pages = PKVM_PCPU_PAGES + extra_percpu +
+				     pkvm_sym(pkvm_per_cpu_nr_pages)();
 
 	return global_pages + percpu_pages * num_possible_cpus();
 }

c2a8b8e0f23e07027cf2c41b2e2fbfe2a34dd947
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-09-28 13:45:44 +0800

    pKVM: x86: Expose PKVM_VMX_VCPU_SIZE
    Expose PKVM_VMX_VCPU_SIZE to allow the VMX host to know the size of a
    vCPU instance which is managed by the pKVM hypervisor, so that no need
    to expose struct pkvm_vcpu which is internal to the pKVM hypervisor.
    
    Defining and exposing the size for the VMX host is sufficient as
    currently VMX host is the only supported host by the pKVM hypervisor.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/pkvm_constants.c
+++ arch/x86/kvm/pkvm/pkvm_constants.c
@@ -3,10 +3,13 @@
 #include <vmx/vmx.h>
 #include "memory.h"
 #include "pkvm.h"
+#include "x86.h"
 
 int main(void)
 {
 	DEFINE(PKVM_VMEMMAP_ENTRY_SIZE, sizeof(struct pkvm_page));
 	DEFINE(PKVM_VMX_VM_SIZE, PKVM_VM_BASE_SIZE + sizeof(struct kvm_vmx));
+	DEFINE(PKVM_VMX_VCPU_SIZE, PKVM_VCPU_BASE_SIZE + sizeof(struct vcpu_vmx) +
+				   KVM_MCE_SIZE + KVM_MCI_CTL2_SIZE);
 	return 0;
 }

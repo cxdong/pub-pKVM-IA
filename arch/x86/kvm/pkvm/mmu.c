cxdong/pub-pKVM-IA

commit f6e09622cf441b5bb2fdadaa6bb1ac50c642b410

pkvm: x86: Add vendor host mmu init ops
Add the host mmu instance to represent the 2nd-level page table for the
host VM. The host_mmu is a x86 generic instance. Different vendors may
have different initializations, e.g., initialize the host_mmu as an EPT
on Intel platforms.

Add the host_mmu_init callback to the pkvm_init_ops, for vendors to
initialize the host mmu with the memory pool divided from the reserved
memory area during the finalize.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/mmu.c
@@ -10,6 +10,8 @@
 static struct pkvm_pgtable hyp_mmu;
 static struct pkvm_pool hyp_mmu_pool;
 
+static struct pkvm_pgtable host_mmu;
+
 static void *hyp_mmu_zalloc_page(void)
 {
 	return pkvm_alloc_pages(&hyp_mmu_pool, 0);
@@ -229,4 +231,9 @@ int pkvm_hyp_mmu_map(unsigned long vaddr, unsigned long phys,
 		     unsigned long size, u64 prot)
 {
 	return pkvm_pgtable_map(&hyp_mmu, vaddr, phys, size, prot);
+}
+
+int pkvm_host_mmu_init(void *pool_base, unsigned long pool_pages, host_mmu_init_fn_t fn)
+{
+	return fn ? fn(&host_mmu, pool_base, pool_pages) : 0;
 }

arch/x86/kvm/pkvm/init_finalize.c
@@ -7,6 +7,7 @@
 #include "mmu.h"
 
 static void *hyp_pgt_base;
+static void *host_pgt_base;
 static void *pkvm_vmemmap_base;
 
 static int divide_memory_pool(phys_addr_t phys, unsigned long size)
@@ -18,6 +19,11 @@ static int divide_memory_pool(phys_addr_t phys, unsigned long size)
 	nr_pages = pkvm_hyp_pgtable_pages();
 	hyp_pgt_base = pkvm_early_alloc_contig(nr_pages);
 	if (!hyp_pgt_base)
+		return -ENOMEM;
+
+	nr_pages = pkvm_host_pgtable_pages();
+	host_pgt_base = pkvm_early_alloc_contig(nr_pages);
+	if (!host_pgt_base)
 		return -ENOMEM;
 
 	nr_pages = pkvm_vmemmap_pages(sizeof(struct pkvm_page));
@@ -129,8 +135,10 @@ static int create_hyp_mmu(const struct pkvm_mem_info infos[], int nr_infos)
 	return pkvm_hyp_mmu_switch_to_buddy(hyp_pgt_base, nr_pages);
 }
 
-static int finalize_global(struct pkvm_mem_info infos[], int nr_infos)
+static int finalize_global(struct pkvm_mem_info infos[], int nr_infos,
+			   struct pkvm_init_ops *init_ops)
 {
+	host_mmu_init_fn_t host_mmu_init_fn = init_ops ? init_ops->host_mmu_init : NULL;
 	phys_addr_t mem_base = INVALID_PAGE;
 	unsigned long mem_size = 0;
 	int i, ret;
@@ -153,7 +161,12 @@ static int finalize_global(struct pkvm_mem_info infos[], int nr_infos)
 	if (ret)
 		return ret;
 
-	return create_hyp_mmu(infos, nr_infos);
+	ret = create_hyp_mmu(infos, nr_infos);
+	if (ret)
+		return ret;
+
+	return pkvm_host_mmu_init(host_pgt_base, pkvm_host_pgtable_pages(),
+				  host_mmu_init_fn);
 }
 
 int pkvm_init_finalize(struct pkvm_mem_info infos[], int nr_infos,
@@ -164,7 +177,7 @@ int pkvm_init_finalize(struct pkvm_mem_info infos[], int nr_infos,
 	static bool global_finalized;
 
 	if (!global_finalized) {
-		int ret = finalize_global(infos, nr_infos);
+		int ret = finalize_global(infos, nr_infos, init_ops);
 
 		if (ret)
 			return ret;

arch/x86/kvm/pkvm/init_finalize.h
@@ -6,6 +6,8 @@
 #include "pgtable.h"
 
 typedef int (*hyp_mmu_finalize_fn_t)(struct pkvm_pgtable *pgt);
+typedef int (*host_mmu_init_fn_t)(struct pkvm_pgtable *pgt, void *pool_base,
+				  unsigned long pool_pages);
 
 /**
  * pkvm_init_ops - The platform vendor specific pkvm finalize operations used by
@@ -13,9 +15,11 @@ typedef int (*hyp_mmu_finalize_fn_t)(struct pkvm_pgtable *pgt);
  *		   not necessary.
  *
  * @hyp_mmu_finalize:	Finalize the hypervisor mmu.
+ * @host_mmu_init:	Initialize the host mmu.
  */
 struct pkvm_init_ops {
 	hyp_mmu_finalize_fn_t		hyp_mmu_finalize;
+	host_mmu_init_fn_t		host_mmu_init;
 };
 
 int pkvm_init_finalize(struct pkvm_mem_info infos[], int nr_info,

arch/x86/kvm/pkvm/mmu.h
@@ -11,4 +11,7 @@ int pkvm_hyp_mmu_finalize(hyp_mmu_finalize_fn_t fn);
 int pkvm_hyp_mmu_map(unsigned long vaddr, unsigned long phys,
 		     unsigned long size, u64 prot);
 
+int pkvm_host_mmu_init(void *pool_base, unsigned long pool_pages,
+		       host_mmu_init_fn_t fn);
+
 #endif /* __PKVM_X86_MMU_H */

commit 0d91cddf1d72a31884422531dc3670c0936dabde

fixes: pkvm: x86: Return negative value if no host mmu init vendor ops
Fixes: f6e0962 ("pkvm: x86: Add vendor host mmu init ops")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/mmu.c
@@ -238,7 +238,7 @@ int pkvm_host_mmu_init(void *pool_base, unsigned long pool_pages, host_mmu_init_
 {
 	pkvm_spin_lock_init(&host_mmu_lock);
 
-	return fn ? fn(&host_mmu, pool_base, pool_pages) : 0;
+	return fn ? fn(&host_mmu, pool_base, pool_pages) : -EOPNOTSUPP;
 }
 
 int pkvm_host_mmu_finalize(host_mmu_finalize_fn_t fn)
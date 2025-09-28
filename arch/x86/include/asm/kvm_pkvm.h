cxdong/pub-pKVM-IA

commit 1f061ef7f2e60f0a1166ca304cc01430c7342d17

pkvm: x86: Create memory pool for pkvm mmu
To create pkvm's own mmu, additional memory pages should be reserved
during the early boot. The number of reserved memory pages are
calculated according to the memory size, with supporting the maximum 5
level page table.

During the finalize, create the pkvm mmu pool from the unused reserved
memory area, which will be used to setup pkvm's own mmu. As this is only
one time job, introduced global_finalize() to do this only on the first
finalized CPU. All the subsequent finalized CPUs will skip this.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/include/asm/kvm_pkvm.h
@@ -3,6 +3,7 @@
 #define _ASM_X86_KVM_PKVM_H
 
 #ifdef CONFIG_PKVM_X86
+#include <linux/memblock.h>
 #include <linux/mm.h>
 #include <linux/memblock.h>
 #include <asm/pkvm_image.h>
@@ -12,6 +13,7 @@
 #define PKVM_STACK_SIZE			SZ_16K
 /* Size of reserved space for private parameter in pkvm stack */
 #define PKVM_STACK_TOP_RESV		16
+#define PKVM_PGTABLE_MAX_LEVELS		5
 
 struct idt_page {
 	gate_desc idt[IDT_ENTRIES];
@@ -100,6 +102,37 @@ static inline unsigned long pkvm_data_pages(unsigned long extra_global,
 static inline unsigned long get_host_stack_top(struct pkvm_pcpu *pcpu)
 {
 	return (unsigned long) &pcpu->stack[sizeof(pcpu->stack)];
+}
+
+static inline unsigned long __pkvm_pgtable_max_pages(unsigned long nr_pages)
+{
+	unsigned long total = 0, i;
+
+	/* Provision the worst case */
+	for (i = 0; i < PKVM_PGTABLE_MAX_LEVELS; i++) {
+		nr_pages = DIV_ROUND_UP(nr_pages, PTRS_PER_PTE);
+		total += nr_pages;
+	}
+
+	return total;
+}
+
+static inline unsigned long __pkvm_pgtable_total_pages(void)
+{
+	unsigned long total = 0, i;
+
+	for (i = 0; i < pkvm_sym(pkvm_memblock_nr); i++) {
+		struct memblock_region *reg = &pkvm_sym(pkvm_memory)[i];
+
+		total += __pkvm_pgtable_max_pages(reg->size >> PAGE_SHIFT);
+	}
+
+	return total;
+}
+
+static inline unsigned long pkvm_hyp_pgtable_pages(void)
+{
+	return __pkvm_pgtable_total_pages();
 }
 
 #endif /* CONFIG_PKVM_X86 */

arch/x86/kvm/pkvm/init_finalize.c
@@ -1,7 +1,44 @@
 // SPDX-License-Identifier: GPL-2.0
+#include <asm/kvm_pkvm.h>
 #include "init_finalize.h"
+#include "memory.h"
 
-int pkvm_init_finalize(void)
+static void *hyp_pgt_base;
+
+static int divide_memory_pool(phys_addr_t phys, unsigned long size)
 {
+	unsigned long nr_pages;
+
+	pkvm_early_alloc_init(__pkvm_va(phys), size);
+
+	nr_pages = pkvm_hyp_pgtable_pages();
+	hyp_pgt_base = pkvm_early_alloc_contig(nr_pages);
+	if (!hyp_pgt_base)
+		return -ENOMEM;
+
+	return 0;
+}
+
+static int finalize_global(phys_addr_t mem_base, unsigned long mem_size)
+{
+	if (!PAGE_ALIGNED(mem_base) || !mem_size)
+		return -EINVAL;
+
+	return divide_memory_pool(mem_base, mem_size);
+}
+
+int pkvm_init_finalize(phys_addr_t mem_base, unsigned long mem_size)
+{
+	static bool global_finalized;
+
+	if (!global_finalized) {
+		int ret = finalize_global(mem_base, mem_size);
+
+		if (ret)
+			return ret;
+
+		global_finalized = true;
+	}
+
 	return 0;
 }

arch/x86/kvm/pkvm/init_finalize.h
@@ -2,6 +2,6 @@
 #ifndef __PKVM_X86_INIT_FINALIZE_H
 #define __PKVM_X86_INIT_FINALIZE_H
 
-int pkvm_init_finalize(void);
+int pkvm_init_finalize(phys_addr_t mem_base, unsigned long mem_size);
 
 #endif /* __PKVM_X86_INIT_FINALIZE_H */

arch/x86/kvm/pkvm/pkvm.c
@@ -23,7 +23,7 @@ int pkvm_handle_kvm_call(unsigned long func, unsigned long a0,
 
 	switch (func) {
 	case __pkvm__init_finalize:
-		ret = pkvm_init_finalize();
+		ret = pkvm_init_finalize(a0, a1);
 		break;
 	default:
 		ret = -EINVAL;

arch/x86/kvm/vmx/pkvm_init.c
@@ -14,6 +14,7 @@ early_param("kvm-intel.pkvm", early_pkvm_parse_cmdline);
 
 static struct vmcs_config host_vmcs_config;
 static DEFINE_PER_CPU(struct vmcs *, pkvm_vmxarea);
+static unsigned long data_pages;
 
 struct pkvm_deprivilege_param {
 	struct pkvm_hyp *pkvm;
@@ -30,7 +31,11 @@ static struct gdt_page pkvm_gdt_page = {
 
 u64 pkvm_total_reserve_pages(void)
 {
-	return pkvm_vmx_data_pages();
+	u64 total = pkvm_vmx_data_pages();
+
+	total += pkvm_hyp_pgtable_pages();
+
+	return total;
 }
 
 static __init void pkvm_setup_syms(void)
@@ -638,8 +643,11 @@ static __init int pkvm_host_deprivilege_cpus(struct pkvm_hyp *pkvm)
 
 static void do_pkvm_finalize(void *data)
 {
-	int ret = kvm_call_pkvm(init_finalize);
+	unsigned long data_size = data_pages << PAGE_SHIFT;
+	int ret;
 
+	ret = kvm_call_pkvm(init_finalize, pkvm_mem_base + data_size,
+			    pkvm_mem_size - data_size);
 	if (data)
 		*(int *)data = ret;
 }
@@ -663,7 +671,6 @@ static __init int pkvm_init_finalize(void)
 
 int __init vmx_pkvm_init(void)
 {
-	unsigned long nr_pages;
 	struct pkvm_hyp *pkvm;
 	int ret, cpu;
 
@@ -676,8 +683,8 @@ int __init vmx_pkvm_init(void)
 		goto out;
 	}
 
-	nr_pages = pkvm_vmx_data_pages();
-	pkvm_sym(pkvm_early_alloc_init)(__va(pkvm_mem_base), nr_pages << PAGE_SHIFT);
+	data_pages = pkvm_vmx_data_pages();
+	pkvm_sym(pkvm_early_alloc_init)(__va(pkvm_mem_base), data_pages << PAGE_SHIFT);
 
 	pkvm = pkvm_sym(pkvm_hyp) = pkvm_sym(pkvm_early_alloc_contig)(PKVM_HYP_PAGES);
 	if (!pkvm) {

commit 151a0eccca5614e80a5fe9ab82e7c0e48235f77b

fixes: pkvm: x86: Remove redundant including of memblock.h
Fixes: 1f061ef ("pkvm: x86: Create memory pool for pkvm mmu")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/include/asm/kvm_pkvm.h
@@ -3,7 +3,6 @@
 #define _ASM_X86_KVM_PKVM_H
 
 #ifdef CONFIG_PKVM_X86
-#include <linux/memblock.h>
 #include <linux/mm.h>
 #include <linux/memblock.h>
 #include <asm/pkvm_image.h>
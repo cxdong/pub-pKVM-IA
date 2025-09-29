a38a8bd2a7d0d8cea39d97ded6f5fdfe802a17e6
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-09-29 12:42:54 +0800

    KVM: pKVM: Implement disable_virtualization_cpu operation
    Enable the host KVM to disable virtualization features which were
    required for running guest VMs on a specific CPU via the
    disable_virtualization_cpu PV interface.
    
    The pKVM hypervisor doesn't support disabling VMX via this PV interface.
    As a result, the CPU will remain in VMX non-root mode during shutting
    down or rebooting if there is no hardware level reset. In this case, if
    INIT-SIPI are send, such events will cause vmexits to the pKVM
    hypervisor rather than the operations associated with these events.
    Handling such vmexits is not supported by the pKVM hypervisor. Put a
    TODO for this.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/pkvm_high.c
+++ arch/x86/kvm/vmx/pkvm_high.c
@@ -12,10 +12,28 @@ static int pkvm_enable_virtualization_cpu(void)
 	return kvm_call_pkvm(enable_virtualization_cpu);
 }
 
+static void pkvm_disable_virtualization_cpu(void)
+{
+	/*
+	 * The pKVM hypervisor doesn't support disabling VMX via this PV
+	 * interface. As a result, the CPU will remain in VMX non-root mode
+	 * during shutting down or rebooting if there is no hardware level
+	 * reset. In this case, if INIT-SIPI are send, such events will cause
+	 * vmexits to the pKVM hypervisor rather than the operations associated
+	 * with these events. Handling such vmexits is not supported by the pKVM
+	 * hypervisor.
+	 *
+	 * TODO: Support handling INIT-SIPI vmexits for CPUs which remain in VMX
+	 * non-root mode and need INIT-SIPI for shutting down or rebooting.
+	 */
+	kvm_call_pkvm(disable_virtualization_cpu);
+}
+
 struct kvm_x86_ops pkvm_host_x86_ops __initdata = {
 	.name = KBUILD_MODNAME,
 
 	.check_processor_compatibility = pkvm_check_processor_compat,
 
 	.enable_virtualization_cpu = pkvm_enable_virtualization_cpu,
+	.disable_virtualization_cpu = pkvm_disable_virtualization_cpu,
 };

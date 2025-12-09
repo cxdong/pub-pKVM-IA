maluka-dmytro/pKVM-IA

pKVM: x86: Add load_mmu_pgd PV interface
Add load_mmu_pgd PV interface for the host to load the guest mmu via the
vendor-specific operation. Currently the guest mmu for both pVM and npVM
are still managed by the host, thus this PV interface loads the guest
mmu from the host. Eventually the guest mmu will be managed inside the
pKVM to achieve pVM's isolation, thus this PV interface will be updated
to load the pKVM managed guest mmu and always accessible to the host.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -1125,6 +1125,23 @@ static void pkvm_write_tsc_multiplier(struct pkvm_vcpu *pkvm_vcpu, u64 ratio)
 	kvm_x86_call(write_tsc_multiplier)(vcpu);
 }
 
+static void pkvm_load_mmu_pgd(struct pkvm_vcpu *pkvm_vcpu, hpa_t root_hpa, int root_level)
+{
+	struct kvm_vcpu *vcpu;
+
+	vcpu = &pkvm_vcpu->vcpu;
+
+	/*
+	 * TODO: Implement guest memory protection rather than directly using
+	 * the EPT controlled by the host.
+	 */
+	vcpu->arch.mmu->root.hpa = root_hpa;
+	vcpu->arch.mmu->root_role.level = root_level;
+
+	kvm_x86_call(load_mmu_pgd)(vcpu, vcpu->arch.mmu->root.hpa,
+				   vcpu->arch.mmu->root_role.level);
+}
+
 static int pkvm_vcpu_handle_host_hypercall(unsigned long nr, union pkvm_hc_data *in,
 					   union pkvm_hc_data *out)
 {
@@ -1280,6 +1297,9 @@ static int pkvm_vcpu_handle_host_hypercall(unsigned long nr, union pkvm_hc_data 
 		break;
 	case __pkvm__write_tsc_multiplier:
 		pkvm_write_tsc_multiplier(pkvm_vcpu, (u64)in->val1);
+		break;
+	case __pkvm__load_mmu_pgd:
+		pkvm_load_mmu_pgd(pkvm_vcpu, (hpa_t)in->val1, (int)in->val2);
 		break;
 	default:
 		ret = -EINVAL;

cxdong/pub-pKVM-IA

arch/x86/kvm/pkvm/pkvm.c
@@ -989,6 +989,19 @@ static void pkvm_write_tsc_multiplier(struct kvm_vcpu *vcpu, u64 ratio)
 	kvm_x86_call(write_tsc_multiplier)(vcpu);
 }
 
+static void pkvm_load_mmu_pgd(struct kvm_vcpu *vcpu, hpa_t root_hpa, int root_level)
+{
+	/*
+	 * TODO: Implement guest memory protection rather than directly using
+	 * the EPT controlled by the host.
+	 */
+	vcpu->arch.mmu->root.hpa = root_hpa;
+	vcpu->arch.mmu->root_role.level = root_level;
+
+	kvm_x86_call(load_mmu_pgd)(vcpu, vcpu->arch.mmu->root.hpa,
+				   vcpu->arch.mmu->root_role.level);
+}
+
 static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc hc,
 					   union pkvm_hc_data *in, union pkvm_hc_data *out)
 {
@@ -1153,6 +1166,9 @@ static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc 
 		break;
 	case __pkvm__write_tsc_multiplier:
 		pkvm_write_tsc_multiplier(vcpu, pkvm_hc_input1(hvcpu));
+		break;
+	case __pkvm__load_mmu_pgd:
+		pkvm_load_mmu_pgd(vcpu, pkvm_hc_input1(hvcpu), pkvm_hc_input2(hvcpu));
 		break;
 	default:
 		ret = -EINVAL;
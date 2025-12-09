maluka-dmytro/pKVM-IA

pKVM: x86: Add idt/gdt PV interfaces
Add idt/gdt PV interfaces for the host to access guest IDT and GDT via
the vendor-specific idt/gdt operations. These PV interfaces are enforced
to ensure pVM's IDT and GDT cannot be accessed by the host once the pVM
started running.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/include/asm/pkvm_hypercalls.h
@@ -38,6 +38,10 @@ PKVM_HC(set_dr7)
 PKVM_HC_INOUT(set_segment)
 PKVM_HC_INOUT(get_segment)
 PKVM_HC_INOUT(get_segment_base)
+PKVM_HC_INOUT(set_idt)
+PKVM_HC_INOUT(get_idt)
+PKVM_HC_INOUT(set_gdt)
+PKVM_HC_INOUT(get_gdt)
 
 #undef PKVM_HC
 #undef PKVM_HC_INOUT

cxdong/pub-pKVM-IA

arch/x86/include/asm/pkvm_hypercalls.h
@@ -42,6 +42,10 @@ PKVM_HC(set_dr7)
 PKVM_HC_IN(set_segment)
 PKVM_HC_OUT(get_segment)
 PKVM_HC_OUT(get_segment_base)
+PKVM_HC_IN(set_idt)
+PKVM_HC_OUT(get_idt)
+PKVM_HC_IN(set_gdt)
+PKVM_HC_OUT(get_gdt)
 
 #undef PKVM_HC
 #undef PKVM_HC_OUT
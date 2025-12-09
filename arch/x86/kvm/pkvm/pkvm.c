maluka-dmytro/pKVM-IA

pKVM: x86: Add sync_pir_to_irr PV interface
Add sync_pir_to_irr PV interface for the host to sync the requested
pir to the guest irr via the vendor-specific operation. This PV
interface is always accessible to the host as it is part of guest irq
emulation.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -997,6 +997,15 @@ static void pkvm_hwapic_isr_update(struct pkvm_vcpu *pkvm_vcpu, int max_isr)
 	kvm_x86_call(hwapic_isr_update)(&pkvm_vcpu->vcpu, max_isr);
 }
 
+static void pkvm_sync_pir_to_irr(struct pkvm_vcpu *pkvm_vcpu, int pir)
+{
+	struct kvm_vcpu *vcpu = &pkvm_vcpu->vcpu;
+
+	vcpu->arch.interrupt.nr = pir;
+
+	kvm_x86_call(sync_pir_to_irr)(vcpu);
+}
+
 static int pkvm_vcpu_handle_host_hypercall(unsigned long nr, union pkvm_hc_data *in,
 					   union pkvm_hc_data *out)
 {
@@ -1135,6 +1144,9 @@ static int pkvm_vcpu_handle_host_hypercall(unsigned long nr, union pkvm_hc_data 
 		break;
 	case __pkvm__hwapic_isr_update:
 		pkvm_hwapic_isr_update(pkvm_vcpu, (int)in->val1);
+		break;
+	case __pkvm__sync_pir_to_irr:
+		pkvm_sync_pir_to_irr(pkvm_vcpu, (int)in->val1);
 		break;
 	default:
 		ret = -EINVAL;

cxdong/pub-pKVM-IA

arch/x86/kvm/pkvm/pkvm.c
@@ -845,6 +845,12 @@ static void pkvm_load_eoi_exitmap(struct kvm_vcpu *vcpu, u64 eoi_exit_bitmap0,
 	kvm_x86_call(load_eoi_exitmap)(vcpu, eoi_exit_bitmap);
 }
 
+static void pkvm_sync_pir_to_irr(struct kvm_vcpu *vcpu, int pir)
+{
+	vcpu->arch.interrupt.nr = pir;
+	kvm_x86_call(sync_pir_to_irr)(vcpu);
+}
+
 static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc hc,
 					   union pkvm_hc_data *in, union pkvm_hc_data *out)
 {
@@ -992,6 +998,9 @@ static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc 
 		break;
 	case __pkvm__hwapic_isr_update:
 		kvm_x86_call(hwapic_isr_update)(vcpu, pkvm_hc_input1(hvcpu));
+		break;
+	case __pkvm__sync_pir_to_irr:
+		pkvm_sync_pir_to_irr(vcpu, pkvm_hc_input1(hvcpu));
 		break;
 	default:
 		ret = -EINVAL;
maluka-dmytro/pKVM-IA

pKVM: x86: Add vcpu_after_set_cpuid PV interface
Add vcpu_after_set_cpuid PV interface for the host to send the CPUID
entries to the pKVM hypervisor, which will be used as the CPUID entries
for the pkvm_vcpu. The CPUID entries memory pages will be donated to
prevent the host from accessing. If the pkvm_vcpu already has a CPUID
entries, the old one's physical addresses and size will be return to the
host via pkvm_memcache.

This PV interface is enforced to ensure the host cannot change pVM's
CPUID entries once pVM started running.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/include/asm/pkvm_hypercalls.h
@@ -64,6 +64,7 @@ PKVM_HC(refresh_apicv_exec_ctrl)
 PKVM_HC(load_eoi_exitmap)
 PKVM_HC(hwapic_isr_update)
 PKVM_HC(sync_pir_to_irr)
+PKVM_HC_INOUT(vcpu_after_set_cpuid)
 
 #undef PKVM_HC
 #undef PKVM_HC_INOUT

cxdong/pub-pKVM-IA

arch/x86/include/asm/pkvm_hypercalls.h
@@ -68,6 +68,7 @@ PKVM_HC(refresh_apicv_exec_ctrl)
 PKVM_HC(load_eoi_exitmap)
 PKVM_HC(hwapic_isr_update)
 PKVM_HC(sync_pir_to_irr)
+PKVM_HC_OUT(vcpu_after_set_cpuid)
 
 #undef PKVM_HC
 #undef PKVM_HC_OUT
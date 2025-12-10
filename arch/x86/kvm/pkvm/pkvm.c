maluka-dmytro/pKVM-IA

pKVM: x86 Add cancel injection PV interface
Add cancel injection PV interface for the host to cancel injection for
an irq/nmi/exception via vendor specific operation. As pVM's exception
is emulated by the pKVM, the PV interface doesn't allow to cancel
exception injection for pVM.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -922,6 +922,33 @@ static void pkvm_inject_exception(struct pkvm_vcpu *pkvm_vcpu)
 	kvm_x86_call(inject_exception)(vcpu);
 }
 
+static void pkvm_cancel_injection(struct pkvm_vcpu *pkvm_vcpu)
+{
+	struct kvm_vcpu *vcpu, *shared_vcpu;
+
+	vcpu = &pkvm_vcpu->vcpu;
+	kvm_x86_call(cancel_injection)(vcpu);
+
+	shared_vcpu = pkvm_vcpu->shared_vcpu;
+	if (vcpu->arch.nmi_injected) {
+		shared_vcpu->arch.nmi_injected = true;
+		vcpu->arch.nmi_injected = false;
+	} else if (vcpu->arch.interrupt.injected) {
+		kvm_queue_interrupt(shared_vcpu, vcpu->arch.interrupt.nr,
+				    vcpu->arch.interrupt.soft);
+		kvm_clear_interrupt_queue(vcpu);
+	} else if (!pkvm_is_protected_vcpu(vcpu) && vcpu->arch.exception.injected) {
+		/*
+		 * For the pVM, the exception can only be injected and canceled
+		 * by the pkvm hypervisor.
+		 * For the npVM, the exception can be injected and caceled by
+		 * both sides.
+		 */
+		shared_vcpu->arch.exception = vcpu->arch.exception;
+		kvm_clear_exception_queue(vcpu);
+	}
+}
+
 static int pkvm_vcpu_handle_host_hypercall(unsigned long nr, union pkvm_hc_data *in,
 					   union pkvm_hc_data *out)
 {
@@ -1042,6 +1069,9 @@ static int pkvm_vcpu_handle_host_hypercall(unsigned long nr, union pkvm_hc_data 
 		break;
 	case __pkvm__inject_exception:
 		pkvm_inject_exception(pkvm_vcpu);
+		break;
+	case __pkvm__cancel_injection:
+		pkvm_cancel_injection(pkvm_vcpu);
 		break;
 	default:
 		ret = -EINVAL;

cxdong/pub-pKVM-IA

arch/x86/kvm/pkvm/pkvm.c
@@ -794,6 +794,33 @@ static void pkvm_inject_exception(struct kvm_vcpu *vcpu)
 	kvm_x86_call(inject_exception)(vcpu);
 }
 
+static void pkvm_cancel_injection(struct kvm_vcpu *vcpu)
+{
+	struct pkvm_vcpu *pkvm_vcpu = to_pkvm_vcpu(vcpu);
+	struct kvm_vcpu *shared_vcpu;
+
+	kvm_x86_call(cancel_injection)(vcpu);
+
+	shared_vcpu = pkvm_vcpu->shared_vcpu;
+	if (vcpu->arch.nmi_injected) {
+		shared_vcpu->arch.nmi_injected = true;
+		vcpu->arch.nmi_injected = false;
+	} else if (vcpu->arch.interrupt.injected) {
+		kvm_queue_interrupt(shared_vcpu, vcpu->arch.interrupt.nr,
+				    vcpu->arch.interrupt.soft);
+		kvm_clear_interrupt_queue(vcpu);
+	} else if (!pkvm_is_protected_vcpu(vcpu) && vcpu->arch.exception.injected) {
+		/*
+		 * For the pVM, the exception can only be injected and canceled
+		 * by the pkvm hypervisor.
+		 * For the npVM, the exception can be injected and caceled by
+		 * both sides.
+		 */
+		shared_vcpu->arch.exception = vcpu->arch.exception;
+		kvm_clear_exception_queue(vcpu);
+	}
+}
+
 static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc hc,
 					   union pkvm_hc_data *in, union pkvm_hc_data *out)
 {
@@ -921,6 +948,9 @@ static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc 
 		break;
 	case __pkvm__inject_exception:
 		pkvm_inject_exception(vcpu);
+		break;
+	case __pkvm__cancel_injection:
+		pkvm_cancel_injection(vcpu);
 		break;
 	default:
 		ret = -EINVAL;
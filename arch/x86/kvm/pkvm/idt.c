cxdong/pub-pKVM-IA

commit f0d4bc4337b7182135abb5428d0d0c3828e4cd4d

pKVM: x86: Support exception fixup
Add exception fixup support to fix the exception generated by accessing
MSR(i.e., rdmsr and wrmsr). The fixup code is majorly ported from the
arch/x86/mm/extable.c and kernel/extable.c.

If an exception can be handled, the ip will be skipped to the fixup
address. Otherwise, the exception will continue be handled by the
installed handler.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/idt.c
@@ -1,7 +1,126 @@
 // SPDX-License-Identifier: GPL-2.0
+#include <linux/extable.h>
+#include <asm/extable.h>
+#include <asm/pkvm_image.h>
 #include <asm/trapnr.h>
 #include "debug.h"
 #include "idt.h"
+
+static const int pt_regoff[] = {
+	offsetof(struct pt_regs, ax),
+	offsetof(struct pt_regs, cx),
+	offsetof(struct pt_regs, dx),
+	offsetof(struct pt_regs, bx),
+	offsetof(struct pt_regs, sp),
+	offsetof(struct pt_regs, bp),
+	offsetof(struct pt_regs, si),
+	offsetof(struct pt_regs, di),
+#ifdef CONFIG_X86_64
+	offsetof(struct pt_regs, r8),
+	offsetof(struct pt_regs, r9),
+	offsetof(struct pt_regs, r10),
+	offsetof(struct pt_regs, r11),
+	offsetof(struct pt_regs, r12),
+	offsetof(struct pt_regs, r13),
+	offsetof(struct pt_regs, r14),
+	offsetof(struct pt_regs, r15),
+#else
+	offsetof(struct pt_regs, ds),
+	offsetof(struct pt_regs, es),
+	offsetof(struct pt_regs, fs),
+	offsetof(struct pt_regs, gs),
+#endif
+};
+
+static int pt_regs_offset(struct pt_regs *regs, int regno)
+{
+	if ((unsigned)regno < ARRAY_SIZE(pt_regoff))
+		return pt_regoff[regno];
+	return -EDOM;
+}
+
+static inline unsigned long
+ex_fixup_addr(const struct exception_table_entry *x)
+{
+	return (unsigned long)&x->fixup + x->fixup;
+}
+
+static inline unsigned long *pt_regs_nr(struct pt_regs *regs, int nr)
+{
+	int reg_offset = pt_regs_offset(regs, nr);
+	static unsigned long __dummy;
+
+	if (WARN_ON_ONCE(reg_offset < 0))
+		return &__dummy;
+
+	return (unsigned long *)((unsigned long)regs + reg_offset);
+}
+
+static bool ex_handler_default(const struct exception_table_entry *e,
+			       struct pt_regs *regs)
+{
+	if (e->data & EX_FLAG_CLEAR_AX)
+		regs->ax = 0;
+	if (e->data & EX_FLAG_CLEAR_DX)
+		regs->dx = 0;
+
+	regs->ip = ex_fixup_addr(e);
+	return true;
+}
+
+static bool ex_handler_msr(const struct exception_table_entry *fixup,
+			   struct pt_regs *regs, bool wrmsr, bool safe, int reg)
+{
+	if (__ONCE_LITE_IF(!safe && wrmsr)) {
+		pkvm_err("unchecked MSR access error: WRMSR to 0x%x (tried to write 0x%08x%08x) at rIP: 0x%lx (%pS)\n",
+			(unsigned int)regs->cx, (unsigned int)regs->dx,
+			(unsigned int)regs->ax,  regs->ip, (void *)regs->ip);
+	}
+
+	if (__ONCE_LITE_IF(!safe && !wrmsr)) {
+		pkvm_err("unchecked MSR access error: RDMSR from 0x%x at rIP: 0x%lx (%pS)\n",
+			(unsigned int)regs->cx, regs->ip, (void *)regs->ip);
+	}
+
+	if (!wrmsr) {
+		/* Pretend that the read succeeded and returned 0. */
+		regs->ax = 0;
+		regs->dx = 0;
+	}
+
+	if (safe)
+		*pt_regs_nr(regs, reg) = -EIO;
+
+	return ex_handler_default(fixup, regs);
+}
+
+static bool pkvm_fixup_exception(struct pt_regs *regs)
+{
+	const struct exception_table_entry *e;
+	int type, reg;
+
+	e = search_extable(__start___ex_table,
+			   __stop___ex_table - __start___ex_table,
+			   regs->ip);
+	if (!e)
+		return false;
+
+	type = FIELD_GET(EX_DATA_TYPE_MASK, e->data);
+	reg  = FIELD_GET(EX_DATA_REG_MASK,  e->data);
+
+	switch (type) {
+	case EX_TYPE_WRMSR:
+		return ex_handler_msr(e, regs, true, false, reg);
+	case EX_TYPE_RDMSR:
+		return ex_handler_msr(e, regs, false, false, reg);
+	case EX_TYPE_WRMSR_SAFE:
+		return ex_handler_msr(e, regs, true, true, reg);
+	case EX_TYPE_RDMSR_SAFE:
+		return ex_handler_msr(e, regs, false, true, reg);
+	default:
+		BUG();
+	}
+}
 
 static void default_exception_handler(struct pt_regs *regs,
 				      int vector, bool has_error_code)
@@ -28,6 +147,9 @@ void handle_exception(struct pt_regs *regs, int vector, bool has_error_code)
 	exception_handler_t handler;
 
 	if (vector >= X86_TRAP_IRET)
+		return;
+
+	if (pkvm_fixup_exception(regs))
 		return;
 
 	handler = exception_handlers[vector];

commit cbb32613d1f5024b8265c854bc71588968438665

SQUASHME: pKVM: x86: Avoid using host's .data..once in exception fixup
As a part of reusing the code from arch/x86/mm/extable.c to support
exception fixup in pKVM hypervisor, we are reusing the __ONCE_LITE_IF()
macro, in order to print an error when reading or writing an
unsupported MSR first time at the given place in the code. As a result,
since __ONCE_LITE_IF() is using the host kernel's .data..once section,
we are inadvertently using this data section in the hypervisor as well.
That is ok to do in pKVM debug mode which does such things anyway (and
which is hacky and insecure anyway), but in normal mode we should avoid
relying on using host kernel's data in pKVM hypervisor at runtime. [*]

Luckily it is only used for printing errors, which only works in the
debug mode anyway. So we can fix this just by not using __ONCE_LITE_IF
in non-debug mode.

[*] Although in this particular case we may not have any real security
    issue here. Nevertheless, this change may still be useful, for
    instance, if in the future we rework pKVM as follows: instead of
    mapping the entire host memory in the hypervisor page tables, only
    map those parts that are actually used by the hypervisor (similarly
    to pKVM-ARM), as that would be useful in general, for easier
    detection of accidental bugs and security issues in pKVM code.

Fixes: f0d4bc4337b7 ("pKVM: x86: Support exception fixup")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/idt.c
@@ -72,6 +72,7 @@ static bool ex_handler_default(const struct exception_table_entry *e,
 static bool ex_handler_msr(const struct exception_table_entry *fixup,
 			   struct pt_regs *regs, bool wrmsr, bool safe, int reg)
 {
+#ifdef CONFIG_PKVM_X86_DEBUG
 	if (__ONCE_LITE_IF(!safe && wrmsr)) {
 		pkvm_err("unchecked MSR access error: WRMSR to 0x%x (tried to write 0x%08x%08x) at rIP: 0x%lx (%pS)\n",
 			(unsigned int)regs->cx, (unsigned int)regs->dx,
@@ -82,6 +83,7 @@ static bool ex_handler_msr(const struct exception_table_entry *fixup,
 		pkvm_err("unchecked MSR access error: RDMSR from 0x%x at rIP: 0x%lx (%pS)\n",
 			(unsigned int)regs->cx, regs->ip, (void *)regs->ip);
 	}
+#endif
 
 	if (!wrmsr) {
 		/* Pretend that the read succeeded and returned 0. */
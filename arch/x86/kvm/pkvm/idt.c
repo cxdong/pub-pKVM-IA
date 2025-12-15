cbb32613d1f5024b8265c854bc71588968438665
Dmytro Maluka <dmaluka@google.com>
2025-12-15 20:16:03 +0000

    SQUASHME: pKVM: x86: Avoid using host's .data..once in exception fixup
    As a part of reusing the code from arch/x86/mm/extable.c to support
    exception fixup in pKVM hypervisor, we are reusing the __ONCE_LITE_IF()
    macro, in order to print an error when reading or writing an
    unsupported MSR first time at the given place in the code. As a result,
    since __ONCE_LITE_IF() is using the host kernel's .data..once section,
    we are inadvertently using this data section in the hypervisor as well.
    That is ok to do in pKVM debug mode which does such things anyway (and
    which is hacky and insecure anyway), but in normal mode we should avoid
    relying on using host kernel's data in pKVM hypervisor at runtime. [*]
    
    Luckily it is only used for printing errors, which only works in the
    debug mode anyway. So we can fix this just by not using __ONCE_LITE_IF
    in non-debug mode.
    
    [*] Although in this particular case we may not have any real security
        issue here. Nevertheless, this change may still be useful, for
        instance, if in the future we rework pKVM as follows: instead of
        mapping the entire host memory in the hypervisor page tables, only
        map those parts that are actually used by the hypervisor (similarly
        to pKVM-ARM), as that would be useful in general, for easier
        detection of accidental bugs and security issues in pKVM code.
    
    Fixes: f0d4bc4337b7 ("pKVM: x86: Support exception fixup")
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>

--- arch/x86/kvm/pkvm/idt.c
+++ arch/x86/kvm/pkvm/idt.c
@@ -72,6 +72,7 @@ static bool ex_handler_default(const struct exception_table_entry *e,
 static bool ex_handler_msr(const struct exception_table_entry *fixup,
 			   struct pt_regs *regs, bool wrmsr, bool safe, int reg)
 {
+#ifdef CONFIG_PKVM_X86_DEBUG
 	if (__ONCE_LITE_IF(!safe && wrmsr)) {
 		pkvm_err("unchecked MSR access error: WRMSR to 0x%x (tried to write 0x%08x%08x) at rIP: 0x%lx (%pS)\n",
 			(unsigned int)regs->cx, (unsigned int)regs->dx,
@@ -82,6 +83,7 @@ static bool ex_handler_msr(const struct exception_table_entry *fixup,
 		pkvm_err("unchecked MSR access error: RDMSR from 0x%x at rIP: 0x%lx (%pS)\n",
 			(unsigned int)regs->cx, regs->ip, (void *)regs->ip);
 	}
+#endif
 
 	if (!wrmsr) {
 		/* Pretend that the read succeeded and returned 0. */

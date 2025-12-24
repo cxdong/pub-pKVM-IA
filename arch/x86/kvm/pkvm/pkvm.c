6dc9aeab4a7b037db83030d4dae8515249d27e27
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-12-24 10:40:37 +0800

    pKVM: x86: Add set_cr0 PV interface
    Add set_cr0 PV interface to allow the host to set guest CR0 via the
    vendor-specific set_cr0 operation. For a pVM, this PV interface allows
    the host to set the CR0 before the pVM has started running, but doesn't
    allow to modify it if the pVM is already running.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/pkvm.c
+++ arch/x86/kvm/pkvm/pkvm.c
@@ -590,6 +590,7 @@ static bool is_guest_vcpu_accessible(struct kvm_vcpu *vcpu, enum pkvm_hc hc)
 	case __pkvm__set_msr:
 	case __pkvm__get_msr:
 	case __pkvm__set_cr4:
+	case __pkvm__set_cr0:
 		/*
 		 * As the host needs to pre-configure the pVM's vCPU state for
 		 * booting, the protection for pVM is only enforced by the pKVM
@@ -751,6 +752,9 @@ static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc 
 		break;
 	case __pkvm__set_cr4:
 		kvm_x86_call(set_cr4)(vcpu, pkvm_hc_input1(hvcpu));
+		break;
+	case __pkvm__set_cr0:
+		kvm_x86_call(set_cr0)(vcpu, pkvm_hc_input1(hvcpu));
 		break;
 	default:
 		ret = -EINVAL;

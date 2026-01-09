80ac96cbb7e70ddca9285a7dcc44fb51edc9e910
Chuanxiao Dong <chuanxiao.dong@intel.com>
2026-01-09 15:49:40 +0800

    pKVM: x86: Add struct pkvm_init_ops
    Add struct pkvm_init_ops as the vendor hook table for generic pKVM
    initialization. The structure is initially empty; subsequent patches add
    operations as the generic initialization sequence needs them.
    
    Keep a pKVM-local init_ops pointer that the trusted host initializes to
    the selected vendor table before deprivileging.
    
    [ Vineeth: define the global init_ops ]
    
    Signed-off-by: Vineeth Pillai (Google) <vineeth@bitbyteword.org>
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/include/asm/kvm_pkvm.h
+++ arch/x86/include/asm/kvm_pkvm.h
@@ -125,6 +125,7 @@ extern pteval_t pkvm_sym(__default_kernel_pte_mask);
 #ifdef CONFIG_AMD_MEM_ENCRYPT
 extern u64 pkvm_sym(sme_me_mask);
 #endif
+extern struct pkvm_init_ops *pkvm_sym(init_ops);
 
 PKVM_DECLARE(void *, pkvm_early_alloc_page, (void));
 PKVM_DECLARE(void *, pkvm_early_alloc_contig, (unsigned int nr_pages));

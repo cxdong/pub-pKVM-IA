cxdong/pub-pKVM-IA

commit bc395be4431f98f7dce8b6565c82fe91b96f0107

pKVM: VMX: Intercept writing to some X2APIC MSRs
The lapic is used by the pKVM in x2apic mode, to kick remote CPU via
sending INIT signal with the remote apic id. So it is important for the
lapic being always in the x2apic mode with its apic id unchanged. To
satisfy this, intercept writing to the MSR_IA32_APICBASE and APIC_ID MSR
to guarantee this.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/vmx/host_vmx.c
@@ -7,6 +7,7 @@
 #include "ept.h"
 #include "host_vmx.h"
 #include "pkvm/init.h"
+#include "pkvm/lapic.h"
 #include "pkvm.h"
 
 #define CR4			4
@@ -179,6 +180,11 @@ static int handle_write_msr(struct kvm_vcpu *vcpu)
 		}
 		break;
 	}
+	case MSR_IA32_APICBASE:
+	case APIC_BASE_MSR ... APIC_BASE_MSR + 0xff:
+		if (!pkvm_lapic_msr_write(msr, val))
+			break;
+		fallthrough;
 	default:
 		/*
 		 * The MSRs intercepted by the writing bitmap should be

arch/x86/kvm/pkvm/lapic.c
@@ -78,3 +78,47 @@ void pkvm_lapic_send_init(int cpu)
 
 	native_x2apic_icr_write(icrlow, remote->apic_id);
 }
+
+int pkvm_lapic_msr_write(u32 msr, u64 val)
+{
+	struct pkvm_lapic *lapic = this_cpu_ptr(&pkvm_lapic);
+	int ret = 0;
+
+	if (!lapic->ready) {
+		/*
+		 * The host may access x2apic before the pKVM lapic is
+		 * initialized. In this case, the pKVM hypervisor doesn't use
+		 * lapic to send INIT (see comment in pkvm_lapic_send_init).
+		 * Thus no need to audit and let the host directly access x2apic.
+		 */
+		wrmsrl(msr, val);
+	} else if (msr == MSR_IA32_APICBASE) {
+		/*
+		 * The lapic should be always in x2apic mode as the pkvm
+		 * hypervisor only support x2apic mode.
+		 */
+		if ((val & LAPIC_MODE_X2APIC) != LAPIC_MODE_X2APIC)
+			return -EINVAL;
+		wrmsrl(msr, val);
+	} else {
+		u32 reg = (msr - APIC_BASE_MSR) << 4;
+
+		switch (reg) {
+		case APIC_ID:
+			/*
+			 * The pKVM hypervisor may be kicking this CPU via its
+			 * original ID. So not allow changing the lapic ID as
+			 * this may result in the on-going kick failed.
+			 */
+			if (lapic->apic_id != (u32)val)
+				ret = -EINVAL;
+			break;
+		default:
+			/* The other MSRs are not emulated */
+			ret = -EINVAL;
+			break;
+		}
+	}
+
+	return ret;
+}

arch/x86/kvm/pkvm/lapic.h
@@ -2,7 +2,10 @@
 #ifndef __PKVM_X86_LAPIC_H
 #define __PKVM_X86_LAPIC_H
 
+#include <linux/types.h>
+
 int pkvm_lapic_init(void);
 void pkvm_lapic_send_init(int cpu);
+int pkvm_lapic_msr_write(u32 msr, u64 val);
 
 #endif /* __PKVM_X86_LAPIC_H */

arch/x86/kvm/vmx/pkvm_init.c
@@ -32,6 +32,8 @@ static struct gdt_page pkvm_gdt_page = {
 
 static unsigned int intercept_w_msrs[] = {
 	MSR_CORE_PERF_GLOBAL_CTRL,
+	MSR_IA32_APICBASE,
+	X2APIC_MSR(APIC_ID),
 };
 
 u64 pkvm_total_reserve_pages(void)

commit b1a423c9d67605ced00b7b164c8721b366e73e98

SQUASHME: pKVM: x86: Inject #GP for the host lapic emulation failure
Inject #GP to the host if the LAPIC emulation is failed rather than
BUG_ON in the pKVM hypervisor.

Fixes: bc395be4431f ("pKVM: VMX: Intercept writing to some X2APIC MSRs")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/vmx/host_vmx.c
@@ -184,9 +184,9 @@ static int handle_write_msr(struct kvm_vcpu *vcpu)
 	}
 	case MSR_IA32_APICBASE:
 	case APIC_BASE_MSR ... APIC_BASE_MSR + 0xff:
-		if (!pkvm_lapic_msr_write(msr, val))
-			break;
-		fallthrough;
+		if (pkvm_lapic_msr_write(msr, val))
+			ret = X86EMUL_UNHANDLEABLE;
+		break;
 	default:
 		/*
 		 * The MSRs intercepted by the writing bitmap should be
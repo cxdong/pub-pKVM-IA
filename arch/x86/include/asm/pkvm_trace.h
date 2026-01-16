cxdong/pub-pKVM-IA

commit a3614e021b75833a7913d16ace235d4274eb71c5

pKVM: x86: trace: Add host vmexit stats tracing support
Add support to trace each host vmexit on each CPU for the profiling
purpose. Each host vcpu will have a struct vmexit_perf which is used to
record the stats for each vmexit reason, i.e., the number of exits and
the cycles used to handle these exits.

By default, the tracing is turned off. It can be turned on by the host
via a specific PV interface which is implemented by the following patch.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/include/asm/pkvm_trace.h
@@ -1,0 +1,30 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_X86_PKVM_TRACE_H
+#define _ASM_X86_PKVM_TRACE_H
+
+#include <asm/pkvm_spinlock.h>
+#include <asm/vmx.h>
+
+/*
+ * TODO: the implementation of vmexit tracing is VMX-specific.
+ * Make it generic.
+ */
+#define MAX_EXIT_REASONS	MAX_VMX_EXIT_REASONS
+
+struct vmexit_stats {
+	u64 count;
+	u64 cycles;
+};
+
+struct perf_data {
+	struct vmexit_stats vmexit_reasons[MAX_EXIT_REASONS];
+	int vcpu_id;
+};
+
+struct vmexit_perf {
+	pkvm_spinlock_t lock;
+	struct perf_data data;
+	unsigned long long tsc;
+};
+
+#endif /* _ASM_X86_PKVM_TRACE_H */

arch/x86/include/uapi/asm/vmx.h
@@ -97,6 +97,7 @@
 #define EXIT_REASON_TDCALL              77
 #define EXIT_REASON_MSR_READ_IMM        84
 #define EXIT_REASON_MSR_WRITE_IMM       85
+#define MAX_VMX_EXIT_REASONS		(EXIT_REASON_MSR_WRITE_IMM + 1)
 
 #define VMX_EXIT_REASONS \
 	{ EXIT_REASON_EXCEPTION_NMI,         "EXCEPTION_NMI" }, \

arch/x86/kvm/pkvm/Makefile
@@ -14,7 +14,7 @@ cmd_deps			:= $(src)/compiling_cmds.sh
 
 pkvm-hyp-y			:= early_alloc.o pkvm.o memory.o cpu.o \
 				   idt.o entry.o init.o pgtable.o mmu.o \
-				   page_alloc.o lapic.o
+				   page_alloc.o lapic.o trace.o
 
 kernel-x86-lib			:= ../../lib
 pkvm-hyp-y			+= $(kernel-x86-lib)/memset_64.o

arch/x86/kvm/pkvm/init.c
@@ -6,6 +6,8 @@
 #include "lapic.h"
 #include "memory.h"
 #include "mmu.h"
+#include "pkvm.h"
+#include "trace.h"
 
 /*
  * Set by the host before deprivilege and used through the initialization
@@ -276,6 +278,8 @@ int pkvm_init(struct pkvm_mem_info infos[], int nr_infos)
 	ret = pkvm_host_mmu_finalize(host_mmu_finalize_fn);
 	if (ret)
 		return ret;
+
+	pkvm_vcpu_perf_init(this_cpu_read(host_vcpu));
 
 	this_cpu_write(cpu_initialized, true);
 	return 0;

arch/x86/kvm/pkvm/trace.c
@@ -1,0 +1,69 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <asm/pkvm_trace.h>
+#include <asm/tsc.h>
+#include "pkvm.h"
+#include "trace.h"
+
+struct perf_ctrl {
+	bool on;
+};
+static DEFINE_PER_CPU(struct vmexit_perf, hvcpu_perf);
+static DEFINE_PER_CPU(struct perf_ctrl, perf_ctrl);
+
+static inline struct vmexit_perf *vcpu_to_perf(struct kvm_vcpu *vcpu)
+{
+	/*
+	 * TODO: Currently there is only host vCPU but not guest vCPU.
+	 * Enable the trace support for guest once the pKVM hypervisor
+	 * support running a guest vCPU.
+	 */
+	BUG_ON(this_cpu_read(host_vcpu) != vcpu);
+
+	return this_cpu_ptr(&hvcpu_perf);
+}
+
+void pkvm_trace_vmexit_start(struct kvm_vcpu *vcpu)
+{
+	struct perf_ctrl *pctrl = this_cpu_ptr(&perf_ctrl);
+	struct vmexit_perf *perf;
+
+	if (!pctrl->on)
+		return;
+
+	perf = vcpu_to_perf(vcpu);
+
+	perf->tsc = rdtsc_ordered();
+}
+
+void pkvm_trace_vmexit_end(struct kvm_vcpu *vcpu, u32 reason)
+{
+	struct perf_ctrl *pctrl = this_cpu_ptr(&perf_ctrl);
+	struct vmexit_perf *perf;
+	unsigned long long cycles;
+
+	if (!pctrl->on)
+		return;
+
+	if (reason >= MAX_EXIT_REASONS)
+		return;
+
+	perf = vcpu_to_perf(vcpu);
+
+	cycles = rdtsc_ordered() - perf->tsc;
+
+	pkvm_spin_lock(&perf->lock);
+
+	perf->data.vmexit_reasons[reason].count++;
+	perf->data.vmexit_reasons[reason].cycles += cycles;
+
+	pkvm_spin_unlock(&perf->lock);
+}
+
+void pkvm_vcpu_perf_init(struct kvm_vcpu *vcpu)
+{
+	struct vmexit_perf *perf = vcpu_to_perf(vcpu);
+
+	perf->data.vcpu_id = vcpu->vcpu_id;
+
+	pkvm_spin_lock_init(&perf->lock);
+}

arch/x86/kvm/pkvm/trace.h
@@ -1,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _HYP_TRACE_H_
+#define _HYP_TRACE_H_
+
+#include <linux/kvm_host.h>
+
+void pkvm_trace_vmexit_start(struct kvm_vcpu *vcpu);
+void pkvm_trace_vmexit_end(struct kvm_vcpu *vcpu, u32 index);
+void pkvm_vcpu_perf_init(struct kvm_vcpu *vcpu);
+
+#endif

arch/x86/kvm/pkvm/vmx/host_vmx.c
@@ -8,6 +8,7 @@
 #include "host_vmx.h"
 #include "pkvm/init.h"
 #include "pkvm/lapic.h"
+#include "pkvm/trace.h"
 #include "pkvm.h"
 
 #define CR4			4
@@ -302,6 +303,8 @@ void pkvm_host_vmexit_main(struct vcpu_vmx *vmx)
 	struct vcpu_vt *vt = &vmx->vt;
 	bool skip_instruction = false;
 
+	pkvm_trace_vmexit_start(vcpu);
+
 	set_vcpu_mode(vcpu, OUTSIDE_GUEST_MODE);
 
 	vcpu->arch.cr2 = native_read_cr2();
@@ -391,4 +394,6 @@ handle_events:
 
 	if (vcpu->arch.cr2 != native_read_cr2())
 		native_write_cr2(vcpu->arch.cr2);
+
+	pkvm_trace_vmexit_end(vcpu, vt->exit_reason.basic);
 }

commit f4adbab939d9a5ec89194b41ddb9c0cb73c5a057

SQUASHME: pKVM: x86: Move MAX_VMX_EXIT_REASONS to asm/pkvm_trace.h
Move MAX_VMX_EXIT_REASONS from uapi/asm/vmx.h to asm/pkvm_trace.h as
currently there is no usage to expose MAX_VMX_EXIT_REASONS to the uapi
namespace.

Fixes: a3614e021b75 ("pKVM: x86: trace: Add host vmexit stats tracing support")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/include/asm/pkvm_trace.h
@@ -7,9 +7,13 @@
 #include <asm/vmx.h>
 
 /*
- * TODO: the implementation of vmexit tracing is VMX-specific.
- * Make it generic.
+ * TODO: If there is new vmx vmexit reason in the future which is larger than
+ * the EXIT_REASON_MSR_WRITE_IMM, the MAX_VMX_EXIT_REASONS should also be
+ * enlarged.
+ *
+ * TODO: The implementation of vmexit tracing is VMX-specific. Make it generic.
  */
+#define MAX_VMX_EXIT_REASONS	(EXIT_REASON_MSR_WRITE_IMM + 1)
 #define MAX_EXIT_REASONS	MAX_VMX_EXIT_REASONS
 
 struct vmexit_stats {

arch/x86/include/uapi/asm/vmx.h
@@ -97,7 +97,6 @@
 #define EXIT_REASON_TDCALL              77
 #define EXIT_REASON_MSR_READ_IMM        84
 #define EXIT_REASON_MSR_WRITE_IMM       85
-#define MAX_VMX_EXIT_REASONS		(EXIT_REASON_MSR_WRITE_IMM + 1)
 
 #define VMX_EXIT_REASONS \
 	{ EXIT_REASON_EXCEPTION_NMI,         "EXCEPTION_NMI" }, \
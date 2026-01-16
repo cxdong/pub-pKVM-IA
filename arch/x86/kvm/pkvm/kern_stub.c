cxdong/pub-pKVM-IA

commit 73925de09525b8cffe56244fac9cea759b30a0f4

pKVM: x86: Import kernel libraries for extable
Import the kernel extable, bsearch and sort libraries to make the pKVM
hypervisor having the capability to search the exception table section.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

lib/sort.c
@@ -264,8 +264,18 @@ static void __sort_r(void *base, size_t num, size_t size,
 			do_swap(base + b, base + c, size, swap_func, priv);
 		}
 
+#ifndef __PKVM_HYP__
 		if (may_schedule)
 			cond_resched();
+#else
+		/*
+		 * The pKVM hypervisor runs in the interrupt disabled context
+		 * and doesn't own scheduling. It doesn't have a capability to
+		 * preempt itself and return back to the host for rescheduling
+		 * so just warn on for this case.
+		 */
+		WARN_ON_ONCE(may_schedule);
+#endif
 	}
 
 	n -= size;

arch/x86/kvm/pkvm/Makefile
@@ -25,6 +25,10 @@ $(obj)/retpoline.pkvm.S: $(srctree)/arch/x86/lib/retpoline.S $(cmd_deps) FORCE
 quiet_cmd_patch_retpoline = PATCH_RETPOLINE $@
       cmd_patch_retpoline = $(BASH) $(src)/compiling_cmds.sh patch_retpoline $< $@
 
+kernel-lib			:= ../../../../lib
+pkvm-hyp-y			+= $(kernel-lib)/sort.o $(kernel-lib)/bsearch.o \
+				   $(kernel-lib)/extable.o
+
 kvm				:= ..
 pkvm-hyp-y			+= $(kvm)/x86.o
 

commit 1f3e402c2db95c6b95f793ebf4fdebeb9b12119e

SQUASHME: pKVM: x86: Remove changes in sort library
Fixes: 73925de09525 ("pKVM: x86: Import kernel libraries for extable")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/kern_stub.c
@@ -1,4 +1,13 @@
 // SPDX-License-Identifier: GPL-2.0
+#include <linux/kernel.h>
 #include "kern_stub.h"
 
 void warn_thunk_thunk(void) {}
+
+#ifndef CONFIG_PREEMPTION
+int __cond_resched(void) {}
+#endif
+
+#ifdef CONFIG_DEBUG_ATOMIC_SLEEP
+void __might_resched(const char *file, int line, unsigned int offsets) {}
+#endif

arch/x86/kvm/pkvm/undef.h
@@ -23,6 +23,7 @@
 #undef CONFIG_USE_X86_SEG_SUPPORT
 #undef CONFIG_MATH_EMULATION
 #undef CONFIG_X86_DEBUG_FPU
+#undef CONFIG_PREEMPT_DYNAMIC
 
 #define NOTRACE
 

lib/sort.c
@@ -264,18 +264,8 @@ static void __sort_r(void *base, size_t num, size_t size,
 			do_swap(base + b, base + c, size, swap_func, priv);
 		}
 
-#ifndef __PKVM_HYP__
 		if (may_schedule)
 			cond_resched();
-#else
-		/*
-		 * The pKVM hypervisor runs in the interrupt disabled context
-		 * and doesn't own scheduling. It doesn't have a capability to
-		 * preempt itself and return back to the host for rescheduling
-		 * so just warn on for this case.
-		 */
-		WARN_ON_ONCE(may_schedule);
-#endif
 	}
 
 	n -= size;
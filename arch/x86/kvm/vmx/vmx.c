cxdong/pub-pKVM-IA

commit e40778e1df0befc683ffed618d9cbb76eaf542e0

KVM: pVMX: Implement sync_pir_to_irr operation
Enable the pKVM VMX host to sync pir to the guest irr via the
corresponding PV interface. The implementation is referred to
vmx_sync_pir_to_irr() with removing L2 as nested is not supported.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/vmx/vmx.c
@@ -7331,10 +7331,14 @@ int vmx_sync_pir_to_irr(struct kvm_vcpu *vcpu)
 	 * attempt to post interrupts.  The posted interrupt vector will cause
 	 * a VM-Exit and the subsequent entry will call sync_pir_to_irr.
 	 */
-	if (!is_guest_mode(vcpu) && kvm_vcpu_apicv_active(vcpu))
-		vmx_set_rvi(max_irr);
-	else if (got_posted_interrupt)
+	if (!is_guest_mode(vcpu) && kvm_vcpu_apicv_active(vcpu)) {
+		if (!enable_pkvm)
+			vmx_set_rvi(max_irr);
+		else if (max_irr != -1)
+			KVM_BUG_ON(pkvm_hypercall(sync_pir_to_irr, max_irr), vcpu->kvm);
+	} else if (got_posted_interrupt) {
 		kvm_make_request(KVM_REQ_EVENT, vcpu);
+	}
 #else
 	int max_irr = to_pkvm_vcpu(vcpu)->max_irr;
 

arch/x86/kvm/vmx/pkvm_host.c
@@ -984,6 +984,7 @@ struct kvm_x86_ops pkvm_host_vt_x86_ops __initdata = {
 	.apicv_pre_state_restore = pi_apicv_pre_state_restore,
 	.required_apicv_inhibits = VMX_REQUIRED_APICV_INHIBITS,
 	.hwapic_isr_update = pkvm_hwapic_isr_update,
+	.sync_pir_to_irr = vmx_sync_pir_to_irr,
 };
 
 bool pkvm_interrupt_blocked(struct kvm_vcpu *vcpu)

commit 52c3879071df8c86383f7678f807024661acb375

SQUASHME: pKVM: x86: Fix compiling issue with CONFIG_PKVM_INTEL=n
Fixes: e40778e1df0b ("KVM: pVMX: Implement sync_pir_to_irr operation")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/vmx/vmx.c
@@ -7328,10 +7328,14 @@ int vmx_sync_pir_to_irr(struct kvm_vcpu *vcpu)
 	 * a VM-Exit and the subsequent entry will call sync_pir_to_irr.
 	 */
 	if (!is_guest_mode(vcpu) && kvm_vcpu_apicv_active(vcpu)) {
+#ifdef CONFIG_PKVM_INTEL
 		if (!enable_pkvm)
 			vmx_set_rvi(max_irr);
 		else if (max_irr != -1)
 			KVM_BUG_ON(pkvm_hypercall(sync_pir_to_irr, max_irr), vcpu->kvm);
+#else
+		vmx_set_rvi(max_irr);
+#endif
 	} else if (got_posted_interrupt) {
 		kvm_make_request(KVM_REQ_EVENT, vcpu);
 	}
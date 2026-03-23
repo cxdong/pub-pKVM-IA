cxdong/pub-pKVM-IA

commit 1ff5d9013996a4f36d045689f811b2b7d8733d65

pKVM: x86: trace: Extend PV interface to dump the guest trace
Extend pkvm_dump_vmexit_trace() to dump the guest trace if the vm_handle
is set to be a valid guest VM handle. If it is a host VM handle, dump
the trace for the host VM and all the guest VMs.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/trace.c
@@ -38,19 +38,67 @@ static void copy_vmexit_perf_data(struct perf_data *dst, struct vmexit_perf *per
 	pkvm_spin_unlock(&perf->lock);
 }
 
-static void copy_host_vm_trace(void *dst, unsigned long size)
+static void copy_host_vm_trace(void **dst, unsigned long *size)
 {
 	struct vmexit_perf *perf;
 	int cpu;
 
 	for_each_possible_cpu(cpu) {
 		perf = per_cpu_ptr(&hvcpu_perf, cpu);
-		if (size >= sizeof(struct perf_data)) {
-			copy_vmexit_perf_data(dst, perf);
-			dst += sizeof(struct perf_data);
-			size -= sizeof(struct perf_data);
+		if (*size >= sizeof(struct perf_data)) {
+			copy_vmexit_perf_data(*dst, perf);
+			*dst += sizeof(struct perf_data);
+			*size -= sizeof(struct perf_data);
 		}
 	}
+}
+
+struct copy_arg {
+	int vm_handle;
+	void *dst;
+	unsigned long size;
+};
+
+static int __copy_guest_vm_trace(struct pkvm_vm *vm, void *param)
+{
+	struct copy_arg *arg = param;
+	int i;
+
+	if (arg->vm_handle != PKVM_HOST_VM_HANDLE &&
+	    arg->vm_handle != vm->kvm.arch.pkvm.handle)
+		return 0;
+
+	pkvm_spin_lock(&vm->lock);
+	for (i = 0; i < vm->kvm.created_vcpus; i++) {
+		if (!vm->vcpus[i])
+			continue;
+
+		if (arg->size < sizeof(struct perf_data))
+			return -ENOSPC;
+
+		copy_vmexit_perf_data(arg->dst, &vm->vcpus[i]->perf);
+		arg->dst += sizeof(struct perf_data);
+		arg->size -= sizeof(struct perf_data);
+	}
+	pkvm_spin_unlock(&vm->lock);
+
+	/*
+	 * If vm_handle != PKVM_HOST_VM_HANDLE, it means the host wants to get
+	 * the trace for a specific guest VM, and the pKVM can stop dumping the
+	 * other guest VM's trace. Otherwise, the pKVM will continue.
+	 */
+	return arg->vm_handle != PKVM_HOST_VM_HANDLE;
+}
+
+static void copy_guest_vm_trace(int vm_handle, void *dst, unsigned long size)
+{
+	struct copy_arg arg = {
+		.vm_handle = vm_handle,
+		.dst = dst,
+		.size = size,
+	};
+
+	pkvm_walk_each_vm(__copy_guest_vm_trace, &arg);
 }
 
 void pkvm_trace_vmexit_start(struct kvm_vcpu *vcpu)
@@ -125,14 +173,19 @@ void pkvm_enable_vmexit_trace(bool en)
 	}
 }
 
-int pkvm_dump_vmexit_trace(phys_addr_t phys, unsigned long size)
+int pkvm_dump_vmexit_trace(phys_addr_t phys, unsigned long size, int vm_handle)
 {
 	int ret = pkvm_host_share_hyp(phys, size);
+	unsigned long dst_size = size;
+	void *dst = __pkvm_va(phys);
 
 	if (ret)
 		return ret;
 
-	copy_host_vm_trace(__pkvm_va(phys), size);
+	if (vm_handle == PKVM_HOST_VM_HANDLE)
+		copy_host_vm_trace(&dst, &dst_size);
+
+	copy_guest_vm_trace(vm_handle, dst, dst_size);
 
 	pkvm_host_unshare_hyp(phys, size);
 

arch/x86/kvm/pkvm/pkvm.c
@@ -1592,7 +1592,7 @@ void pkvm_handle_host_hypercall(struct kvm_vcpu *vcpu)
 		break;
 	case __pkvm__dump_vmexit_trace:
 		ret = pkvm_dump_vmexit_trace(pkvm_host_gpa_to_phys(pkvm_hc_input1(vcpu)),
-					     pkvm_hc_input2(vcpu));
+					     pkvm_hc_input2(vcpu), PKVM_HOST_VM_HANDLE);
 		break;
 	case __pkvm__check_processor_compatibility:
 		ret = kvm_x86_call(check_processor_compatibility)();
@@ -1768,4 +1768,25 @@ unsigned long pkvm_pcpu_tss(int cpu)
 void pkvm_x86_ops_init(struct pkvm_x86_ops *ops)
 {
 	memcpy(&pkvm_x86_ops, ops, sizeof(struct pkvm_x86_ops));
+}
+
+int pkvm_walk_each_vm(pkvm_vm_func_t func, void *arg)
+{
+	struct pkvm_vm *vm;
+	int i, ret = 0;
+
+	pkvm_spin_lock(&pkvm_vms_lock);
+
+	for (i = 0; i < MAX_PKVM_VMS; i++) {
+		vm = pkvm_vms_ref[i].pkvm_vm;
+		if (!vm)
+			continue;
+		ret = func(vm, arg);
+		if (ret)
+			break;
+	}
+
+	pkvm_spin_unlock(&pkvm_vms_lock);
+
+	return ret;
 }

arch/x86/kvm/pkvm/pkvm.h
@@ -131,5 +131,7 @@ int pkvm_vcpu_enter_guest(struct kvm_vcpu *vcpu, bool force_immediate_exit,
 			  unsigned long *reqs_to_host);
 void pkvm_x86_ops_init(struct pkvm_x86_ops *ops);
 int pkvm_emulate_hypercall(struct kvm_vcpu *vcpu);
+typedef int (*pkvm_vm_func_t)(struct pkvm_vm *vm, void *arg);
+int pkvm_walk_each_vm(pkvm_vm_func_t func, void *arg);
 
 #endif /* __PKVM_X86_PKVM_H */

arch/x86/kvm/pkvm/trace.h
@@ -8,6 +8,6 @@ void pkvm_trace_vmexit_start(struct kvm_vcpu *vcpu);
 void pkvm_trace_vmexit_end(struct kvm_vcpu *vcpu, u32 index);
 void pkvm_vcpu_perf_init(struct kvm_vcpu *vcpu);
 void pkvm_enable_vmexit_trace(bool en);
-int pkvm_dump_vmexit_trace(phys_addr_t pa, unsigned long size);
+int pkvm_dump_vmexit_trace(phys_addr_t phys, unsigned long size, int vm_handle);
 
 #endif

commit aa645f9f66db7c181692a18e2108ba83b34ff6e5

SQUASHME: pKVM: x86: Fix vmexit trace dump lockup
The vmexit trace dump path can return -ENOSPC from the hypervisor
while holding vm->lock in __copy_guest_vm_trace(), which leaves the
per-VM lock permanently held and can strand a CPU in hyp with
interrupts disabled.

Fix it by always dropping vm->lock on the error path.

Fixes: 1ff5d9013996 ("pKVM: x86: trace: Extend PV interface to dump the guest trace")
Signed-off-by: Vineeth Pillai <vineethrp@google.com>

arch/x86/kvm/pkvm/trace.c
@@ -100,8 +100,10 @@ static int __copy_guest_vm_trace(struct pkvm_vm *vm, void *param)
 		if (!vm->vcpus[i])
 			continue;
 
-		if (arg->size < sizeof(struct perf_data))
+		if (arg->size < sizeof(struct perf_data)) {
+			pkvm_spin_unlock(&vm->lock);
 			return -ENOSPC;
+		}
 
 		copy_vmexit_perf_data(arg->dst, &vm->vcpus[i]->perf);
 		arg->dst += sizeof(struct perf_data);
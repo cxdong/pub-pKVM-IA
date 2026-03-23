cxdong/pub-pKVM-IA

commit 8ec836911f8d7b6f36632409746bc7a4fe70ede6

pKVM: x86: Add refresh_apicv_exec_ctrl PV interface
Add refresh_apicv_exec_ctrl PV interface for the host to refresh the
apicv execution control via the vendor-specific operation. This PV
interface is always accessible to the host as virtual APIC is emulated
by the host.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -663,6 +663,7 @@ static bool is_guest_vcpu_accessible(struct kvm_vcpu *vcpu, enum pkvm_hc hc)
 	case __pkvm__cancel_injection:
 	case __pkvm__update_cr8_intercept:
 	case __pkvm__set_virtual_apic_mode:
+	case __pkvm__refresh_apicv_exec_ctrl:
 		/*
 		 * The host is responsible for running vCPU, injecting
 		 * interrupts, emulating lapic etc. Always allow the related PV
@@ -915,6 +916,15 @@ static void pkvm_set_virtual_apic_mode(struct kvm_vcpu *vcpu)
 	kvm_x86_call(set_virtual_apic_mode)(vcpu);
 }
 
+static void pkvm_refresh_apicv_exec_ctrl(struct kvm_vcpu *vcpu, bool apicv_active)
+{
+	if (!lapic_in_kernel(vcpu))
+		return;
+
+	vcpu->arch.apic->apicv_active = apicv_active;
+	kvm_x86_call(refresh_apicv_exec_ctrl)(vcpu);
+}
+
 static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc hc,
 					   union pkvm_hc_data *in, union pkvm_hc_data *out)
 {
@@ -1059,6 +1069,9 @@ static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc 
 		break;
 	case __pkvm__set_virtual_apic_mode:
 		pkvm_set_virtual_apic_mode(vcpu);
+		break;
+	case __pkvm__refresh_apicv_exec_ctrl:
+		pkvm_refresh_apicv_exec_ctrl(vcpu, pkvm_hc_input1(hvcpu));
 		break;
 	default:
 		ret = -EINVAL;

arch/x86/include/asm/pkvm_hypercalls.h
@@ -65,6 +65,7 @@ PKVM_HC(inject_exception)
 PKVM_HC(cancel_injection)
 PKVM_HC(update_cr8_intercept)
 PKVM_HC(set_virtual_apic_mode)
+PKVM_HC(refresh_apicv_exec_ctrl)
 
 #undef PKVM_HC
 #undef PKVM_HC_OUT

commit b6ee3e97d6c3dd2acec34e31a1ba0b81fa0b1b39

SQUASHME: pKVM: x86: Validate enable_apicv in pkvm_refresh_apicv_exec_ctrl
The host may request the pKVM to enable the APICv via the PV interface
__pkvm__refresh_apicv_exec_ctrl. If the platform doesn't support APICv
or the APICv is disabled by the kernel command line, forbid the host to
enable it.

Fixes: 8ec836911f8d ("pKVM: x86: Add refresh_apicv_exec_ctrl PV interface")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -1112,13 +1112,15 @@ static void pkvm_set_virtual_apic_mode(struct kvm_vcpu *vcpu)
 	kvm_x86_call(set_virtual_apic_mode)(vcpu);
 }
 
-static void pkvm_refresh_apicv_exec_ctrl(struct kvm_vcpu *vcpu, bool apicv_active)
+static int pkvm_refresh_apicv_exec_ctrl(struct kvm_vcpu *vcpu, bool apicv_active)
 {
-	if (!lapic_in_kernel(vcpu))
-		return;
+	if (!lapic_in_kernel(vcpu) || (!enable_apicv && apicv_active))
+		return -EINVAL;
 
 	vcpu->arch.apic->apicv_active = apicv_active;
 	kvm_make_request(KVM_REQ_APICV_UPDATE, vcpu);
+
+	return 0;
 }
 
 static void pkvm_load_eoi_exitmap(struct kvm_vcpu *vcpu, u64 eoi_exit_bitmap0,
@@ -1806,7 +1808,7 @@ static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc 
 		pkvm_set_virtual_apic_mode(vcpu);
 		break;
 	case __pkvm__refresh_apicv_exec_ctrl:
-		pkvm_refresh_apicv_exec_ctrl(vcpu, pkvm_hc_input1(hvcpu));
+		ret = pkvm_refresh_apicv_exec_ctrl(vcpu, pkvm_hc_input1(hvcpu));
 		break;
 	case __pkvm__load_eoi_exitmap:
 		pkvm_load_eoi_exitmap(vcpu, pkvm_hc_input1(hvcpu), pkvm_hc_input2(hvcpu),
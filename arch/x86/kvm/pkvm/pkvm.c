cxdong/pub-pKVM-IA

commit b2743f17b8c9b4d14e9a07f1507fbd8f621470b9

pKVM: x86: Add hwapic_isr_update PV interface
Although the virtual APIC is emulated by the host, the hardware
acceleration for virtual APIC emulation is controlled by the pKVM. Add
hwapic_isr_update PV interface for the host to update the ISR for
virtual APIC emulation via the vendor-specific operation.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -665,6 +665,7 @@ static bool is_guest_vcpu_accessible(struct kvm_vcpu *vcpu, enum pkvm_hc hc)
 	case __pkvm__set_virtual_apic_mode:
 	case __pkvm__refresh_apicv_exec_ctrl:
 	case __pkvm__load_eoi_exitmap:
+	case __pkvm__hwapic_isr_update:
 		/*
 		 * The host is responsible for running vCPU, injecting
 		 * interrupts, emulating lapic etc. Always allow the related PV
@@ -1091,6 +1092,9 @@ static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc 
 	case __pkvm__load_eoi_exitmap:
 		pkvm_load_eoi_exitmap(vcpu, pkvm_hc_input1(hvcpu), pkvm_hc_input2(hvcpu),
 				      pkvm_hc_input3(hvcpu), pkvm_hc_input4(hvcpu));
+		break;
+	case __pkvm__hwapic_isr_update:
+		kvm_x86_call(hwapic_isr_update)(vcpu, pkvm_hc_input1(hvcpu));
 		break;
 	default:
 		ret = -EINVAL;

arch/x86/include/asm/pkvm_hypercalls.h
@@ -67,6 +67,7 @@ PKVM_HC(update_cr8_intercept)
 PKVM_HC(set_virtual_apic_mode)
 PKVM_HC(refresh_apicv_exec_ctrl)
 PKVM_HC(load_eoi_exitmap)
+PKVM_HC(hwapic_isr_update)
 
 #undef PKVM_HC
 #undef PKVM_HC_OUT

commit 2e9d29b660dc948e32a1ed1de2843f67ff44f3b3

SQUASHME: pKVM: x86: Enhance handling for __pkvm__hwapic_isr_update PV interface
The hwapic_isr_update callback can be accessed by the untrusted host via
the PV interface __pkvm__hwapic_isr_update. And since this callback
should only be used when APICv is active, check this to prevent the host
from misusing this on the platform which doesn't support APICv.

As the host is untrusted, validate the passed in max_isr value for the
same security reason with the PV interface __pkvm__inject_irq, to make
sure it is not an exception vector for the pVM as the pVM's exceptions
are emulated and injected by the pKVM itself, and the host is not
allowed to inject exceptions to the pVM.

Fixes: b2743f17b8c9 ("pKVM: x86: Add hwapic_isr_update PV interface")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -1132,6 +1132,27 @@ static void pkvm_load_eoi_exitmap(struct kvm_vcpu *vcpu, u64 eoi_exit_bitmap0,
 	kvm_x86_call(load_eoi_exitmap)(vcpu, eoi_exit_bitmap);
 }
 
+static int pkvm_hwapic_isr_update(struct kvm_vcpu *vcpu, int max_isr)
+{
+	if (!lapic_in_kernel(vcpu) || !vcpu->arch.apic->apicv_active)
+		return -EOPNOTSUPP;
+
+	/*
+	 * Validate the passed in max_isr value from the host to make sure it is
+	 * not an exception vector for the pVM for the same security reason with
+	 * the PV interface __pkvm__inject_irq. See comments in the function
+	 * pkvm_inject_irq.
+	 *
+	 * The value -1 is allowed as it represents no interrupt.
+	 */
+	if (pkvm_is_protected_vcpu(vcpu) && max_isr != -1 && max_isr < 32)
+		return -EPERM;
+
+	kvm_x86_call(hwapic_isr_update)(vcpu, max_isr);
+
+	return 0;
+}
+
 static void pkvm_sync_pir_to_irr(struct kvm_vcpu *vcpu, int pir)
 {
 	to_pkvm_vcpu(vcpu)->max_irr = pir;
@@ -1789,7 +1810,7 @@ static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc 
 				      pkvm_hc_input3(hvcpu), pkvm_hc_input4(hvcpu));
 		break;
 	case __pkvm__hwapic_isr_update:
-		kvm_x86_call(hwapic_isr_update)(vcpu, pkvm_hc_input1(hvcpu));
+		ret = pkvm_hwapic_isr_update(vcpu, pkvm_hc_input1(hvcpu));
 		break;
 	case __pkvm__sync_pir_to_irr:
 		pkvm_sync_pir_to_irr(vcpu, pkvm_hc_input1(hvcpu));
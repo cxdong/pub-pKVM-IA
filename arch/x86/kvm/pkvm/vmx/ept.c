cxdong/pub-pKVM-IA

commit 06e4eae63c91dac79e62d89bc3e613b72f98d7df

pKVM: VMX: Implement flush_tlb for the host ept
Implement the flush_tlb callback for the host ept to set the request
KVM_REQ_TLB_FLUSH_CURRENT for each host vcpu and kick them out of
non-root mode. And this request is handled by syncing the host ept
context.

As the host ept flushing requires to shoot down all the vcpus, and it
also doesn't have an address granularity flushing, mark the host ept as
flush_tlb_lazy.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/vmx/ept.c
@@ -133,7 +133,14 @@ static u64 ept_pte_get(void *ptep)
 static void host_ept_flush_tlb(struct pkvm_pgtable *pgt,
 			       unsigned long vaddr, unsigned long size)
 {
-	/* TODO: Flush TLB for the host EPT */
+	int i;
+
+	for (i = 0; i < pkvm_hyp->num_cpus; i++) {
+		struct kvm_vcpu *vcpu = pkvm_hyp->host_vcpus[i];
+
+		kvm_make_request(KVM_REQ_TLB_FLUSH_CURRENT, vcpu);
+		pkvm_kick_vcpu(vcpu);
+	}
 }
 
 static const struct pkvm_pgtable_ops host_ept_pgt_ops = {
@@ -172,6 +179,7 @@ int pkvm_host_ept_init(struct pkvm_pgtable *pgt, void *pool_base,
 		.level = cpu_has_vmx_ept_5levels() ? 5 : 4,
 		.allowed_pgsz = 1 << PG_LEVEL_4K,
 		.table_prot = VMX_EPT_RWX_MASK,
+		.flush_tlb_lazy = true,
 	};
 	int ret;
 
@@ -232,6 +240,12 @@ int pkvm_host_ept_finalize(struct pkvm_pgtable *pgt)
 	}
 
 	ept_sync_global();
+	/*
+	 * Clear the pending TLB flush request left after updating host EPT
+	 * mappings in initialize_global(), as EPT has just been flushed with
+	 * global context anyway.
+	 */
+	kvm_clear_request(KVM_REQ_TLB_FLUSH_CURRENT, hvcpu);
 
 	return 0;
 }
@@ -303,4 +317,12 @@ int pkvm_handle_host_ept_violation(void)
 
 	pkvm_host_mmu_unlock();
 	return ret;
+}
+
+void pkvm_flush_host_ept(void)
+{
+	if (WARN_ON(!host_ept))
+		return;
+
+	ept_sync_context(construct_host_eptp(host_ept));
 }

arch/x86/kvm/pkvm/vmx/ept.h
@@ -8,5 +8,6 @@ int pkvm_host_ept_init(struct pkvm_pgtable *pgt, void *pool_base,
 		       unsigned long pool_pages);
 int pkvm_host_ept_finalize(struct pkvm_pgtable *pgt);
 int pkvm_handle_host_ept_violation(void);
+void pkvm_flush_host_ept(void);
 
 #endif /* __PKVM_VMX_EPT_H */

arch/x86/kvm/pkvm/vmx/host_vmx.c
@@ -274,6 +274,9 @@ static void handle_pending_events(struct kvm_vcpu *vcpu, bool *req_immediate_exi
 				*req_immediate_exit = true;
 		}
 	}
+
+	if (kvm_check_request(KVM_REQ_TLB_FLUSH_CURRENT, vcpu))
+		pkvm_flush_host_ept();
 }
 
 static inline void set_vcpu_mode(struct kvm_vcpu *vcpu, int mode)

commit 5f4d333289c7accea319b1167df5169199dd61ba

pKVM: VMX: Wait host vCPU kicked out after flushing TLB
After unmapping pages from the host EPT and sending INIT to kick remote
host vCPUs to flush their TLBs. The current code proceeds without
waiting for the remote CPUs to actually exit VMX non-root mode.
Receiving the INIT signal is not instantaneous, as it takes time to
transfer the INIT message from the sender to the receiver over the
system/apic bus. So a small window exists where a remote CPU can still
access the unmapped pages via stale TLB entries.

Fix this by waiting for all kicked host vCPUs to exit guest mode after
the kick loop completes. The wait is placed after pkvm_iommu_pt_flush()
so that the IOMMU flush time overlaps with the spinning overhead.

Fixes: 06e4eae63c91 ("pKVM: VMX: Implement flush_tlb for the host ept")
Reported-by: Yi Ran <yyi@stu.pku.edu.cn>
Closes: https://github.com/intel-staging/pKVM-IA/issues/83
Suggested-by: Dmytro Maluka <dmaluka@google.com>
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/vmx/ept.c
@@ -205,6 +205,9 @@ static void host_ept_flush_tlb(struct pkvm_pgtable *pgt,
 	}
 
 	pkvm_iommu_pt_flush(vaddr, size);
+
+	for_each_pkvm_initialized_cpu(i, vcpu)
+		pkvm_wait_vcpu_kicked_out(vcpu);
 }
 
 static void guest_ept_flush_tlb(struct pkvm_pgtable *pgt,
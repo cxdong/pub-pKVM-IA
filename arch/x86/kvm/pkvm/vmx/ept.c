cxdong/pub-pKVM-IA

commit dfeaf9a6456c2ef3632ed50ea1cf9a8edfccc1a3

pKVM: VMX: Implement TLB flush for guest EPT
Implement the flush_tlb callback for guest EPT page table, by kicking
all the guest's vCPUs to let them handle the TLB flush request (i.e.
similarly to the flush_tlb callback for the host EPT, which does the
same but with host vCPUs).

The callback shall only be called while all vCPUs are still operational,
i.e. it is not expected to be called during VM teardown (when all vCPUs
should be already torn down, so there is no need to flush TLBs).

Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/vmx/ept.c
@@ -158,7 +158,26 @@ static void host_ept_flush_tlb(struct pkvm_pgtable *pgt,
 static void guest_ept_flush_tlb(struct pkvm_pgtable *pgt,
 				unsigned long vaddr, unsigned long size)
 {
-	/* TODO */
+	struct pkvm_vm *pkvm_vm = pgt_to_pkvm(pgt);
+	int i;
+
+	pkvm_spin_lock(&pkvm_vm->lock);
+
+	for (i = 0; i < pkvm_vm->kvm.created_vcpus; i++) {
+		struct pkvm_vcpu *pkvm_vcpu;
+		struct kvm_vcpu *vcpu;
+
+		pkvm_vcpu = pkvm_vm->vcpus[i];
+		if (WARN_ON_ONCE(!pkvm_vcpu))
+			continue;
+
+		vcpu = &pkvm_vcpu->vcpu;
+
+		kvm_make_request(KVM_REQ_TLB_FLUSH_CURRENT, vcpu);
+		pkvm_kick_vcpu(vcpu);
+	}
+
+	pkvm_spin_unlock(&pkvm_vm->lock);
 }
 
 static u64 ept_pgstate_mask(void)

commit 4e723cbb2940651614b6fa32a5e968bbda418d5a

pKVM: VMX: Wait guest vCPU kicked out after flushing TLB
Apply the same TLB flush synchronization fix to the guest EPT path that
was done for the host EPT in a prior commit. After kicking all guest
vCPUs to flush their TLBs due to a guest EPT unmap, wait for each vCPU
to exit VMX non-root mode before returning. This closes the window where
a remote CPU could access unmapped guest pages via stale TLB entries.

The wait loop is placed after the kick loop so that the INIT delivery
latency to each remote CPU overlaps, minimizing the total stall time.

Fixes: dfeaf9a6456c ("pKVM: VMX: Implement TLB flush for guest EPT")
Reported-by: Yi Ran <yyi@stu.pku.edu.cn>
Closes: https://github.com/intel-staging/pKVM-IA/issues/83
Suggested-by: Dmytro Maluka <dmaluka@google.com>
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/vmx/ept.c
@@ -224,6 +224,9 @@ static void guest_ept_flush_tlb(struct pkvm_pgtable *pgt,
 		pkvm_kick_vcpu(vcpu);
 	}
 
+	for_each_pkvm_guest_vcpu(i, vcpu, pkvm_vm)
+		pkvm_wait_vcpu_kicked_out(vcpu);
+
 	pkvm_spin_unlock(&pkvm_vm->lock);
 }
 
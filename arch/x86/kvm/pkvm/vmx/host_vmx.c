cxdong/pub-pKVM-IA

commit 2e6f8963c4b74e6563d88fc2df34b60e034a7eb8

pKVM: x86: Add helper to kick vcpu
Add helper to kick a running vcpu out of non-root mode. As the interrupt
is passthrough to the host VM, sending IPI interrupt while a host vcpu
is running will not make it out of non-root mode. Use INIT which can
kick a vcpu out of non-root mode for this case. The limitation of using
INIT is that, the CPU cannot receive this INIT signal if it is in root
mode, until enter the guest. The INIT signal vmexit is handle by
handling the pending events, which is already covered in the
pkvm_host_vmexit_main().

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/vmx/host_vmx.c
@@ -276,12 +276,24 @@ static void handle_pending_events(struct kvm_vcpu *vcpu, bool *req_immediate_exi
 	}
 }
 
+static inline void set_vcpu_mode(struct kvm_vcpu *vcpu, int mode)
+{
+	vcpu->mode = mode;
+	/*
+	 * Make sure vcpu->mode is set before checking/handling the pending
+	 * requests. Pairs with kvm_vcpu_exiting_guest_mode().
+	 */
+	smp_wmb();
+}
+
 void pkvm_host_vmexit_main(struct vcpu_vmx *vmx)
 {
 	struct kvm_vcpu *vcpu = &vmx->vcpu;
 	bool req_immediate_exit = false;
 	struct vcpu_vt *vt = &vmx->vt;
 	bool skip_instruction = false;
+
+	set_vcpu_mode(vcpu, OUTSIDE_GUEST_MODE);
 
 	vcpu->arch.cr2 = native_read_cr2();
 	vcpu->arch.exception.injected = false;
@@ -290,6 +302,13 @@ void pkvm_host_vmexit_main(struct vcpu_vmx *vmx)
 	vt->exit_qualification = vmcs_readl(EXIT_QUALIFICATION);
 
 	switch (vt->exit_reason.full) {
+	case EXIT_REASON_INIT_SIGNAL:
+		/*
+		 * INIT is used as kick when making a request.
+		 * So just break the vmexits and go to pending
+		 * events handling.
+		 */
+		break;
 	case EXIT_REASON_INTERRUPT_WINDOW:
 		handle_irq_window(vcpu);
 		break;
@@ -337,11 +356,28 @@ void pkvm_host_vmexit_main(struct vcpu_vmx *vmx)
 	if (skip_instruction)
 		skip_emulated_instruction();
 
+handle_events:
 	handle_pending_events(vcpu, &req_immediate_exit);
+
+	/*
+	 * Once the pending events have been handled, set IN_GUEST_MODE to
+	 * indicate kick is required for the new pending events.
+	 */
+	set_vcpu_mode(vcpu, IN_GUEST_MODE);
 
 	if (req_immediate_exit) {
 		kvm_make_request(KVM_REQ_EVENT, vcpu);
 		request_host_immediate_exit(vmx);
+	} else if (READ_ONCE(vcpu->mode) == EXITING_GUEST_MODE ||
+		   kvm_request_pending(vcpu)) {
+		/*
+		 * Some vcpu requests may be set after handle_pending_events()
+		 * but before set vcpu mode to IN_GUEST_MODE. In this case the
+		 * init signal will not be send to kick the vcpu. To guarantee
+		 * such vcpu requests can be handled timely, try to handle
+		 * pending event again.
+		 */
+		goto handle_events;
 	}
 
 	if (vcpu->arch.cr2 != native_read_cr2())

arch/x86/kvm/pkvm/lapic.c
@@ -22,7 +22,11 @@ static int setup_lapic(struct pkvm_lapic *lapic, u64 apicbase)
 	}
 
 	lapic->apic_id = native_apic_msr_read(APIC_ID);
-	lapic->ready = true;
+	/*
+	 * Make sure ready is updated after the apic_id, and pairs with the
+	 * smp_load_acquire() in the pkvm_lapic_send_init().
+	 */
+	smp_store_release(&lapic->ready, true);
 	return 0;
 }
 
@@ -37,4 +41,40 @@ int pkvm_lapic_init(void)
 	rdmsrq(MSR_IA32_APICBASE, apicbase);
 
 	return setup_lapic(lapic, apicbase);
+}
+
+void pkvm_lapic_send_init(int cpu)
+{
+	u32 icrlow = APIC_INT_ASSERT | APIC_DM_INIT;
+	struct pkvm_lapic *local, *remote;
+
+	/*
+	 * Check the CPU number to make sure only sending the INIT to other CPU
+	 * but not self.
+	 */
+	if (unlikely(cpu == raw_smp_processor_id()))
+		return;
+
+	local = this_cpu_ptr(&pkvm_lapic);
+	remote = per_cpu_ptr(&pkvm_lapic, cpu);
+
+	/*
+	 * Pairs with the smp_store_release() in the setup_lapic().
+	 * If remote lapic is not ready, it means the remote CPU is not
+	 * initialized yet(by pkvm_init()). In this case, it is not necessary to
+	 * send INIT to kick as this remote CPU will handle all the pending
+	 * requests before being initialized.
+	 */
+	if (unlikely(!smp_load_acquire(&remote->ready)))
+		return;
+
+	/*
+	 * If remote lapic is ready, it means the remote CPU has already been
+	 * initialized and may run in non-root mode. It does need to be kicked
+	 * in this case. To do so, the local lapic must be initialized. If it is
+	 * not, it means a bug somewhere.
+	 */
+	BUG_ON(!local->ready);
+
+	native_x2apic_icr_write(icrlow, remote->apic_id);
 }

arch/x86/kvm/pkvm/lapic.h
@@ -3,5 +3,6 @@
 #define __PKVM_X86_LAPIC_H
 
 int pkvm_lapic_init(void);
+void pkvm_lapic_send_init(int cpu);
 
 #endif /* __PKVM_X86_LAPIC_H */

arch/x86/kvm/pkvm/pkvm.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <linux/types.h>
 #include "init.h"
+#include "lapic.h"
 #include "pkvm.h"
 
 /*
@@ -36,4 +37,13 @@ void pkvm_handle_host_hypercall(struct kvm_vcpu *vcpu)
 	}
 
 	pkvm_hc_set_ret(vcpu, ret);
+}
+
+void pkvm_kick_vcpu(struct kvm_vcpu *vcpu)
+{
+	/* No need to kick if a vcpu is already out of guest mode */
+	if (kvm_vcpu_exiting_guest_mode(vcpu) != IN_GUEST_MODE)
+		return;
+
+	pkvm_lapic_send_init(READ_ONCE(vcpu->cpu));
 }

arch/x86/kvm/pkvm/pkvm.h
@@ -8,5 +8,6 @@ DECLARE_PER_CPU(struct pkvm_pcpu *, phys_cpu);
 DECLARE_PER_CPU(struct kvm_vcpu *, host_vcpu);
 
 void pkvm_handle_host_hypercall(struct kvm_vcpu *vcpu);
+void pkvm_kick_vcpu(struct kvm_vcpu *vcpu);
 
 #endif /* __PKVM_X86_PKVM_H */

commit f0c9d5f0c5a241eb1512b3ec480187d8763871b4

pKVM: VMX: Fix setting for the host vcpu->mode
The helper set_vcpu_mode() with smp_wmb() is not safe to be used for
setting the vcpu->mode to IN_GUEST_MODE. Setting to IN_GUEST_MODE is
immediately followed by reading vcpu->requests in kvm_request_pending().
Without a full memory barrier, it allows a race where a remote CPU makes
a request but skips the kick (because it reads OUTSIDE_GUEST_MODE),
while the local CPU misses the request (because its read was executed
before changing to the IN_GUEST_MODE). So change to use the helper
pkvm_set_vcpu_in_guest() with a full memory barrier.

For the OUTSIDE_GUEST_MODE, the previous write barrier is functionally
fine, but change to use the helper pkvm_set_vcpu_outside_guest() to
propagate the OUTSIDE_GUEST_MODE as early as possible for the other CPUs
to skip the unnecessary kicks.

Also change the vcpu->mode to OUTSIDE_GUEST_MODE if there is any pending
request before VM-Entry to make the other CPUs skip the unnecessary
kicks.

Then the helper set_vcpu_mode() is not needed and removed.

Fixes: 2e6f8963c4b7 ("pKVM: x86: Add helper to kick vcpu")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/vmx/host_vmx.c
@@ -289,16 +289,6 @@ static void handle_pending_events(struct kvm_vcpu *vcpu, bool *req_immediate_exi
 		pkvm_flush_host_ept();
 }
 
-static inline void set_vcpu_mode(struct kvm_vcpu *vcpu, int mode)
-{
-	vcpu->mode = mode;
-	/*
-	 * Make sure vcpu->mode is set before checking/handling the pending
-	 * requests. Pairs with kvm_vcpu_exiting_guest_mode().
-	 */
-	smp_wmb();
-}
-
 static void fixup_host_vmx(struct vcpu_vmx *vmx)
 {
 	if (boot_cpu_has(X86_FEATURE_INTEL_PT)) {
@@ -327,7 +317,7 @@ void pkvm_host_vmexit_main(struct vcpu_vmx *vmx)
 
 	pkvm_trace_vmexit_start(vcpu);
 
-	set_vcpu_mode(vcpu, OUTSIDE_GUEST_MODE);
+	pkvm_set_vcpu_outside_guest(vcpu);
 
 	vcpu->arch.cr2 = native_read_cr2();
 	vcpu->arch.exception.injected = false;
@@ -393,17 +383,14 @@ void pkvm_host_vmexit_main(struct vcpu_vmx *vmx)
 handle_events:
 	handle_pending_events(vcpu, &req_immediate_exit);
 
-	/*
-	 * Once the pending events have been handled, set IN_GUEST_MODE to
-	 * indicate kick is required for the new pending events.
-	 */
-	set_vcpu_mode(vcpu, IN_GUEST_MODE);
+	pkvm_set_vcpu_in_guest(vcpu);
 
 	if (req_immediate_exit) {
 		kvm_make_request(KVM_REQ_EVENT, vcpu);
 		request_host_immediate_exit(vmx);
 	} else if (READ_ONCE(vcpu->mode) == EXITING_GUEST_MODE ||
 		   kvm_request_pending(vcpu)) {
+		pkvm_set_vcpu_outside_guest(vcpu);
 		/*
 		 * Some vcpu requests may be set after handle_pending_events()
 		 * but before set vcpu mode to IN_GUEST_MODE. In this case the
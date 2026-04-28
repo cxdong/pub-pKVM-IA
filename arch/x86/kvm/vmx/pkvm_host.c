cxdong/pub-pKVM-IA

commit 0dee734bb75793b22b6e5eec4c76ed25f7759ab9

KVM: pVMX: Finalize pVM before running it
Modify the host-side `vcpu_pre_run` hook to automatically invoke the
`vm_finalize` hypercall just before the primary vCPU (aka BSP i.e.
Bootstrap Processor) of a protected VM is run for the first time.

This completes the handshake between the host VMM and the pKVM hypervisor,
ensuring that the `pvmfw` configuration is committed and validated
before guest execution begins.

[ Grzegorz: carve-out part of [1] and prepare commit log. Also squash [2]
  leftovers.
  [1] ANDROID: pkvm: x86: Refactor pvmfw handling as a part of new PV code
      https://android-review.googlesource.com/c/kernel/common/+/3632018
  [2] ANDROID: pkvm: x86: Properly return error in pvmfw address set ioctl
      https://android-review.googlesource.com/c/kernel/common/+/3632020 ]

Signed-off-by: Dmytro Maluka <dmaluka@google.com>
Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>

arch/x86/kvm/vmx/pkvm_host.c
@@ -1295,6 +1295,18 @@ static void pkvm_flush_tlb_guest(struct kvm_vcpu *vcpu)
 
 static int pkvm_vcpu_pre_run(struct kvm_vcpu *vcpu)
 {
+	struct kvm_pkvm_vm *pkvm = &vcpu->kvm->arch.pkvm;
+	int ret;
+
+	if (unlikely(pkvm_is_protected_vcpu(vcpu) && !kvm_vcpu_has_run(vcpu) &&
+		     kvm_vcpu_is_reset_bsp(vcpu))) {
+		mutex_lock(&pkvm->finalized_lock);
+		ret = pkvm_hypercall(vm_finalize, vcpu->kvm->arch.pkvm.handle);
+		mutex_unlock(&pkvm->finalized_lock);
+		if (ret < 0)
+			return ret;
+	}
+
 	return 1;
 }
 

commit 5ab1e996690c4c75aff451c3165de46df35e23ed

KVM: pVMX: Check finalized state before vm_finalize hypercall
pkvm_vcpu_pre_run() guards the vm_finalize hypercall with
!kvm_vcpu_has_run(), but last_vmentry_cpu is only set inside
vcpu_enter_guest() after the actual VM entry. If vcpu_enter_guest()
takes an early return before guest entry (e.g., pending signal or
KVM_REQ_* handling), last_vmentry_cpu remains -1 and the next KVM_RUN
re-triggers the hypercall, which returns -EBUSY because the hypervisor
already set finalized = true.

Check pkvm->finalized under the finalized_lock before calling the
hypercall, consistent with pkvm_vm_ioctl_set_fw_gpa() which already
performs this check.

Fixes: 0dee734bb757 ("KVM: pVMX: Finalize pVM before running it")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/vmx/pkvm_host.c
@@ -1299,12 +1299,13 @@ static void pkvm_flush_tlb_guest(struct kvm_vcpu *vcpu)
 static int pkvm_vcpu_pre_run(struct kvm_vcpu *vcpu)
 {
 	struct kvm_pkvm_vm *pkvm = &vcpu->kvm->arch.pkvm;
-	int ret;
+	int ret = 0;
 
 	if (unlikely(pkvm_is_protected_vcpu(vcpu) && !kvm_vcpu_has_run(vcpu) &&
 		     kvm_vcpu_is_reset_bsp(vcpu))) {
 		mutex_lock(&pkvm->finalized_lock);
-		ret = pkvm_hypercall(vm_finalize, vcpu->kvm->arch.pkvm.handle);
+		if (!pkvm->finalized)
+			ret = pkvm_hypercall(vm_finalize, pkvm->handle);
 		mutex_unlock(&pkvm->finalized_lock);
 		if (ret < 0)
 			return ret;
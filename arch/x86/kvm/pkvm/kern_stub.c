cxdong/pub-pKVM-IA

commit e563958f43da821cc76805c0b775a1d520449bbf

pKVM: x86: Import kernel libraries for extable
Import the kernel extable, bsearch and sort libraries to make the pKVM
hypervisor having the capability to search the exception table section.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/kern_stub.c
@@ -1,4 +1,13 @@
 // SPDX-License-Identifier: GPL-2.0
+#include <linux/kernel.h>
 #include "kern_stub.h"
 
 void warn_thunk_thunk(void) {}
+
+#ifndef CONFIG_PREEMPTION
+int __cond_resched(void) {}
+#endif
+
+#ifdef CONFIG_DEBUG_ATOMIC_SLEEP
+void __might_resched(const char *file, int line, unsigned int offsets) {}
+#endif

arch/x86/kvm/pkvm/Makefile
@@ -25,6 +25,10 @@ $(obj)/retpoline.pkvm.S: $(srctree)/arch/x86/lib/retpoline.S $(cmd_deps) FORCE
 quiet_cmd_patch_retpoline = PATCH_RETPOLINE $@
       cmd_patch_retpoline = $(BASH) $(src)/compiling_cmds.sh patch_retpoline $< $@
 
+kernel-lib			:= ../../../../lib
+pkvm-hyp-y			+= $(kernel-lib)/sort.o $(kernel-lib)/bsearch.o \
+				   $(kernel-lib)/extable.o
+
 kvm				:= ..
 pkvm-hyp-y			+= $(kvm)/x86.o
 

arch/x86/kvm/pkvm/undef.h
@@ -12,6 +12,7 @@
 #undef CONFIG_PRINTK
 #undef CONFIG_DEBUG_VIRTUAL
 #undef CONFIG_CALL_THUNKS_DEBUG
+#undef CONFIG_PREEMPT_DYNAMIC
 
 #define NOTRACE
 

commit 8fdb97467fe59343213ecd694e00431e015d2cef

pKVM: x86: Fix missing return value in __cond_resched stub
The __cond_resched() stub is declared as returning int but has no return
statement. Flowing off the end of a non-void function is undefined
behavior per the C standard.

Add return 0 since the stub is a no-op that should indicate no
rescheduling occurred.

Fixes: e563958f43da ("pKVM: x86: Import kernel libraries for extable")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/kern_stub.c
@@ -7,7 +7,7 @@
 void warn_thunk_thunk(void) {}
 
 #ifndef CONFIG_PREEMPTION
-int __cond_resched(void) {}
+int __cond_resched(void) { return 0; }
 #endif
 
 #ifdef CONFIG_DEBUG_ATOMIC_SLEEP
cxdong/pub-pKVM-IA

commit fe2e1dbabdebc4d2254443f9e269bf50b65d2ee3

pKVM: VMX: Handle the deprivileged host vmexit
Although the VMCS is configured to minimize the vmexits for the
deprivileged host, there are still some vmexits cannot be avoid.

1. The CPUID/XSETBV vmexit is handled by re-executing the same
instruction on the native and returning the corresponding result to the
host.

2. The CR vmexit only handles mov-to-cr4 to prevent the host really
controlling the VMX on hardware.

3. The MSR vmexits are handled by injecting the #GP to the host VM as
currently only invalid MSR accessing will cause vmexits. Reuse the KVM
source code to queue and inject exception.

The other types of vmexits should not happen at this moment.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/vmx/host_vmx.c
@@ -1,9 +1,144 @@
 // SPDX-License-Identifier: GPL-2.0
+#include <linux/kvm_types.h>
+#include <linux/memblock.h>
+#include <kvm_emulate.h>
+#include <vmx/x86_ops.h>
 #include "host_vmx.h"
+
+#define CR4			4
+#define MOV_TO_CR		0
 
 struct vmcs_config host_vmcs_config;
 
+static void skip_emulated_instruction(void)
+{
+	unsigned long rip;
+
+	rip = vmcs_readl(GUEST_RIP);
+	rip += vmcs_read32(VM_EXIT_INSTRUCTION_LEN);
+	vmcs_writel(GUEST_RIP, rip);
+}
+
+static void handle_cpuid(struct kvm_vcpu *vcpu)
+{
+	u32 eax, ebx, ecx, edx;
+
+	eax = vcpu->arch.regs[VCPU_REGS_RAX];
+	ecx = vcpu->arch.regs[VCPU_REGS_RCX];
+	native_cpuid(&eax, &ebx, &ecx, &edx);
+	vcpu->arch.regs[VCPU_REGS_RAX] = eax;
+	vcpu->arch.regs[VCPU_REGS_RBX] = ebx;
+	vcpu->arch.regs[VCPU_REGS_RCX] = ecx;
+	vcpu->arch.regs[VCPU_REGS_RDX] = edx;
+}
+
+static void handle_cr(struct kvm_vcpu *vcpu)
+{
+	struct vcpu_vt *vt = to_vt(vcpu);
+	unsigned long exit_qual, val;
+	int cr, type, reg;
+
+	exit_qual = vt->exit_qualification;
+	cr = exit_qual & 15;
+	type = (exit_qual >> 4)	& 3;
+	reg = (exit_qual >> 8) & 15;
+
+	switch (type) {
+	case MOV_TO_CR:
+		switch (cr) {
+		case CR4:
+			/*
+			 * VMXE bit is owned by pkvm, others are owned by host
+			 * So only when guest is trying to modify VMXE bit it
+			 * can cause vmexit and get here.
+			 */
+			val = vcpu->arch.regs[reg];
+			vmcs_writel(CR4_READ_SHADOW, val);
+			break;
+		default:
+			break;
+		}
+		break;
+	default:
+		break;
+	}
+}
+
+static int handle_read_msr(struct kvm_vcpu *vcpu)
+{
+	kvm_inject_gp(vcpu, 0);
+	return X86EMUL_UNHANDLEABLE;
+}
+
+static int handle_write_msr(struct kvm_vcpu *vcpu)
+{
+	kvm_inject_gp(vcpu, 0);
+	return X86EMUL_UNHANDLEABLE;
+}
+
+static void handle_xsetbv(struct kvm_vcpu *vcpu)
+{
+	u32 eax = (u32)(vcpu->arch.regs[VCPU_REGS_RAX] & -1u);
+	u32 edx = (u32)(vcpu->arch.regs[VCPU_REGS_RDX] & -1u);
+	u32 ecx = (u32)(vcpu->arch.regs[VCPU_REGS_RCX] & -1u);
+
+	asm volatile(".byte 0x0f,0x01,0xd1"
+			: : "a" (eax), "d" (edx), "c" (ecx));
+}
+
+static void handle_pending_events(struct kvm_vcpu *vcpu)
+{
+	if (kvm_check_request(KVM_REQ_EVENT, vcpu)) {
+		if (vcpu->arch.exception.pending) {
+			vmx_inject_exception(vcpu);
+			vcpu->arch.exception.pending = false;
+			vcpu->arch.exception.injected = true;
+		}
+	}
+}
+
 void pkvm_host_vmexit_main(struct vcpu_vmx *vmx)
 {
-	/* TODO */
+	struct kvm_vcpu *vcpu = &vmx->vcpu;
+	struct vcpu_vt *vt = &vmx->vt;
+	bool skip_instruction = false;
+
+	vcpu->arch.cr2 = native_read_cr2();
+	vcpu->arch.exception.injected = false;
+
+	vt->exit_reason.full = vmcs_read32(VM_EXIT_REASON);
+	vt->exit_qualification = vmcs_readl(EXIT_QUALIFICATION);
+
+	switch (vt->exit_reason.full) {
+	case EXIT_REASON_CPUID:
+		handle_cpuid(vcpu);
+		skip_instruction = true;
+		break;
+	case EXIT_REASON_CR_ACCESS:
+		handle_cr(vcpu);
+		skip_instruction = true;
+		break;
+	case EXIT_REASON_MSR_READ:
+		if (handle_read_msr(vcpu) == X86EMUL_CONTINUE)
+			skip_instruction = true;
+		break;
+	case EXIT_REASON_MSR_WRITE:
+		if (handle_write_msr(vcpu) == X86EMUL_CONTINUE)
+			skip_instruction = true;
+		break;
+	case EXIT_REASON_XSETBV:
+		handle_xsetbv(vcpu);
+		skip_instruction = true;
+		break;
+	default:
+		break;
+	}
+
+	if (skip_instruction)
+		skip_emulated_instruction();
+
+	handle_pending_events(vcpu);
+
+	if (vcpu->arch.cr2 != native_read_cr2())
+		native_write_cr2(vcpu->arch.cr2);
 }

arch/x86/kvm/vmx/pkvm_init.c
@@ -102,6 +102,17 @@ static __init int pkvm_setup_host_vm(struct pkvm_hyp *pkvm)
 		return -ENOMEM;
 	}
 
+	/*
+	 * Only a few fields in the kvm structure will be used, e.g.,
+	 * hlt_in_guest for exception injection code to clear hlt state.
+	 * As HLT instruction will be passthrough to the host VM, set
+	 * hlt_in_guest as true. As the mwait/pause/cstate will also be
+	 * passthrough, initialized them as well to reflect the fact.
+	 */
+	kvm_disable_exits(&kvmx->kvm, KVM_X86_DISABLE_EXITS_MWAIT |
+				      KVM_X86_DISABLE_EXITS_HLT   |
+				      KVM_X86_DISABLE_EXITS_PAUSE |
+				      KVM_X86_DISABLE_EXITS_CSTATE);
 	pkvm->host_kvm = &kvmx->kvm;
 
 	return 0;

commit 4048319ccffaa4f7ab78c58ce5e1580e3c1abbc6

pKVM: x86: Add exception fixup for XSETBV in host vmexit handler
handle_xsetbv() passes host-supplied register values directly to the
native XSETBV instruction without validation. If the host sets invalid
XCR0 values (e.g., clearing XFEATURE_MASK_FP), the XSETBV in VMX root
mode generates a #GP that reaches default_exception_handler() which
halts the CPU, as there is no exception table fixup.

Add _ASM_EXTABLE to the XSETBV inline assembly and handle faults
by injecting #GP back to the host VM instead of crashing the pKVM.

Fixes: fe2e1dbabdeb ("pKVM: VMX: Handle the deprivileged host vmexit")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/vmx/host_vmx.c
@@ -214,14 +214,37 @@ static void handle_preemption_timer(struct kvm_vcpu *vcpu)
 	pin_controls_clearbit(to_vmx(vcpu), PIN_BASED_VMX_PREEMPTION_TIMER);
 }
 
-static void handle_xsetbv(struct kvm_vcpu *vcpu)
+static int handle_xsetbv(struct kvm_vcpu *vcpu)
 {
 	u32 eax = (u32)(vcpu->arch.regs[VCPU_REGS_RAX] & -1u);
 	u32 edx = (u32)(vcpu->arch.regs[VCPU_REGS_RDX] & -1u);
 	u32 ecx = (u32)(vcpu->arch.regs[VCPU_REGS_RCX] & -1u);
 
-	asm volatile(".byte 0x0f,0x01,0xd1"
-			: : "a" (eax), "d" (edx), "c" (ecx));
+	asm goto("1: xsetbv\n\t"
+		 _ASM_EXTABLE(1b, %l[fault])
+		 : : "a" (eax), "d" (edx), "c" (ecx) : : fault);
+
+	return X86EMUL_CONTINUE;
+
+fault:
+	/*
+	 * Although the SDM doesn't describe the priority of #UD and
+	 * interception for xsetbv, the experiment shows that #UD due to
+	 * CR4.OSXSAVE[bit 18] == 0 and the LOCK prefix has priority
+	 * over the interception.
+	 *
+	 * So the pKVM hypervisor itself won't generate #UD when
+	 * executes the xsetbv instruction, only #GP can be generated
+	 * due to invalid configurations. Always inject #GP if xsetbv
+	 * is failed.
+	 *
+	 * TODO: CPUID.01H:ECX.XSAVE[bit 26] == 0 will also result in
+	 * #UD but all modern Intel CPUs have XSAVE. If the pKVM runs
+	 * on such CPU without XSAVE, verify if this #UD also has
+	 * priority over the interception.
+	 */
+	kvm_inject_gp(vcpu, 0);
+	return X86EMUL_UNHANDLEABLE;
 }
 
 static void inject_pending_nmi(struct kvm_vcpu *vcpu)
@@ -364,8 +387,8 @@ void pkvm_host_vmexit_main(struct vcpu_vmx *vmx)
 		handle_preemption_timer(vcpu);
 		break;
 	case EXIT_REASON_XSETBV:
-		handle_xsetbv(vcpu);
-		skip_instruction = true;
+		if (handle_xsetbv(vcpu) == X86EMUL_CONTINUE)
+			skip_instruction = true;
 		break;
 	default:
 		pkvm_err_ratelimited("Unsupported vmexit reason 0x%x.\n",
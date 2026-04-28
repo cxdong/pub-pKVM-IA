cxdong/pub-pKVM-IA

commit 61fc3a83a9b97a729e3046b99673b3e8d3a83cea

pKVM: x86: Add helper to find the address range for a given phys addr
Add helper pkvm_find_addr_range which can find out the address range
that contains the given physical address. The address range would be
either a memory range if the given physical address is memory, or a hole
(which is between memory ranges and usually can be used as the MMIO)
range if the given physical address is not memory, which is indicated by
the return value.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/memory.c
@@ -2,6 +2,7 @@
 #include <linux/cache.h>
 #include <linux/types.h>
 #include <asm/kvm_pkvm.h>
+#include "memory.h"
 
 unsigned long page_offset_base __ro_after_init;
 unsigned long phys_base;
@@ -15,6 +16,50 @@ u64 sme_me_mask;
 
 struct memblock_region pkvm_memory[PKVM_MEMBLOCK_REGIONS];
 unsigned int pkvm_memblock_nr;
+
+/**
+ * pkvm_find_addr_range - Find out the addr range for the given physical addr.
+ * @phys:	The physical address to check.
+ * @range:	The structure to return the address range which contains the
+ *		@phys.
+ *
+ * A binary search is performed to find out the address range which contains
+ * @phys and this address range is returned via @range. The returned address
+ * range would be either a memory range if @phys is memory, or a hole (which is
+ * between memory ranges and usually can be used as the MMIO) range if @phys is
+ * not memory, which is indicated by the return value.
+ *
+ * Return true if @phys is memory. Otherwise, return false.
+ */
+bool pkvm_find_addr_range(unsigned long phys, struct range *range)
+{
+	int cur, left = 0, right = pkvm_memblock_nr;
+	struct memblock_region *reg;
+	unsigned long end;
+
+	range->start = 0;
+	range->end = ULONG_MAX;
+
+	/* The list of memblock regions is sorted, binary search it */
+	while (left < right) {
+		cur = (left + right) >> 1;
+		reg = &pkvm_memory[cur];
+		end = reg->base + reg->size;
+		if (phys < reg->base) {
+			right = cur;
+			range->end = reg->base;
+		} else if (phys >= end) {
+			left = cur + 1;
+			range->start = end;
+		} else {
+			range->start = reg->base;
+			range->end = end;
+			return true;
+		}
+	}
+
+	return false;
+}
 
 /*
  * Ensure that __kcfi_typeid_ symbols are emitted for functions that may

arch/x86/kvm/pkvm/memory.h
@@ -3,6 +3,7 @@
 #define __PKVM_X86_MEMORY_H
 
 #include <linux/types.h>
+#include <linux/range.h>
 #include <vdso/limits.h>
 #include <asm/page.h>
 
@@ -62,5 +63,7 @@ static inline void pkvm_set_page_refcounted(struct pkvm_page *p)
 	BUG_ON(p->refcount);
 	p->refcount = 1;
 }
+
+bool pkvm_find_addr_range(unsigned long phys, struct range *range);
 
 #endif /* __PKVM_X86_MEMORY_H */

commit 2475268ed1d2b088f1b170170b7987889fa680dc

pKVM: x86: Fix struct range end convention in pkvm_find_addr_range()
The kernel's struct range uses an inclusive end convention (range_len()
returns end - start + 1, range_contains() checks r1->end >= r2->end),
but pkvm_find_addr_range() stores exclusive end values in range->end.
For example, a memory region with base=0x1000, size=0x1000 would set
range->end to 0x2000 (exclusive) rather than 0x1FFF (inclusive). This
causes incorrect results when the range is later used with kernel range
helpers.

Use end - 1 and base - 1 to store inclusive end values.

Fixes: 61fc3a83a9b9 ("pKVM: x86: Add helper to find the address range for a given phys addr")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/memory.c
@@ -66,13 +66,13 @@ bool pkvm_find_addr_range(unsigned long phys, struct range *range)
 		end = reg->base + reg->size;
 		if (phys < reg->base) {
 			right = cur;
-			range->end = reg->base;
+			range->end = reg->base - 1;
 		} else if (phys >= end) {
 			left = cur + 1;
 			range->start = end;
 		} else {
 			range->start = reg->base;
-			range->end = end;
+			range->end = end - 1;
 			return true;
 		}
 	}
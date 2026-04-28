cxdong/pub-pKVM-IA

commit ba7b65c4389f288d7bafcd825945779390471f3b

SQUASHME: pKVM: x86: Enhance handling for __pkvm__hwapic_isr_update PV interface
The hwapic_isr_update callback can be accessed by the untrusted host via
the PV interface __pkvm__hwapic_isr_update. And since this callback
should only be used when APICv is active, check this to prevent the host
from misusing this on the platform which doesn't support APICv.

As the host is untrusted, validate the passed in max_isr value for the
same security reason with the PV interface __pkvm__inject_irq, to make
sure it is not an exception vector for the pVM as the pVM's exceptions
are emulated and injected by the pKVM itself, and the host is not
allowed to inject exceptions to the pVM.

Fixes: b2743f17b8c9 ("pKVM: x86: Add hwapic_isr_update PV interface")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -1132,6 +1132,27 @@ static void pkvm_load_eoi_exitmap(struct kvm_vcpu *vcpu, u64 eoi_exit_bitmap0,
 	kvm_x86_call(load_eoi_exitmap)(vcpu, eoi_exit_bitmap);
 }
 
+static int pkvm_hwapic_isr_update(struct kvm_vcpu *vcpu, int max_isr)
+{
+	if (!lapic_in_kernel(vcpu) || !vcpu->arch.apic->apicv_active)
+		return -EOPNOTSUPP;
+
+	/*
+	 * Validate the passed in max_isr value from the host to make sure it is
+	 * not an exception vector for the pVM for the same security reason with
+	 * the PV interface __pkvm__inject_irq. See comments in the function
+	 * pkvm_inject_irq.
+	 *
+	 * The value -1 is allowed as it represents no interrupt.
+	 */
+	if (pkvm_is_protected_vcpu(vcpu) && max_isr != -1 && max_isr < 32)
+		return -EPERM;
+
+	kvm_x86_call(hwapic_isr_update)(vcpu, max_isr);
+
+	return 0;
+}
+
 static void pkvm_sync_pir_to_irr(struct kvm_vcpu *vcpu, int pir)
 {
 	to_pkvm_vcpu(vcpu)->max_irr = pir;
@@ -1789,7 +1810,7 @@ static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc 
 				      pkvm_hc_input3(hvcpu), pkvm_hc_input4(hvcpu));
 		break;
 	case __pkvm__hwapic_isr_update:
-		kvm_x86_call(hwapic_isr_update)(vcpu, pkvm_hc_input1(hvcpu));
+		ret = pkvm_hwapic_isr_update(vcpu, pkvm_hc_input1(hvcpu));
 		break;
 	case __pkvm__sync_pir_to_irr:
 		pkvm_sync_pir_to_irr(vcpu, pkvm_hc_input1(hvcpu));

commit 6d4dd7d7b7a3943445fea0f04538e07537035ccc

pKVM: x86: Fix max_isr truncation bypass in hwapic_isr_update
For the pVM without protected apic, the max_isr validation only checks
max_isr < 32 to reject exception vectors. However, since max_isr comes
from the host as an int and is eventually written to GUEST_INTR_STATUS
as u16 with (max_isr << 8), values >= 256 bypass the < 32 check but
truncate to SVI values in [0, 31] after the u16 truncation. For example,
max_isr=256 results in SVI=0 (#DE).

Actually if max_isr >= 256, the max_isr eventually cannot be set to the
hardware, thus this is also not a permitted cases. Fix by validating the
max_isr to make sure it is in the range of [0,255] before checking for
the pVM.

Fixes: ba7b65c4389f ("SQUASHME: pKVM: x86: Enhance handling for __pkvm__hwapic_isr_update PV interface")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -1246,22 +1246,29 @@ static int pkvm_hwapic_isr_update(struct kvm_vcpu *vcpu, int max_isr)
 	 * The value -1 represents no interrupt, thus always allow the host to
 	 * update the ISR in this case. For the other values, should do proper
 	 * checks.
-	 *
-	 * For a protected APIC, the ISR state is protected so don't allow the
-	 * host to update it. But the host will recognize the protected apic
-	 * after the vCPU starts running. Before that the host may still use
-	 * this PV interface with value -1 for the protected apic to indicate no
-	 * interrupts when reset the vCPU. So don't allow for any other max_isr
-	 * values for the protected apic.
-	 *
-	 * For pVMs which don't have protected apic, also needs to validate the
-	 * max_isr value to make sure the host cannot inject an exception vector
-	 * for the same security reason with the PV interface __pkvm__inject_irq.
-	 * See comments in the function pkvm_inject_irq.
 	 */
-	if ((max_isr != -1) && (vcpu->arch.apic->guest_apic_protected ||
-				(pkvm_is_protected_vcpu(vcpu) && max_isr < 32)))
-		return -EPERM;
+	if (max_isr != -1) {
+		/* A valid max_isr should be in [0-255]. */
+		if (max_isr & ~0xff)
+			return -EINVAL;
+
+		/*
+		 * For a protected APIC, the ISR state is protected so don't allow the
+		 * host to update it. But the host will recognize the protected apic
+		 * after the vCPU starts running. Before that the host may still use
+		 * this PV interface with value -1 for the protected apic to indicate no
+		 * interrupts when reset the vCPU. So don't allow for any other max_isr
+		 * values for the protected apic.
+		 *
+		 * For pVMs which don't have protected apic, also needs to validate the
+		 * max_isr value to make sure the host cannot inject an exception vector
+		 * for the same security reason with the PV interface __pkvm__inject_irq.
+		 * See comments in the function pkvm_inject_irq.
+		 */
+		if (vcpu->arch.apic->guest_apic_protected ||
+		    (pkvm_is_protected_vcpu(vcpu) && max_isr < 32))
+			return -EPERM;
+	}
 
 	kvm_x86_call(hwapic_isr_update)(vcpu, max_isr);
 
cxdong/pub-pKVM-IA

commit bba4bfed66491b4ff15ee00cb0cc3e292a7d6404

pKVM: x86: Add segment PV interfaces
Add segment PV interfaces for the host to access the guest segment
registers via vendor-specific segment operations. For a pVM, these PV
interfaces allow the host to access segments before the pVM has started
running, but doesn't allow to modify segments if the pVM is already
running.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -661,6 +661,9 @@ static bool is_guest_vcpu_accessible(struct kvm_vcpu *vcpu, enum pkvm_hc hc)
 	case __pkvm__set_rflags:
 	case __pkvm__get_rflags:
 	case __pkvm__vcpu_reset:
+	case __pkvm__set_segment:
+	case __pkvm__get_segment:
+	case __pkvm__get_segment_base:
 		/*
 		 * As the host needs to pre-configure the pVM's vCPU state for
 		 * booting, the protection for pVM is only enforced by the pKVM
@@ -862,6 +865,18 @@ static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc 
 		 * Once this is implemented, make the __pkvm__vcpu_reset only for npVM.
 		 */
 		kvm_vcpu_reset(vcpu, true);
+		break;
+	case __pkvm__set_segment:
+		kvm_x86_call(set_segment)(vcpu, &in->set_segment.seg_val,
+					  in->set_segment.seg);
+		break;
+	case __pkvm__get_segment:
+		kvm_x86_call(get_segment)(vcpu, &out->get_segment.seg_val,
+					  pkvm_hc_input1(hvcpu));
+		break;
+	case __pkvm__get_segment_base:
+		out->get_segment_base.data =
+			kvm_x86_call(get_segment_base)(vcpu, pkvm_hc_input1(hvcpu));
 		break;
 	default:
 		ret = -EINVAL;

arch/x86/include/asm/kvm_pkvm.h
@@ -85,6 +85,16 @@ union pkvm_hc_data {
 		unsigned long data;
 	} get_rflags;
 	struct {
+		struct kvm_segment seg_val;
+		int seg;
+	} set_segment;
+	struct {
+		struct kvm_segment seg_val;
+	} get_segment;
+	struct {
+		u64 data;
+	} get_segment_base;
+	struct {
 		u64 data[PKVM_HC_DATA_MAX_NUM];
 	} raw;
 };

arch/x86/include/asm/pkvm_hypercalls.h
@@ -40,6 +40,9 @@ PKVM_HC(set_cr0)
 PKVM_HC_OUT(get_rflags)
 PKVM_HC(set_rflags)
 PKVM_HC(set_dr7)
+PKVM_HC_IN(set_segment)
+PKVM_HC_OUT(get_segment)
+PKVM_HC_OUT(get_segment_base)
 
 #undef PKVM_HC
 #undef PKVM_HC_OUT

commit 1bcf9bd5a1ffd0a1c7cdb66651fc33c019fbdb93

pKVM: x86: Validate segment index in segment PV interfaces
The segment index for set_segment, get_segment, and get_segment_base PV
interfaces comes directly from host hypercall parameters without
validation. These indices are used to access fixed-size arrays of 8
elements (kvm_vmx_segment_fields[], vmx->rmode.segs[], etc.). The pKVM
PV interface allows arbitrary host-supplied values, leading to
out-of-bounds array access.

Define NR_VCPU_SEGMENTS as the number of vCPU segment registers, which
is used to validate the passed segment index, and apply the validation
to all three segment PV interfaces.

The define of vmx->segment_cache.seg[] and vmx->rmode.seg[] are also
updated to use NR_VCPU_SEGMENTS rather than the hard coded value 8.

Fixes: bba4bfed6649 ("pKVM: x86: Add segment PV interfaces")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/include/asm/kvm_host.h
@@ -213,6 +213,7 @@ enum {
 	VCPU_SREG_GS,
 	VCPU_SREG_TR,
 	VCPU_SREG_LDTR,
+	NR_VCPU_SEGMENTS,
 };
 
 enum exit_fastpath_completion {

arch/x86/kvm/pkvm/pkvm.c
@@ -1074,6 +1074,33 @@ static void pkvm_set_dr7(struct kvm_vcpu *vcpu, unsigned long val)
 		vcpu->arch.switch_db_regs |= KVM_DEBUGREG_BP_ENABLED;
 }
 
+static int pkvm_set_segment(struct kvm_vcpu *vcpu, struct kvm_segment *var, int seg)
+{
+	if (seg < 0 || seg >= NR_VCPU_SEGMENTS)
+		return -EINVAL;
+
+	kvm_x86_call(set_segment)(vcpu, var, seg);
+	return 0;
+}
+
+static int pkvm_get_segment(struct kvm_vcpu *vcpu, struct kvm_segment *var, int seg)
+{
+	if (seg < 0 || seg >= NR_VCPU_SEGMENTS)
+		return -EINVAL;
+
+	kvm_x86_call(get_segment)(vcpu, var, seg);
+	return 0;
+}
+
+static int pkvm_get_segment_base(struct kvm_vcpu *vcpu, int seg, u64 *data)
+{
+	if (seg < 0 || seg >= NR_VCPU_SEGMENTS)
+		return -EINVAL;
+
+	*data = kvm_x86_call(get_segment_base)(vcpu, seg);
+	return 0;
+}
+
 static inline bool pkvm_event_injection_allowed(struct kvm_vcpu *vcpu)
 {
 	return !kvm_event_needs_reinjection(vcpu) &&
@@ -1830,16 +1857,14 @@ static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc 
 		kvm_vcpu_reset(vcpu, true);
 		break;
 	case __pkvm__set_segment:
-		kvm_x86_call(set_segment)(vcpu, &in->set_segment.seg_val,
-					  in->set_segment.seg);
+		ret = pkvm_set_segment(vcpu, &in->set_segment.seg_val, in->set_segment.seg);
 		break;
 	case __pkvm__get_segment:
-		kvm_x86_call(get_segment)(vcpu, &out->get_segment.seg_val,
-					  pkvm_hc_input1(hvcpu));
+		ret = pkvm_get_segment(vcpu, &out->get_segment.seg_val, pkvm_hc_input1(hvcpu));
 		break;
 	case __pkvm__get_segment_base:
-		out->get_segment_base.data =
-			kvm_x86_call(get_segment_base)(vcpu, pkvm_hc_input1(hvcpu));
+		ret = pkvm_get_segment_base(vcpu, pkvm_hc_input1(hvcpu),
+					    &out->get_segment_base.data);
 		break;
 	case __pkvm__set_idt:
 		kvm_x86_call(set_idt)(vcpu, &in->set_idt.desc);

arch/x86/kvm/vmx/vmx.h
@@ -253,7 +253,7 @@ struct vcpu_vmx {
 	struct {
 		int vm86_active;
 		ulong save_rflags;
-		struct kvm_segment segs[8];
+		struct kvm_segment segs[NR_VCPU_SEGMENTS];
 	} rmode;
 	struct {
 		u32 bitmask; /* 4 bits per segment (1 bit per field) */
@@ -262,7 +262,7 @@ struct vcpu_vmx {
 			unsigned long base;
 			u32 limit;
 			u32 ar;
-		} seg[8];
+		} seg[NR_VCPU_SEGMENTS];
 	} segment_cache;
 	int vpid;
 
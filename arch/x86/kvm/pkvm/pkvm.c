cxdong/pub-pKVM-IA

commit a89364a062ad05eae327ca136a3f27f66ff276e8

pKVM: x86: Add two helpers to manage pkvm_vcpu instances
The pkvm_get_vcpu() helper retrieves a pkvm_vcpu based on VM and vCPU
handles, increments its reference count, and acquires the associated
pkvm_vm.

The pkvm_put_vcpu() helper releases the pkvm_vcpu, decrements its
reference count, and releases the associated pkvm_vm.

These helpers ensure proper life cycle handling of pkvm_vcpu and pkvm_vm
instances.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -668,3 +668,31 @@ void pkvm_put_vm(struct pkvm_vm *pkvm_vm)
 
 	WARN_ON(atomic_dec_if_positive(&pkvm_vm_ref->refcount) <= 0);
 }
+
+struct pkvm_vcpu *pkvm_get_vcpu(int vm_handle, int vcpu_handle)
+{
+	struct pkvm_vm *pkvm_vm;
+
+	if (vcpu_handle < 0 || vcpu_handle >= KVM_MAX_VCPUS)
+		return NULL;
+
+	pkvm_vm = pkvm_get_vm(vm_handle);
+	if (!pkvm_vm)
+		return NULL;
+
+	vcpu_handle = array_index_nospec(vcpu_handle, KVM_MAX_VCPUS);
+	if (atomic_inc_not_zero(&pkvm_vm->vcpu_refs[vcpu_handle]))
+		return pkvm_vm->vcpus[vcpu_handle];
+
+	pkvm_put_vm(pkvm_vm);
+	return NULL;
+}
+
+void pkvm_put_vcpu(struct pkvm_vcpu *pkvm_vcpu)
+{
+	int vcpu_handle = pkvm_vcpu->vcpu.arch.pkvm.handle;
+
+	WARN_ON(atomic_dec_if_positive(&pkvm_vcpu->pkvm_vm->vcpu_refs[vcpu_handle]) <= 0);
+
+	pkvm_put_vm(pkvm_vcpu->pkvm_vm);
+}

arch/x86/kvm/pkvm/pkvm.h
@@ -96,5 +96,7 @@ void pkvm_kick_vcpu(struct kvm_vcpu *vcpu);
 int pkvm_x86_vendor_init(struct kvm_x86_init_ops *ops);
 struct pkvm_vm *pkvm_get_vm(int vm_handle);
 void pkvm_put_vm(struct pkvm_vm *pkvm_vm);
+struct pkvm_vcpu *pkvm_get_vcpu(int vm_handle, int vcpu_handle);
+void pkvm_put_vcpu(struct pkvm_vcpu *pkvm_vcpu);
 
 #endif /* __PKVM_X86_PKVM_H */

commit 074170b7452cb67057c3671c5d223e8145a1b913

pKVM: x86: Fix use-after-free race in pkvm_put_vcpu()
After atomic_dec_if_positive() drops vcpu_refs in pkvm_put_vcpu(),
another CPU can race with detach_pkvm_vcpu_from_vm() which may succeed
with atomic_cmpxchg(vcpu_refs, 1, 0) and then free the pkvm_vcpu
structure via __pkvm_vcpu_free(). The subsequent pkvm_put_vm(pkvm_vcpu->
pkvm_vm) would then dereference freed memory.

Save pkvm_vm to a local variable before the atomic decrement to avoid
accessing the potentially freed pkvm_vcpu structure.

Fixes: a89364a062ad ("pKVM: x86: Add two helpers to manage pkvm_vcpu instances")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -2243,10 +2243,11 @@ struct pkvm_vcpu *pkvm_get_vcpu(int vm_handle, int vcpu_handle)
 void pkvm_put_vcpu(struct pkvm_vcpu *pkvm_vcpu)
 {
 	int vcpu_handle = pkvm_vcpu->vcpu.arch.pkvm.handle;
+	struct pkvm_vm *pkvm_vm = pkvm_vcpu->pkvm_vm;
 
-	WARN_ON(atomic_dec_if_positive(&pkvm_vcpu->pkvm_vm->vcpu_refs[vcpu_handle]) <= 0);
+	WARN_ON(atomic_dec_if_positive(&pkvm_vm->vcpu_refs[vcpu_handle]) <= 0);
 
-	pkvm_put_vm(pkvm_vcpu->pkvm_vm);
+	pkvm_put_vm(pkvm_vm);
 }
 
 unsigned long pkvm_pcpu_tss(int cpu)
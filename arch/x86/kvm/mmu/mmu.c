cxdong/pub-pKVM-IA

commit d0b9e10eee97785f1b85437113e63b7359f10a34

KVM: x86/mmu: pKVM: Implement page fault handler
Implement host KVM's handler for guest page faults, which, unlike the
existing KVM's TDP MMU and shadow MMU page fault handlers, does not
deal with page tables on its own but simply asks pKVM to map pages for
for the guest via the MMU map hypercall.

(This patch implements this new handler but doesn't switch to using it
yet. We will switch to it when switching pKVM to use its own guest MMU
instead of relying on the host's one.)

Before asking pKVM to map pages, the host tops up the per-vCPU memcache
with the needed number of pages to be used by pKVM for installing the
mapping in the page table.

Guest mappings are not remembered in any data structure on the host side
yet, so the host cannot yet detect if the page fault is spurious, i.e.
if the same GPA has been already mapped on another vCPU in the meantime.
So if such a page fault happens, the host will attempt to map this GPA
again, and pKVM will return an error. This error will be returned to
userspace, and as a result, the VMM will kill the VM. That will be fixed
in the next patches.

Since enable_mmio_caching is disabled, fault->slot is NULL if and only
if this is an emulated MMIO page fault, and kvm_mmu_faultin_pfn() is
expected to return RET_PF_EMULATE in this case. In this case the page
fault handler emulates the MMIO access for the guest, instead of asking
pKVM to map pages. Also in this case the guest must be a npVM, since
MMIO emulation cannot work for pVMs (since it would require the host to
read instructions from pVM memory, to which it has no access), and pVMs
are required to access MMIO via hypercalls instead. So if such a page
fault happens with a pVM, -EFAULT is returned to userspace, and the VMM
should kill the VM.

Co-developed-by: Grzegorz Jaszczyk <jaszczyk@google.com>
Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/mmu/mmu.c
@@ -56,6 +56,7 @@
 #include <asm/set_memory.h>
 #include <asm/spec-ctrl.h>
 #include <asm/vmx.h>
+#include <asm/kvm_pkvm.h>
 
 #include "trace.h"
 
@@ -4759,22 +4760,24 @@ static int kvm_mmu_faultin_pfn(struct kvm_vcpu *vcpu,
 static bool is_page_fault_stale(struct kvm_vcpu *vcpu,
 				struct kvm_page_fault *fault)
 {
-	struct kvm_mmu_page *sp = root_to_sp(vcpu->arch.mmu->root.hpa);
+	if (/*!enable_pkvm*/ 1) {
+		struct kvm_mmu_page *sp = root_to_sp(vcpu->arch.mmu->root.hpa);
 
-	/* Special roots, e.g. pae_root, are not backed by shadow pages. */
-	if (sp && is_obsolete_sp(vcpu->kvm, sp))
-		return true;
+		/* Special roots, e.g. pae_root, are not backed by shadow pages. */
+		if (sp && is_obsolete_sp(vcpu->kvm, sp))
+			return true;
 
-	/*
-	 * Roots without an associated shadow page are considered invalid if
-	 * there is a pending request to free obsolete roots.  The request is
-	 * only a hint that the current root _may_ be obsolete and needs to be
-	 * reloaded, e.g. if the guest frees a PGD that KVM is tracking as a
-	 * previous root, then __kvm_mmu_prepare_zap_page() signals all vCPUs
-	 * to reload even if no vCPU is actively using the root.
-	 */
-	if (!sp && kvm_test_request(KVM_REQ_MMU_FREE_OBSOLETE_ROOTS, vcpu))
-		return true;
+		/*
+		 * Roots without an associated shadow page are considered invalid if
+		 * there is a pending request to free obsolete roots.  The request is
+		 * only a hint that the current root _may_ be obsolete and needs to be
+		 * reloaded, e.g. if the guest frees a PGD that KVM is tracking as a
+		 * previous root, then __kvm_mmu_prepare_zap_page() signals all vCPUs
+		 * to reload even if no vCPU is actively using the root.
+		 */
+		if (!sp && kvm_test_request(KVM_REQ_MMU_FREE_OBSOLETE_ROOTS, vcpu))
+			return true;
+	}
 
 	/*
 	 * Check for a relevant mmu_notifier invalidation event one last time
@@ -4914,11 +4917,78 @@ out_unlock:
 }
 #endif
 
+#ifdef CONFIG_PKVM_X86
+static unsigned long pkvm_mmu_cache_min_pages(void)
+{
+	/*
+	 * Minimum number of pages required to install stage-2 translation.
+	 * The root page table was pre-allocated during per-vm pool creation.
+	 */
+	return kvm_mmu_get_max_tdp_level() - 1;
+}
+
+static int pkvm_page_fault(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault)
+{
+	gfn_t base_gfn;
+	gfn_t nr_pages;
+	int r;
+
+	r = kvm_mmu_faultin_pfn(vcpu, fault, ACC_ALL);
+	if (r != RET_PF_CONTINUE) {
+		/* MMIO emulation works for non-protected VMs only. */
+		if (unlikely(pkvm_is_protected_vcpu(vcpu) && r == RET_PF_EMULATE))
+			return -EFAULT;
+
+		if (unlikely(r != RET_PF_EMULATE && r != RET_PF_RETRY &&
+			     r != -EINTR && r != -EAGAIN && r != -EFAULT)) {
+			pr_warn_ratelimited("pkvm: unexpected page fault result %d\n", r);
+			WARN_ON_ONCE(1);
+		}
+		return r;
+	}
+
+	WARN_ON_ONCE(!fault->slot);
+
+	r = kvm_topup_pkvm_memcache(&vcpu->arch.pkvm.guest_mmu_memcache,
+				    pkvm_mmu_cache_min_pages());
+	if (r)
+		return r;
+
+	r = RET_PF_RETRY;
+	write_lock(&vcpu->kvm->mmu_lock);
+
+	if (is_page_fault_stale(vcpu, fault))
+		goto out_unlock;
+
+	kvm_mmu_hugepage_adjust(vcpu, fault);
+
+	base_gfn = gfn_round_for_level(fault->gfn, fault->goal_level);
+	nr_pages = KVM_PAGES_PER_HPAGE(fault->goal_level);
+
+	r = pkvm_hypercall(vm_mmu_map,
+			   base_gfn << PAGE_SHIFT, fault->pfn << PAGE_SHIFT,
+			   nr_pages << PAGE_SHIFT, fault->map_writable);
+	if (r)
+		goto out_unlock;
+
+	r = RET_PF_FIXED;
+
+out_unlock:
+	kvm_mmu_finish_page_fault(vcpu, fault, r);
+	write_unlock(&vcpu->kvm->mmu_lock);
+	return r;
+}
+#endif /* CONFIG_PKVM_X86 */
+
 int kvm_tdp_page_fault(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault)
 {
 #ifdef CONFIG_X86_64
 	if (tdp_mmu_enabled)
 		return kvm_tdp_mmu_page_fault(vcpu, fault);
+#endif
+#ifdef CONFIG_PKVM_X86
+	if (/*enable_pkvm*/ 0)
+		return pkvm_page_fault(vcpu, fault);
 #endif
 
 	return direct_page_fault(vcpu, fault);

commit 151568a45f1dad04eed21e8e85bc4ffa551fd790

KVM: x86/mmu: pKVM: Fix page reference leak in pkvm_page_fault()
When kvm_mmu_faultin_pfn() returns RET_PF_CONTINUE, it may have stored a
page reference in fault->refcounted_page. If kvm_topup_pkvm_memcache()
or kzalloc() subsequently fails, the early returns bypass
kvm_mmu_finish_page_fault(), leaking the page reference.

So add releasing the faulted-in page on these error paths.

Fixes: d0b9e10eee97 ("KVM: x86/mmu: pKVM: Implement page fault handler")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/mmu/mmu.c
@@ -5040,12 +5040,14 @@ static int pkvm_page_fault(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault)
 	r = kvm_topup_pkvm_memcache(&vcpu->arch.pkvm.guest_mmu_memcache,
 				    pkvm_mmu_cache_min_pages());
 	if (r)
-		return r;
+		goto out_release_pfn;
 
 	/* Allocate non-atomically before taking mmu_lock. */
 	mapping = kzalloc(sizeof(struct pkvm_mapping), GFP_KERNEL_ACCOUNT);
-	if (!mapping)
-		return -ENOMEM;
+	if (!mapping) {
+		r = -ENOMEM;
+		goto out_release_pfn;
+	}
 
 	r = RET_PF_RETRY;
 	write_lock(&vcpu->kvm->mmu_lock);
@@ -5107,6 +5109,10 @@ out_unlock:
 
 	if (r != RET_PF_FIXED)
 		kfree(mapping);
+	return r;
+out_release_pfn:
+	kvm_release_faultin_page(vcpu->kvm, fault->refcounted_page,
+				 true, fault->map_writable);
 	return r;
 }
 #endif /* CONFIG_PKVM_X86 */
cxdong/pub-pKVM-IA

commit 7b1a8387c35bc8d6fc574164723db274aa78ac1b

KVM: x86/mmu: pKVM: Handle MMU age notifiers
Plumb KVM's .test_young(), .clear_young() and .clear_flush_young() MMU
notifiers in pKVM, to support aging of guest pages for non-protected
VMs and thus let the host kernel efficiently perform swapping and other
memory reclaiming tasks for npVM memory.

For pVMs, those MMU notifiers are triggered too, but are ignored, since
pKVM does not allow aging of pVM pages. This means that the host Linux
MM will silently fail to correctly check if a page is young or to make
it old, but that's ok, since the host cannot reclaim pVM pages on its
own initiative, so it could not use this aging information for anything
useful anyway.

Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/mmu/mmu.c
@@ -1668,6 +1668,31 @@ static bool pkvm_unmap_gfn_range(struct kvm *kvm, struct kvm_gfn_range *range)
 	/* pKVM itself always flushes TLB on unmap */
 	return false;
 }
+
+static bool pkvm_age_gfn_range(struct kvm *kvm, struct kvm_gfn_range *range,
+			       bool mkold)
+{
+	struct pkvm_mapping *m;
+	bool young = false;
+
+	if (pkvm_is_protected_vm(kvm))
+		return false;
+
+	read_lock(&kvm->mmu_lock);
+
+	for_each_pkvm_mapping(kvm, range->start, range->end, m) {
+		young |= pkvm_hypercall(vm_mmu_age, kvm->arch.pkvm.handle,
+					m->gfn << PAGE_SHIFT,
+					m->nr_pages << PAGE_SHIFT,
+					mkold);
+		if (young && !mkold)
+			break;
+	}
+
+	read_unlock(&kvm->mmu_lock);
+
+	return young;
+}
 #endif /* CONFIG_PKVM_X86 */
 
 bool kvm_unmap_gfn_range(struct kvm *kvm, struct kvm_gfn_range *range)
@@ -1796,6 +1821,11 @@ bool kvm_age_gfn(struct kvm *kvm, struct kvm_gfn_range *range)
 {
 	bool young = false;
 
+#ifdef CONFIG_PKVM_X86
+	if (/*enable_pkvm*/ 0)
+		young = pkvm_age_gfn_range(kvm, range, true);
+#endif
+
 	if (tdp_mmu_enabled)
 		young = kvm_tdp_mmu_age_gfn_range(kvm, range);
 
@@ -1808,6 +1838,11 @@ bool kvm_age_gfn(struct kvm *kvm, struct kvm_gfn_range *range)
 bool kvm_test_age_gfn(struct kvm *kvm, struct kvm_gfn_range *range)
 {
 	bool young = false;
+
+#ifdef CONFIG_PKVM_X86
+	if (/*enable_pkvm*/ 0)
+		young = pkvm_age_gfn_range(kvm, range, false);
+#endif
 
 	if (tdp_mmu_enabled)
 		young = kvm_tdp_mmu_test_age_gfn(kvm, range);

commit 4485e193fd56f80cc7719007f5ccc4816e03070e

pKVM: x86: Clamp negative mmu_age error codes to zero for host
pkvm_host_test_clear_young_guest() can return negative error codes
(-EINVAL, -EPERM), but the host KVM expects a boolean-like return value
from the __pkvm__vm_mmu_age hypercall: non-zero means "young", zero
means "not young". If a negative error code is passed through, the host
would misinterpret it as the page being young. Fix this by checking the
returning error code at the host side, and warns if it is negative
without updating the young flag.

The Returns comment for pkvm_host_test_clear_young_guest() says "true
if any of the pages in the range had the access flag set" but the
function returns int and can also return negative error codes (-EINVAL,
-EPERM). Also fixed the documentation to describe all possible return
values.

Fixes: 7b1a8387c35b ("KVM: x86/mmu: pKVM: Handle MMU age notifiers")
Fixes: 9e385fffe03f ("pKVM: x86: Add support for aging npVM pages by host")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/mmu/mmu.c
@@ -1686,10 +1686,18 @@ static bool pkvm_age_gfn_range(struct kvm *kvm, struct kvm_gfn_range *range,
 	read_lock(&kvm->mmu_lock);
 
 	for_each_pkvm_mapping(kvm, range->start, range->end, m) {
-		young |= pkvm_hypercall(vm_mmu_age, kvm->arch.pkvm.handle,
-					m->gfn << PAGE_SHIFT,
-					m->nr_pages << PAGE_SHIFT,
-					mkold);
+		int ret = pkvm_hypercall(vm_mmu_age, kvm->arch.pkvm.handle,
+					 m->gfn << PAGE_SHIFT,
+					 m->nr_pages << PAGE_SHIFT,
+					 mkold);
+		if (ret < 0) {
+			WARN_ONCE(1, "pkvm %s gfn[%llx..%llx] failed, err = %d\n",
+				  mkold ? "age" : "test_age",
+				  m->gfn, m->gfn + m->nr_pages, ret);
+			continue;
+		}
+
+		young |= ret;
 		if (young && !mkold)
 			break;
 	}

arch/x86/kvm/pkvm/mmu.c
@@ -1613,7 +1613,8 @@ unlock:
  * Does not flush the TLB after clearing the access flag. It is the caller's
  * responsibility to flush the TLB when needed.
  *
- * Returns: true if any of the pages in the range had the access flag set.
+ * Returns: 1 if any of the pages in the range had the access flag set, 0 if
+ * none of the pages had it set, or a negative error code on failure.
  */
 int pkvm_host_test_clear_young_guest(struct kvm *kvm, unsigned long gpa,
 				     unsigned long size, bool mkold)
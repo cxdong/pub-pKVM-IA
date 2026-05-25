cxdong/pub-pKVM-IA

commit ed781b00c01a27568e1761b1dc31c0c3d6444d5f

pKVM: VMX: Implement page aging for guest EPT
Implement .pte_young() and .pte_mkold() pgtable callbacks for guest EPT,
so that we can use pkvm_pgtable_test_clear_young() for guest MMU page
tables.

The implementation relies on the EPT A/D bits capability, so for
simplicity just require this capability for pKVM.

Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/vmx/ept.c
@@ -70,6 +70,18 @@ static void ept_pte_mkhuge(void *ptep)
 	*(u64 *)ptep |= PT_PAGE_SIZE_MASK;
 }
 
+static bool ept_pte_young(void *ptep)
+{
+	u64 val = READ_ONCE(*(u64 *)ptep);
+
+	return !!(val & VMX_EPT_ACCESS_BIT);
+}
+
+static void ept_pte_mkold(void *ptep)
+{
+	*(u64 *)ptep &= ~VMX_EPT_ACCESS_BIT;
+}
+
 static unsigned long ept_pte_to_phys(void *ptep)
 {
 	return *(u64 *)ptep & SPTE_BASE_ADDR_MASK;
@@ -231,6 +243,8 @@ static const struct pkvm_pgtable_ops guest_ept_pgt_ops = {
 	.pte_annotated = ept_pte_annotated,
 	.pte_huge = ept_pte_huge,
 	.pte_mkhuge = ept_pte_mkhuge,
+	.pte_young = ept_pte_young,
+	.pte_mkold = ept_pte_mkold,
 	.pte_to_phys = ept_pte_to_phys,
 	.pte_to_prot = ept_pte_to_prot,
 	.calc_pte_perm = ept_calc_pte_perm,

arch/x86/kvm/vmx/vmx.c
@@ -9378,7 +9378,15 @@ __init int vmx_hardware_setup(void)
 		allow_smaller_maxphyaddr = true;
 
 	if (!cpu_has_vmx_ept_ad_bits() || !enable_ept)
+#ifndef __PKVM_HYP__
 		enable_ept_ad_bits = 0;
+#else
+		/*
+		 * The pKVM hypervisor requires EPT A/D bits capability to
+		 * support aging of guest pages in a simple efficient way.
+		 */
+		return -EOPNOTSUPP;
+#endif
 
 	if (!cpu_has_vmx_unrestricted_guest() || !enable_ept)
 #ifndef __PKVM_HYP__
@@ -9655,6 +9663,7 @@ static void __init do_vmx_pkvm_init(void)
 		 * simply use the SW timer instead.
 		 */
 		enable_ept = true;
+		enable_ept_ad_bits = true;
 		enable_unrestricted_guest = true;
 		enable_vnmi = true;
 	}

commit a81bde6bb4567a60be255ffec5a826677fe8f1fd

pKVM: VMX: Use atomic RMW to clear EPT accessed bit
The non-atomic read-modify-write in ept_pte_mkold() can race with
hardware setting the dirty bit on the same PTE. If the CPU sets the
dirty bit between the read and the write-back, the dirty bit is
silently lost, leading to incorrect page aging and potential data
corruption if dirty bit is used by software.

Use atomic64_and() to clear only the accessed bit without clobbering
concurrent hardware updates to other PTE bits.

Fixes: ed781b00c01a ("pKVM: VMX: Implement page aging for guest EPT")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/vmx/ept.c
@@ -92,7 +92,7 @@ static bool ept_pte_young(void *ptep)
 
 static void ept_pte_mkold(void *ptep)
 {
-	*(u64 *)ptep &= ~VMX_EPT_ACCESS_BIT;
+	atomic64_and(~VMX_EPT_ACCESS_BIT, (atomic64_t *)ptep);
 }
 
 static unsigned long ept_pte_to_phys(void *ptep)
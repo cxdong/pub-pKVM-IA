cxdong/pub-pKVM-IA

commit e1bb7d519f38e52462586640a4182ecdecfb6576

iommu/vt-d: pKVM: Hypercalls for iommu domain management
Implement hypercalls for creating and freeing IOMMU domains.
Host uses these hypercalls during creation and freeing of its
own representation of a domain(dmar_domain).

When pKVM is enabled, use these hypercalls in host to manage
IOMMU domains.

Signed-off-by: Vineeth Pillai <vineethrp@google.com>
Signed-off-by: Aashish Sharma <shraash@google.com>

drivers/iommu/intel/pkvm/iommu_hc.c
@@ -417,3 +417,94 @@ int pkvm_iommu_pasid_teardown(struct pasid_teardown_data *data)
 	intel_pasid_tear_down_entry(iommu, &dev, data->pasid, false);
 	return 0;
 }
+
+static int __validate_domain_params(struct intel_iommu *iommu, struct alloc_domain_data *data)
+{
+	int iommu_superpage = iommu_superpage_capability(iommu, data->use_first_level);
+	int gaw = agaw_to_width(iommu->agaw);
+	int ret = -EINVAL;
+
+	if (gaw > cap_mgaw(iommu->cap))
+		gaw = cap_mgaw(iommu->cap);
+
+	if (data->iommu_superpage != iommu_superpage) {
+		pkvm_err("%s: invalid iommu_superpage(%u) from host!\n",
+			 __func__, data->iommu_superpage);
+	} else if (data->iommu_coherency != iommu_paging_structure_coherency(iommu)) {
+		pkvm_err("%s: invalid iommu_coherency(%u) from host!\n",
+			 __func__, data->iommu_coherency);
+	} else if (data->agaw != iommu->agaw) {
+		pkvm_err("%s: invalid agaw(%u) from host!\n", __func__, data->agaw);
+	} else if (data->gaw != gaw) {
+		pkvm_err("%s: invalid gaw(%u) from host!\n", __func__, data->gaw);
+	} else if (data->max_addr != __DOMAIN_MAX_ADDR(data->gaw)) {
+		pkvm_err("%s: invalid max_addr(%llx) from host!\n",
+			 __func__, data->max_addr);
+	} else {
+		ret = 0;
+	}
+
+	return ret;
+}
+
+int pkvm_iommu_alloc_domain(struct alloc_domain_data *data)
+{
+	struct dmar_domain *domain;
+	struct intel_iommu *iommu;
+	void *pgd;
+	int ret;
+
+	iommu = iommu_from_phys(data->phys);
+	if (!iommu)
+		return -EINVAL;
+
+	ret = __validate_domain_params(iommu, data);
+	if (ret)
+		return ret;
+
+	pgd = pkvm_host_gpa_to_virt(data->pgd_gpa);
+	pkvm_dbg("%s: write protecting pgd: %p\n", __func__, pgd);
+	ret = pkvm_host_donate_hyp_share_ro(__pkvm_pa(pgd), VTD_PAGE_SIZE, true);
+	if (ret) {
+		pkvm_err("%s: failed to write protect pgd: %p (err=%d)\n",
+			 __func__, pgd, ret);
+		return ret;
+	}
+
+	domain = pkvm_alloc_iommu_domain(data);
+	if (IS_ERR(domain)) {
+		pkvm_err("%s: domain alloc failed for device[%x] (err=%ld)\n",
+			 __func__, data->bdf, PTR_ERR(domain));
+		return PTR_ERR(domain);
+	}
+
+	domain_flush_cache(domain, pgd, VTD_PAGE_SIZE);
+
+	pkvm_dbg("%s: allocated domain(pgd=%p) for device[%x]\n", __func__,
+		 pgd, data->bdf);
+	return 0;
+}
+
+int pkvm_iommu_free_domain(u64 pgd_gpa)
+{
+	struct dmar_domain *domain;
+	void *pgd = pkvm_host_gpa_to_virt(pgd_gpa);
+	int ret;
+
+	domain = pkvm_get_iommu_domain_noref(pgd);
+	if (!domain) {
+		pkvm_err("%s: no domain exist for pgd: %p\n", __func__, pgd);
+		return -EINVAL;
+	}
+	ret = pkvm_free_iommu_domain(domain);
+	if (ret) {
+		pkvm_err("%s: failed to free the domain[pgd:%p] (err=%d)\n",
+			 __func__, pgd, ret);
+		return ret;
+	}
+
+	pkvm_dbg("%s: remove write protect pgd: %p\n", __func__, pgd);
+	pkvm_hyp_donate_host(__pkvm_pa(pgd), VTD_PAGE_SIZE, false);
+
+	return ret;
+}

arch/x86/include/asm/kvm_pkvm.h
@@ -126,6 +126,18 @@ struct pasid_teardown_data {
 	u8 ats_enabled: 1;
 	u8 ats_supported: 1;
 };
+
+struct alloc_domain_data {
+	u64 phys;
+	u64 max_addr;
+	u64 pgd_gpa;
+	u16 bdf;
+	u16 gaw;
+	u8 agaw;
+	u8 iommu_superpage;
+	u8 iommu_coherency;
+	u8 use_first_level;
+};
 #endif
 
 #define TO_PKVM_HC(f)		CONCATENATE(__pkvm__, f)
@@ -228,6 +240,9 @@ union pkvm_hc_data {
 	struct {
 		struct pasid_teardown_data data;
 	} iommu_pasid_teardown;
+	struct {
+		struct alloc_domain_data data;
+	} iommu_alloc_domain;
 #endif
 	struct {
 		u64 data[PKVM_HC_DATA_MAX_NUM];

arch/x86/include/asm/pkvm_hypercalls.h
@@ -100,6 +100,8 @@ PKVM_HC_INOUT(iommu_set_sm_ce)
 PKVM_HC_INOUT(iommu_pasid_setup_fl)
 PKVM_HC_INOUT(iommu_pasid_setup_sl)
 PKVM_HC_IN(iommu_pasid_teardown)
+PKVM_HC_IN(iommu_alloc_domain)
+PKVM_HC(iommu_free_domain)
 #endif
 
 #undef PKVM_HC

arch/x86/kvm/pkvm/pkvm.c
@@ -1919,6 +1919,12 @@ void pkvm_handle_host_hypercall(struct kvm_vcpu *vcpu)
 	case __pkvm__iommu_pasid_teardown:
 		ret = pkvm_iommu_pasid_teardown(&in.iommu_pasid_teardown.data);
 		break;
+	case __pkvm__iommu_alloc_domain:
+		ret = pkvm_iommu_alloc_domain(&in.iommu_alloc_domain.data);
+		break;
+	case __pkvm__iommu_free_domain:
+		ret = pkvm_iommu_free_domain(pkvm_hc_input1(vcpu));
+		break;
 #endif
 	default:
 		ret = pkvm_vcpu_handle_host_hypercall(vcpu, hc, &in, &out);

drivers/iommu/intel/iommu.c
@@ -562,13 +562,6 @@ out:
 	return iommu;
 }
 
-static void domain_flush_cache(struct dmar_domain *domain,
-			       void *addr, int size)
-{
-	if (!domain->iommu_coherency)
-		clflush_cache_range(addr, size);
-}
-
 static void free_context_table(struct intel_iommu *iommu)
 {
 	struct context_entry *context;
@@ -3496,7 +3489,20 @@ static struct dmar_domain *paging_domain_alloc(struct device *dev, bool first_st
 		kfree(domain);
 		return ERR_PTR(-ENOMEM);
 	}
-	domain_flush_cache(domain, domain->pgd, PAGE_SIZE);
+
+	if (pkvm_enabled()) {
+		int ret = pkvm_alloc_domain(info, domain);
+
+		if (ret) {
+			pr_err("pkvm_alloc_domain failed [pgd=%p] (err=%d)\n",
+				domain->pgd, ret);
+			iommu_free_pages(domain->pgd);
+			kfree(domain);
+			return ERR_PTR(ret);
+		}
+	} else {
+		domain_flush_cache(domain, domain->pgd, PAGE_SIZE);
+	}
 
 	return domain;
 }
@@ -3608,6 +3614,16 @@ static void intel_iommu_domain_free(struct iommu_domain *domain)
 		domain_unmap(dmar_domain, 0, DOMAIN_MAX_PFN(dmar_domain->gaw),
 			     &freelist);
 		iommu_put_pages_list(&freelist);
+
+		if (pkvm_enabled()) {
+			int ret = pkvm_free_domain(dmar_domain);
+
+			if (ret)
+				pr_err("pkvm_free_domain failed [pgd=%p] (err=%d)\n",
+					dmar_domain->pgd, ret);
+			else
+				iommu_free_pages(dmar_domain->pgd);
+		}
 	}
 
 	kfree(dmar_domain->qi_batch);

drivers/iommu/intel/iommu.h
@@ -943,6 +943,13 @@ static inline void __iommu_flush_cache(
 		clflush_cache_range(addr, size);
 }
 
+static inline void domain_flush_cache(struct dmar_domain *domain,
+			       void *addr, int size)
+{
+	if (!domain->iommu_coherency)
+		clflush_cache_range(addr, size);
+}
+
 #ifndef __PKVM_HYP__
 /* Convert generic struct iommu_domain to private struct dmar_domain */
 static inline struct dmar_domain *to_dmar_domain(struct iommu_domain *dom)

drivers/iommu/intel/pkvm/domain.c
@@ -5,6 +5,7 @@
 #include <linux/hashtable.h>
 #include <asm/pkvm_spinlock.h>
 #include "pkvm/debug.h"
+#include "pkvm/memory.h"
 #include "../iommu.h"
 
 /*
@@ -124,8 +125,9 @@ int pkvm_free_iommu_domain(struct dmar_domain *domain)
 	return 0;
 }
 
-struct dmar_domain *pkvm_alloc_iommu_domain(void *pgd)
+struct dmar_domain *pkvm_alloc_iommu_domain(struct alloc_domain_data *data)
 {
+	void *pgd = pkvm_host_gpa_to_virt(data->pgd_gpa);
 	struct dmar_domain *domain;
 	unsigned long index;
 
@@ -142,6 +144,12 @@ struct dmar_domain *pkvm_alloc_iommu_domain(void *pgd)
 		domain = &iommu_domains[index];
 		INIT_LIST_HEAD(&domain->cache_tags);
 		domain->pgd = pgd;
+		domain->use_first_level = data->use_first_level;
+		domain->iommu_superpage = data->iommu_superpage;
+		domain->iommu_coherency = data->iommu_coherency;
+		domain->agaw = data->agaw;
+		domain->gaw = data->gaw;
+		domain->max_addr = data->max_addr;
 		domain->index = index;
 		domain->qi_batch = &domain->_qi_batch;
 		atomic_set(&domain->refcount, 1);

drivers/iommu/intel/pkvm_iommu.c
@@ -325,3 +325,32 @@ int pkvm_pasid_teardown(struct device_domain_info *info, u32 pasid)
 
 	return pkvm_hypercall_in(iommu_pasid_teardown, &d);
 }
+
+int pkvm_alloc_domain(struct device_domain_info *info, struct dmar_domain *domain)
+{
+	union pkvm_hc_data d = { 0 };
+	struct alloc_domain_data *data = &d.iommu_alloc_domain.data;
+	int ret;
+
+	data->phys = info->iommu->reg_phys;
+	data->bdf = PCI_DEVID(info->bus, info->devfn);
+	data->use_first_level = domain->use_first_level;
+	data->pgd_gpa = virt_to_phys(domain->pgd);
+	data->gaw = domain->gaw;
+	data->agaw = domain->agaw;
+	data->max_addr = domain->max_addr;
+	data->iommu_coherency = domain->iommu_coherency;
+	data->iommu_superpage = domain->iommu_superpage;
+
+	ret = pkvm_hypercall_in(iommu_alloc_domain, &d);
+	if (ret)
+		pr_err("%s: pkvm failed to alloc domain for device[%x:%x.%x] (err=%d)\n", __func__,
+		       info->bus, PCI_SLOT(info->devfn), PCI_FUNC(info->devfn), ret);
+
+	return ret;
+}
+
+int pkvm_free_domain(struct dmar_domain *domain)
+{
+	return pkvm_hypercall(iommu_free_domain, virt_to_phys(domain->pgd));
+}

commit bfa9a94712fcdb2da2623eac4d852d0a7db1ccaf

iommu/vt-d: pKVM: Undo pgd write protection on domain alloc failure
In pkvm_iommu_alloc_domain(), when pkvm_alloc_iommu_domain() fails after
the pgd page has already been write-protected via
pkvm_host_donate_hyp_share_ro(), the error path returns without undoing
the write protection. This leaves the pgd page in a write-protected. Add
a pkvm_hyp_donate_host() call in the error path to restore the pgd page
to host-owned state before returning the error.

Fixes: e1bb7d519f38 ("iommu/vt-d: pKVM: Hypercalls for iommu domain management")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

drivers/iommu/intel/pkvm/iommu_hc.c
@@ -472,6 +472,7 @@ int pkvm_iommu_alloc_domain(struct alloc_domain_data *data)
 	if (IS_ERR(domain)) {
 		pkvm_err("%s: domain alloc failed for device[%x] (err=%ld)\n",
 			 __func__, data->bdf, PTR_ERR(domain));
+		pkvm_hyp_donate_host(__pkvm_pa(pgd), VTD_PAGE_SIZE, false);
 		return PTR_ERR(domain);
 	}
 
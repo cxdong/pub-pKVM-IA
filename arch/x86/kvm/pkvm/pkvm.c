cxdong/pub-pKVM-IA

commit dbbc86b2ef452ec1d952b14b39ecb90c42d6e941

pKVM: x86: Add vcpu_add_fpstate PV interface
Add vcpu_add_fpstate PV interface for the host to add a new fpstate
memory if needs to support xfd, as the fpstate memory allocated when
creating vcpu may not be sufficient. As the pVM's FPU state is managed
by the pKVM hypervisor while the npVM's FPU state is managed by the
host, re-allocating the fpstate is only necessary for the pVM. This PV
interface is not accessible to the host once the pVM started running to
ensure the host won't manipulate pVM's FPU. The old fpstate memory
physical addresses and size are returned via pkvm_memcache for the host
to reclaim.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -702,6 +702,7 @@ static bool is_guest_vcpu_accessible(struct kvm_vcpu *vcpu, enum pkvm_hc hc)
 	case __pkvm__flush_tlb_gva:
 	case __pkvm__flush_tlb_guest:
 	case __pkvm__vcpu_after_set_cpuid:
+	case __pkvm__vcpu_add_fpstate:
 		/*
 		 * As the host needs to pre-configure the pVM's vCPU state for
 		 * booting, the protection for pVM is only enforced by the pKVM
@@ -1015,6 +1016,55 @@ undonate:
 	return ret;
 }
 
+static int pkvm_vcpu_add_fpstate(struct kvm_vcpu *vcpu,
+				 phys_addr_t fpstate_pa, size_t size,
+				 struct pkvm_memcache *mc)
+{
+	struct fpstate *new, *old;
+	int ret;
+
+	/* Expect the host to use this PV interface for pVM only. */
+	if (!pkvm_is_protected_vcpu(vcpu))
+		return -EINVAL;
+
+	ret = pkvm_host_donate_hyp(fpstate_pa, size, true);
+	if (ret)
+		return ret;
+
+	memset(mc, 0, sizeof(*mc));
+
+	old = vcpu->arch.guest_fpu.fpstate;
+	new = __pkvm_va(fpstate_pa);
+	/*
+	 * Reuse the existing fpstate memory if it's sufficiently large. At this
+	 * stage, we can't determine whether the new fpstate size matches the
+	 * vCPUID or not, because that check only occurs when the host calls
+	 * __pkvm__vcpu_after_set_cpuid to update the vCPUID. If the new fpstate
+	 * size is smaller than what the new vCPUID requires, the vCPUID won't
+	 * be updated. Therefore, ensuring the new fpstate size is at least as
+	 * large as the previous one allows continued support for this scenario.
+	 */
+	if (old && old->size >= size) {
+		teardown_donated_memory(mc, new, size);
+		return 0;
+	}
+
+	new->size = size;
+	vcpu->arch.guest_fpu.fpstate = new;
+
+	pkvm_init_guest_fpu(&vcpu->arch.guest_fpu);
+	fpstate_set_confidential(&vcpu->arch.guest_fpu);
+
+	/*
+	 * New physical fpstate memory is consumed. Tear down the old fpstate
+	 * memory if there is.
+	 */
+	if (old)
+		teardown_donated_memory(mc, old, old->size);
+
+	return 0;
+}
+
 static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc hc,
 					   union pkvm_hc_data *in, union pkvm_hc_data *out)
 {
@@ -1176,6 +1226,10 @@ static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc 
 	case __pkvm__vcpu_after_set_cpuid:
 		ret = pkvm_vcpu_after_set_cpuid(vcpu, pkvm_host_gpa_to_phys(pkvm_hc_input1(hvcpu)),
 						&out->vcpu_after_set_cpuid.memcache);
+		break;
+	case __pkvm__vcpu_add_fpstate:
+		ret = pkvm_vcpu_add_fpstate(vcpu, pkvm_host_gpa_to_phys(pkvm_hc_input1(hvcpu)),
+					    pkvm_hc_input2(hvcpu), &out->vcpu_add_fpstate.memcache);
 		break;
 	default:
 		ret = -EINVAL;

arch/x86/include/asm/kvm_pkvm.h
@@ -116,6 +116,9 @@ union pkvm_hc_data {
 		struct pkvm_memcache memcache;
 	} vcpu_after_set_cpuid;
 	struct {
+		struct pkvm_memcache memcache;
+	} vcpu_add_fpstate;
+	struct {
 		u64 data[PKVM_HC_DATA_MAX_NUM];
 	} raw;
 };

arch/x86/include/asm/pkvm_hypercalls.h
@@ -70,6 +70,7 @@ PKVM_HC(load_eoi_exitmap)
 PKVM_HC(hwapic_isr_update)
 PKVM_HC(sync_pir_to_irr)
 PKVM_HC_OUT(vcpu_after_set_cpuid)
+PKVM_HC_OUT(vcpu_add_fpstate)
 
 #undef PKVM_HC
 #undef PKVM_HC_OUT

commit 3159578dde1c9e04032a05a8b528d11a72ec1e71

pKVM: x86: Assert old fpstate existence in vcpu_add_fpstate
The old fpstate is always guaranteed to exist when
pkvm_vcpu_add_fpstate() is called because it is created together with
the vCPU during pkvm_vcpu_create(). Add a BUG_ON(!old) assertion to
enforce this invariant explicitly.

With old guaranteed non-NULL, simplify the conditional null checks
to unconditional code paths and update comments accordingly.

Fixes: dbbc86b2ef45 ("pKVM: x86: Add vcpu_add_fpstate PV interface")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -1378,6 +1378,12 @@ static int pkvm_vcpu_add_fpstate(struct kvm_vcpu *vcpu,
 	memset(mc, 0, sizeof(*mc));
 
 	old = vcpu->arch.guest_fpu.fpstate;
+	/*
+	 * The old fpstate should always exist if the vCPU is successfully
+	 * created as it is created together with vCPU via pkvm_vcpu_create().
+	 */
+	BUG_ON(!old);
+
 	new = __pkvm_va(fpstate_pa);
 	/*
 	 * Reuse the existing fpstate memory if it's sufficiently large. At this
@@ -1388,7 +1394,7 @@ static int pkvm_vcpu_add_fpstate(struct kvm_vcpu *vcpu,
 	 * be updated. Therefore, ensuring the new fpstate size is at least as
 	 * large as the previous one allows continued support for this scenario.
 	 */
-	if (old && old->size >= size) {
+	if (size <= old->size) {
 		teardown_donated_memory(mc, new, size);
 		return 0;
 	}
@@ -1401,10 +1407,9 @@ static int pkvm_vcpu_add_fpstate(struct kvm_vcpu *vcpu,
 
 	/*
 	 * New physical fpstate memory is consumed. Tear down the old fpstate
-	 * memory if there is.
+	 * memory.
 	 */
-	if (old)
-		teardown_donated_memory(mc, old, old->size);
+	teardown_donated_memory(mc, old, old->size);
 
 	return 0;
 }
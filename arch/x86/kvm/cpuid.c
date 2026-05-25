cxdong/pub-pKVM-IA

commit 2f17691688990e14c0ed1b0a6646e4bed7f9968c

pKVM: x86: Expose TSC frequency to pVM
Since pKVM does not support kvmclock for protected VMs, a pVM can't find
out the TSC frequency from the host directly. So Linux pVMs find it out
by calibrating the TSC against a PIT or HPET timer emulated by the host,
which is inherently unreliable: if the host is under a heavy load during
this calibration, the calibration will produce inaccurate results or,
more likely, will just fail. And if the calibration fails, the guest
will deem the TSC unstable (whereas actually it is the timer that is
inaccurate, while the TSC is accurate, since it is native), and will
fall back to using a jiffies-based clocksource instead of TSC, thus
requiring periodic timer interrupts and thus causing permanently high
CPU usage as long as the pVM is running.

Furthermore, on PTL (not on older CPUs) for some reason this problem
(failed TSC calibration) is observed all the time, even if the system is
not under a high load when the pVM starts.

So to avoid this problem and yet avoid special changes on the guest
side, enforce exposing the TSC frequency info to the pVM via the CPUID
leaf 0x15, so that the pVM doesn't need to calibrate the TSC.

In the future this enforcement might be also reused for security
purposes, as a part of providing secure TSC for pVMs (which pKVM doesn't
provide yet).

Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/cpuid.c
@@ -2138,6 +2138,38 @@ bool kvm_cpuid(struct kvm_vcpu *vcpu, u32 *eax, u32 *ebx,
 				*eax = vcpu->arch.hw_tsc_khz;
 			}
 #endif
+		} else if (pkvm_is_protected_vcpu(vcpu) && function == 0x15) {
+			/*
+			 * Since protected VMs cannot use kvmclock to find out the
+			 * TSC frequency, expose the native TSC frequency directly
+			 * via cpuid. For now this is not for security (pKVM does not
+			 * provide secure TSC yet) but just to let the guest know the
+			 * TSC frequency so that it doesn't need to calibrate the TSC
+			 * against host-emulated PIT/HPET timers which are inherently
+			 * inaccurate as they are sensitive to the system load in the
+			 * host.
+			 */
+			if (tsc_khz) {
+				/*
+				 * Core crystal clock must match the LAPIC clock.
+				 * See native_calibrate_tsc().
+				 */
+				*ecx = 1000000000 / vcpu->kvm->arch.apic_bus_cycle_ns;
+				/*
+				 * Try to minimize calculation error: use 38.4 MHz
+				 * which is the typical Core crystal clock frequency
+				 * on today's Intel's desktop CPUs.
+				 *
+				 * TODO: we could avoid any calculation error, by
+				 * exposing tsc_khz via some PV (akin to kvmclock)
+				 * instead of cpuid.
+				 */
+				*eax = *ecx / 38400000;	/* denominator */
+				*ebx = tsc_khz / 38400;	/* numerator */
+				*edx = 0;
+			} else {
+				*eax = *ebx = *ecx = *edx = 0;
+			}
 		}
 	} else {
 		*eax = *ebx = *ecx = *edx = 0;

arch/x86/kvm/pkvm/pkvm.c
@@ -163,6 +163,7 @@ static int pkvm_vm_init(phys_addr_t host_kvm_pa, phys_addr_t pkvm_vm_pa,
 		kvm->arch.disabled_quirks = (kvm_caps.inapplicable_quirks |
 					     pkvm_vm->shared_kvm->arch.disabled_quirks) &
 					    kvm_caps.supported_quirks;
+	kvm->arch.apic_bus_cycle_ns = APIC_BUS_CYCLE_NS_DEFAULT;
 	kvm->arch.pkvm.pvmfw_load_addr = INVALID_GPA;
 
 	pkvm_spin_lock_init(&pkvm_vm->lock);
@@ -462,6 +463,8 @@ static int __vcpu_create(struct kvm *kvm, struct kvm_vcpu *vcpu, struct fpstate 
 		kvm->arch.notify_window = pkvm_vm->shared_kvm->arch.notify_window;
 		kvm->arch.notify_vmexit_flags = pkvm_vm->shared_kvm->arch.notify_vmexit_flags;
 	}
+	if (pkvm_vm->shared_kvm->arch.apic_bus_cycle_ns)
+		kvm->arch.apic_bus_cycle_ns = pkvm_vm->shared_kvm->arch.apic_bus_cycle_ns;
 	if (!pkvm_is_protected_vm(kvm))
 		kvm->arch.disabled_exits = pkvm_vm->shared_kvm->arch.disabled_exits;
 

commit eab8fbbcbca4031b81b840c0aa0f693e4468c139

pKVM: x86: Fix TOCTOU race reading apic_bus_cycle_ns from shared memory
The original code reads apic_bus_cycle_ns from shared kvm twice: once
for the non-zero check and once for the assignment. A malicious host
could change the value to zero between the two reads, storing zero into
the pKVM private kvm->arch.apic_bus_cycle_ns.

This leads to a division by zero in kvm_cpuid() when a protected vCPU
queries CPUID leaf 0x15:
    *ecx = 1000000000 / vcpu->kvm->arch.apic_bus_cycle_ns;

Fix by reading the shared value to local variable and only assign it to
pKVM's vcpu if it is a non-zero value, ensuring the divisor is always
non-zero.

The CPUID leaf 0x15 eax(denominator) is calculated by:
    *eax = *ecx / 38400000;	/* denominator */

If the calculated ecx is smaller than 38400000, eax will be 0. According
to SDM: the TSC/”core crystal clock” ratio is not enumerated if
ebx(numerator) is 0 rather than eax, so enforce the ebx as zero if eax
is zero.

Note: the code is refactored a bit to move the postponed per-vm setup to
a dedicated function. This should be squashed from the beginning where
needs this postponed code rather than the fix tag.

Fixes: 2f1769168899 ("pKVM: x86: Expose TSC frequency to pVM")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/cpuid.c
@@ -2165,7 +2165,10 @@ bool kvm_cpuid(struct kvm_vcpu *vcpu, u32 *eax, u32 *ebx,
 				 * instead of cpuid.
 				 */
 				*eax = *ecx / 38400000;	/* denominator */
-				*ebx = tsc_khz / 38400;	/* numerator */
+				if (*eax)
+					*ebx = tsc_khz / 38400;	/* numerator */
+				else
+					*ebx = 0;
 				*edx = 0;
 			} else {
 				*eax = *ebx = *ecx = *edx = 0;

arch/x86/kvm/pkvm/pkvm.c
@@ -483,6 +483,49 @@ static void pkvm_vcpu_reset(struct kvm_vcpu *vcpu, bool init_event)
 	}
 }
 
+static void postponed_per_vm_setup(struct kvm *kvm)
+{
+	struct pkvm_vm *pkvm_vm = to_pkvm(kvm);
+	struct kvm *shared_kvm;
+	u64 apic_bus_cycle_ns;
+
+	pkvm_spin_lock(&pkvm_vm->lock);
+
+	shared_kvm = pkvm_vm->shared_kvm;
+	/*
+	 * The following setup is per VM, not per vCPU, however it cannot be
+	 * done during VM creation, since these values are set by the host VMM
+	 * via an ioctl after a VM is already created. At the same time, the
+	 * host KVM relies on these values being already set when setting up a
+	 * vCPU, thus implicitly assuming that the VMM should set them before
+	 * creating vCPUs. So it is ok to assume these host's values here are
+	 * up-to-date.
+	 */
+	if (!kvm->arch.bus_lock_detection_enabled &&
+	    shared_kvm->arch.bus_lock_detection_enabled &&
+	    kvm_caps.has_bus_lock_exit)
+		kvm->arch.bus_lock_detection_enabled = true;
+
+	if (!kvm->arch.notify_vmexit_flags &&
+	    shared_kvm->arch.notify_vmexit_flags &&
+	    kvm_caps.has_notify_vmexit) {
+		kvm->arch.notify_window = shared_kvm->arch.notify_window;
+		kvm->arch.notify_vmexit_flags = shared_kvm->arch.notify_vmexit_flags;
+	}
+
+	apic_bus_cycle_ns = READ_ONCE(shared_kvm->arch.apic_bus_cycle_ns);
+	if (apic_bus_cycle_ns)
+		kvm->arch.apic_bus_cycle_ns = apic_bus_cycle_ns;
+
+	if (!pkvm_is_protected_vm(kvm))
+		kvm->arch.disabled_exits = shared_kvm->arch.disabled_exits;
+
+	if (!kvm->created_vcpus)
+		kvm->arch.bsp_vcpu_id = shared_kvm->arch.bsp_vcpu_id;
+
+	pkvm_spin_unlock(&pkvm_vm->lock);
+}
+
 static int __vcpu_create(struct kvm *kvm, struct kvm_vcpu *vcpu, struct fpstate *fps,
 			 struct kvm_lapic *shared_apic)
 {
@@ -497,36 +540,7 @@ static int __vcpu_create(struct kvm *kvm, struct kvm_vcpu *vcpu, struct fpstate 
 	if (ret)
 		return ret;
 
-	pkvm_spin_lock(&pkvm_vm->lock);
-
-	/*
-	 * The following setup is per VM, not per vCPU, however it cannot be
-	 * done during VM creation, since these values are set by the host VMM
-	 * via an ioctl after a VM is already created. At the same time, the
-	 * host KVM relies on these values being already set when setting up a
-	 * vCPU, thus implicitly assuming that the VMM should set them before
-	 * creating vCPUs. So it is ok to assume these host's values here are
-	 * up-to-date.
-	 */
-	if (!kvm->arch.bus_lock_detection_enabled &&
-	    pkvm_vm->shared_kvm->arch.bus_lock_detection_enabled &&
-	    kvm_caps.has_bus_lock_exit)
-		kvm->arch.bus_lock_detection_enabled = true;
-	if (!kvm->arch.notify_vmexit_flags &&
-	    pkvm_vm->shared_kvm->arch.notify_vmexit_flags &&
-	    kvm_caps.has_notify_vmexit) {
-		kvm->arch.notify_window = pkvm_vm->shared_kvm->arch.notify_window;
-		kvm->arch.notify_vmexit_flags = pkvm_vm->shared_kvm->arch.notify_vmexit_flags;
-	}
-	if (pkvm_vm->shared_kvm->arch.apic_bus_cycle_ns)
-		kvm->arch.apic_bus_cycle_ns = pkvm_vm->shared_kvm->arch.apic_bus_cycle_ns;
-	if (!pkvm_is_protected_vm(kvm))
-		kvm->arch.disabled_exits = pkvm_vm->shared_kvm->arch.disabled_exits;
-
-	if (!kvm->created_vcpus)
-		kvm->arch.bsp_vcpu_id = pkvm_vm->shared_kvm->arch.bsp_vcpu_id;
-
-	pkvm_spin_unlock(&pkvm_vm->lock);
+	postponed_per_vm_setup(kvm);
 
 	vcpu->kvm = kvm;
 	/* Set cpu to -1 to indicate it is not loaded on any CPU */
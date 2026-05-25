cxdong/pub-pKVM-IA

commit 78336e1494feedf026845792c621328bdbf21309

pKVM: x86: Add cache_reg PV interface
Add cache_reg PV interface to allow the host to get some of the guest
registers via the vendor cache_reg operation. This PV interface is not
allowed for the host to cache registers for a pVM.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -402,6 +402,8 @@ static int __vcpu_create(struct kvm *kvm, struct kvm_vcpu *vcpu, struct fpstate 
 		vcpu->arch.perf_capabilities = kvm_caps.supported_perf_cap;
 	}
 
+	vcpu->arch.walk_mmu = &vcpu->arch.root_mmu;
+
 	ret = kvm_x86_call(vcpu_create)(vcpu);
 	if (ret)
 		goto unsetup_lapic;
@@ -727,6 +729,43 @@ static int pkvm_set_msr(struct kvm_vcpu *vcpu, u32 index, u64 data)
 	return kvm_msr_write(vcpu, index, data);
 }
 
+static int pkvm_cache_reg(struct kvm_vcpu *vcpu, enum kvm_reg reg,
+			  union pkvm_hc_data *out)
+{
+	kvm_x86_call(cache_reg)(vcpu, reg);
+
+	switch (reg) {
+	case VCPU_REGS_RSP:
+		out->cache_reg.rsp = vcpu->arch.regs[VCPU_REGS_RSP];
+		break;
+	case VCPU_REGS_RIP:
+		out->cache_reg.rip = vcpu->arch.regs[VCPU_REGS_RIP];
+		break;
+	case VCPU_EXREG_PDPTR: {
+		struct kvm_mmu *mmu = vcpu->arch.walk_mmu;
+
+		out->cache_reg.pdptrs[0] = mmu->pdptrs[0];
+		out->cache_reg.pdptrs[1] = mmu->pdptrs[1];
+		out->cache_reg.pdptrs[2] = mmu->pdptrs[2];
+		out->cache_reg.pdptrs[3] = mmu->pdptrs[3];
+		break;
+	}
+	case VCPU_EXREG_CR0:
+		out->cache_reg.cr0 = vcpu->arch.cr0;
+		break;
+	case VCPU_EXREG_CR3:
+		out->cache_reg.cr3 = vcpu->arch.cr3;
+		break;
+	case VCPU_EXREG_CR4:
+		out->cache_reg.cr4 = vcpu->arch.cr4;
+		break;
+	default:
+		return -EOPNOTSUPP;
+	}
+
+	return 0;
+}
+
 static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc hc,
 					   union pkvm_hc_data *out)
 {
@@ -756,6 +795,9 @@ static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc 
 		break;
 	case __pkvm__get_msr:
 		ret = kvm_msr_read(vcpu, pkvm_hc_input1(hvcpu), &out->get_msr.data);
+		break;
+	case __pkvm__cache_reg:
+		ret = pkvm_cache_reg(vcpu, pkvm_hc_input1(hvcpu), out);
 		break;
 	default:
 		ret = -EINVAL;

arch/x86/include/asm/kvm_pkvm.h
@@ -73,6 +73,14 @@ union pkvm_hc_data {
 	struct {
 		u64 data;
 	} get_msr;
+	union {
+		unsigned long rsp;
+		unsigned long rip;
+		unsigned long cr0;
+		unsigned long cr3;
+		unsigned long cr4;
+		u64 pdptrs[4];
+	} cache_reg;
 	struct {
 		u64 data[PKVM_HC_DATA_MAX_NUM];
 	} raw;

arch/x86/include/asm/pkvm_hypercalls.h
@@ -29,6 +29,7 @@ PKVM_HC(update_exception_bitmap)
 PKVM_HC(set_efer)
 PKVM_HC(set_msr)
 PKVM_HC_OUT(get_msr)
+PKVM_HC_OUT(cache_reg)
 
 #undef PKVM_HC
 #undef PKVM_HC_OUT

commit f8bcc77c7882b0b88ff5c0eadfdaac658436de1f

pKVM: x86: Validate register index in cache_reg PV interface
Add input validation for the register index in pkvm_cache_reg() before
calling the vendor cache_reg operation. The reg parameter comes from
the host via hypercall and is passed directly to vmx_cache_reg(), which
uses it to get the register values. A not validated reg value may cause
out-of-bounds write past the regs_avail by kvm_register_mark_avaiable()
in vmx_cache_reg, and trigger panic in the pKVM. Validate against the
set of supported registers before dispatching to the vendor operation
and return -EOPNOTSUPP if reg is not supported.

Fixes: 78336e1494fe ("pKVM: x86: Add cache_reg PV interface")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -1030,6 +1030,18 @@ static int pkvm_set_msr(struct kvm_vcpu *vcpu, u32 index, u64 data)
 static int pkvm_cache_reg(struct kvm_vcpu *vcpu, enum kvm_reg reg,
 			  union pkvm_hc_data *out)
 {
+	switch (reg) {
+	case VCPU_REGS_RSP:
+	case VCPU_REGS_RIP:
+	case VCPU_EXREG_PDPTR:
+	case VCPU_EXREG_CR0:
+	case VCPU_EXREG_CR3:
+	case VCPU_EXREG_CR4:
+		break;
+	default:
+		return -EOPNOTSUPP;
+	}
+
 	kvm_x86_call(cache_reg)(vcpu, reg);
 
 	switch (reg) {
@@ -1058,7 +1070,7 @@ static int pkvm_cache_reg(struct kvm_vcpu *vcpu, enum kvm_reg reg,
 		out->cache_reg.cr4 = vcpu->arch.cr4;
 		break;
 	default:
-		return -EOPNOTSUPP;
+		BUG();
 	}
 
 	return 0;
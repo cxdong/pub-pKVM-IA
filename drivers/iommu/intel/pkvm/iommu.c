cxdong/pub-pKVM-IA

commit 920d3cd88902c8e4ef279eceb288994dacfebe1e

iommu/vt-d: pKVM: Handle DMA_GCMD_TE
Trap TE bit and enable translation on behalf of the host. For a disable
translation request, pKVM doesn't really disable translation, as it
allows devices to bypass pKVM security guarantees in accessing memory.
So GSTS is marked to reflect the disable request so that host sees the
translation disabled when reading GSTS.

Signed-off-by: Vineeth Pillai <vineethrp@google.com>

drivers/iommu/intel/pkvm/iommu.c
@@ -23,7 +23,7 @@ unsigned int iommu_pglvl_mask = IOMMU_PGT_4LEVEL | IOMMU_PGT_5LEVEL;
 /* GCMD oneshot bits where unsetting the bit doesn't have an effect */
 #define DMAR_GCMD_ONESHOT	(DMA_GCMD_SRTP | DMA_GCMD_SIRTP)
 /* Mask of bits the host is allowed to access directly (passed through to hardware) */
-#define DMAR_GCMD_DIRECT	(DMA_GCMD_TE | DMA_GCMD_IRE | DMA_GCMD_CFI | DMA_GCMD_SIRTP)
+#define DMAR_GCMD_DIRECT	(DMA_GCMD_IRE | DMA_GCMD_CFI | DMA_GCMD_SIRTP)
 /* Mask of bits supported by pKVM */
 #define DMAR_GCMD_SUPPORTED_BITS	(DMAR_GSTS_EN_BITS | DMA_GCMD_SRTP | DMA_GCMD_SIRTP)
 
@@ -219,6 +219,31 @@ static int handle_gcmd_srtp(struct intel_iommu *iommu)
 	return 0;
 }
 
+static int handle_gcmd_te(struct intel_iommu *iommu, bool enable)
+{
+	if (enable) {
+		if (iommu->vgsts & DMA_GSTS_TES) {
+			pkvm_err("iommu%d: TE allowed only once\n", iommu->seq_id);
+			return -EBUSY;
+		} else if (!(iommu->vgsts & DMA_GSTS_RTPS)) {
+			pkvm_err("iommu%d: TE not allowed before SRTP\n", iommu->seq_id);
+			return -EINVAL;
+		}
+
+		handle_gcmd_direct(iommu, DMA_GCMD_TE, true);
+		pkvm_dbg("iommu%d: Translation enabled!\n", iommu->seq_id);
+	} else {
+		/*
+		 * Translation is not really disabled as it would
+		 * compromise pKVM security guarantees.
+		 */
+		iommu->vgsts &= ~DMA_GSTS_TES;
+		pkvm_dbg("iommu%d: Translation marked as disabled!", iommu->seq_id);
+	}
+
+	return 0;
+}
+
 static int handle_global_cmd(struct intel_iommu *iommu, u32 val)
 {
 	u32 changed = (iommu->vgsts & DMAR_GSTS_EN_BITS) ^ val;
@@ -246,6 +271,9 @@ static int handle_global_cmd(struct intel_iommu *iommu, u32 val)
 
 	if (changed & DMA_GCMD_SRTP)
 		return handle_gcmd_srtp(iommu);
+
+	if (changed & DMA_GCMD_TE)
+		return handle_gcmd_te(iommu, !!(val & changed));
 
 	/*
 	 * Check if the bits are allowed to be directly accessible by the host

commit a50d56df8e3776688b34a77f3d216cef855aa2d8

iommu/vt-d: pKVM: Derive GCMD from hardware state instead of vgsts
handle_gcmd_direct() constructs the value to write to DMAR_GCMD_REG
from iommu->vgsts, which tracks the virtual status presented to the
host. When pKVM fake-disables TE (clearing DMA_GSTS_TES from vgsts
while leaving hardware translation enabled), any subsequent
handle_gcmd_direct() call for another bit (e.g., IRE, CFI, SIRTP)
writes a GCMD value without the TE bit set. This will cause writing
multiple bits to the GCMD register (disabling TE and enable another one)
which violates the IOMMU spec. Not sure if this can cause the hardware
disable DMA translation, but if it can, this will be allow the host to
bypass the IOMMU isolation.

Fix this by reading the actual hardware GSTS register to derive the
current GCMD value instead of using vgsts. This ensures that
hardware-enabled bits like TE are always preserved when toggling
other GCMD bits. Add early-return paths to avoid unnecessary hardware
writes when the target bit already matches the desired state, and
update vgsts per-bit after each operation instead of wholesale
reassignment.

Also fix a missing newline in the pkvm_dbg message on the TE disable
path.

Fixes: 920d3cd88902 ("iommu/vt-d: pKVM: Handle DMA_GCMD_TE")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

drivers/iommu/intel/pkvm/iommu.c
@@ -131,24 +131,34 @@ static int iommu_direct_mmio_write(struct intel_iommu *iommu, u64 phys,
 
 static int handle_gcmd_direct(struct intel_iommu *iommu, u32 gcmd_bit, bool set)
 {
-	u32 gcmd = iommu->vgsts & DMAR_GSTS_EN_BITS;
+	u32 gcmd = readl(iommu->reg + DMAR_GSTS_REG) & DMAR_GSTS_EN_BITS;
 	u32 sts;
 
 	if ((gcmd_bit & DMAR_GCMD_ONESHOT) && !set)
 		return -EINVAL;
 
-	if (set)
+	if (set) {
+		if (gcmd & gcmd_bit) {
+			iommu->vgsts |= gcmd_bit;
+			return 0;
+		}
 		gcmd |= gcmd_bit;
-	else
+	} else {
+		if (!(gcmd & gcmd_bit)) {
+			iommu->vgsts &= ~gcmd_bit;
+			return 0;
+		}
 		gcmd &= ~gcmd_bit;
+	}
 
 	writel(gcmd, iommu->reg + DMAR_GCMD_REG);
-	if (set)
+	if (set) {
 		IOMMU_WAIT_OP(iommu, DMAR_GSTS_REG, readl, (sts & gcmd_bit), sts);
-	else
+		iommu->vgsts |= gcmd_bit;
+	} else {
 		IOMMU_WAIT_OP(iommu, DMAR_GSTS_REG, readl, !(sts & gcmd_bit), sts);
-
-	iommu->vgsts = (iommu->vgsts & DMAR_GCMD_ONESHOT) | gcmd;
+		iommu->vgsts &= ~gcmd_bit;
+	}
 
 	return 0;
 }
@@ -304,7 +314,7 @@ static int handle_gcmd_te(struct intel_iommu *iommu, bool enable)
 		 * compromise pKVM security guarantees.
 		 */
 		iommu->vgsts &= ~DMA_GSTS_TES;
-		pkvm_dbg("iommu%d: Translation marked as disabled!", iommu->seq_id);
+		pkvm_dbg("iommu%d: Translation marked as disabled!\n", iommu->seq_id);
 	}
 
 	return 0;
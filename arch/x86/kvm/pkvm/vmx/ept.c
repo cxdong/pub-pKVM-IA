cxdong/pub-pKVM-IA

commit f1baf65d401200c50d15d29070f544d65b85b8e8

pKVM: VMX: Handle host EPT violation
Although all the memory information is mapped during the initialize, as
well as the MMIO holes, the MMIO in the high-end address is not mapped
and accessing such MMIO from the host VM will trigger EPT violation.
Meanwhile, accessing an isolated memory from the host VM can also
trigger the EPT violation.

To handle this vmexit, the GPA should be checked to see if it is not
a memory address or not. The EPT violation caused by accessing memory is
considered as a security violation, and the pKVM hypervisor will inject
GP exception to the host. Only the EPT violation caused by accessing the
MMIO will be handled by install a mapping in the host ept with the
possible maximum page size, to minimize the memory consumptions. As this
would happen on multiple CPU at the same time, introduce the host_mmu
spinlock to avoid concurrency.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/vmx/ept.c
@@ -4,8 +4,10 @@
 #include <vmx/capabilities.h>
 #include <mmu/spte.h>
 #include <vmx/vmx.h>
+#include <vmx/vmx_ops.h>
 #include "ept.h"
 #include "gfp.h"
+#include "pkvm/mmu.h"
 
 static struct pkvm_pgtable *host_ept;
 static struct pkvm_pool host_ept_pool;
@@ -182,4 +184,66 @@ int pkvm_host_ept_init(struct pkvm_pgtable *pgt, void *pool_base,
 
 	host_ept = pgt;
 	return 0;
+}
+
+int pkvm_handle_host_ept_violation(void)
+{
+	struct range range, cur;
+	int level, ret = -EPERM;
+	unsigned long hpa, gpa;
+
+	BUG_ON(!host_ept);
+
+	gpa = vmcs_read64(GUEST_PHYSICAL_ADDRESS);
+	/*
+	 * All the memory and MMIO holes have already been mapped in the host
+	 * EPT when initialize, except for the MMIO in the high-end address.
+	 * Handle the MMIO only.
+	 */
+	if (pkvm_find_addr_range(gpa, &range))
+		return ret;
+
+	pkvm_host_mmu_lock();
+
+	pkvm_pgtable_lookup(host_ept, gpa, &hpa, NULL, &level);
+	if (VALID_PAGE(hpa)) {
+		/*
+		 * Some other CPU has just mapped in the host EPT. Vmenter
+		 * the host VM and retry.
+		 */
+		pkvm_host_mmu_unlock();
+		return 0;
+	}
+
+	BUG_ON(level > host_ept->cap.level);
+	/*
+	 * Based on the looked up level, find out the possible maximum page-size
+	 * aligned address range which contains the GPA address for installing
+	 * the MMIO mapping, to minimize the memory consumption.
+	 */
+	for (; level > PG_LEVEL_NONE; level--) {
+		unsigned long size;
+
+		if (!((1 << level) & host_ept->cap.allowed_pgsz))
+			continue;
+
+		size = ept_level_to_size(level);
+		cur.start = ALIGN_DOWN(gpa, size);
+		cur.end = cur.start + size - 1;
+
+		if (range_contains(&range, &cur)) {
+			/*
+			 * TODO: In case the host mmu free pages are not
+			 * enough, -ENOMEM will be returned. This could be
+			 * handled by unmaping some other MMIO mapped for the
+			 * host VM to reclaim some mmu pages and try again.
+			 */
+			ret = pkvm_host_mmu_map(cur.start, size, true, true,
+						true, true);
+			break;
+		}
+	}
+
+	pkvm_host_mmu_unlock();
+	return ret;
 }

arch/x86/kvm/pkvm/vmx/ept.h
@@ -6,5 +6,6 @@
 
 int pkvm_host_ept_init(struct pkvm_pgtable *pgt, void *pool_base,
 		       unsigned long pool_pages);
+int pkvm_handle_host_ept_violation(void);
 
 #endif /* __PKVM_VMX_EPT_H */

arch/x86/kvm/pkvm/vmx/host_vmx.c
@@ -310,6 +310,14 @@ void pkvm_host_vmexit_main(struct vcpu_vmx *vmx)
 		if (handle_write_msr(vcpu) == X86EMUL_CONTINUE)
 			skip_instruction = true;
 		break;
+	case EXIT_REASON_EPT_VIOLATION:
+		/*
+		 * Inject #GP to the host VM if its EPT violation
+		 * cannot be handled.
+		 */
+		if (pkvm_handle_host_ept_violation())
+			kvm_inject_gp(vcpu, 0);
+		break;
 	case EXIT_REASON_PREEMPTION_TIMER:
 		handle_preemption_timer(vcpu);
 		break;

arch/x86/kvm/pkvm/mmu.c
@@ -11,6 +11,7 @@ static struct pkvm_pgtable hyp_mmu;
 static struct pkvm_pool hyp_mmu_pool;
 
 static struct pkvm_pgtable host_mmu;
+pkvm_spinlock_t host_mmu_lock;
 
 static void *hyp_mmu_zalloc_page(void)
 {
@@ -235,6 +236,8 @@ int pkvm_hyp_mmu_map(unsigned long vaddr, unsigned long phys,
 
 int pkvm_host_mmu_init(void *pool_base, unsigned long pool_pages, host_mmu_init_fn_t fn)
 {
+	pkvm_spin_lock_init(&host_mmu_lock);
+
 	return fn ? fn(&host_mmu, pool_base, pool_pages) : -EOPNOTSUPP;
 }
 

arch/x86/kvm/pkvm/mmu.h
@@ -2,7 +2,10 @@
 #ifndef __PKVM_X86_MMU_H
 #define __PKVM_X86_MMU_H
 
+#include <asm/pkvm_spinlock.h>
 #include "init.h"
+
+extern pkvm_spinlock_t host_mmu_lock;
 
 int pkvm_hyp_mmu_init(void *pool_base, unsigned long pool_pages);
 int pkvm_hyp_mmu_switch_to_buddy(void *pool_base, unsigned long pool_pages);
@@ -16,5 +19,15 @@ int pkvm_host_mmu_init(void *pool_base, unsigned long pool_pages,
 int pkvm_host_mmu_map(unsigned long phys, unsigned long size, bool read,
 		      bool write, bool exec, bool mmio);
 int pkvm_host_mmu_unmap(unsigned long vaddr, unsigned long size);
+
+static inline void pkvm_host_mmu_lock(void)
+{
+	pkvm_spin_lock(&host_mmu_lock);
+}
+
+static inline void pkvm_host_mmu_unlock(void)
+{
+	pkvm_spin_unlock(&host_mmu_lock);
+}
 
 #endif /* __PKVM_X86_MMU_H */

commit 344edbb5c3ec0c0f13d79e40d8aad5c1783942d2

pKVM: VMX: Inject #PF to host on protected memory EPT violation
When the host kernel accesses a page removed from the host EPT by
pKVM (e.g., a protected private memory, pvmfw or IOMMU MMIO), the EPT
violation is handled by pkvm_handle_host_ept_violation(). Previously
this injected #GP unconditionally. This could break the usage of the
load_unaligned_zeropad() which expects to receive a #PF.

Supporting this by injecting #PF for this case if the valid guest linear
address (GLA) can be detected. The GLA is used as the #PF fault address
and the error code is constructed from the EPT exit qualification bits:
- PFERR_PRESENT: set if any of proper bits are set.
- PFERR_WRITE:   set if the access was a write (ACC_WRITE).
- PFERR_USER:    set if guest CPL was 3 (SS.DPL == 3).
- PFERR_FETCH:   set if the access was an instruction fetch (ACC_INSTR).

The #GP(0) will only be injected if the GLA is not valid.

Fixes: f1baf65d4012 ("pKVM: VMX: Handle host EPT violation")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/vmx/ept.c
@@ -399,7 +399,55 @@ int pkvm_host_ept_finalize(struct pkvm_pgtable *pgt)
 	return 0;
 }
 
-int pkvm_handle_host_ept_violation(void)
+static void handle_host_ept_violation_failure(struct kvm_vcpu *vcpu)
+{
+	unsigned long exit_qualification = vmx_get_exit_qual(vcpu);
+	struct x86_exception fault = { 0 };
+	u64 error_code = 0;
+
+	/*
+	 * The guest linear address(GLA) should be valid for injecting #PF.
+	 * But the GLA may be invalid according to SDM vol. 3 Exit Qualification
+	 * for EPT Violations:
+	 * "The guest linear-address field is valid for all EPT violations
+	 * except those resulting from an attempt to load the guest PDPTEs as
+	 * part of the execution of the MOV CR instruction and those due to
+	 * trace-address pre-translation (TAPT; Section 27.5.4)."
+	 *
+	 * For loading guest PDPTEs via MOV CR, SDM Page-Fault Exceptions:
+	 * "Failures to load the PDPTE registers with PAE paging (see Section
+	 * 5.4.1) cause general-protection exceptions (#GP(0)) and not
+	 * page-fault exceptions.", #GP(0) can be injected.
+	 *
+	 * For TAPT, SDM IA32_RTIT_OUTPUT_BASE MSR:
+	 * "however, if any of those bits are set outside SEAM, trace output may
+	 * fail in a model-specific manner." Similar for pKVM, injecting #GP(0)
+	 * can be pKVM specific manner.
+	 */
+	if (!(exit_qualification & EPT_VIOLATION_GVA_IS_VALID)) {
+		kvm_inject_gp(vcpu, 0);
+		return;
+	}
+
+	error_code |= (exit_qualification & EPT_VIOLATION_PROT_MASK)
+		      ? PFERR_PRESENT_MASK : 0;
+	error_code |= (exit_qualification & EPT_VIOLATION_ACC_WRITE)
+		      ? PFERR_WRITE_MASK : 0;
+	error_code |= VMX_AR_DPL(vmcs_read32(GUEST_SS_AR_BYTES)) == 3
+		      ? PFERR_USER_MASK : 0;
+	error_code |= (exit_qualification & EPT_VIOLATION_ACC_INSTR)
+		      ? PFERR_FETCH_MASK : 0;
+
+	fault.vector = PF_VECTOR;
+	fault.error_code_valid = true;
+	fault.error_code = error_code;
+	fault.address = vmcs_readl(GUEST_LINEAR_ADDRESS);
+	fault.exit_qualification = exit_qualification;
+
+	kvm_inject_page_fault(vcpu, &fault);
+}
+
+void pkvm_handle_host_ept_violation(struct kvm_vcpu *vcpu)
 {
 	struct range range, cur;
 	int level, ret = -EPERM;
@@ -415,8 +463,8 @@ int pkvm_handle_host_ept_violation(void)
 	 */
 	if (pkvm_find_addr_range(gpa, &range) || is_pvmfw(gpa) ||
 	    is_iommu_mmio(gpa)) {
-		pkvm_err("Host access to protected memory at 0x%lx\n", gpa);
-		return ret;
+		pkvm_err_ratelimited("Host access to protected memory at 0x%lx\n", gpa);
+		goto failed;
 	}
 
 	pkvm_host_mmu_lock();
@@ -428,7 +476,7 @@ int pkvm_handle_host_ept_violation(void)
 		 * the host VM and retry.
 		 */
 		pkvm_host_mmu_unlock();
-		return 0;
+		return;
 	}
 
 	BUG_ON(level > host_ept->cap.level);
@@ -456,17 +504,19 @@ int pkvm_handle_host_ept_violation(void)
 			 * host VM to reclaim some mmu pages and try again.
 			 */
 			ret = pkvm_hyp_donate_host_mmio_locked(cur.start, size);
-			if (ret == -ENOMEM)
-				pkvm_err("No page-table page to map host GPA 0x%lx\n", gpa);
 			break;
 		}
 	}
 
 	if (ret)
-		pkvm_err("No valid range found to map host GPA 0x%lx\n", gpa);
+		pkvm_err("No valid range found to map host GPA 0x%lx, err %d\n", gpa, ret);
 
 	pkvm_host_mmu_unlock();
-	return ret;
+
+	if (likely(!ret))
+		return;
+failed:
+	handle_host_ept_violation_failure(vcpu);
 }
 
 void pkvm_flush_host_ept(void)

arch/x86/kvm/pkvm/vmx/ept.h
@@ -7,7 +7,7 @@
 int pkvm_host_ept_init(struct pkvm_pgtable *pgt, void *pool_base,
 		       unsigned long pool_pages);
 int pkvm_host_ept_finalize(struct pkvm_pgtable *pgt);
-int pkvm_handle_host_ept_violation(void);
+void pkvm_handle_host_ept_violation(struct kvm_vcpu *vcpu);
 void pkvm_flush_host_ept(void);
 
 u64 pkvm_host_ept_root(void);

arch/x86/kvm/pkvm/vmx/host_vmx.c
@@ -376,12 +376,7 @@ void pkvm_host_vmexit_main(struct vcpu_vmx *vmx)
 			skip_instruction = true;
 		break;
 	case EXIT_REASON_EPT_VIOLATION:
-		/*
-		 * Inject #GP to the host VM if its EPT violation
-		 * cannot be handled.
-		 */
-		if (pkvm_handle_host_ept_violation())
-			kvm_inject_gp(vcpu, 0);
+		pkvm_handle_host_ept_violation(vcpu);
 		break;
 	case EXIT_REASON_PREEMPTION_TIMER:
 		handle_preemption_timer(vcpu);

arch/x86/kvm/x86.c
@@ -844,7 +844,6 @@ void kvm_deliver_exception_payload(struct kvm_vcpu *vcpu,
 }
 EXPORT_SYMBOL_FOR_KVM_INTERNAL(kvm_deliver_exception_payload);
 
-#ifndef __PKVM_HYP__
 static void kvm_queue_exception_vmexit(struct kvm_vcpu *vcpu, unsigned int vector,
 				       bool has_error_code, u32 error_code,
 				       bool has_payload, unsigned long payload)
@@ -859,7 +858,6 @@ static void kvm_queue_exception_vmexit(struct kvm_vcpu *vcpu, unsigned int vecto
 	ex->has_payload = has_payload;
 	ex->payload = payload;
 }
-#endif /* !__PKVM_HYP__ */
 
 static void kvm_multiple_exception(struct kvm_vcpu *vcpu, unsigned int nr,
 				   bool has_error, u32 error_code,
@@ -940,13 +938,11 @@ void kvm_queue_exception_p(struct kvm_vcpu *vcpu, unsigned nr,
 }
 EXPORT_SYMBOL_FOR_KVM_INTERNAL(kvm_queue_exception_p);
 
-#ifndef __PKVM_HYP__
 static void kvm_queue_exception_e_p(struct kvm_vcpu *vcpu, unsigned nr,
 				    u32 error_code, unsigned long payload)
 {
 	kvm_multiple_exception(vcpu, nr, true, error_code, true, payload);
 }
-#endif /* !__PKVM_HYP__ */
 
 void kvm_requeue_exception(struct kvm_vcpu *vcpu, unsigned int nr,
 			   bool has_error_code, u32 error_code)
@@ -999,6 +995,7 @@ static int complete_emulated_insn_gp(struct kvm_vcpu *vcpu, int err)
 	return kvm_emulate_instruction(vcpu, EMULTYPE_NO_DECODE | EMULTYPE_SKIP |
 				       EMULTYPE_COMPLETE_USER_EXIT);
 }
+#endif /* !__PKVM_HYP__ */
 
 void kvm_inject_page_fault(struct kvm_vcpu *vcpu, struct x86_exception *fault)
 {
@@ -1017,6 +1014,7 @@ void kvm_inject_page_fault(struct kvm_vcpu *vcpu, struct x86_exception *fault)
 					fault->address);
 }
 
+#ifndef __PKVM_HYP__
 void kvm_inject_emulated_page_fault(struct kvm_vcpu *vcpu,
 				    struct x86_exception *fault)
 {
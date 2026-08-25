cxdong/pub-pKVM-IA

commit fe89535e31479eeeff57b7e2ce0cc579fc5d356a

pKVM: VMX: Ensure the host VMCS is loaded when handling NMI
The pKVM captured NMI should be injected to the host VM. With the
vcpu_load PV interface, it is possible that a guest VMCS is currently
loaded when an NMI is captured. To ensure NMI being correctly injected
to the host VM, load the host VMCS before handling NMI and restore the
previous VMCS after that.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/vmx/idt.c
@@ -5,11 +5,23 @@
 #include <vmx/x86_ops.h>
 #include "host_vmx.h"
 #include "idt.h"
+#include "memory.h"
 #include "pkvm.h"
 
 static void handle_nmi(struct pt_regs *regs, int vector, bool has_error_code)
 {
 	struct kvm_vcpu *vcpu = this_cpu_read(host_vcpu);
+	struct vcpu_vmx *vmx = to_vmx(vcpu);
+	u64 cur_vmcs_pa = vmcs_store();
+	bool is_host_vmcs;
+
+	/* There should always be a loaded VMCS, otherwise it is a code bug. */
+	BUG_ON(!VALID_PAGE(cur_vmcs_pa));
+
+	/* Switch to the host VMCS if the current one is not for host. */
+	is_host_vmcs = (cur_vmcs_pa == __pkvm_pa(vmx->loaded_vmcs->vmcs));
+	if (!is_host_vmcs)
+		vmcs_load(vmx->loaded_vmcs->vmcs);
 
 	/*
 	 * The NMI happens while the pKVM hypervisor is running, but it should
@@ -23,7 +35,11 @@ static void handle_nmi(struct pt_regs *regs, int vector, bool has_error_code)
 	 * Request host immediate exit in case the pending NMI has already been
 	 * handled in this host vmexit handling cycle.
 	 */
-	request_host_immediate_exit(to_vmx(vcpu));
+	request_host_immediate_exit(vmx);
+
+	/* Restore to the previous VMCS if it is not for host. */
+	if (!is_host_vmcs)
+		vmcs_load(__pkvm_va(cur_vmcs_pa));
 }
 
 void pkvm_vmx_register_excp_handlers(void)

commit 5e2913bfa889ddb9d730553faae8e74b59c2a58d

pKVM: VMX: Don't BUG_ON if no loaded VMCS
pKVM's handle_nmi() asserts if there is no valid current-VMCS, assuming
that there must always be either the host VMCS or a guest VMCS loaded.
This assumption is not 100% correct: in pkvm_vcpu_put() there is a short
window when pKVM has already cleared the guest VMCS but hasn't loaded
the host VMCS yet. So if an NMI fires during this window, pKVM panics
unexpectedly.

So don't panic in this case, just proceed to loading the host VMCS like
in the normal case. Keep it loaded afterwards once the NMI is handled,
as there is no need to replace it with another VMCS in this case.

Fixes: fe89535e3147 ("pKVM: VMX: Ensure the host VMCS is loaded when handling NMI")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/vmx/idt.c
@@ -15,9 +15,6 @@ static void handle_nmi(struct pt_regs *regs, int vector, bool has_error_code)
 	u64 cur_vmcs_pa = vmcs_store();
 	bool is_host_vmcs;
 
-	/* There should always be a loaded VMCS, otherwise it is a code bug. */
-	BUG_ON(!VALID_PAGE(cur_vmcs_pa));
-
 	/* Switch to the host VMCS if the current one is not for host. */
 	is_host_vmcs = (cur_vmcs_pa == __pkvm_pa(vmx->loaded_vmcs->vmcs));
 	if (!is_host_vmcs)
@@ -38,7 +35,7 @@ static void handle_nmi(struct pt_regs *regs, int vector, bool has_error_code)
 	request_host_immediate_exit(vmx);
 
 	/* Restore to the previous VMCS if it is not for host. */
-	if (!is_host_vmcs)
+	if (!is_host_vmcs && VALID_PAGE(cur_vmcs_pa))
 		vmcs_load(__pkvm_va(cur_vmcs_pa));
 }
 
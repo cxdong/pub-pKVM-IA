cxdong/pub-pKVM-IA

commit 3974ce6517e210519f1fc11aa0ce2aeb16850b9e

pKVM: x86: Add event injection PV interfaces
Add event injection PV interfaces for the host to inject irq, nmi and
exceptions via vendor-specific operations.

The PV interfaces for injecting irq/nmi are always accessible to the
host as the host emulates the irq/nmi injections for both npVM and pVM.
The exception for pVM is emulated by the pKVM, thus making this PV
interface always inaccessible to the host.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -658,6 +658,8 @@ static bool is_guest_vcpu_accessible(struct kvm_vcpu *vcpu, enum pkvm_hc hc)
 	case __pkvm__interrupt_allowed:
 	case __pkvm__nmi_allowed:
 	case __pkvm__get_nmi_mask:
+	case __pkvm__inject_irq:
+	case __pkvm__inject_nmi:
 		/*
 		 * The host is responsible for running vCPU, injecting
 		 * interrupts, emulating lapic etc. Always allow the related PV
@@ -840,6 +842,38 @@ static int pkvm_nmi_allowed(struct kvm_vcpu *vcpu, bool for_injection)
 	return kvm_x86_call(nmi_allowed)(vcpu, for_injection);
 }
 
+static void pkvm_inject_irq(struct kvm_vcpu *vcpu)
+{
+	struct kvm_vcpu *shared_vcpu = to_pkvm_vcpu(vcpu)->shared_vcpu;
+
+	if (WARN_ON_ONCE(pkvm_interrupt_allowed(vcpu, true) <= 0))
+		return;
+
+	vcpu->arch.interrupt.soft = shared_vcpu->arch.interrupt.soft;
+	vcpu->arch.interrupt.nr = shared_vcpu->arch.interrupt.nr;
+	kvm_x86_call(inject_irq)(vcpu, false);
+}
+
+static void pkvm_inject_nmi(struct kvm_vcpu *vcpu)
+{
+	if (WARN_ON_ONCE(pkvm_nmi_allowed(vcpu, true) <= 0))
+		return;
+
+	kvm_x86_call(inject_nmi)(vcpu);
+}
+
+static void pkvm_inject_exception(struct kvm_vcpu *vcpu)
+{
+	/*
+	 * As the __pkvm__inject_exception is always denied for the pVM,
+	 * it must be a code bug if the vcpu is protected.
+	 */
+	BUG_ON(pkvm_is_protected_vcpu(vcpu));
+	vcpu->arch.exception = to_pkvm_vcpu(vcpu)->shared_vcpu->arch.exception;
+
+	kvm_x86_call(inject_exception)(vcpu);
+}
+
 static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc hc,
 					   union pkvm_hc_data *in, union pkvm_hc_data *out)
 {
@@ -965,6 +999,15 @@ static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc 
 		break;
 	case __pkvm__set_nmi_mask:
 		kvm_x86_call(set_nmi_mask)(vcpu, pkvm_hc_input1(hvcpu));
+		break;
+	case __pkvm__inject_irq:
+		pkvm_inject_irq(vcpu);
+		break;
+	case __pkvm__inject_nmi:
+		pkvm_inject_nmi(vcpu);
+		break;
+	case __pkvm__inject_exception:
+		pkvm_inject_exception(vcpu);
 		break;
 	default:
 		ret = -EINVAL;

arch/x86/include/asm/pkvm_hypercalls.h
@@ -59,6 +59,9 @@ PKVM_HC(interrupt_allowed)
 PKVM_HC(nmi_allowed)
 PKVM_HC_OUT(get_nmi_mask)
 PKVM_HC(set_nmi_mask)
+PKVM_HC(inject_irq)
+PKVM_HC(inject_nmi)
+PKVM_HC(inject_exception)
 
 #undef PKVM_HC
 #undef PKVM_HC_OUT

commit fb6d728d17948f0423a0e1665df9e9740f344299

pKVM: x86: Share injection information with host
When the guest vmexits with valid irq or nmi not being injected, pKVM
queues the irq or nmi in its own vcpu structure and inject again at the
next time of running vcpu after this vmexit is handled. It is possible
that this vmexit is reflected to the host KVM, and the host KVM has an
irq or nmi to inject (particularly kvm_cpu_has_injectable_intr() or
vcpu->arch.nmi_pending is true), the kvm_x86_call(interrupt_allowed) or
kvm_x86_call(nmi_allowed) will return negative result before commit
ad2fee4349c6 ("ANDROID: pKVM: x86: Fix TOCTOU and shared_vcpu accessing
for event injection") as there is a queued irq or nmi in the pKVM to
prevent the host to inject.

Now with this commit, the nmi_allowed and interrupt_allowed report the
hardware interrupt injection state. When there is a queued nmi or irq in
the pKVM but not injected yet, these two hypercall will return allowed
and results in the host to inject, which can trigger the WARN_ON_ONCE in
the pkvm_inject_irq() and pkvm_inject_nmi(). This will panic pKVM in the
debug version.

Fix this by queuing the not injected irq or nmi in the host vcpu
structure before returning back to the host.

Fixes: 3974ce6517e2 ("pKVM: x86: Add event injection PV interfaces")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -1227,25 +1227,30 @@ static void pkvm_inject_exception(struct kvm_vcpu *vcpu)
 	kvm_x86_call(inject_exception)(vcpu);
 }
 
-static void pkvm_cancel_injection(struct kvm_vcpu *vcpu)
+static void pkvm_share_injection_with_host(struct kvm_vcpu *vcpu)
 {
-	struct pkvm_vcpu *pkvm_vcpu = to_pkvm_vcpu(vcpu);
-	struct kvm_vcpu *shared_vcpu;
+	struct kvm_vcpu *shared_vcpu = to_pkvm_vcpu(vcpu)->shared_vcpu;
 
-	kvm_x86_call(cancel_injection)(vcpu);
-
-	shared_vcpu = pkvm_vcpu->shared_vcpu;
-	if (vcpu->arch.nmi_injected) {
+	if (!pkvm_is_protected_vcpu(vcpu) && vcpu->arch.exception.injected) {
+		/*
+		 * For the pVM, the exception can only be injected by the pKVM
+		 * thus the pending exception should not be handed over to the
+		 * host.
+		 * For the npVM, the exception can be injected by both sides.
+		 */
+		shared_vcpu->arch.exception = vcpu->arch.exception;
+		kvm_clear_exception_queue(vcpu);
+	} else if (vcpu->arch.nmi_injected) {
 		shared_vcpu->arch.nmi_injected = true;
 		vcpu->arch.nmi_injected = false;
 	} else if (vcpu->arch.interrupt.injected) {
 		/*
 		 * The npVM's injected software and external interrupts can be
-		 * canceled as the host is allowed to inject both. But the host
+		 * pending as the host is allowed to inject both. But the host
 		 * is not allowed to inject the pVM's software interrupt, and
 		 * the pending pVM's software interrupt (exits during delivering
 		 * a software interrupt) should be injected by the pKVM, thus
-		 * the canceled software interrupt should not be handed over to
+		 * the pending software interrupt should not be handed over to
 		 * the host.
 		 */
 		if (!pkvm_is_protected_vcpu(vcpu) || !vcpu->arch.interrupt.soft) {
@@ -1253,16 +1258,14 @@ static void pkvm_cancel_injection(struct kvm_vcpu *vcpu)
 					    vcpu->arch.interrupt.soft);
 			kvm_clear_interrupt_queue(vcpu);
 		}
-	} else if (!pkvm_is_protected_vcpu(vcpu) && vcpu->arch.exception.injected) {
-		/*
-		 * For the pVM, the exception can only be injected by the pKVM
-		 * thus the canceled exception should not be handed over to the
-		 * host.
-		 * For the npVM, the exception can be injected by both sides.
-		 */
-		shared_vcpu->arch.exception = vcpu->arch.exception;
-		kvm_clear_exception_queue(vcpu);
 	}
+}
+
+static void pkvm_cancel_injection(struct kvm_vcpu *vcpu)
+{
+	kvm_x86_call(cancel_injection)(vcpu);
+
+	pkvm_share_injection_with_host(vcpu);
 }
 
 static int pkvm_refresh_apicv_exec_ctrl(struct kvm_vcpu *vcpu, bool apicv_active)
@@ -1688,7 +1691,7 @@ static void share_vcpu_state_with_host(struct kvm_vcpu *vcpu)
 		shared_vcpu->arch.efer = vcpu->arch.efer;
 
 		/* Share the exception information to the host if there is any */
-		if (vcpu->arch.exception.pending || vcpu->arch.exception.injected) {
+		if (vcpu->arch.exception.pending) {
 			shared_vcpu->arch.exception = vcpu->arch.exception;
 			kvm_clear_exception_queue(vcpu);
 		}
@@ -1702,6 +1705,8 @@ static void share_vcpu_state_with_host(struct kvm_vcpu *vcpu)
 		shared_vcpu->arch.dr7 = vcpu->arch.dr7;
 		shared_vcpu->arch.xcr0 = vcpu->arch.xcr0;
 	}
+
+	pkvm_share_injection_with_host(vcpu);
 
 	pkvm_x86_call(share_vcpu_state_with_host)(vcpu);
 }

arch/x86/kvm/vmx/pkvm_host.c
@@ -1394,7 +1394,8 @@ static fastpath_t pkvm_vcpu_run(struct kvm_vcpu *vcpu, u64 run_flags)
 	if (unlikely(vmx_get_exit_reason(vcpu).failed_vmentry))
 		return EXIT_FASTPATH_NONE;
 
-	if (vcpu->arch.exception.pending || vcpu->arch.exception.injected)
+	if (vcpu->arch.nmi_injected || vcpu->arch.interrupt.injected ||
+	    vcpu->arch.exception.pending || vcpu->arch.exception.injected)
 		kvm_make_request(KVM_REQ_EVENT, vcpu);
 
 	exit_fastpath = EXIT_FASTPATH_EXIT_HANDLED;
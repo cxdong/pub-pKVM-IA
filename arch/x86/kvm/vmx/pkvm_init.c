cxdong/pub-pKVM-IA

commit 1582caab9b42e7aa6ee2557677711ded36983d47

pKVM: VMX: Reprivilege host cpus if pkvm init fails
If pKVM initialization fails after deprivilege, the system boots in a
degraded state and KVM is not initialized successfully. This breaks the
virtualization functionality of the host and potentially other
functionality as well due to the failed pKVM. So reprivilege the cpus to
ensure a normal boot with KVM enabled.

A per-cpu variable tracks the state of the cpu - whether deprivileged or
not. In the event of a failure during pKVM initialization, reprivilege
all the deprivileged cpus by invoking the reprivilege_cpu PV interface.

Signed-off-by: Vineeth Pillai (Google) <vineeth@bitbyteword.org>

arch/x86/kvm/vmx/pkvm_init.c
@@ -604,6 +604,66 @@ static noinline int local_deprivilege_cpu(void)
 	return -EINVAL;
 }
 
+static DEFINE_PER_CPU(bool, deprivileged);
+static __init void pkvm_host_reprivilege_cpu(void *data)
+{
+	unsigned long flags;
+	int cpu = get_cpu();
+	int ret;
+
+	if (!this_cpu_read(deprivileged)) {
+		put_cpu();
+		return;
+	}
+
+	local_irq_save(flags);
+
+	/*
+	 * Load the RW GDT page for reprivilege code
+	 * to reload TR.
+	 */
+	load_direct_gdt(cpu);
+
+	/*
+	 * Intel CET requires indirect jmp/call to return to
+	 * endbr64 instruction. So we can't use kvm_hypercall
+	 * here.
+	 */
+	asm volatile(
+		"vmcall\n"
+		"endbr64\n"
+		: "=a"(ret)
+		: "a"(__pkvm__reprivilege_cpu)
+		: "memory");
+
+	/* Switch back to RO GDT page */
+	load_fixmap_gdt(cpu);
+
+	if (!ret) {
+		this_cpu_write(deprivileged, false);
+		kvm_cpu_vmxoff();
+		pr_info("%s: CPU%d back in host mode\n", __func__, cpu);
+	} else {
+		pr_warn("%s: CPU%d failed to reprivilege(err=%d)\n", __func__, cpu, ret);
+	}
+
+	local_irq_restore(flags);
+	put_cpu();
+}
+
+static __init void pkvm_host_reprivilege_cpus(void)
+{
+	int cpu;
+
+	for_each_possible_cpu(cpu) {
+		if (!per_cpu(deprivileged, cpu))
+			continue;
+
+		smp_call_function_single(cpu, pkvm_host_reprivilege_cpu,
+					 NULL, true);
+	}
+}
+
 static __init void pkvm_host_deprivilege_cpu(void *data)
 {
 	struct pkvm_deprivilege_param *p = data;
@@ -634,6 +694,7 @@ static __init void pkvm_host_deprivilege_cpu(void *data)
 	}
 
 	vcpu->mode = IN_GUEST_MODE;
+	this_cpu_write(deprivileged, true);
 	pr_info("CPU%d in guest mode\n", cpu);
 	return;
 vmxoff:
@@ -800,19 +861,18 @@ int __init vmx_pkvm_init(void)
 	pkvm_sym(init_ops) = pkvm_sym(pkvm_vmx_init_ops);
 
 	ret = pkvm_host_deprivilege_cpus(pkvm);
-	if (ret) {
-		/* TODO: Re-privilege the deprivileged CPUs */
-		goto out;
-	}
+	if (ret)
+		goto repriv_cpus;
 
 	ret = pkvm_hyp_init();
-	if (ret) {
-		/* TODO: Re-privilege the deprivileged CPUs */
-		goto out;
-	}
+	if (ret)
+		goto repriv_cpus;
 
 	pr_info("Hypervisor is up and running!\n");
 	return 0;
+
+repriv_cpus:
+	pkvm_host_reprivilege_cpus();
 out:
 	/*
 	 * As the reserved memory at the pkvm_mem_base will not be

commit 7a5f494c066913bf2595c339e5a6b1ba279c3eac

pKVM: VMX: remove unnecessary preemption/IRQ handling for reprivilege
pkvm_host_reprivilege_cpu() is called via smp call which disables
preemption and interrupts before calling the function. Remove redundant
get_cpu()/put_cpu() and local_irq_save()/local_irq_restore(). Also
change the failure path from pr_warn() to panic() after exit from the
smp call since a failed reprivilege leaves the CPU in an unrecoverable
state.

Fixes: 1582caab9b42 ("pKVM: VMX: Reprivilege host cpus if pkvm init fails")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/vmx/pkvm_init.c
@@ -1168,16 +1168,11 @@ static noinline int local_deprivilege_cpu(void)
 static DEFINE_PER_CPU(bool, deprivileged);
 static __init void pkvm_host_reprivilege_cpu(void *data)
 {
-	unsigned long flags;
-	int cpu = get_cpu();
+	int cpu = smp_processor_id();
 	int ret;
 
-	if (!this_cpu_read(deprivileged)) {
-		put_cpu();
+	if (!this_cpu_read(deprivileged))
 		return;
-	}
-
-	local_irq_save(flags);
 
 	/*
 	 * Load the RW GDT page for reprivilege code
@@ -1204,12 +1199,9 @@ static __init void pkvm_host_reprivilege_cpu(void *data)
 		this_cpu_write(deprivileged, false);
 		kvm_cpu_vmxoff();
 		pr_info("%s: CPU%d back in host mode\n", __func__, cpu);
-	} else {
-		pr_warn("%s: CPU%d failed to reprivilege(err=%d)\n", __func__, cpu, ret);
 	}
 
-	local_irq_restore(flags);
-	put_cpu();
+	*(int *)data = ret;
 }
 
 static __init void pkvm_host_reprivilege_cpus(void)
@@ -1217,11 +1209,16 @@ static __init void pkvm_host_reprivilege_cpus(void)
 	int cpu;
 
 	for_each_possible_cpu(cpu) {
+		int ret, reprivilege_ret = 0;
+
 		if (!per_cpu(deprivileged, cpu))
 			continue;
 
-		smp_call_function_single(cpu, pkvm_host_reprivilege_cpu,
-					 NULL, true);
+		ret = smp_call_function_single(cpu, pkvm_host_reprivilege_cpu,
+					       &reprivilege_ret, true);
+		if (ret || reprivilege_ret)
+			panic("CPU%d failed to reprivilege(smp_call=%d, reprivilege=%d)\n",
+			      cpu, ret, reprivilege_ret);
 	}
 }
 
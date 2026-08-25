cxdong/pub-pKVM-IA

commit 5f5d7c848bc8a57e84dd073d6f1e3198d5d57c79

pKVM: VMX: Handle EXIT_REASON_IO_INSTRUCTION
The EXIT_REASON_IO_INSTRUCTION is mostly handled by the host as the IO
devices are emulated by the host alone. However the string IO
instruction needs to be decoded by the host, which cannot be supported
for the pVM due to isolation. So the pVM should be enlightened to unroll
the string IO instructions. In case this happened, inject #GP to the
pVM.

Once the IO instruction is handled, this IO instruction should be
skipped. But due to isolation, the host can only skip instruction for
the npVM but not for the pVM. So the pKVM hypervisor will skip this
instruction before the next vmenter.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/vmx/vmx.c
@@ -5833,9 +5833,11 @@ static int handle_triple_fault(struct kvm_vcpu *vcpu)
 	vcpu->mmio_needed = 0;
 	return 0;
 }
+#endif /* !__PKVM_HYP__ */
 
 static int handle_io(struct kvm_vcpu *vcpu)
 {
+#ifndef __PKVM_HYP__
 	unsigned long exit_qualification;
 	int size, in, string;
 	unsigned port;
@@ -5853,8 +5855,29 @@ static int handle_io(struct kvm_vcpu *vcpu)
 	in = (exit_qualification & 8) != 0;
 
 	return kvm_fast_pio(vcpu, size, port, in);
+#else
+	/*
+	 * The pKVM hypervisor doesn't emulate IO device thus the IO exit should
+	 * be handled by the host.
+	 *
+	 * To emulate a string IO instruction, decoder is required. But the host
+	 * cannot decode for a pVM due to isolation. Thus a pVM should be
+	 * enlightened to unroll string IO instructions to IO instructions. In
+	 * case this is not done by the pVM, handling such IO exit by injecting
+	 * a #GP exception to the pVM.
+	 */
+	if ((vmx_get_exit_qual(vcpu) & 16) != 0) {
+		if (pkvm_is_protected_vcpu(vcpu)) {
+			kvm_queue_exception(vcpu, GP_VECTOR);
+			return 1;
+		}
+	}
+
+	return 0;
+#endif
 }
 
+#ifndef __PKVM_HYP__
 void vmx_patch_hypercall(struct kvm_vcpu *vcpu, unsigned char *hypercall)
 {
 	/*
@@ -6626,8 +6649,8 @@ static int (*kvm_vmx_exit_handlers[])(struct kvm_vcpu *vcpu) = {
 	[EXIT_REASON_TRIPLE_FAULT]            = handle_triple_fault,
 #endif
 	[EXIT_REASON_NMI_WINDOW]	      = handle_nmi_window,
-#ifndef __PKVM_HYP__
 	[EXIT_REASON_IO_INSTRUCTION]          = handle_io,
+#ifndef __PKVM_HYP__
 	[EXIT_REASON_CR_ACCESS]               = handle_cr,
 	[EXIT_REASON_DR_ACCESS]               = handle_dr,
 	[EXIT_REASON_CPUID]                   = kvm_emulate_cpuid,

arch/x86/kvm/vmx/pkvm_host.c
@@ -241,6 +241,32 @@ static int handle_nmi_window(struct kvm_vcpu *vcpu)
 }
 
 /*
+ * This function is a 1:1 copy of handle_io in vmx.c.
+ * TODO: Check whether this requires updates whenever handle_io in vmx.c is
+ * updated in the future.
+ */
+static int handle_io(struct kvm_vcpu *vcpu)
+{
+	unsigned long exit_qualification;
+	int size, in, string;
+	unsigned int port;
+
+	exit_qualification = vmx_get_exit_qual(vcpu);
+	string = (exit_qualification & 16) != 0;
+
+	++vcpu->stat.io_exits;
+
+	if (string)
+		return kvm_emulate_instruction(vcpu, 0);
+
+	port = exit_qualification >> 16;
+	size = (exit_qualification & 7) + 1;
+	in = (exit_qualification & 8) != 0;
+
+	return kvm_fast_pio(vcpu, size, port, in);
+}
+
+/*
  * The exit handlers return 1 if the exit was handled fully and guest execution
  * may resume.  Otherwise they set the kvm_run parameter to indicate what needs
  * to be done to userspace and return 0.
@@ -250,6 +276,7 @@ static int (*pkvm_vmx_exit_handlers[])(struct kvm_vcpu *vcpu) = {
 	[EXIT_REASON_EXTERNAL_INTERRUPT]      = handle_external_interrupt,
 	[EXIT_REASON_TRIPLE_FAULT]            = handle_triple_fault,
 	[EXIT_REASON_NMI_WINDOW]	      = handle_nmi_window,
+	[EXIT_REASON_IO_INSTRUCTION]          = handle_io,
 };
 
 static const int pkvm_vmx_max_exit_handlers = ARRAY_SIZE(pkvm_vmx_exit_handlers);

commit 713452bbb705e5a42c5cafb82628c053e1574d08

pKVM: VMX: Use kvm_inject_gp() for protected vCPU IO instruction rejection
The exception #GP has error code. Even the error code is 0, the #GP
should be injected with setting error code. Use kvm_inject_gp(vcpu, 0)
instead of kvm_queue_exception(vcpu, GP_VECTOR) for injecting #GP with
error code 0.

Fixes: 5f5d7c848bc8 ("pKVM: VMX: Handle EXIT_REASON_IO_INSTRUCTION")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/vmx/vmx.c
@@ -5882,7 +5882,7 @@ static int handle_io(struct kvm_vcpu *vcpu)
 	 */
 	if ((vmx_get_exit_qual(vcpu) & 16) != 0) {
 		if (pkvm_is_protected_vcpu(vcpu)) {
-			kvm_queue_exception(vcpu, GP_VECTOR);
+			kvm_inject_gp(vcpu, 0);
 			return 1;
 		}
 	}
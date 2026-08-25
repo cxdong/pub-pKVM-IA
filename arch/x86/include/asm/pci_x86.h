cxdong/pub-pKVM-IA

commit 05804102b36778e802edec607ae272d333e10384

x86/paravirt: Extend pv_mmio_ops to support pci mmcfg read and write
Add the pci mmcfg reading and writing callbacks to the pv_mmio_ops to
allow providing platform specific implementations, e.g., an enlightened
kernel can use hypercalls to access the pci mmcfg.

The reason why not reuse the existing generic MMIO callbacks from
pv_mmio_ops is to let the default implementation implement the existing
behavior for PCI MMIO, i.e.: ensure using "mov" instruction with %eax
to satisfy the AMD CPUs mentioned in the comment for mmio_config_xxx().

[ Dmytro: amend commit message ]

Suggested-by: Dmytro Maluka <dmaluka@google.com>
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>

arch/x86/include/asm/pci_x86.h
@@ -199,41 +199,103 @@ extern struct list_head pci_mmcfg_list;
  * Guide (BKDG) For AMD Family 10h Processors", rev. 3.48, sec 2.11.1,
  * "MMIO Configuration Coding Requirements".
  */
-static inline unsigned char mmio_config_readb(void __iomem *pos)
+static inline unsigned char pci_mmcfg_readb(const volatile void __iomem *pos)
 {
 	u8 val;
 	asm volatile("movb (%1),%%al" : "=a" (val) : "r" (pos));
 	return val;
 }
 
-static inline unsigned short mmio_config_readw(void __iomem *pos)
+static inline unsigned short pci_mmcfg_readw(const volatile void __iomem *pos)
 {
 	u16 val;
 	asm volatile("movw (%1),%%ax" : "=a" (val) : "r" (pos));
 	return val;
 }
 
-static inline unsigned int mmio_config_readl(void __iomem *pos)
+static inline unsigned int pci_mmcfg_readl(const volatile void __iomem *pos)
 {
 	u32 val;
 	asm volatile("movl (%1),%%eax" : "=a" (val) : "r" (pos));
 	return val;
 }
 
-static inline void mmio_config_writeb(void __iomem *pos, u8 val)
+static inline void pci_mmcfg_writeb(unsigned char val, volatile void __iomem *pos)
 {
 	asm volatile("movb %%al,(%1)" : : "a" (val), "r" (pos) : "memory");
 }
 
-static inline void mmio_config_writew(void __iomem *pos, u16 val)
+static inline void pci_mmcfg_writew(unsigned short val, volatile void __iomem *pos)
 {
 	asm volatile("movw %%ax,(%1)" : : "a" (val), "r" (pos) : "memory");
 }
 
-static inline void mmio_config_writel(void __iomem *pos, u32 val)
+static inline void pci_mmcfg_writel(unsigned int val, volatile void __iomem *pos)
 {
 	asm volatile("movl %%eax,(%1)" : : "a" (val), "r" (pos) : "memory");
 }
+
+#ifdef CONFIG_PARAVIRT
+static inline unsigned char mmio_config_readb(void __iomem *pos)
+{
+	return pv_pci_mmcfg_readb(pos);
+}
+
+static inline unsigned short mmio_config_readw(void __iomem *pos)
+{
+	return pv_pci_mmcfg_readw(pos);
+}
+
+static inline unsigned int mmio_config_readl(void __iomem *pos)
+{
+	return pv_pci_mmcfg_readl(pos);
+}
+
+static inline void mmio_config_writeb(void __iomem *pos, u8 val)
+{
+	pv_pci_mmcfg_writeb(val, pos);
+}
+
+static inline void mmio_config_writew(void __iomem *pos, u16 val)
+{
+	pv_pci_mmcfg_writew(val, pos);
+}
+
+static inline void mmio_config_writel(void __iomem *pos, u32 val)
+{
+	pv_pci_mmcfg_writel(val, pos);
+}
+#else
+static inline unsigned char mmio_config_readb(void __iomem *pos)
+{
+	return pci_mmcfg_readb(pos);
+}
+
+static inline unsigned short mmio_config_readw(void __iomem *pos)
+{
+	return pci_mmcfg_readw(pos);
+}
+
+static inline unsigned int mmio_config_readl(void __iomem *pos)
+{
+	return pci_mmcfg_readl(pos);
+}
+
+static inline void mmio_config_writeb(void __iomem *pos, u8 val)
+{
+	pci_mmcfg_writeb(val, pos);
+}
+
+static inline void mmio_config_writew(void __iomem *pos, u16 val)
+{
+	pci_mmcfg_writew(val, pos);
+}
+
+static inline void mmio_config_writel(void __iomem *pos, u32 val)
+{
+	pci_mmcfg_writel(val, pos);
+}
+#endif
 
 #ifdef CONFIG_PCI
 # ifdef CONFIG_ACPI

arch/x86/include/asm/paravirt.h
@@ -763,6 +763,36 @@ static __always_inline void pv_writeq_relaxed(u64 val, volatile void __iomem *ad
 }
 #endif
 
+static __always_inline unsigned char pv_pci_mmcfg_readb(const volatile void __iomem *addr)
+{
+	return PVOP_CALL1(unsigned char, mmio.pci_mmcfg_readb, addr);
+}
+
+static __always_inline unsigned short pv_pci_mmcfg_readw(const volatile void __iomem *addr)
+{
+	return PVOP_CALL1(unsigned short, mmio.pci_mmcfg_readw, addr);
+}
+
+static __always_inline unsigned int pv_pci_mmcfg_readl(const volatile void __iomem *addr)
+{
+	return PVOP_CALL1(unsigned int, mmio.pci_mmcfg_readl, addr);
+}
+
+static __always_inline void pv_pci_mmcfg_writeb(unsigned char val, volatile void __iomem *addr)
+{
+	PVOP_VCALL2(mmio.pci_mmcfg_writeb, val, addr);
+}
+
+static __always_inline void pv_pci_mmcfg_writew(unsigned short val, volatile void __iomem *addr)
+{
+	PVOP_VCALL2(mmio.pci_mmcfg_writew, val, addr);
+}
+
+static __always_inline void pv_pci_mmcfg_writel(unsigned int val, volatile void __iomem *addr)
+{
+	PVOP_VCALL2(mmio.pci_mmcfg_writel, val, addr);
+}
+
 /* Make sure as little as possible of this mess escapes. */
 #undef PARAVIRT_CALL
 #undef __PVOP_CALL

arch/x86/include/asm/paravirt_types.h
@@ -243,6 +243,13 @@ struct pv_mmio_ops {
 	void (*raw_writeq)(u64 val, volatile void __iomem *addr);
 	void (*raw_writeq_relaxed)(u64 val, volatile void __iomem *addr);
 #endif
+	unsigned char (*pci_mmcfg_readb)(const volatile void __iomem *addr);
+	unsigned short (*pci_mmcfg_readw)(const volatile void __iomem *addr);
+	unsigned int (*pci_mmcfg_readl)(const volatile void __iomem *addr);
+
+	void (*pci_mmcfg_writeb)(unsigned char val, volatile void __iomem *addr);
+	void (*pci_mmcfg_writew)(unsigned short val, volatile void __iomem *addr);
+	void (*pci_mmcfg_writel)(unsigned int val, volatile void __iomem *addr);
 } __no_randomize_layout;
 
 

arch/x86/kernel/paravirt.c
@@ -34,6 +34,7 @@
 #include <asm/io_bitmap.h>
 #include <asm/gsseg.h>
 #include <asm/msr.h>
+#include <asm/pci_x86.h>
 
 /* stub always returning 0. */
 DEFINE_ASM_FUNC(paravirt_ret0, "xor %eax,%eax", .entry.text);
@@ -263,6 +264,12 @@ struct paravirt_patch_template pv_ops = {
 	.mmio.raw_writeq		= raw_writeq,
 	.mmio.raw_writeq_relaxed	= raw_writeq_relaxed,
 #endif
+	.mmio.pci_mmcfg_readb		= pci_mmcfg_readb,
+	.mmio.pci_mmcfg_readw		= pci_mmcfg_readw,
+	.mmio.pci_mmcfg_readl		= pci_mmcfg_readl,
+	.mmio.pci_mmcfg_writeb		= pci_mmcfg_writeb,
+	.mmio.pci_mmcfg_writew		= pci_mmcfg_writew,
+	.mmio.pci_mmcfg_writel		= pci_mmcfg_writel,
 };
 
 #ifdef CONFIG_PARAVIRT_XXL

commit 767e761a186a79df24032a597d4072599125b180

x86/paravirt: Eliminate PCI MMCFG function call overhead on non-pKVM systems
PCI MMCFG accessors (mmio_config_readb/w/l, mmio_config_writeb/w/l)
unconditionally route through PVOP_CALL1/PVOP_VCALL2 when
CONFIG_PARAVIRT=y. This incurs a function call for every PCI
configuration space access, which should be a single mov instruction
through %eax (required by AMD Fam10h MMIO Configuration Coding).

Use the same __pv_mmio_read/__pv_mmio_write static_branch dispatch
pattern as the generic MMIO fix: on non-pKVM systems the static branch
NOP falls through to the native pci_mmcfg_* inline functions with zero
call overhead. On pKVM guests, the branch is patched to dispatch to
the pv_pci_mmcfg_* PVOP handlers.

Fixes: 05804102b367 ("x86/paravirt: Extend pv_mmio_ops to support pci mmcfg read and write")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/include/asm/pci_x86.h
@@ -236,34 +236,36 @@ static inline void pci_mmcfg_writel(unsigned int val, volatile void __iomem *pos
 }
 
 #ifdef CONFIG_PARAVIRT
+#include <asm/io.h>
+
 static inline unsigned char mmio_config_readb(void __iomem *pos)
 {
-	return pv_pci_mmcfg_readb(pos);
+	return __pv_mmio_read(pci_mmcfg_readb, pv_pci_mmcfg_readb, pos);
 }
 
 static inline unsigned short mmio_config_readw(void __iomem *pos)
 {
-	return pv_pci_mmcfg_readw(pos);
+	return __pv_mmio_read(pci_mmcfg_readw, pv_pci_mmcfg_readw, pos);
 }
 
 static inline unsigned int mmio_config_readl(void __iomem *pos)
 {
-	return pv_pci_mmcfg_readl(pos);
+	return __pv_mmio_read(pci_mmcfg_readl, pv_pci_mmcfg_readl, pos);
 }
 
 static inline void mmio_config_writeb(void __iomem *pos, u8 val)
 {
-	pv_pci_mmcfg_writeb(val, pos);
+	__pv_mmio_write(pci_mmcfg_writeb, pv_pci_mmcfg_writeb, val, pos);
 }
 
 static inline void mmio_config_writew(void __iomem *pos, u16 val)
 {
-	pv_pci_mmcfg_writew(val, pos);
+	__pv_mmio_write(pci_mmcfg_writew, pv_pci_mmcfg_writew, val, pos);
 }
 
 static inline void mmio_config_writel(void __iomem *pos, u32 val)
 {
-	pv_pci_mmcfg_writel(val, pos);
+	__pv_mmio_write(pci_mmcfg_writel, pv_pci_mmcfg_writel, val, pos);
 }
 #else
 static inline unsigned char mmio_config_readb(void __iomem *pos)
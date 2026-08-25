cxdong/pub-pKVM-IA

commit 119d69b3f8552d8bda51291bbd9308e63a61ba8f

pKVM: x86: Add hypervisor panic and reset infrastructure
The pKVM hypervisor should be capable of handling fatal internal errors
(BUGs or hardware exceptions) without relying on host kernel code or
state. Letting the hypervisor jump back into the host panic path would
allow an untrusted host to inspect an unpredictable state of a buggy
hypervisor.

Introduce a standalone hypervisor panic and recovery subsystem:

1. Redefine BUG() and BUG_ON() within the hypervisor to trigger
   'pkvm_panic()', ensuring all fatal assertions are handled
   locally.
2. Implement 'pkvm_panic()':
   - Uses a global 'pkvm_panic_in_progress' atomic flag to coordinate
     the shootdown across all cores.
   - Broadcasts an INIT to all other cores via the x2APIC to pull
     them out of the host VM.
   - VM exit handler catch and hold all CPUs in a halt loop when panic
     is in-progress, ensuring the host VM cannot interfere with the
     panic bookkeeping.
3. Implement 'pkvm_emergency_reset()' to ensure system recovery
   regardless of host state. It attempts a multi-stage hardware reset:
   - PCI System Soft Reset (0xCF9/0x06)
   - PS/2 Keyboard Controller reset (0x64/0xFE).
   - PCI System Hard Reset (0xCF9/0x0E).
   - Triple Fault (Invalid IDT + int3) as a final fail-safe.
4. Redefine BUG() to pass __FILE__ and __LINE__ as direct arguments,
   consciously avoiding the use of 'ud2' instructions and the
   associated 'bug_table' binary metadata. This is important for
   production (non-debug) environments where the hypervisor doesn't
   have its own kallsyms.

This commit provides the infrastructure for hypervisor bug reporting,
which will be extended in subsequent patches to support persistent
ramoops logging.

Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>

arch/x86/kvm/pkvm/panic.c
@@ -1,0 +1,91 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <linux/kernel.h>
+#include <asm/io.h>
+#include <asm/apic.h>
+#include <asm/kvm_pkvm.h>
+#include <asm/msr.h>
+#include <asm-generic/bug.h>
+#include "debug.h"
+#include "pkvm.h"
+#include "memory.h"
+
+/*
+ * To not include ACPI based reboot and its complexity, try to reset the system
+ * via PCI warm reset (0xCF9), the keyboard controller (PS/2), PCI cold reset
+ * register (CF9), or triple fault.
+ *
+ * All based on arch/x86/kernel/reboot.c native_machine_emergency_restart().
+ */
+static void __noreturn pkvm_emergency_reset(void)
+{
+	struct desc_ptr idt;
+
+	/*
+	 * PCI warm reset (0xCF9), which is usually effectively the ACPI reset,
+	 * i.e. the modern recommended default.
+	 */
+	outb(0x06, 0xcf9);
+	pkvm_udelay(50000);
+
+	/* Keyboard controller (PS/2) reset */
+	outb(0xfe, 0x64);
+	pkvm_udelay(50000);
+
+	/* PCI cold reset */
+	outb(0x0e, 0xcf9);
+	pkvm_udelay(50000);
+
+	/* Triple fault */
+	idt.size = 0;
+	idt.address = 0;
+	asm volatile("lidt %0" : : "m"(idt));
+	asm volatile("int3");
+
+	while (1)
+		asm volatile("cli; hlt");
+}
+
+atomic_t pkvm_panic_in_progress = ATOMIC_INIT(0);
+
+void __noreturn pkvm_panic(const char *fmt, ...)
+{
+	static char panic_msg[1024];
+	va_list args;
+
+	/*
+	 * Ensure only one CPU handles the panic and writes to ramoops.
+	 * This also signals the VM exit handler to catch and hold all other
+	 * CPUs.
+	 */
+	if (atomic_cmpxchg(&pkvm_panic_in_progress, 0, 1) != 0) {
+		while (1)
+			asm volatile("cli; hlt");
+	}
+
+	/*
+	 * Broadcast INIT to all other CPUs (excluding self) via x2APIC ICR
+	 * to pull them out of the guest mode. This ensures that the host will not
+	 * interfere with panic handling and e.g. will not interfere with
+	 * ramoops update.
+	 */
+	native_x2apic_icr_write(APIC_DEST_ALLBUT | APIC_INT_ASSERT | APIC_DM_INIT, 0);
+
+	/*
+	 * Wait for all IPIs to deliver and targets to enter Wait-for-SIPI.
+	 * Note: using pkvm_wait_vcpu_kicked_out() is tempting but for reliability,
+	 * since we are in the middle of fatal error handling, it is better to
+	 * use simple hw delay. This avoids relying on various sw states (like
+	 * vcpu->mode) that could be corrupted by the fatal error itself.
+	 */
+	pkvm_udelay(10000);
+
+	va_start(args, fmt);
+	vscnprintf(panic_msg, sizeof(panic_msg), fmt, args);
+	va_end(args);
+
+	pkvm_err("%s", panic_msg);
+
+	/* TODO: add ramoops logging */
+
+	pkvm_emergency_reset();
+}

arch/x86/kvm/pkvm/panic.h
@@ -1,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __PKVM_X86_PANIC_H
+#define __PKVM_X86_PANIC_H
+
+extern atomic_t pkvm_panic_in_progress;
+
+void __noreturn pkvm_panic(const char *fmt, ...);
+
+#endif /* __PKVM_X86_PANIC_H */

arch/x86/include/asm/kvm_pkvm.h
@@ -842,7 +842,6 @@ static inline size_t pkvm_guest_initial_fpstate_size(struct kvm *kvm)
 #undef WARN
 #undef WARN_ON_ONCE
 #undef WARN_ONCE
-#undef _BUG_FLAGS
 
 #define WARN_ON(condition) ({						\
 	int __ret_warn_on = !!(condition);				\
@@ -858,9 +857,28 @@ static inline size_t pkvm_guest_initial_fpstate_size(struct kvm *kvm)
 #define WARN_ON_ONCE(condition) WARN_ON(condition)
 #define WARN_ONCE(condition, format...) WARN(condition, format)
 
-#define _BUG_FLAGS(ins, flags, extra)  asm volatile(ins)
-
 #endif /* CONFIG_PKVM_X86_DEBUG */
+
+void __noreturn pkvm_panic(const char *fmt, ...);
+
+/*
+ * Directly call the panic handler with file/line info. This avoids the use
+ * of 'ud2' instructions and associated 'bug_table' metadata parsing, which
+ * would unnecessarily increase the TCB and complexity of the hypervisor's
+ * emergency recovery path. This is also critical for production (non-debug)
+ * environments where hypervisor doesn't have its own kallsyms or access to
+ * the host's metadata.
+ */
+#undef BUG
+#define BUG() do { pkvm_panic("\n==================================\n"	\
+			      "pKVM BUG at %s:%u\n"			\
+			      "==================================\n",	\
+			       __FILE__, __LINE__);			\
+			      __builtin_unreachable();			\
+		} while (0)
+
+#undef BUG_ON
+#define BUG_ON(condition) do { if (unlikely(condition)) BUG(); } while (0)
 
 #undef KVM_BUG_ON
 #define KVM_BUG_ON(cond, kvm)						\

arch/x86/kvm/pkvm/Makefile
@@ -14,7 +14,8 @@ cmd_deps			:= $(src)/compiling_cmds.sh
 
 pkvm-hyp-y			:= early_alloc.o pkvm.o memory.o cpu.o \
 				   idt.o entry.o kern_stub.o init.o pgtable.o \
-				   mmu.o page_alloc.o lapic.o trace.o fpu.o
+				   mmu.o page_alloc.o lapic.o trace.o fpu.o \
+				   panic.o
 pkvm-lib			:= lib
 pkvm-hyp-y			+= $(pkvm-lib)/find_bit.o $(pkvm-lib)/printf.o
 

arch/x86/kvm/pkvm/pkvm.c
@@ -9,6 +9,7 @@
 #include "mem_protect.h"
 #include "memory.h"
 #include "mmu.h"
+#include "panic.h"
 #include "pkvm.h"
 #include "trace.h"
 #include "../x86.h"
@@ -2133,6 +2134,14 @@ void pkvm_wait_vcpu_kicked_out(struct kvm_vcpu *vcpu)
 			relax_iters = 0;
 		}
 	} while (READ_ONCE(vcpu->mode) == EXITING_GUEST_MODE);
+}
+
+void pkvm_handle_init_signal(void)
+{
+	if (unlikely(atomic_read(&pkvm_panic_in_progress))) {
+		while (1)
+			asm volatile("cli; hlt");
+	}
 }
 
 void pkvm_udelay(unsigned int usecs)

arch/x86/kvm/pkvm/pkvm.h
@@ -198,6 +198,7 @@ struct pkvm_x86_ops {
 void pkvm_handle_host_hypercall(struct kvm_vcpu *vcpu);
 void pkvm_kick_vcpu(struct kvm_vcpu *vcpu);
 void pkvm_wait_vcpu_kicked_out(struct kvm_vcpu *vcpu);
+void pkvm_handle_init_signal(void);
 void pkvm_udelay(unsigned int usecs);
 int pkvm_x86_vendor_init(struct kvm_x86_init_ops *ops);
 struct pkvm_vm *pkvm_get_vm(int vm_handle);

arch/x86/kvm/pkvm/vmx/host_vmx.c
@@ -327,11 +327,7 @@ void pkvm_host_vmexit_main(struct vcpu_vmx *vmx)
 
 	switch (vt->exit_reason.full) {
 	case EXIT_REASON_INIT_SIGNAL:
-		/*
-		 * INIT is used as kick when making a request.
-		 * So just break the vmexits and go to pending
-		 * events handling.
-		 */
+		pkvm_handle_init_signal();
 		break;
 	case EXIT_REASON_INTERRUPT_WINDOW:
 		handle_irq_window(vcpu);

arch/x86/kvm/vmx/vmx.c
@@ -6762,10 +6762,11 @@ static int handle_wrmsr_imm(struct kvm_vcpu *vcpu)
 #ifdef __PKVM_HYP__
 static int handle_init(struct kvm_vcpu *vcpu)
 {
+	pkvm_handle_init_signal();
 	/*
-	 * EXIT_REASON_INIT_SIGNAL is caused by the pKVM hypervisor sending INIT
-	 * signal to kick vCPU out of non-root mode. Nothing needs to be handled
-	 * by the pKVM hypervisor, and also no need to involve the host.
+	 * EXIT_REASON_INIT_SIGNAL is only caused by the pKVM hypervisor sending
+	 * INIT signal to kick vCPU out of non-root mode. So no need to involve
+	 * the host.
 	 */
 	return 1;
 }

commit c00e799a6baed6f5c4a7b702dd5d253f595d97f3

pKVM: x86: Rename pkvm_panic to panic
Rename pkvm_panic to panic to facilitate pKVM reusing the kernel code
which calls kernel's panic. In this case, also no need to declare the
pkvm_panic in pKVM's header but just re-use the kernel's declaration for
panic.

Fixes: 119d69b3f855 ("pKVM: x86: Add hypervisor panic and reset infrastructure")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/include/asm/pkvm_redef.h
@@ -52,8 +52,6 @@
 
 #endif /* CONFIG_PKVM_X86_DEBUG */
 
-void __noreturn pkvm_panic(const char *fmt, ...);
-
 /*
  * Directly call the panic handler with file/line info. This avoids the use
  * of 'ud2' instructions and associated 'bug_table' metadata parsing, which
@@ -63,11 +61,11 @@ void __noreturn pkvm_panic(const char *fmt, ...);
  * the host's metadata.
  */
 #undef BUG
-#define BUG() do { pkvm_panic("\n==================================\n"	\
-			      "pKVM BUG at %s:%u\n"			\
-			      "==================================\n",	\
-			       __FILE__, __LINE__);			\
-			      __builtin_unreachable();			\
+#define BUG() do { panic("\n==================================\n"	\
+			 "pKVM BUG at %s:%u\n"				\
+			 "==================================\n",	\
+			 __FILE__, __LINE__);				\
+		  __builtin_unreachable();				\
 		} while (0)
 
 #undef BUG_ON

arch/x86/kvm/pkvm/idt.c
@@ -138,15 +138,15 @@ static void default_exception_handler(struct pt_regs *regs,
 	void *adj_rip = (void *)(rip - kaslr_offset_val);
 
 	if (has_error_code)
-		pkvm_panic("\n========================================\n"
-			   "pKVM Exception %d @ip %pS (%px), err code 0x%lx\n"
-			   "========================================\n",
-			   vector, adj_rip, (void *)rip, regs->orig_ax);
+		panic("\n========================================\n"
+		      "pKVM Exception %d @ip %pS (%px), err code 0x%lx\n"
+		      "========================================\n",
+		      vector, adj_rip, (void *)rip, regs->orig_ax);
 	else
-		pkvm_panic("\n========================================\n"
-			   "pKVM Exception %d @ip %pS (%px), no err code\n"
-			   "========================================\n",
-			   vector, adj_rip, (void *)rip);
+		panic("\n========================================\n"
+		      "pKVM Exception %d @ip %pS (%px), no err code\n"
+		      "========================================\n",
+		      vector, adj_rip, (void *)rip);
 }
 
 static exception_handler_t exception_handlers[X86_TRAP_IRET] = {

arch/x86/kvm/pkvm/panic.c
@@ -135,7 +135,7 @@ static void __noreturn pkvm_emergency_reset(void)
 
 atomic_t pkvm_panic_in_progress = ATOMIC_INIT(0);
 
-void __noreturn pkvm_panic(const char *fmt, ...)
+void __noreturn panic(const char *fmt, ...)
 {
 	static char panic_msg[1024];
 	va_list args;

arch/x86/kvm/pkvm/panic.h
@@ -4,6 +4,4 @@
 
 extern atomic_t pkvm_panic_in_progress;
 
-void __noreturn pkvm_panic(const char *fmt, ...);
-
 #endif /* __PKVM_X86_PANIC_H */
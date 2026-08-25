cxdong/pub-pKVM-IA

commit 7c0ba1255a61b0505f8bad5dfe8bcc1d40282188

iommu/vt-d: pKVM: Initialize Queued Invalidation (QI)
Initialize QI subsystem in pKVM. QI would already be initialized in
the host before pKVM is initialized if IRQ remapping is enabled. In
that case, the host disables it for pKVM to start with a clean state.

pKVM traps DMA_GCMD_QIE for QI initialization. pKVM gets the
Invalidation Queue Address from IQA register which would be already set
by the host and then initializes QI by interacting with hardware.

intel_iommu structure in host has a pointer to struct qi_inval and is
dynamically allocated during initialization. pKVM has the same structure
embedded to avoid runtime allocation. To enable easy re-use of the host
code, intel_iommu in pKVM has a qi_inval pointer as well which points to
the embedded structure and this pointer is initialized during QI
initialization in pKVM.

TODO: Implement the QI support in pKVM which is currently handled by
host.

Signed-off-by: Aashish Sharma <shraash@google.com>
Signed-off-by: Vineeth Pillai <vineethrp@google.com>

drivers/iommu/intel/iommu.c
@@ -1887,8 +1887,10 @@ static int device_def_domain_type(struct device *dev)
 	return 0;
 }
 
-static void intel_iommu_init_qi(struct intel_iommu *iommu)
+static int intel_iommu_init_qi(struct intel_iommu *iommu)
 {
+	int ret = 0;
+
 	/*
 	 * Start from the sane iommu hardware state.
 	 * If the queued invalidation is already initialized by us
@@ -1905,9 +1907,17 @@ static void intel_iommu_init_qi(struct intel_iommu *iommu)
 		 * before OS handover.
 		 */
 		dmar_disable_qi(iommu);
+	} else if (pkvm_enabled()) {
+		/* Re-initialize QI to start from a clean state. */
+		dmar_reenable_qi(iommu);
 	}
 
-	if (dmar_enable_qi(iommu)) {
+	ret = dmar_enable_qi(iommu);
+	if (ret) {
+		/* pKVM requires QI enabled */
+		if (pkvm_enabled())
+			return ret;
+
 		/*
 		 * Queued Invalidate not enabled, use Register Based Invalidate
 		 */
@@ -1920,6 +1930,8 @@ static void intel_iommu_init_qi(struct intel_iommu *iommu)
 		iommu->flush.flush_iotlb = qi_flush_iotlb;
 		pr_info("%s: Using Queued invalidation\n", iommu->name);
 	}
+
+	return ret;
 }
 
 static int copy_context_table(struct intel_iommu *iommu,
@@ -2125,7 +2137,10 @@ static int __init init_dmars(void)
 						   intel_pasid_max_id);
 		}
 
-		intel_iommu_init_qi(iommu);
+		ret = intel_iommu_init_qi(iommu);
+		if (ret)
+			goto free_iommu;
+
 		init_translation_status(iommu);
 
 		if (translation_pre_enabled(iommu) && !is_kdump_kernel()) {

drivers/iommu/intel/pkvm/iommu.c
@@ -23,7 +23,8 @@ unsigned int iommu_pglvl_mask = IOMMU_PGT_4LEVEL | IOMMU_PGT_5LEVEL;
 /* GCMD oneshot bits where unsetting the bit doesn't have an effect */
 #define DMAR_GCMD_ONESHOT	(DMA_GCMD_SRTP | DMA_GCMD_SIRTP)
 /* Mask of bits the host is allowed to access directly (passed through to hardware) */
-#define DMAR_GCMD_DIRECT	(DMAR_GSTS_EN_BITS | DMA_GCMD_SRTP | DMA_GCMD_SIRTP)
+#define DMAR_GCMD_DIRECT	(DMA_GCMD_TE | DMA_GCMD_IRE | DMA_GCMD_CFI | DMA_GCMD_SRTP | \
+				 DMA_GCMD_SIRTP)
 /* Mask of bits supported by pKVM */
 #define DMAR_GCMD_SUPPORTED_BITS	(DMAR_GSTS_EN_BITS | DMA_GCMD_SRTP | DMA_GCMD_SIRTP)
 
@@ -109,6 +110,70 @@ static int handle_gcmd_direct(struct intel_iommu *iommu, u32 gcmd_bit, bool set)
 	return 0;
 }
 
+static int initialize_qi(struct intel_iommu *iommu)
+{
+	struct q_inval *qi = iommu->qi;
+	u64 val = __pkvm_pa(qi->desc);
+
+	/*
+	 * TODO: Write protect QI descriptor page once we have
+	 *       hypervisor take care of all QI logic.
+	 */
+
+	pkvm_spin_lock_init(&qi->q_lock);
+	qi->free_head = qi->free_tail = 0;
+	qi->free_cnt = QI_LENGTH;
+
+	/*
+	 * Set DW=1 and QS=1 in IQA_REG when Scalable Mode capability
+	 * is present.
+	 */
+	if (sm_supported(iommu))
+		val |= BIT_ULL(11) | BIT_ULL(0);
+
+	/* write zero to the tail reg */
+	writel(0, iommu->reg + DMAR_IQT_REG);
+	/* Set IQA */
+	writeq(val, iommu->reg + DMAR_IQA_REG);
+
+	return handle_gcmd_direct(iommu, DMA_GCMD_QIE, true);
+}
+
+static int handle_gcmd_qie(struct intel_iommu *iommu, bool enable)
+{
+	int ret = 0;
+
+	if (enable) {
+		if (iommu->qi || iommu->vgsts & DMA_GSTS_QIES) {
+			pkvm_err("iommu%d: QI already enabled\n", iommu->seq_id);
+			return -EBUSY;
+		} else if (!iommu->viqa) {
+			pkvm_err("iommu%d: QIE before setting IQA\n", iommu->seq_id);
+			return -EINVAL;
+		}
+
+		/*
+		 * Host IOMMU driver dynamically allocates iommu->qi, but pKVM has it
+		 * embedded. For easy re-use of host code, the embedded field is named
+		 * as iommu->_qi, and the pointer iommu->qi points to iommu->_qi.
+		 * Also, it serves as a flag to denote whether qi is
+		 * enabled(similar to how host driver does)
+		 */
+		iommu->qi = &iommu->_qi;
+		iommu->qi->desc = pkvm_host_gpa_to_virt(iommu->viqa & VTD_PAGE_MASK);
+		ret = initialize_qi(iommu);
+	} else {
+		if (!iommu->qi)
+			ret = handle_gcmd_direct(iommu, DMA_GCMD_QIE, false);
+		else
+			iommu->vgsts &= ~DMA_GSTS_QIES;
+	}
+
+	pkvm_dbg("iommu%d: Quueued Invalidation %s!\n", iommu->seq_id,
+		 enable ? "enabled" : "disabled");
+	return ret;
+}
+
 static int handle_global_cmd(struct intel_iommu *iommu, u32 val)
 {
 	u32 changed = (iommu->vgsts & DMAR_GSTS_EN_BITS) ^ val;
@@ -130,6 +195,9 @@ static int handle_global_cmd(struct intel_iommu *iommu, u32 val)
 
 	pkvm_dbg("iommu%d: handle gcmd val 0x%x gsts 0x%x changed 0x%x\n",
 		 iommu->seq_id, val, iommu->vgsts, changed);
+
+	if (changed & DMA_GCMD_QIE)
+		return handle_gcmd_qie(iommu, !!(val & changed));
 
 	/*
 	 * Check if the bits are allowed to be directly accessible by the host
@@ -166,6 +234,9 @@ int pkvm_iommu_mmio_read(u64 phys, int len, u64 *val)
 	case DMAR_ECAP_REG:
 		*val = iommu->ecap;
 		break;
+	case DMAR_IQA_REG:
+		*val = iommu->viqa;
+		break;
 	default:
 		/* Not emulated MMIO can directly go to hardware */
 		ret = iommu_direct_mmio_read(iommu, phys, len, val);
@@ -197,6 +268,15 @@ int pkvm_iommu_mmio_write(u64 phys, int len, u64 val)
 		break;
 	case DMAR_GCMD_REG:
 		ret = handle_global_cmd(iommu, val);
+		break;
+	case DMAR_IQA_REG:
+		if (iommu->viqa) {
+			pkvm_err("iommu%d: IQA set more than once!\n",
+				 iommu->seq_id);
+			ret = -EINVAL;
+		} else {
+			iommu->viqa = val;
+		}
 		break;
 	default:
 		/* Not emulated MMIO can directly go to hardware */

drivers/iommu/intel/iommu.h
@@ -826,9 +826,12 @@ struct intel_iommu {
 	u64		cap;
 	u64		ecap;
 	u32		vgsts;	/* Virtual GSTS register */
+	u64		viqa;  /* Virtual IQA register */
 	int		seq_id;	/* sequence id of the iommu */
 	int		agaw; /* agaw of this iommu */
 	int		msagaw; /* max sagaw of this iommu */
+	struct q_inval  _qi;    /* Queued invalidation info */
+	struct q_inval  *qi;    /* Pointer to _qi. Enables host code re-use */
 	pkvm_spinlock_t lock;
 };
 #endif /* !__PKVM_HYP__ */

commit dafada544fb628e90d4be46b65e835c63b4d846e

iommu/vt-d: pKVM: Fix intel_iommu_init_qi() to return 0 on success
The 'ret' holds the return value from dmar_enable_qi(). As the pKVM
relies on QI, 'ret' means a real error value in this case and returns
earlier. But if pKVM is not enabled, the IOMMU driver can still fallback
to use the traditional way to flush context and iotlb. So returns 0
explicitly on success.

Fixes: 7c0ba1255a61 ("iommu/vt-d: pKVM: Initialize Queued Invalidation (QI)")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

drivers/iommu/intel/iommu.c
@@ -2250,7 +2250,7 @@ static int device_def_domain_type(struct device *dev)
 
 static int intel_iommu_init_qi(struct intel_iommu *iommu)
 {
-	int ret = 0;
+	int ret;
 
 	/*
 	 * Start from the sane iommu hardware state.
@@ -2292,7 +2292,7 @@ static int intel_iommu_init_qi(struct intel_iommu *iommu)
 		pr_info("%s: Using Queued invalidation\n", iommu->name);
 	}
 
-	return ret;
+	return 0;
 }
 
 static int copy_context_table(struct intel_iommu *iommu,
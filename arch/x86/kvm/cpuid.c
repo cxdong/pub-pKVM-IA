cxdong/pub-pKVM-IA

commit e8beed186268688fe0232d74a4066e780d333954

pKVM: VMX: Handle EXIT_REASON_CPUID
As the pKVM hypervisor maintains the guest CPUID entries, the
EXIT_REASON_CPUID is handled alone inside the pKVM hypervisor.

The guest should have a way to detect it is running in a pVM or a npVM
so that it can enable the pVM's enlightenment, emulating the pVM's CPUID
leaf KVM_CPUID_SIGNATURE with the signature "PKVMPKVMPKVM".

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/cpuid.c
@@ -258,6 +258,25 @@ static u32 kvm_apply_cpuid_pv_features_quirk(struct kvm_vcpu *vcpu)
 		 * the corresponding CPUID.
 		 */
 		best->eax = 0;
+#ifdef __PKVM_HYP__
+		best = kvm_find_cpuid_entry(vcpu, KVM_CPUID_SIGNATURE);
+		if (best) {
+			/*
+			 * TODO:
+			 * Overriding the KVM_CPUID_SIGNATURE leaf with
+			 * "PKVMPKVMPKVM" is to make the guest aware that it is
+			 * running with the protection from the pKVM hypervisor
+			 * so it has to enable some enlightenment.
+			 * To achieve this, is there any better solution rather
+			 * than changing the KVM_CPUID_SIGNATURE leaf?
+			 */
+			const u32 *sigptr = (const u32 *)"PKVMPKVMPKVM";
+
+			best->ebx = sigptr[0];
+			best->ecx = sigptr[1];
+			best->edx = sigptr[2];
+		}
+#endif
 		return 0;
 	}
 
@@ -1994,6 +2013,7 @@ out_free:
 	kvfree(array.entries);
 	return r;
 }
+#endif /* !__PKVM_HYP__ */
 
 /*
  * Intel CPUID semantics treats any query for an out-of-range leaf as if the
@@ -2092,6 +2112,7 @@ bool kvm_cpuid(struct kvm_vcpu *vcpu, u32 *eax, u32 *ebx,
 			    !kvm_msr_read(vcpu, MSR_IA32_TSX_CTRL, &data) &&
 			    (data & TSX_CTRL_CPUID_CLEAR))
 				*ebx &= ~(feature_bit(RTM) | feature_bit(HLE));
+#ifndef __PKVM_HYP__
 		} else if (function == 0x80000007) {
 			if (kvm_hv_invtsc_suppressed(vcpu))
 				*edx &= ~feature_bit(CONSTANT_TSC);
@@ -2111,6 +2132,7 @@ bool kvm_cpuid(struct kvm_vcpu *vcpu, u32 *eax, u32 *ebx,
 			} else if (index == 2) {
 				*eax = vcpu->arch.hw_tsc_khz;
 			}
+#endif
 		}
 	} else {
 		*eax = *ebx = *ecx = *edx = 0;
@@ -2152,7 +2174,8 @@ int kvm_emulate_cpuid(struct kvm_vcpu *vcpu)
 	return kvm_skip_emulated_instruction(vcpu);
 }
 EXPORT_SYMBOL_FOR_KVM_INTERNAL(kvm_emulate_cpuid);
-#else  /* !__PKVM_HYP__ */
+
+#ifdef __PKVM_HYP__
 
 static DEFINE_PER_CPU(struct kvm_cpuid_entry2, cpuid_def[KVM_MAX_CPUID_ENTRIES]);
 
@@ -2405,4 +2428,4 @@ int pkvm_enforce_cpuid(struct kvm_cpuid_entry2 *e2, int *nent, int max_nent)
 
 	return 0;
 }
-#endif /* !__PKVM_HYP__ */
+#endif /* __PKVM_HYP__ */

arch/x86/kvm/vmx/vmx.c
@@ -6678,8 +6678,8 @@ static int (*kvm_vmx_exit_handlers[])(struct kvm_vcpu *vcpu) = {
 	[EXIT_REASON_IO_INSTRUCTION]          = handle_io,
 	[EXIT_REASON_CR_ACCESS]               = handle_cr,
 	[EXIT_REASON_DR_ACCESS]               = handle_dr,
-#ifndef __PKVM_HYP__
 	[EXIT_REASON_CPUID]                   = kvm_emulate_cpuid,
+#ifndef __PKVM_HYP__
 	[EXIT_REASON_MSR_READ]                = kvm_emulate_rdmsr,
 	[EXIT_REASON_MSR_WRITE]               = kvm_emulate_wrmsr,
 	[EXIT_REASON_INTERRUPT_WINDOW]        = handle_interrupt_window,

arch/x86/kvm/x86.c
@@ -1043,7 +1043,6 @@ void kvm_queue_exception_e(struct kvm_vcpu *vcpu, unsigned nr, u32 error_code)
 }
 EXPORT_SYMBOL_FOR_KVM_INTERNAL(kvm_queue_exception_e);
 
-#ifndef __PKVM_HYP__
 /*
  * Checks if cpl <= required_cpl; if true, return true.  Otherwise queue
  * a #GP and return false.
@@ -1055,7 +1054,6 @@ bool kvm_require_cpl(struct kvm_vcpu *vcpu, int required_cpl)
 	kvm_queue_exception_e(vcpu, GP_VECTOR, 0);
 	return false;
 }
-#endif /* !__PKVM_HYP__ */
 
 bool kvm_require_dr(struct kvm_vcpu *vcpu, int dr)
 {

commit e8936bcc302705f2e4cf67f9fcdfb8e75be8cee4

pKVM: x86: Fix KVM CPUID signature lookup to use dynamic base
kvm_apply_cpuid_pv_features_quirk() uses KVM_CPUID_SIGNATURE
(0x40000000) to find the KVM signature leaf. However, the base may be
relocated. Use kvm_cpuid.base | KVM_CPUID_SIGNATURE to look up the
correct leaf with the dynamically detected base address.

Fixes: e8beed186268 ("pKVM: VMX: Handle EXIT_REASON_CPUID")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/cpuid.c
@@ -263,7 +263,7 @@ static u32 kvm_apply_cpuid_pv_features_quirk(struct kvm_vcpu *vcpu)
 		 */
 		best->eax = 0;
 #ifdef __PKVM_HYP__
-		best = kvm_find_cpuid_entry(vcpu, KVM_CPUID_SIGNATURE);
+		best = kvm_find_cpuid_entry(vcpu, kvm_cpuid.base);
 		if (best) {
 			/*
 			 * TODO:
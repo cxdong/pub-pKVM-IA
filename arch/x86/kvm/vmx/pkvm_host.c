cxdong/pub-pKVM-IA

commit 834886ca3eb62d4b7387bfabefd0e4f9cdad269b

KVM: pVMX: Implement set_efer operation
Enable the pKVM VMX host to set the guest EFER MSR via the set_efer PV
interface. Skipping this PV interface once the guest state is marked as
protected as further changing to EFER is no longer permitted by the pKVM
hypervisor.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/vmx/pkvm_host.c
@@ -237,6 +237,17 @@ static void pkvm_update_exception_bitmap(struct kvm_vcpu *vcpu)
 		KVM_BUG_ON(pkvm_hypercall(update_exception_bitmap), vcpu->kvm);
 }
 
+static int pkvm_set_efer(struct kvm_vcpu *vcpu, u64 efer)
+{
+	int ret = -EINVAL;
+
+	if (!vcpu->arch.guest_state_protected)
+		ret = pkvm_hypercall(set_efer, efer);
+
+	vcpu->arch.efer = efer;
+	return ret;
+}
+
 struct kvm_x86_ops pkvm_host_vt_x86_ops __initdata = {
 	.name = KBUILD_MODNAME,
 
@@ -258,6 +269,7 @@ struct kvm_x86_ops pkvm_host_vt_x86_ops __initdata = {
 	.vcpu_put = pkvm_vcpu_put,
 
 	.update_exception_bitmap = pkvm_update_exception_bitmap,
+	.set_efer = pkvm_set_efer,
 };
 
 bool pkvm_interrupt_blocked(struct kvm_vcpu *vcpu)

commit f9d680009d5cb8a226a6caeb193d0ad550a2880c

pKVM: x86: Fix return value of set_efer
For the state protected guest, pkvm_set_efer was returning error code.
To make it consistent with pkvm_set_cr0/cr4, as well as the TDX's usage,
returns 0.

For the state non-protected guest, respect the return value from the
pKVM set_efer hypercall and only save the efer to vcpu when the
hypercall is successful.

Fixes: 834886ca3eb6 ("KVM: pVMX: Implement set_efer operation")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/vmx/pkvm_host.c
@@ -1133,13 +1133,15 @@ static void pkvm_set_cr4(struct kvm_vcpu *vcpu, unsigned long cr4)
 
 static int pkvm_set_efer(struct kvm_vcpu *vcpu, u64 efer)
 {
-	int ret = -EINVAL;
+	if (!vcpu->arch.guest_state_protected) {
+		int ret = pkvm_hypercall(set_efer, efer);
 
-	if (!vcpu->arch.guest_state_protected)
-		ret = pkvm_hypercall(set_efer, efer);
+		if (ret)
+			return ret;
+	}
 
 	vcpu->arch.efer = efer;
-	return ret;
+	return 0;
 }
 
 static void pkvm_get_idt(struct kvm_vcpu *vcpu, struct desc_ptr *dt)
cxdong/pub-pKVM-IA

commit 7f844b1043025597be56e196c5cf8a3f6081d227

DEBUG: pKVM: x86: Add log printing capability for debug
Introduce the kernel config option CONFIG_PKVM_X86_DEBUG to provide
support for the pKVM hypervisor to print logs by using the linux kernel
symbols. This is achieved by the pKVM hypervisor cloning the host VM's
mmu mapping to its own mmu, as well as reusing some of the host VM's CPU
state as the pKVM hypervisor's CPU state, including the percpu area.

With this, the pKVM hypervisor can use printk/trace_printk symbols to
print logs. As the symbols used by the pKVM hypervisor will be renamed
during compiling, alias these debug symbols.

As enabling CONFIG_PKVM_X86_DEBUG will allow the pKVM hypervisor to use
the linux kernel symbols, which are untrusted code as can be modified
easily by the host, the security of the pKVM hypervisor cannot be
guaranteed. So suggest to enable this only for debugging purpose.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/include/asm/pkvm_image_vars.h
@@ -1,0 +1,24 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef _ASM_x86_PKVM_IMAGE_VARS_H
+#define _ASM_x86_PKVM_IMAGE_VARS_H
+
+#ifdef CONFIG_PKVM_X86_DEBUG
+
+#include <asm/pkvm_image.h>
+/*
+ * Defines a linker script alias of a kernel-proper symbol referenced by
+ * PKVM code.
+ */
+#define PKVM_ALIAS(sym)  pkvm_sym(sym) = sym;
+
+PKVM_ALIAS(_printk);
+PKVM_ALIAS(__warn_printk);
+PKVM_ALIAS(___ratelimit);
+PKVM_ALIAS(__trace_bputs);
+PKVM_ALIAS(__trace_bprintk);
+PKVM_ALIAS(__dynamic_pr_debug);
+PKVM_ALIAS(mem_dump_obj);
+PKVM_ALIAS(vmalloc_base);
+#endif
+
+#endif /* _ASM_x86_PKVM_IMAGE_VARS_H */

arch/x86/include/asm/kvm_pkvm.h
@@ -232,6 +232,8 @@ static inline unsigned long pkvm_host_pgtable_pages(void)
 
 #ifdef __PKVM_HYP__
 
+#ifndef CONFIG_PKVM_X86_DEBUG
+
 #undef WARN_ON
 #undef WARN
 #undef WARN_ON_ONCE
@@ -253,6 +255,8 @@ static inline unsigned long pkvm_host_pgtable_pages(void)
 #define WARN_ONCE(condition, format...) WARN(condition, format)
 
 #define _BUG_FLAGS(ins, flags, extra)  asm volatile(ins)
+
+#endif /* CONFIG_PKVM_X86_DEBUG */
 
 #endif /* __PKVM_HYP__ */
 

arch/x86/kernel/vmlinux.lds.S
@@ -603,6 +603,8 @@ PROVIDE(__ref_stack_chk_guard = __stack_chk_guard);
 
 #endif /* CONFIG_X86_64 */
 
+#include <asm/pkvm_image_vars.h>
+
 /*
  * The symbols below are referenced using relative relocations in the
  * respective ELF notes. This produces build time constants that the

arch/x86/kvm/Kconfig
@@ -153,6 +153,18 @@ config PKVM_INTEL
 
 	  If unsure, say N.
 
+config PKVM_X86_DEBUG
+	bool "pKVM debugging support"
+	depends on PKVM_X86
+	help
+	  Provides support for the pKVM hypervisor to print logs by using the
+	  linux kernel symbols. With this config enabled, the security of the
+	  pKVM hypervisor cannot be guaranteed as the linux kernel symbols are
+	  untrusted code, which could be modified easily by the host. So suggest
+	  to enable this only for debugging purpose.
+
+	  If unsure, say N.
+
 config X86_SGX_KVM
 	bool "Software Guard eXtensions (SGX) Virtualization"
 	depends on X86_SGX && KVM_INTEL

arch/x86/kvm/pkvm/cpu.c
@@ -21,10 +21,14 @@ unsigned int nr_cpu_ids;
 
 unsigned int pkvm_per_cpu_nr_pages(void)
 {
+#ifndef CONFIG_PKVM_X86_DEBUG
 	unsigned long per_cpu_size = (unsigned long)__per_cpu_end -
 				     (unsigned long)__per_cpu_start;
 
 	return ALIGN(per_cpu_size, PAGE_SIZE) >> PAGE_SHIFT;
+#else
+	return 0;
+#endif
 }
 
 int pkvm_setup_per_cpu(int cpu, unsigned long base)
@@ -41,8 +45,12 @@ int pkvm_setup_per_cpu(int cpu, unsigned long base)
 	if (!vcpu)
 		return -EINVAL;
 
+#ifndef CONFIG_PKVM_X86_DEBUG
 	__per_cpu_offset[cpu] = (unsigned long)__pkvm_va(base) -
 				(unsigned long)__per_cpu_start;
+#else
+	__per_cpu_offset[cpu] = (unsigned long)__pkvm_va(base);
+#endif
 	per_cpu(this_cpu_off, cpu) = __per_cpu_offset[cpu];
 	per_cpu(cpu_number, cpu) = cpu;
 	per_cpu(phys_cpu, cpu) = pcpu;

arch/x86/kvm/pkvm/init.c
@@ -122,6 +122,15 @@ static int create_hyp_mmu(const struct pkvm_mem_info infos[], int nr_infos)
 			return ret;
 	}
 
+#ifdef CONFIG_PKVM_X86_DEBUG
+	/*
+	 * Clone host CR3 page mapping starting from VMALLOC_START for the pKVM
+	 * hypervisor to run the linux kernel's symbols in the root mode. To
+	 * reduce the differences between w/ DEBUG and w/o DEBUG, keep using
+	 * the direct mapping created by the pKVM hypervisor itself.
+	 */
+	pkvm_hyp_mmu_clone_host(VMALLOC_START);
+#else
 	/*
 	 * Map pkvm's TEXT/DATA memory. The virtual address is the same
 	 * with the kernel symbol mapping.
@@ -134,6 +143,7 @@ static int create_hyp_mmu(const struct pkvm_mem_info infos[], int nr_infos)
 				return ret;
 		}
 	}
+#endif
 
 	ret = back_vmemmap(__pkvm_pa(pkvm_vmemmap_base));
 	if (ret)
@@ -184,9 +194,23 @@ static int create_host_mmu(const struct pkvm_mem_info infos[], int nr_infos,
 	 * from the host VM.
 	 */
 	for (i = 0; i < nr_infos; i++) {
+#ifdef CONFIG_PKVM_X86_DEBUG
+		/*
+		 * Only keep the pKVM TEXT/DATA mapped in the host mmu to allow
+		 * the host to access pKVM's text and data for debugging, and
+		 * unmap all the other regions i.e., pKVM reserved memory region
+		 * which the host doesn't need to access.
+		 */
+		if (infos[i].type != PKVM_TEXT_DATA) {
+			ret = pkvm_host_mmu_unmap(infos[i].pa, infos[i].size);
+			if (ret)
+				return ret;
+		}
+#else
 		ret = pkvm_host_mmu_unmap(infos[i].pa, infos[i].size);
 		if (ret)
 			return ret;
+#endif
 	}
 
 	return 0;

arch/x86/kvm/pkvm/mmu.c
@@ -162,11 +162,20 @@ static int fix_hyp_mmu_page_refcnt(void)
 	};
 	unsigned long size;
 
+#ifdef CONFIG_PKVM_X86_DEBUG
+	/*
+	 * Only the memory addresses under VMALLOC_START are mapped by the
+	 * pKVM hypervisor itself, thus only need to fix vmmemap for this
+	 * range.
+	 */
+	size = VMALLOC_START & ~hyp_mmu.pgt_ops->level_to_mask(hyp_mmu.cap.level + 1);
+#else
 	/*
 	 * Calculate the max address space, then walk the [0, size) address
 	 * range to fixup refcount of every page-table page.
 	 */
 	size = pkvm_pgtable_max_size(&hyp_mmu);
+#endif
 
 	return pkvm_pgtable_walk(&hyp_mmu, 0, size, &walker);
 }
@@ -233,6 +242,18 @@ int pkvm_hyp_mmu_map(unsigned long vaddr, unsigned long phys,
 {
 	return pkvm_pgtable_map(&hyp_mmu, vaddr, phys, size, prot);
 }
+
+#ifdef CONFIG_PKVM_X86_DEBUG
+void pkvm_hyp_mmu_clone_host(unsigned long start_vaddr)
+{
+	int i = hyp_mmu_vaddr_to_index(start_vaddr, hyp_mmu.cap.level);
+	u64 *host_cr3 = __pkvm_va(__native_read_cr3() & PAGE_MASK);
+	u64 *ptep = __pkvm_va(hyp_mmu.root_pa);
+
+	for (; i < PTRS_PER_PTE; i++)
+		ptep[i] = host_cr3[i];
+}
+#endif
 
 int pkvm_host_mmu_init(void *pool_base, unsigned long pool_pages, host_mmu_init_fn_t fn)
 {

arch/x86/kvm/pkvm/mmu.h
@@ -13,6 +13,9 @@ void pkvm_hyp_mmu_load(void);
 int pkvm_hyp_mmu_finalize(hyp_mmu_finalize_fn_t fn);
 int pkvm_hyp_mmu_map(unsigned long vaddr, unsigned long phys,
 		     unsigned long size, u64 prot);
+#ifdef CONFIG_PKVM_X86_DEBUG
+void pkvm_hyp_mmu_clone_host(unsigned long start_vaddr);
+#endif
 
 int pkvm_host_mmu_init(void *pool_base, unsigned long pool_pages,
 		       host_mmu_init_fn_t fn);

commit 013d48d6f77af8989111bb9a0d7d881b802adaa7

pKVM: x86: Fix link issue when CONFIG_BUG=n
Wrap PKVM_ALIAS(__warn_printk) with #ifdef CONFIG_BUG as __warn_printk
is a symbol when CONFIG_BUG=y.

Fixes: 7f844b104302 ("DEBUG: pKVM: x86: Add log printing capability for debug")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/include/asm/pkvm_image_vars.h
@@ -12,7 +12,9 @@
 #define PKVM_ALIAS(sym)  pkvm_sym(sym) = sym;
 
 PKVM_ALIAS(_printk);
+#ifdef CONFIG_BUG
 PKVM_ALIAS(__warn_printk);
+#endif
 PKVM_ALIAS(___ratelimit);
 PKVM_ALIAS(__trace_bputs);
 PKVM_ALIAS(__trace_bprintk);
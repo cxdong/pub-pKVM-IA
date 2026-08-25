cxdong/pub-pKVM-IA

commit a457c4e1edfec2af73253b87a841bc39fd9a7a0c

pKVM: x86: trace: Support printing the guest VM trace
Extend the vmexit_trace_show() for the debug file to print the guest
VM's trace. If a debug file has a struct kvm as its private data, it
means catting this debug file will show the trace of a specific guest
VM. Otherwise it will show the host VM trace and the trace of all guest
VMs.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm_debugfs.c
@@ -74,17 +74,25 @@ static int print_perf_data(struct seq_file *m, struct perf_data *summary,
 	struct perf_data *print = pervcpu ? pervcpu : summary;
 	struct vmexit_stats *stats, *summary_stats;
 	const char *prefix;
-	int *vcpu_id;
+	int vm_handle;
 	int i;
 
 	if (!print)
 		return -EINVAL;
 
-	vcpu_id = (print == pervcpu) ? &pervcpu->vcpu_id : NULL;
-	if (vcpu_id)
-		prefix = kasprintf(GFP_KERNEL, "Host-vcpu%d: ", *vcpu_id);
-	else
-		prefix = kasprintf(GFP_KERNEL, "Host: ");
+	vm_handle = print->vm_handle;
+	if (print == pervcpu) {
+		if (vm_handle == PKVM_HOST_VM_HANDLE)
+			prefix = kasprintf(GFP_KERNEL, "Host-vcpu%d: ", print->vcpu_id);
+		else
+			prefix = kasprintf(GFP_KERNEL, "VM%d-vcpu%d: ",
+					   vm_handle, print->vcpu_id);
+	} else {
+		if (vm_handle == PKVM_HOST_VM_HANDLE)
+			prefix = kasprintf(GFP_KERNEL, "Host: ");
+		else
+			prefix = kasprintf(GFP_KERNEL, "VM%d: ", vm_handle);
+	}
 
 	if (!prefix)
 		return -ENOMEM;
@@ -98,8 +106,9 @@ static int print_perf_data(struct seq_file *m, struct perf_data *summary,
 	for_each_stats(vmexit_reasons, MAX_EXIT_REASONS)
 		__print_perf_data(m, prefix, get_vmexit_reason(i), stats, summary_stats);
 
-	for_each_stats(hypercalls, MAX_PKVM_HYPERCALLS)
-		__print_perf_data(m, prefix, pkvm_hypercalls[i], stats, summary_stats);
+	if (vm_handle == PKVM_HOST_VM_HANDLE)
+		for_each_stats(hypercalls, MAX_PKVM_HYPERCALLS)
+			__print_perf_data(m, prefix, pkvm_hypercalls[i], stats, summary_stats);
 
 #undef for_each_stats
 
@@ -108,48 +117,120 @@ static int print_perf_data(struct seq_file *m, struct perf_data *summary,
 	return 0;
 }
 
+static int dump_host_vmexit_trace(struct seq_file *m, struct perf_data *summary,
+				  struct perf_data **dump, unsigned long *size)
+{
+	struct perf_data *pervcpu;
+	int cpu, ret;
+
+	for (cpu = 0, pervcpu = *dump;
+	     cpu < num_possible_cpus() && *size >= sizeof(struct perf_data);
+	     cpu++, *size -= sizeof(struct perf_data), pervcpu++, *dump = pervcpu) {
+		/*
+		 * The trace data from the pKVM hypervisor is grouped by VM.
+		 * If the vm_handle in this perf data is not for the host, it
+		 * means all the perf data for the host are already printed.
+		 */
+		if (pervcpu->vm_handle != PKVM_HOST_VM_HANDLE)
+			break;
+
+		ret = print_perf_data(m, summary, pervcpu);
+		if (ret)
+			return ret;
+
+		cond_resched();
+	}
+
+	return print_perf_data(m, summary, NULL);
+}
+
+static int dump_guest_vmexit_trace(struct seq_file *m, struct perf_data *summary,
+				   struct perf_data **dump, unsigned long *size)
+{
+	struct perf_data *pervcpu;
+	int ret;
+
+	for (pervcpu = *dump;
+	     *size >= sizeof(struct perf_data);
+	     *size -= sizeof(struct perf_data), pervcpu++, *dump = pervcpu) {
+		/*
+		 * The trace data from the pKVM hypervisor is grouped by VM.
+		 * If the vm_handle in this perf data is not for this guest VM,
+		 * it means all the perf data for this guest VM are already
+		 * printed.
+		 */
+		if (pervcpu->vm_handle != summary->vm_handle)
+			break;
+
+		ret = print_perf_data(m, summary, pervcpu);
+		if (ret)
+			return ret;
+
+		cond_resched();
+	}
+
+	return print_perf_data(m, summary, NULL);
+}
+
 static int pkvm_dump_vmexit_trace(struct seq_file *m, struct perf_data *dump,
 				  unsigned long size)
 {
 	struct perf_data *summary = kmalloc(sizeof(struct perf_data), GFP_KERNEL_ACCOUNT);
-	struct perf_data *pervcpu;
-	int cpu, ret;
+	int ret;
 
 	if (!summary) {
 		pr_err("failed to allocate perf summary buffer\n");
 		return -ENOMEM;
 	}
 
-	for (cpu = 0, pervcpu = dump;
-	     cpu < num_possible_cpus() && size >= sizeof(struct perf_data);
-	     cpu++, size -= sizeof(struct perf_data), pervcpu++) {
-		ret = print_perf_data(m, summary, pervcpu);
-		if (ret)
-			goto out;
+	while (size >= sizeof(struct perf_data)) {
+		memset(summary, 0, sizeof(struct perf_data));
+		summary->vm_handle = dump->vm_handle;
 
-		cond_resched();
+		if (dump->vm_handle == PKVM_HOST_VM_HANDLE)
+			ret = dump_host_vmexit_trace(m, summary, &dump, &size);
+		else
+			ret = dump_guest_vmexit_trace(m, summary, &dump, &size);
+
+		if (ret) {
+			pr_err("failed to dump vmexit trace for VM handle 0x%x\n",
+			       dump->vm_handle);
+			break;
+		}
 	}
 
-	ret = print_perf_data(m, summary, NULL);
-out:
 	kfree(summary);
 	return ret;
 }
 
 static int vmexit_trace_show(struct seq_file *m, void *unused)
 {
+	struct kvm *kvm = (struct kvm *)m->private;
 	struct perf_data *perf;
 	unsigned long size;
-	int ret;
+	int ret, vm_handle;
 
-	size = sizeof(struct perf_data) * num_possible_cpus();
+	if (kvm) {
+		/* Dump vmexit trace for a specific VM */
+		size = atomic_read(&kvm->online_vcpus) * sizeof(struct perf_data);
+		vm_handle = kvm->arch.pkvm.handle;
+	} else {
+		/* Dump vmexit trace for all VMs including the host VM */
+		size = sizeof(struct perf_data) * num_possible_cpus();
+		mutex_lock(&kvm_lock);
+		list_for_each_entry(kvm, &vm_list, vm_list)
+			size += atomic_read(&kvm->online_vcpus) * sizeof(struct perf_data);
+		mutex_unlock(&kvm_lock);
+		vm_handle = PKVM_HOST_VM_HANDLE;
+	}
+
 	perf = alloc_pages_exact(size, GFP_KERNEL_ACCOUNT);
 	if (!perf) {
 		pr_err("failed to allocate perf buffer\n");
 		return -ENOMEM;
 	}
 
-	ret = pkvm_hypercall(dump_vmexit_trace, __pa(perf), size);
+	ret = pkvm_hypercall(dump_vmexit_trace, __pa(perf), size, vm_handle);
 	if (ret) {
 		pr_err("failed to get vmexit trace: err %d\n", ret);
 		goto out;

commit c63cc3ed3f62b6ca0b43b12bd302033ce7612c0a

pKVM: x86: trace: Check size to prevent allocating zero bytes
When dump vmexit trace for a specific VM, it is possible that the size
calculation may return 0 if there is zero online vcpu. The original code
will allocate perf buffer with zero size which is a wast. So add the
size==0 early return to avoid allocating zero bytes.

Fixes: a457c4e1edfe ("pKVM: x86: trace: Support printing the guest VM trace")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm_debugfs.c
@@ -208,6 +208,9 @@ static int vmexit_trace_show(struct seq_file *m, void *unused)
 		vm_handle = PKVM_HOST_VM_HANDLE;
 	}
 
+	if (!size)
+		return 0;
+
 	perf = alloc_pages_exact(size, GFP_KERNEL_ACCOUNT);
 	if (!perf) {
 		pr_err("failed to allocate perf buffer\n");
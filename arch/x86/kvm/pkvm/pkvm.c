cxdong/pub-pKVM-IA

commit af17a56e15c058fd57ce76ea47ec06861a1091f2

pKVM: x86: Add vm_destroy PV interface
Add vm_destroy PV interface to destroy a guest VM. This interface
releases the pkvm_vm handle, and involves the vendor-specific operation
to destroy vendor resources. It also tears down donated memory pages and
unshares the host KVM structure.

The physical address and size of the torn-down memory pages are stored
in the pkvm_memcache structure, which will be returned for the host to
reclaim.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -82,6 +82,7 @@ static struct pkvm_vm *free_pkvm_vm_handle(int handle)
 
 	pkvm_spin_lock(&pkvm_vms_lock);
 
+	idx = array_index_nospec(idx, MAX_PKVM_VMS);
 	pkvm_vm_ref = &pkvm_vms_ref[idx];
 	if (atomic_cmpxchg(&pkvm_vm_ref->refcount, 1, 0) != 1) {
 		pkvm_err("VM%d is busy, refcount %d\n", handle,
@@ -156,6 +157,42 @@ unshare:
 	return ret;
 }
 
+static void teardown_donated_memory(struct pkvm_memcache *mc, void *addr, size_t size)
+{
+	BUG_ON(!PAGE_ALIGNED(addr) || !PAGE_ALIGNED(size));
+
+	pkvm_clear_memory(addr, size);
+
+	push_pkvm_memcache(mc, addr, size, pkvm_virt_to_host_gpa);
+
+	/*
+	 * Sensitive data in this memory range has been already cleared
+	 * by pkvm_clear_memory(). Now this memory is used to store the
+	 * information about the memory pages for the host to free by
+	 * push_pkvm_memcache(), so undonate without clearing.
+	 */
+	pkvm_hyp_donate_host(__pkvm_pa(addr), size, false);
+}
+
+static void pkvm_vm_destroy(int vm_handle, struct pkvm_memcache *mc)
+{
+	struct pkvm_vm *pkvm_vm = free_pkvm_vm_handle(vm_handle);
+	unsigned long shared_kvm_pa;
+
+	if (!pkvm_vm)
+		return;
+
+	memset(mc, 0, sizeof(*mc));
+
+	shared_kvm_pa = __pkvm_pa(pkvm_vm->shared_kvm);
+
+	kvm_x86_call(vm_destroy)(&pkvm_vm->kvm);
+
+	teardown_donated_memory(mc, (void *)pkvm_vm, pkvm_vm->size);
+
+	pkvm_host_unshare_hyp(shared_kvm_pa, kvm_x86_ops.vm_size);
+}
+
 void pkvm_handle_host_hypercall(struct kvm_vcpu *vcpu)
 {
 	enum pkvm_hc hc = pkvm_hc(vcpu);
@@ -189,6 +226,9 @@ void pkvm_handle_host_hypercall(struct kvm_vcpu *vcpu)
 	case __pkvm__vm_init:
 		ret = pkvm_vm_init(pkvm_host_gpa_to_phys(pkvm_hc_input1(vcpu)),
 				   pkvm_host_gpa_to_phys(pkvm_hc_input2(vcpu)));
+		break;
+	case __pkvm__vm_destroy:
+		pkvm_vm_destroy(pkvm_hc_input1(vcpu), &out.vm_destroy.memcache);
 		break;
 	default:
 		ret = -EINVAL;

arch/x86/include/asm/kvm_pkvm.h
@@ -65,6 +65,9 @@ enum pkvm_hc {
 
 union pkvm_hc_data {
 	struct {
+		struct pkvm_memcache memcache;
+	} vm_destroy;
+	struct {
 		u64 data[PKVM_HC_DATA_MAX_NUM];
 	} raw;
 };

arch/x86/include/asm/pkvm_hypercalls.h
@@ -20,6 +20,7 @@ PKVM_HC(dump_vmexit_trace)
 PKVM_HC(check_processor_compatibility)
 PKVM_HC(enable_virtualization_cpu)
 PKVM_HC(vm_init)
+PKVM_HC_OUT(vm_destroy)
 
 #undef PKVM_HC
 #undef PKVM_HC_OUT

commit aff6e69c0d554af44533e0e6833017d6fee08192

pKVM: x86: Propagate error from pkvm_vm_destroy() to host
pkvm_vm_destroy() silently returns void even when the VM handle is
invalid or VM is busy. Make free_pkvm_vm_handle() to return an error
pointer for the failures, then pkvm_vm_destroy() extracts the error code
from the error pointer and propagate to the host hypercall handler.

Fixes: af17a56e15c0 ("pKVM: x86: Add vm_destroy PV interface")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -104,11 +104,11 @@ static int allocate_pkvm_vm_handle(struct pkvm_vm *pkvm_vm)
 static struct pkvm_vm *free_pkvm_vm_handle(int handle)
 {
 	struct pkvm_vm_ref *pkvm_vm_ref;
-	struct pkvm_vm *pkvm_vm = NULL;
+	struct pkvm_vm *pkvm_vm;
 	int idx = handle;
 
 	if (idx < 0 || idx >= MAX_PKVM_VMS)
-		return NULL;
+		return ERR_PTR(-EINVAL);
 
 	pkvm_spin_lock(&pkvm_vms_lock);
 
@@ -117,14 +117,16 @@ static struct pkvm_vm *free_pkvm_vm_handle(int handle)
 	if (atomic_cmpxchg(&pkvm_vm_ref->refcount, 1, 0) != 1) {
 		pkvm_err("VM%d is busy, refcount %d\n", handle,
 			 atomic_read(&pkvm_vm_ref->refcount));
-		goto out;
+		pkvm_spin_unlock(&pkvm_vms_lock);
+		return ERR_PTR(-EBUSY);
 	}
 
 	pkvm_vm = pkvm_vm_ref->pkvm_vm;
+	BUG_ON(!pkvm_vm);
 	pkvm_vm_ref->pkvm_vm = NULL;
 
 	__clear_bit(idx, pkvm_vms_bitmap);
-out:
+
 	pkvm_spin_unlock(&pkvm_vms_lock);
 	return pkvm_vm;
 }
@@ -213,14 +215,14 @@ static void teardown_donated_memory(struct pkvm_memcache *mc, void *addr, size_t
 	pkvm_hyp_donate_host(__pkvm_pa(addr), size, false);
 }
 
-static void pkvm_vm_destroy(int vm_handle, struct pkvm_memcache *mc)
+static int pkvm_vm_destroy(int vm_handle, struct pkvm_memcache *mc)
 {
 	struct pkvm_vm *pkvm_vm = free_pkvm_vm_handle(vm_handle);
 	unsigned long shared_kvm_pa;
 	int i;
 
-	if (!pkvm_vm)
-		return;
+	if (IS_ERR(pkvm_vm))
+		return PTR_ERR(pkvm_vm);
 
 	memset(mc, 0, sizeof(*mc));
 
@@ -241,6 +243,8 @@ static void pkvm_vm_destroy(int vm_handle, struct pkvm_memcache *mc)
 	teardown_donated_memory(mc, (void *)pkvm_vm, pkvm_vm->size);
 
 	pkvm_host_unshare_hyp(shared_kvm_pa, kvm_x86_ops.vm_size);
+
+	return 0;
 }
 
 static int attach_pkvm_vcpu_to_vm(struct pkvm_vm *pkvm_vm, struct pkvm_vcpu *pkvm_vcpu)
@@ -2098,7 +2102,7 @@ void pkvm_handle_host_hypercall(struct kvm_vcpu *vcpu)
 		ret = pkvm_vm_finalize(pkvm_hc_input1(vcpu));
 		break;
 	case __pkvm__vm_destroy:
-		pkvm_vm_destroy(pkvm_hc_input1(vcpu), &out.vm_destroy.memcache);
+		ret = pkvm_vm_destroy(pkvm_hc_input1(vcpu), &out.vm_destroy.memcache);
 		break;
 	case __pkvm__vcpu_create:
 		ret = pkvm_vcpu_create(pkvm_hc_input1(vcpu),
cxdong/pub-pKVM-IA

commit f9256c367e587d0757ae134d806a2d5a57680d2e

KVM: pVMX: Implement vcpu_run operation
Enable the pKVM VMX host to run guest vCPU via the vcpu_run PV
interface. The vmexits from the guest may cause the PV interface returns
back to the host if such vmexit cannot be handled alone by the pKVM
hypervisor. For the NMI and machine check, they will be handled similar
as the KVM VMX host. The other vmexit handlers will be added by the
subsequent patches.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/vmx/pkvm_host.c
@@ -3,6 +3,7 @@
 
 #include <linux/kvm_host.h>
 #include <asm/kvm_pkvm.h>
+#include <asm/fred.h>
 #include "pkvm_constants.h"
 #include "posted_intr.h"
 #include "trace.h"
@@ -120,6 +121,12 @@ static void pkvm_cache_segment(struct vcpu_vmx *vmx, struct kvm_segment *var, in
 		   (var->s << 4)	 |
 		   var->type;
 	pkvm_segment_cache_set(vmx, seg, SEG_FIELD_AR);
+}
+
+static fastpath_t pkvm_exit_handlers_fastpath(struct kvm_vcpu *vcpu)
+{
+	/* TODO Handle vmexit for fastpath */
+	return EXIT_FASTPATH_NONE;
 }
 
 static int pkvm_check_processor_compat(void)
@@ -776,6 +783,82 @@ static void pkvm_flush_tlb_guest(struct kvm_vcpu *vcpu)
 		KVM_BUG_ON(pkvm_hypercall(flush_tlb_guest), vcpu->kvm);
 }
 
+static fastpath_t pkvm_vcpu_run(struct kvm_vcpu *vcpu, u64 run_flags)
+{
+	bool force_immediate_exit = run_flags & KVM_RUN_FORCE_IMMEDIATE_EXIT;
+	struct vcpu_vmx *vmx = to_vmx(vcpu);
+	unsigned long reqs_to_host;
+	fastpath_t exit_fastpath;
+	union pkvm_hc_data out;
+
+	trace_kvm_entry(vcpu, force_immediate_exit);
+
+	kvm_wait_lapic_expire(vcpu);
+
+	guest_state_enter_irqoff();
+
+	vcpu->arch.nmi_injected = false;
+	kvm_clear_exception_queue(vcpu);
+	kvm_clear_interrupt_queue(vcpu);
+
+	vmx->vt.exit_reason.full = 0xdead;
+	vmx->fail = 0;
+
+	vcpu->arch.regs_avail &= ~VMX_REGS_LAZY_LOAD_SET;
+	if (pkvm_hypercall_out(vcpu_run, &out, force_immediate_exit)) {
+		vmx->fail = 1;
+		guest_state_exit_irqoff();
+		return EXIT_FASTPATH_NONE;
+	}
+
+	reqs_to_host = out.vcpu_run.reqs_to_host;
+	vcpu->arch.regs_dirty = 0;
+
+	/*
+	 * The host still needs to pre-configure pVM's vCPU state for booting.
+	 * Once the vcpu has started running, some PV interfaces will be
+	 * inaccessible to the host. Setting the guest_state_protected flag for
+	 * the host to avoid using such PV interface.
+	 */
+	if (unlikely(vcpu->kvm->arch.has_protected_state &&
+		     !vcpu->arch.guest_state_protected)) {
+		vcpu->arch.guest_state_protected = true;
+		fpstate_set_confidential(&vcpu->arch.guest_fpu);
+	}
+
+	if (unlikely(vmx_get_exit_reason(vcpu).full == 0xdead))
+		vmx->fail = 1;
+	else
+		vmx_handle_nmi(vcpu);
+
+	guest_state_exit_irqoff();
+
+	if (unlikely(vmx->fail))
+		return EXIT_FASTPATH_NONE;
+
+	if (unlikely((u16)vmx_get_exit_reason(vcpu).basic == EXIT_REASON_MCE_DURING_VMENTRY))
+		kvm_machine_check();
+
+	trace_kvm_exit(vcpu, KVM_ISA_VMX);
+
+	if (unlikely(vmx_get_exit_reason(vcpu).failed_vmentry))
+		return EXIT_FASTPATH_NONE;
+
+	if (vcpu->arch.exception.pending || vcpu->arch.exception.injected)
+		kvm_make_request(KVM_REQ_EVENT, vcpu);
+
+	exit_fastpath = EXIT_FASTPATH_EXIT_HANDLED;
+	if (reqs_to_host) {
+		if (test_and_clear_bit(HOST_HANDLE_EXIT, &reqs_to_host))
+			exit_fastpath = EXIT_FASTPATH_NONE;
+	}
+
+	if (exit_fastpath == EXIT_FASTPATH_EXIT_HANDLED)
+		return exit_fastpath;
+
+	return pkvm_exit_handlers_fastpath(vcpu);
+}
+
 static void pkvm_set_interrupt_shadow(struct kvm_vcpu *vcpu, int mask)
 {
 	if (!pkvm_is_protected_vcpu(vcpu))
@@ -1104,6 +1187,7 @@ struct kvm_x86_ops pkvm_host_vt_x86_ops __initdata = {
 	.flush_tlb_gva = pkvm_flush_tlb_gva,
 	.flush_tlb_guest = pkvm_flush_tlb_guest,
 
+	.vcpu_run = pkvm_vcpu_run,
 	.set_interrupt_shadow = pkvm_set_interrupt_shadow,
 	.get_interrupt_shadow = pkvm_get_interrupt_shadow,
 	.inject_irq = pkvm_inject_irq,

commit e58beba9d66923b61b0830a6a6cdc53c8d0de138

KVM: pVMX: Factor guest_state enter/exit into noinstr helper
pkvm_vcpu_run() calls guest_state_enter_irqoff() and
guest_state_exit_irqoff() but is not marked noinstr. Between those two
calls the function invokes instrumentable code (kvm_clear_exception_queue(),
kvm_clear_interrupt_queue(), fpstate_set_confidential(), and conditional
field accesses). Instrumentation callbacks (ftrace, KASAN, KCOV) can
fire in this window while RCU considers the CPU to be in a guest
quiescent state, potentially corrupting context tracking state. The
standard VMX and TDX paths avoid this by factoring the critical region
into a dedicated noinstr helper (vmx_vcpu_enter_exit(),
tdx_vcpu_enter_exit()).

Factor the guest_state enter/exit region out of pkvm_vcpu_run() into a
new static noinstr helper pkvm_vcpu_enter_exit(). Move instrumentable
calls (nmi_injected reset, queue clears) before pkvm_vcpu_enter_exit().
The noinstr region now contains only plain struct writes, the inlined
hypercall asm, and vmx_handle_nmi() which is already noinstr.

Also removed unused header file asm/fred.h as the pkvm_host.c calls the
vmx symbols to handle interrupts/nmi and doesn't directly calls the fred
symbols.

Fixes: f9256c367e58 ("KVM: pVMX: Implement vcpu_run operation")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/vmx/pkvm_host.c
@@ -3,7 +3,6 @@
 
 #include <linux/kvm_host.h>
 #include <asm/kvm_pkvm.h>
-#include <asm/fred.h>
 #include "pkvm_constants.h"
 #include "posted_intr.h"
 #include "trace.h"
@@ -1335,6 +1334,33 @@ static int pkvm_vcpu_pre_run(struct kvm_vcpu *vcpu)
 	return 1;
 }
 
+static noinstr void pkvm_vcpu_enter_exit(struct kvm_vcpu *vcpu, u64 run_flags,
+					 union pkvm_hc_data *out)
+{
+	bool force_immediate_exit = run_flags & KVM_RUN_FORCE_IMMEDIATE_EXIT;
+	struct vcpu_vmx *vmx = to_vmx(vcpu);
+
+	guest_state_enter_irqoff();
+
+	vmx->vt.exit_reason.full = 0xdead;
+	vcpu->arch.regs_avail &= ~VMX_REGS_LAZY_LOAD_SET;
+
+	vmx->fail = !!pkvm_hypercall_out(vcpu_run, out, force_immediate_exit);
+	if (unlikely(vmx->fail))
+		goto done;
+
+	if (unlikely(vmx_get_exit_reason(vcpu).full == 0xdead)) {
+		vmx->fail = 1;
+		goto done;
+	}
+
+	vcpu->arch.regs_dirty = 0;
+
+	vmx_handle_nmi(vcpu);
+done:
+	guest_state_exit_irqoff();
+}
+
 static fastpath_t pkvm_vcpu_run(struct kvm_vcpu *vcpu, u64 run_flags)
 {
 	bool force_immediate_exit = run_flags & KVM_RUN_FORCE_IMMEDIATE_EXIT;
@@ -1347,24 +1373,13 @@ static fastpath_t pkvm_vcpu_run(struct kvm_vcpu *vcpu, u64 run_flags)
 
 	kvm_wait_lapic_expire(vcpu);
 
-	guest_state_enter_irqoff();
-
 	vcpu->arch.nmi_injected = false;
 	kvm_clear_exception_queue(vcpu);
 	kvm_clear_interrupt_queue(vcpu);
 
-	vmx->vt.exit_reason.full = 0xdead;
-	vmx->fail = 0;
-
-	vcpu->arch.regs_avail &= ~VMX_REGS_LAZY_LOAD_SET;
-	if (pkvm_hypercall_out(vcpu_run, &out, force_immediate_exit)) {
-		vmx->fail = 1;
-		guest_state_exit_irqoff();
+	pkvm_vcpu_enter_exit(vcpu, run_flags, &out);
+	if (unlikely(vmx->fail))
 		return EXIT_FASTPATH_NONE;
-	}
-
-	reqs_to_host = out.vcpu_run.reqs_to_host;
-	vcpu->arch.regs_dirty = 0;
 
 	/*
 	 * The host still needs to pre-configure pVM's vCPU state for booting.
@@ -1392,16 +1407,6 @@ static fastpath_t pkvm_vcpu_run(struct kvm_vcpu *vcpu, u64 run_flags)
 		}
 	}
 
-	if (unlikely(vmx_get_exit_reason(vcpu).full == 0xdead))
-		vmx->fail = 1;
-	else
-		vmx_handle_nmi(vcpu);
-
-	guest_state_exit_irqoff();
-
-	if (unlikely(vmx->fail))
-		return EXIT_FASTPATH_NONE;
-
 	if (unlikely((u16)vmx_get_exit_reason(vcpu).basic == EXIT_REASON_MCE_DURING_VMENTRY))
 		kvm_machine_check();
 
@@ -1415,6 +1420,7 @@ static fastpath_t pkvm_vcpu_run(struct kvm_vcpu *vcpu, u64 run_flags)
 		kvm_make_request(KVM_REQ_EVENT, vcpu);
 
 	exit_fastpath = EXIT_FASTPATH_EXIT_HANDLED;
+	reqs_to_host = out.vcpu_run.reqs_to_host;
 	if (reqs_to_host) {
 		if (test_and_clear_bit(HOST_HANDLE_EXIT, &reqs_to_host))
 			exit_fastpath = EXIT_FASTPATH_NONE;
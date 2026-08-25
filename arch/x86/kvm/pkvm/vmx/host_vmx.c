cxdong/pub-pKVM-IA

commit 11b8efc1c43d7b5aac19672472220e6618cedc6c

pKVM: VMX: Improve the host MSR vmexit handler
With the VMX MSR bitmap support, the MSR which the corresponding bit in
the bitmap is 1 will cause the vmexit to the pKVM. So if an MSR in the
bitmap, it should be emulated otherwise it is a code bug.

If an MSR out of the range of 00000000 ~ 00001FFFF and C00000000 ~
C0001FFF, accessing will cause vmexit unconditionally which was handled
by injecting #GP to the host. However such MSR may be implemented by the
platform. So to make sure these MSR can be emulated properly, use the
safe version MSR accessing wrapper to access this MSR first. If it is
supported, pass through the accessing result to the host. Otherwise
inject the #GP exception.

The symbols enabled by CONFIG_PARAVIRT/_XXL/_SPINLOCKS can be removed by
undefine these configs in the undef.h. However the symbols enabled by
CONFIG_TRACEPOINTS cannot be removed similarly due to the same reason
with CONFIG_BUG/CONFIG_GENERIC_BUG. Directly modify the corresponding
kernel's header files to remove these symbols for the pKVM hypervisor.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/vmx/host_vmx.c
@@ -64,16 +64,55 @@ static void handle_cr(struct kvm_vcpu *vcpu)
 	}
 }
 
+static bool is_msr_in_bitmap_range(unsigned long msr)
+{
+	return msr <= 0x1FFF || (msr >= 0xC0000000 && msr <= 0xC0001FFF);
+}
+
 static int handle_read_msr(struct kvm_vcpu *vcpu)
 {
-	kvm_inject_gp(vcpu, 0);
-	return X86EMUL_UNHANDLEABLE;
+	unsigned long msr = vcpu->arch.regs[VCPU_REGS_RCX];
+	u32 low, high;
+
+	/*
+	 * The MSR reading bitmap doesn't intercept any MSR. If the vmexit is
+	 * caused by such MSR in the range of the bitmap, it should be a code
+	 * bug.
+	 */
+	BUG_ON(is_msr_in_bitmap_range(msr));
+
+	if (rdmsr_safe(msr, &low, &high)) {
+		kvm_inject_gp(vcpu, 0);
+		return X86EMUL_UNHANDLEABLE;
+	}
+
+	vcpu->arch.regs[VCPU_REGS_RAX] = low;
+	vcpu->arch.regs[VCPU_REGS_RDX] = high;
+
+	return X86EMUL_CONTINUE;
 }
 
 static int handle_write_msr(struct kvm_vcpu *vcpu)
 {
-	kvm_inject_gp(vcpu, 0);
-	return X86EMUL_UNHANDLEABLE;
+	unsigned long msr = vcpu->arch.regs[VCPU_REGS_RCX];
+	u32 low, high;
+
+	/*
+	 * The MSR writing bitmap doesn't intercept any MSR. If the vmexit is
+	 * caused by such MSR in the range of the bitmap, it should be a code
+	 * bug.
+	 */
+	BUG_ON(is_msr_in_bitmap_range(msr));
+
+	low = vcpu->arch.regs[VCPU_REGS_RAX];
+	high = vcpu->arch.regs[VCPU_REGS_RDX];
+
+	if (wrmsr_safe(msr, low, high)) {
+		kvm_inject_gp(vcpu, 0);
+		return X86EMUL_UNHANDLEABLE;
+	}
+
+	return X86EMUL_CONTINUE;
 }
 
 static void handle_xsetbv(struct kvm_vcpu *vcpu)

arch/x86/include/asm/msr.h
@@ -43,6 +43,11 @@ struct saved_msrs {
 #include <asm/atomic.h>
 #include <linux/tracepoint-defs.h>
 
+#if defined(__PKVM_HYP__) && defined(CONFIG_TRACEPOINTS)
+#undef tracepoint_enabled
+#define tracepoint_enabled(tracepoint) false
+#endif
+
 #ifdef CONFIG_TRACEPOINTS
 DECLARE_TRACEPOINT(read_msr);
 DECLARE_TRACEPOINT(write_msr);

arch/x86/kvm/pkvm/undef.h
@@ -13,6 +13,9 @@
 #undef CONFIG_DEBUG_VIRTUAL
 #undef CONFIG_CALL_THUNKS_DEBUG
 #undef CONFIG_PREEMPT_DYNAMIC
+#undef CONFIG_PARAVIRT
+#undef CONFIG_PARAVIRT_XXL
+#undef CONFIG_PARAVIRT_SPINLOCKS
 
 #define NOTRACE
 
@@ -42,6 +45,7 @@
 /*
 #undef CONFIG_BUG
 #undef CONFIG_GENERIC_BUG
+#undef CONFIG_TRACEPOINTS
 */
 
 #endif /* __PKVM_X86_UNDEF_H */

commit 68687cc9f96b5bf943ee507aad597999e79e0be8

pKVM: VMX: Fix using 64-bit RCX value as MSR index
WRMSR and RDMSR instructions use only the ECX, i.e. the lower 32 bits
of the RCX, as the MSR index. The upper 32 bits are ignored (the SDM
explicitly states that).

Whereas pKVM erroneously treats the whole 64-bit value of the host's
RCX as the MSR index when handling host's writes to intercepted MSRs.
As a result, if the host sets non-zero upper bits of the MSR, the
resulting 64-bit value doesn't match any of the MSRs handled in the
switch in handle_write_msr(), so it goes straight to the "default"
branch which writes the MSR directly to hardware, i.e. bypassing MSR
emulation (since hardware, unlike pKVM, ignores upper bits).

Fix this by using u32 instead of unsigned long for any variables used
to store an MSR index, thus make pKVM itself ignore upper bits as well.
See also e.g. KVM's kvm_emulate_wrmsr() -> __kvm_emulate_wrmsr() for
comparison.

Fixes: 11b8efc1c43d ("pKVM: VMX: Improve the host MSR vmexit handler")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/vmx/host_vmx.c
@@ -108,14 +108,14 @@ static void handle_cr(struct kvm_vcpu *vcpu)
 	}
 }
 
-static bool is_msr_in_bitmap_range(unsigned long msr)
+static bool is_msr_in_bitmap_range(u32 msr)
 {
 	return msr <= 0x1FFF || (msr >= 0xC0000000 && msr <= 0xC0001FFF);
 }
 
 static int handle_read_msr(struct kvm_vcpu *vcpu)
 {
-	unsigned long msr = vcpu->arch.regs[VCPU_REGS_RCX];
+	u32 msr = vcpu->arch.regs[VCPU_REGS_RCX];
 	u32 low, high;
 
 	/*
@@ -138,7 +138,7 @@ static int handle_read_msr(struct kvm_vcpu *vcpu)
 
 static int handle_write_msr(struct kvm_vcpu *vcpu)
 {
-	unsigned long msr = vcpu->arch.regs[VCPU_REGS_RCX];
+	u32 msr = vcpu->arch.regs[VCPU_REGS_RCX];
 	int ret = X86EMUL_CONTINUE;
 	u32 low, high;
 	u64 val;
@@ -214,7 +214,7 @@ static int handle_write_msr(struct kvm_vcpu *vcpu)
 		if (slot >= 0) {
 			cur = kvm_get_user_return_msr(slot);
 			if (val != cur) {
-				pkvm_warn("Host attempt to modify user-return MSR 0x%lx: 0x%llx (expected 0x%llx)\n",
+				pkvm_warn("Host attempt to modify user-return MSR 0x%x: 0x%llx (expected 0x%llx)\n",
 					  msr, val, cur);
 				ret = X86EMUL_UNHANDLEABLE;
 				break;
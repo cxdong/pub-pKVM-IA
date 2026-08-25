cxdong/pub-pKVM-IA

commit bb75cc6cff9fd19ae7a669834d9e4ae2ce2202c0

pKVM: x86: Import fpu_swap_kvm_fpstate
Import fpu_swap_kvm_fpstate() from kernel fpu with the modification to
switch the fpstate between host and pVM. Before switching from the host
to a pVM, load the FPU with the pVM's fpstate (the host should save its
own FPU state already before requesting entering a pVM). And before
switching from a pVM to the host, saving the pVM's FPU state and loading
with pKVM's percpu fpu in the current_task, which will configure the FPU
to initial state, so that to protect the pVM's FPU state from the host.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kernel/fpu/core.c
@@ -123,6 +123,7 @@ static void update_avx_timestamp(struct fpu *fpu)
 	if (fpu->fpstate->regs.xsave.header.xfeatures & AVX512_TRACKING_MASK)
 		fpu->avx512_timestamp = jiffies;
 }
+#endif /* !__PKVM_HYP__ */
 
 /*
  * Save the FPU register state in fpu->fpstate->regs. The register state is
@@ -142,7 +143,9 @@ void save_fpregs_to_fpstate(struct fpu *fpu)
 {
 	if (likely(use_xsave())) {
 		os_xsave(fpu->fpstate);
+#ifndef __PKVM_HYP__
 		update_avx_timestamp(fpu);
+#endif
 		return;
 	}
 
@@ -213,6 +216,7 @@ void restore_fpregs_from_fpstate(struct fpstate *fpstate, u64 mask)
 	}
 }
 
+#ifndef __PKVM_HYP__
 void fpu_reset_from_exception_fixup(void)
 {
 	restore_fpregs_from_fpstate(&init_fpstate, XFEATURE_MASK_FPSTATE);
@@ -380,17 +384,41 @@ EXPORT_SYMBOL_GPL(fpu_sync_guest_vmexit_xfd_state);
 #endif /* !__PKVM_HYP__ */
 #endif /* CONFIG_X86_64 */
 
-#ifndef __PKVM_HYP__
 int fpu_swap_kvm_fpstate(struct fpu_guest *guest_fpu, bool enter_guest)
 {
 	struct fpstate *guest_fps = guest_fpu->fpstate;
 	struct fpu *fpu = x86_task_fpu(current);
 	struct fpstate *cur_fps = fpu->fpstate;
 
+#ifndef __PKVM_HYP__
 	fpregs_lock();
 	if (!cur_fps->is_confidential && !test_thread_flag(TIF_NEED_FPU_LOAD))
 		save_fpregs_to_fpstate(fpu);
-
+#else
+#ifdef CONFIG_X86_64
+	if (fpu_state_size_dynamic() && enter_guest) {
+		/*
+		 * Refresh the xfd_state percpu cache before guest vmenter so
+		 * that the xfd can be restored after guest vmexit.
+		 */
+		rdmsrl(MSR_IA32_XFD, cur_fps->xfd);
+		__this_cpu_write(xfd_state, cur_fps->xfd);
+	}
+#endif
+	/*
+	 * If entering the npVM, the FPU are already loaded with the npVM fpu
+	 * state by the host. If exiting from the npVM, the fpu registers will be
+	 * saved by the host. So no need to save FPU for the npVM.
+	 *
+	 * If entering the pVM, the FPU are loaded with the host fpu state, which
+	 * is already saved by the host itself before switching to the pkvm
+	 * hypervisor. If exiting from the pVM, then the fpu state should be saved
+	 * by the pkvm hypervisor as the host is not allowed to do this for
+	 * isolation purpose.
+	 */
+	if (guest_fps->is_confidential && !enter_guest)
+		save_fpregs_to_fpstate(fpu);
+#endif
 	/* Swap fpstate */
 	if (enter_guest) {
 		fpu->__task_fpstate = cur_fps;
@@ -404,6 +432,7 @@ int fpu_swap_kvm_fpstate(struct fpu_guest *guest_fpu, bool enter_guest)
 
 	cur_fps = fpu->fpstate;
 
+#ifndef __PKVM_HYP__
 	if (!cur_fps->is_confidential) {
 		/* Includes XFD update */
 		restore_fpregs_from_fpstate(cur_fps, XFEATURE_MASK_FPSTATE);
@@ -418,10 +447,29 @@ int fpu_swap_kvm_fpstate(struct fpu_guest *guest_fpu, bool enter_guest)
 
 	fpregs_mark_activate();
 	fpregs_unlock();
+#else
+	/*
+	 * Similarly to the FPU saving case, no need to restore FPU for the npVM
+	 * as this will be handled by the host.
+	 *
+	 * If entering the pVM, restore the FPU with the pVM fpu state. If
+	 * exiting the pVM, wipe the FPU by restoring FPU with an initial fpu
+	 * state.
+	 */
+	if (guest_fps->is_confidential) {
+		/* Includes XFD update */
+		restore_fpregs_from_fpstate(cur_fps, XFEATURE_MASK_FPSTATE);
+	} else {
+		/* Only update XFD as npVM FPU is already loaded by the host */
+		xfd_update_state(cur_fps);
+	}
+#endif
+
 	return 0;
 }
 EXPORT_SYMBOL_GPL(fpu_swap_kvm_fpstate);
 
+#ifndef __PKVM_HYP__
 void fpu_copy_guest_fpstate_to_uabi(struct fpu_guest *gfpu, void *buf,
 				    unsigned int size, u64 xfeatures, u32 pkru)
 {

commit 437bb2d105e0f0124b84d220ce89a0724ecb790d

pKVM: x86: load XFD for npVM from host side only
The MSR_IA32_XFD is emulated by the pKVM. Either the guest modifying XFD
or the host modifying XFD for the guest is handled by the pKVM. And before
returning back from the pKVM to the host, pKVM switches the XFD, thus
the guest's up-to-date XFD value is maintained inside the pKVM. This can
cause the host's vcpu->arch.guest_fpu.fpstate->xfd for the npVM is not
updated. Then the fpu_swap_kvm_fpstate() is not able to load an expected
XFD, which results in loading additional useless fpu components.
Although later the pKVM can fix the XFD with the right value, this is
unclean implementation.

To properly manage the npVM's XFD, sync the up-to-date XFD value to the
host for both guest modifying (by enabling xfd_no_write_intercept in the
shared_vcpu for the host to sync xfd) and the host modifying (by update
the XFD with the msr data from the host side). So that the host can load
the right XFD for the npVM, while the pKVM cannot switch the XFD for the
npVM anymore.

Fixes: bb75cc6cff9f ("pKVM: x86: Import fpu_swap_kvm_fpstate")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kernel/fpu/core.c
@@ -457,9 +457,6 @@ int fpu_swap_kvm_fpstate(struct fpu_guest *guest_fpu, bool enter_guest)
 	if (guest_fps->is_confidential) {
 		/* Includes XFD update */
 		restore_fpregs_from_fpstate(cur_fps, XFEATURE_MASK_FPSTATE);
-	} else {
-		/* Only update XFD as npVM FPU is already loaded by the host */
-		xfd_update_state(cur_fps);
 	}
 #endif
 

arch/x86/kvm/vmx/pkvm_host.c
@@ -921,8 +921,22 @@ static int pkvm_set_msr(struct kvm_vcpu *vcpu, struct msr_data *msr_info)
 	if (pkvm_host_has_emulated_msr(vcpu->kvm, msr_info->index))
 		return kvm_set_msr_common(vcpu, msr_info);
 
-	if (!vcpu->arch.guest_state_protected)
-		return pkvm_hypercall(set_msr, msr_info->index, msr_info->data);
+	if (!vcpu->arch.guest_state_protected) {
+		int ret = pkvm_hypercall(set_msr, msr_info->index, msr_info->data);
+
+		if (ret)
+			return ret;
+
+		switch (msr_info->index) {
+		case MSR_IA32_XFD:
+			fpu_update_guest_xfd(&vcpu->arch.guest_fpu, msr_info->data);
+			break;
+		default:
+			break;
+		}
+
+		return 0;
+	}
 
 	return -EPERM;
 }

arch/x86/kvm/vmx/vmx.c
@@ -2416,6 +2416,10 @@ int vmx_set_msr(struct kvm_vcpu *vcpu, struct msr_data *msr_info)
 						      MSR_TYPE_RW);
 			vcpu->arch.xfd_no_write_intercept = true;
 			vmx_update_exception_bitmap(vcpu);
+#ifdef __PKVM_HYP__
+			if (!pkvm_is_protected_vcpu(vcpu))
+				to_pkvm_vcpu(vcpu)->shared_vcpu->arch.xfd_no_write_intercept = true;
+#endif
 		}
 		break;
 #endif
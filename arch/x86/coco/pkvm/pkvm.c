cxdong/pub-pKVM-IA

commit 056f607f79d1e6fda9b72ae9b224abb19a189f2e

x86/pkvm: Use hypercalls to access MMIO
Emulated MMIO cannot work for protected VMs on x86 in the usual way,
since in order to emulate guest's access to an MMIO memory address, the
host needs to read and decode the trapped instruction from the guest
memory (to find out what kind of memory access it is, i.e. is it read or
write and how many bytes), while the host has no access to protected VM
memory.

So overwrite the MMIO PV ops with pkvm's virt MMIO operations which will
use special hypercalls to access MMIO. pKVM will forward handling those
hypercalls to the host, to let the host emulate MMIO access without the
need to decode instructions.

[ Dmytro: fix error handling + extend commit message ]

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/coco/pkvm/pkvm.c
@@ -3,6 +3,7 @@
 #include <linux/kvm_para.h>
 #include <asm/coco.h>
 #include <asm/pkvm_guest.h>
+#include <asm/pgtable.h>
 
 DEFINE_STATIC_KEY_FALSE(pkvm_guest_detected);
 EXPORT_SYMBOL(pkvm_guest_detected);
@@ -41,9 +42,112 @@ int pkvm_set_mem_host_visibility(unsigned long addr, int numpages, bool enc)
 	return ret;
 }
 
+static int pkvm_virt_mmio(int size, bool write, unsigned long vaddr, unsigned long *val)
+{
+	unsigned long paddr;
+	pte_t *pte;
+	int level;
+
+	pte = lookup_address(vaddr, &level);
+	if (WARN_ON_ONCE(!pte))
+		return -EIO;
+
+	paddr = (pte_pfn(*pte) << PAGE_SHIFT) | (vaddr & ~page_level_mask(level));
+
+	if (write)
+		kvm_hypercall3(PKVM_GHC_IOWRITE, paddr, size, *val);
+	else
+		*val = kvm_hypercall2(PKVM_GHC_IOREAD, paddr, size);
+
+	return 0;
+}
+
+static unsigned char pkvm_mmio_readb(const volatile void __iomem *addr)
+{
+	unsigned long val;
+
+	if (pkvm_virt_mmio(1, false, (unsigned long)addr, &val))
+		return 0xff;
+	return val;
+}
+
+static unsigned short pkvm_mmio_readw(const volatile void __iomem *addr)
+{
+	unsigned long val;
+
+	if (pkvm_virt_mmio(2, false, (unsigned long)addr, &val))
+		return 0xffff;
+	return val;
+}
+
+static unsigned int pkvm_mmio_readl(const volatile void __iomem *addr)
+{
+	unsigned long val;
+
+	if (pkvm_virt_mmio(4, false, (unsigned long)addr, &val))
+		return 0xffffffff;
+	return val;
+}
+
+static u64 pkvm_mmio_readq(const volatile void __iomem *addr)
+{
+	unsigned long val;
+
+	if (pkvm_virt_mmio(8, false, (unsigned long)addr, &val))
+		return 0xffffffffffffffff;
+	return val;
+}
+
+static void pkvm_mmio_writeb(unsigned char v, volatile void __iomem *addr)
+{
+	unsigned long val = v;
+
+	pkvm_virt_mmio(1, true, (unsigned long)addr, &val);
+}
+
+static void pkvm_mmio_writew(unsigned short v, volatile void __iomem *addr)
+{
+	unsigned long val = v;
+
+	pkvm_virt_mmio(2, true, (unsigned long)addr, &val);
+}
+
+static void pkvm_mmio_writel(unsigned int v, volatile void __iomem *addr)
+{
+	unsigned long val = v;
+
+	pkvm_virt_mmio(4, true, (unsigned long)addr, &val);
+}
+
+static void pkvm_mmio_writeq(u64 v, volatile void __iomem *addr)
+{
+	unsigned long val = v;
+
+	pkvm_virt_mmio(8, true, (unsigned long)addr, &val);
+}
+
 __init void pkvm_guest_init_coco(void)
 {
 	cc_vendor = CC_VENDOR_PKVM;
 
 	static_branch_enable(&pkvm_guest_detected);
+
+	pv_ops.mmio.raw_readb = pkvm_mmio_readb;
+	pv_ops.mmio.raw_readw = pkvm_mmio_readw;
+	pv_ops.mmio.raw_readl = pkvm_mmio_readl;
+	pv_ops.mmio.raw_readb_relaxed = pkvm_mmio_readb;
+	pv_ops.mmio.raw_readw_relaxed = pkvm_mmio_readw;
+	pv_ops.mmio.raw_readl_relaxed = pkvm_mmio_readl;
+	pv_ops.mmio.raw_writeb = pkvm_mmio_writeb;
+	pv_ops.mmio.raw_writew = pkvm_mmio_writew;
+	pv_ops.mmio.raw_writel = pkvm_mmio_writel;
+	pv_ops.mmio.raw_writeb_relaxed = pkvm_mmio_writeb;
+	pv_ops.mmio.raw_writew_relaxed = pkvm_mmio_writew;
+	pv_ops.mmio.raw_writel_relaxed = pkvm_mmio_writel;
+#ifdef CONFIG_X86_64
+	pv_ops.mmio.raw_readq = pkvm_mmio_readq;
+	pv_ops.mmio.raw_readq_relaxed = pkvm_mmio_readq;
+	pv_ops.mmio.raw_writeq = pkvm_mmio_writeq;
+	pv_ops.mmio.raw_writeq_relaxed = pkvm_mmio_writeq;
+#endif
 }

commit 616a2ace9e9cc9c121517567165d23b2d4b4d785

x86/pkvm: Fix type mismatch and missing present check in pkvm_virt_mmio()
Fix type mismatch: lookup_address() takes unsigned int *level but
the local variable was declared as int. Change to unsigned int to
match the API and avoid signedness warnings.

Add _PAGE_PRESENT check after lookup_address(). The function can
return a non-NULL pte_t* for non-present entries (e.g., a PUD/PMD
that stopped the walk). Calling pte_pfn() on a non-present entry
extracts metadata bits as a physical frame number, producing a
bogus address passed to the hypervisor. Check the hardware Present
bit (not pte_present() which includes PROTNONE) before proceeding.

Fixes: 056f607f79d1 ("x86/pkvm: Use hypercalls to access MMIO")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/coco/pkvm/pkvm.c
@@ -46,11 +46,11 @@ int pkvm_set_mem_host_visibility(unsigned long addr, int numpages, bool enc)
 static int pkvm_virt_mmio(int size, bool write, unsigned long vaddr, unsigned long *val)
 {
 	unsigned long paddr;
+	unsigned int level;
 	pte_t *pte;
-	int level;
 
 	pte = lookup_address(vaddr, &level);
-	if (WARN_ON_ONCE(!pte))
+	if (WARN_ON_ONCE(!pte || !(pte_flags(*pte) & _PAGE_PRESENT)))
 		return -EIO;
 
 	paddr = (pte_pfn(*pte) << PAGE_SHIFT) | (vaddr & ~page_level_mask(level));
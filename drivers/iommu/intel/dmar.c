cxdong/pub-pKVM-IA

commit 0d97f772940699ae54ea6d718325450ef166cc85

iommu/vt-d: pKVM: Statically allocate desc_status in qi_inval
Currently qi_inval->desc_status is dynamically allocated during QI
initialization. This integer array is of length QI_LENGTH(256) and used
by qi_submit to wait on the status of a invalidation request.

There is no advantage in it being dynamically allocated. More over,
having it statically allocated would help in pKVM implementation of QI
as it need not be donated by host.

Signed-off-by: Vineeth Pillai <vineethrp@google.com>

drivers/iommu/intel/dmar.c
@@ -1209,7 +1209,6 @@ static void free_iommu(struct intel_iommu *iommu)
 
 	if (iommu->qi) {
 		iommu_free_pages(iommu->qi->desc);
-		kfree(iommu->qi->desc_status);
 		kfree(iommu->qi);
 	}
 
@@ -1733,14 +1732,6 @@ int dmar_enable_qi(struct intel_iommu *iommu)
 	}
 
 	qi->desc = desc;
-
-	qi->desc_status = kcalloc(QI_LENGTH, sizeof(int), GFP_ATOMIC);
-	if (!qi->desc_status) {
-		iommu_free_pages(qi->desc);
-		kfree(qi);
-		iommu->qi = NULL;
-		return -ENOMEM;
-	}
 
 	raw_spin_lock_init(&qi->q_lock);
 

drivers/iommu/intel/iommu.h
@@ -506,7 +506,7 @@ struct q_inval {
 	pkvm_spinlock_t	q_lock;
 #endif
 	void		*desc;          /* invalidation queue */
-	int             *desc_status;   /* desc status */
+	int             desc_status[QI_LENGTH]; /* desc status */
 	int             free_head;      /* first free entry */
 	int             free_tail;      /* last free entry */
 	int             free_cnt;

commit d790e316f28e35658d31c278a83b681923bca140

iommu/vt-d: pKVM: Use kzalloc for qi struct to zero-initialize desc_status
dmar_enable_qi() allocates the qi struct with kmalloc, leaving
desc_status uninitialized. Previously the desc_status is allocated by
kcalloc which will initialize the desc_status as 0. Use kzalloc to
ensure all fields are zero-initialized.

Fixes: 0d97f7729406 ("iommu/vt-d: pKVM: Statically allocate desc_status in qi_inval")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

drivers/iommu/intel/dmar.c
@@ -1820,7 +1820,7 @@ int dmar_enable_qi(struct intel_iommu *iommu)
 	if (iommu->qi)
 		return 0;
 
-	iommu->qi = kmalloc(sizeof(*qi), GFP_ATOMIC);
+	iommu->qi = kzalloc(sizeof(*qi), GFP_ATOMIC);
 	if (!iommu->qi)
 		return -ENOMEM;
 
cxdong/pub-pKVM-IA

commit 9f0d7b717c6458337ba48e739d5b846ee320994e

pKVM: x86: trace: Register per guest VM vmexit trace debug file
Register a per guest VM debug file pkvm_vmexit_trace, which can be used
to show the trace of this single guest VM.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm_debugfs.c
@@ -288,3 +288,12 @@ out_dir:
 	}
 	debugfs_remove(debugfs_dir);
 }
+
+void pkvm_create_vm_debugfs(struct kvm *kvm)
+{
+	if (!enable_pkvm)
+		return;
+
+	debugfs_create_file("pkvm_vmexit_trace", 0444, kvm->debugfs_dentry,
+			    kvm, &vmexit_trace_fops);
+}

arch/x86/include/asm/kvm_host.h
@@ -2072,10 +2072,12 @@ extern phys_addr_t pkvm_mem_base;
 extern phys_addr_t pkvm_mem_size;
 void __init pkvm_reserve(void);
 void pkvm_init_debugfs(void);
+void pkvm_create_vm_debugfs(struct kvm *kvm);
 void kvm_free_pkvm_memcache(struct pkvm_memcache *mc);
 #else
 #define enable_pkvm		false
 static inline void __init pkvm_reserve(void) {}
+static inline void pkvm_create_vm_debugfs(struct kvm *kvm) {}
 #endif
 
 #ifdef __PKVM_HYP__

arch/x86/kvm/debugfs.c
@@ -193,4 +193,6 @@ void kvm_arch_create_vm_debugfs(struct kvm *kvm)
 {
 	debugfs_create_file("mmu_rmaps_stat", 0644, kvm->debugfs_dentry, kvm,
 			    &mmu_rmaps_stat_fops);
+
+	pkvm_create_vm_debugfs(kvm);
 }

commit 58bce25355bb2b4d51a70fafcff8341339ce3197

pKVM: x86: trace: Fix use-after-free in per-VM vmexit_trace debugfs
The per-VM pkvm_vmexit_trace debugfs file uses DEFINE_SHOW_ATTRIBUTE
which does not hold a reference to the kvm struct. If the VM is
destroyed while the file is open, vmexit_trace_show() accesses freed
kvm structure memory.

Replace with custom open/release that acquire a kvm reference via
kvm_get_kvm_safe() on open and release it via kvm_put_kvm() on close.

Fixes: 9f0d7b717c64 ("pKVM: x86: trace: Register per guest VM vmexit trace debug file")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm_debugfs.c
@@ -228,7 +228,42 @@ out:
 
 	return ret;
 }
-DEFINE_SHOW_ATTRIBUTE(vmexit_trace);
+
+static int vmexit_trace_open(struct inode *inode, struct file *file)
+{
+	struct kvm *kvm = inode->i_private;
+	int r;
+
+	if (kvm && !kvm_get_kvm_safe(kvm))
+		return -ENOENT;
+
+	r = single_open(file, vmexit_trace_show, kvm);
+	if (r < 0 && kvm)
+		kvm_put_kvm(kvm);
+
+	return r;
+}
+
+static int vmexit_trace_release(struct inode *inode, struct file *file)
+{
+	struct kvm *kvm = inode->i_private;
+	int ret;
+
+	ret = single_release(inode, file);
+
+	if (kvm)
+		kvm_put_kvm(kvm);
+
+	return ret;
+}
+
+static const struct file_operations vmexit_trace_fops = {
+	.owner		= THIS_MODULE,
+	.open		= vmexit_trace_open,
+	.read		= seq_read,
+	.llseek		= seq_lseek,
+	.release	= vmexit_trace_release,
+};
 
 struct debugfs_item {
 	const char *name;
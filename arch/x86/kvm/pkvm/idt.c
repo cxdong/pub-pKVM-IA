cxdong/pub-pKVM-IA

commit 0ef21ec1cf35b4b11075d25499536f8d6c02a8f1

pKVM: x86: Support exception fixup
Add exception fixup support to fix the exception generated by accessing
MSR(i.e., rdmsr and wrmsr). The fixup code is majorly ported from the
arch/x86/mm/extable.c and kernel/extable.c.

If an exception can be handled, the ip will be skipped to the fixup
address. Otherwise, the exception will continue be handled by the
installed handler.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/idt.c
@@ -1,6 +1,117 @@
 // SPDX-License-Identifier: GPL-2.0
+#include <linux/array_size.h>
+#include <linux/bitfield.h>
+#include <linux/extable.h>
+#include <asm/processor.h>
+#include <asm/extable.h>
+#include <asm/pkvm_image.h>
 #include <asm/trapnr.h>
 #include "idt.h"
+
+static const int pt_regoff[] = {
+	offsetof(struct pt_regs, ax),
+	offsetof(struct pt_regs, cx),
+	offsetof(struct pt_regs, dx),
+	offsetof(struct pt_regs, bx),
+	offsetof(struct pt_regs, sp),
+	offsetof(struct pt_regs, bp),
+	offsetof(struct pt_regs, si),
+	offsetof(struct pt_regs, di),
+#ifdef CONFIG_X86_64
+	offsetof(struct pt_regs, r8),
+	offsetof(struct pt_regs, r9),
+	offsetof(struct pt_regs, r10),
+	offsetof(struct pt_regs, r11),
+	offsetof(struct pt_regs, r12),
+	offsetof(struct pt_regs, r13),
+	offsetof(struct pt_regs, r14),
+	offsetof(struct pt_regs, r15),
+#else
+	offsetof(struct pt_regs, ds),
+	offsetof(struct pt_regs, es),
+	offsetof(struct pt_regs, fs),
+	offsetof(struct pt_regs, gs),
+#endif
+};
+
+static int pt_regs_offset(struct pt_regs *regs, int regno)
+{
+	if ((unsigned)regno < ARRAY_SIZE(pt_regoff))
+		return pt_regoff[regno];
+	return -EDOM;
+}
+
+static inline unsigned long
+ex_fixup_addr(const struct exception_table_entry *x)
+{
+	return (unsigned long)&x->fixup + x->fixup;
+}
+
+static inline unsigned long *pt_regs_nr(struct pt_regs *regs, int nr)
+{
+	int reg_offset = pt_regs_offset(regs, nr);
+	static unsigned long __dummy;
+
+	if (WARN_ON_ONCE(reg_offset < 0))
+		return &__dummy;
+
+	return (unsigned long *)((unsigned long)regs + reg_offset);
+}
+
+static bool ex_handler_default(const struct exception_table_entry *e,
+			       struct pt_regs *regs)
+{
+	if (e->data & EX_FLAG_CLEAR_AX)
+		regs->ax = 0;
+	if (e->data & EX_FLAG_CLEAR_DX)
+		regs->dx = 0;
+
+	regs->ip = ex_fixup_addr(e);
+	return true;
+}
+
+static bool ex_handler_msr(const struct exception_table_entry *fixup,
+			   struct pt_regs *regs, bool wrmsr, bool safe, int reg)
+{
+	if (!wrmsr) {
+		/* Pretend that the read succeeded and returned 0. */
+		regs->ax = 0;
+		regs->dx = 0;
+	}
+
+	if (safe)
+		*pt_regs_nr(regs, reg) = -EIO;
+
+	return ex_handler_default(fixup, regs);
+}
+
+static bool pkvm_fixup_exception(struct pt_regs *regs)
+{
+	const struct exception_table_entry *e;
+	int type, reg;
+
+	e = search_extable(__start___ex_table,
+			   __stop___ex_table - __start___ex_table,
+			   regs->ip);
+	if (!e)
+		return false;
+
+	type = FIELD_GET(EX_DATA_TYPE_MASK, e->data);
+	reg  = FIELD_GET(EX_DATA_REG_MASK,  e->data);
+
+	switch (type) {
+	case EX_TYPE_WRMSR:
+		return ex_handler_msr(e, regs, true, false, reg);
+	case EX_TYPE_RDMSR:
+		return ex_handler_msr(e, regs, false, false, reg);
+	case EX_TYPE_WRMSR_SAFE:
+		return ex_handler_msr(e, regs, true, true, reg);
+	case EX_TYPE_RDMSR_SAFE:
+		return ex_handler_msr(e, regs, false, true, reg);
+	default:
+		BUG();
+	}
+}
 
 static void default_exception_handler(struct pt_regs *regs,
 				      int vector, bool has_error_code)
@@ -20,6 +131,9 @@ void handle_exception(struct pt_regs *regs, int vector, bool has_error_code)
 	exception_handler_t handler;
 
 	if (vector >= X86_TRAP_IRET)
+		return;
+
+	if (pkvm_fixup_exception(regs))
 		return;
 
 	handler = exception_handlers[vector];

commit f2ce4d424645a573ba79a57039a91fc299fd4edb

pKVM: x86: Remove exception fixup for non-safe MSR access
The pKVM is expected to only use the safe variant to access the MSR. If
it uses non-safe variant, the MSR accessing should be guaranteed as
success rather than generating #GP. To prevent the #GP generated by
non-safe variant being ignored by automatically fixing it in the
exception fixup handler, remove EX_TYPE_WRMSR and EX_TYPE_RDMSR
exception types to make sure such #GP is propagated to panic the pKVM.
Thus ex_handler_msr is also simplified by assuming it is always for the
safe variant.

Fixes: 0ef21ec1cf35 ("pKVM: x86: Support exception fixup")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/idt.c
@@ -74,30 +74,15 @@ static bool ex_handler_default(const struct exception_table_entry *e,
 }
 
 static bool ex_handler_msr(const struct exception_table_entry *fixup,
-			   struct pt_regs *regs, bool wrmsr, bool safe, int reg)
+			   struct pt_regs *regs, bool wrmsr, int reg)
 {
-#ifdef CONFIG_PKVM_X86_DEBUG
-	if (__ONCE_LITE_IF(!safe && wrmsr)) {
-		pkvm_err("unchecked MSR access error: WRMSR to 0x%x (tried to write 0x%08x%08x) at rIP: 0x%lx (%pS)\n",
-			(unsigned int)regs->cx, (unsigned int)regs->dx,
-			(unsigned int)regs->ax,  regs->ip, (void *)regs->ip);
-	}
-
-	if (__ONCE_LITE_IF(!safe && !wrmsr)) {
-		pkvm_err("unchecked MSR access error: RDMSR from 0x%x at rIP: 0x%lx (%pS)\n",
-			(unsigned int)regs->cx, regs->ip, (void *)regs->ip);
-	}
-#endif
-
 	if (!wrmsr) {
 		/* Pretend that the read succeeded and returned 0. */
 		regs->ax = 0;
 		regs->dx = 0;
 	}
 
-	if (safe)
-		*pt_regs_nr(regs, reg) = -EIO;
-
+	*pt_regs_nr(regs, reg) = -EIO;
 	return ex_handler_default(fixup, regs);
 }
 
@@ -118,14 +103,10 @@ static bool pkvm_fixup_exception(struct pt_regs *regs)
 	switch (type) {
 	case EX_TYPE_DEFAULT:
 		return ex_handler_default(e, regs);
-	case EX_TYPE_WRMSR:
-		return ex_handler_msr(e, regs, true, false, reg);
-	case EX_TYPE_RDMSR:
-		return ex_handler_msr(e, regs, false, false, reg);
 	case EX_TYPE_WRMSR_SAFE:
-		return ex_handler_msr(e, regs, true, true, reg);
+		return ex_handler_msr(e, regs, true, reg);
 	case EX_TYPE_RDMSR_SAFE:
-		return ex_handler_msr(e, regs, false, true, reg);
+		return ex_handler_msr(e, regs, false, reg);
 	default:
 		BUG();
 	}
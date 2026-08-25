cxdong/pub-pKVM-IA

commit 31a5126920c90c7537bd8ceb9160fc49eaab8b81

x86/paravirt: Add pv_mmio_ops
Add pv_mmio_ops in the paravirt to support PV MMIO accessors.
By default, the MMIO ops in pv_ops are initialized by the native MMIO
accessors. A virtual platform kernel can overwrite these MMIO ops by its
own PV methods.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/include/asm/io.h
@@ -43,6 +43,7 @@
 #include <asm/pgtable_types.h>
 #include <asm/shared/io.h>
 #include <asm/special_insns.h>
+#include <asm/paravirt.h>
 
 #define build_mmio_read(name, size, type, reg, barrier) \
 static inline type name(const volatile void __iomem *addr) \
@@ -54,58 +55,79 @@ static inline void name(type val, volatile void __iomem *addr) \
 { asm volatile("mov" size " %0,%1": :reg (val), \
 "m" (*(volatile type __force *)addr) barrier); }
 
-build_mmio_read(readb, "b", unsigned char, "=q", :"memory")
-build_mmio_read(readw, "w", unsigned short, "=r", :"memory")
-build_mmio_read(readl, "l", unsigned int, "=r", :"memory")
+build_mmio_read(raw_readb, "b", unsigned char, "=q", : "memory")
+build_mmio_read(raw_readw, "w", unsigned short, "=r", : "memory")
+build_mmio_read(raw_readl, "l", unsigned int, "=r", : "memory")
 
-build_mmio_read(__readb, "b", unsigned char, "=q", )
-build_mmio_read(__readw, "w", unsigned short, "=r", )
-build_mmio_read(__readl, "l", unsigned int, "=r", )
+build_mmio_read(raw_readb_relaxed, "b", unsigned char, "=q", )
+build_mmio_read(raw_readw_relaxed, "w", unsigned short, "=r", )
+build_mmio_read(raw_readl_relaxed, "l", unsigned int, "=r", )
 
-build_mmio_write(writeb, "b", unsigned char, "q", :"memory")
-build_mmio_write(writew, "w", unsigned short, "r", :"memory")
-build_mmio_write(writel, "l", unsigned int, "r", :"memory")
+build_mmio_write(raw_writeb, "b", unsigned char, "q", : "memory")
+build_mmio_write(raw_writew, "w", unsigned short, "r", : "memory")
+build_mmio_write(raw_writel, "l", unsigned int, "r", : "memory")
 
-build_mmio_write(__writeb, "b", unsigned char, "q", )
-build_mmio_write(__writew, "w", unsigned short, "r", )
-build_mmio_write(__writel, "l", unsigned int, "r", )
+build_mmio_write(raw_writeb_relaxed, "b", unsigned char, "q", )
+build_mmio_write(raw_writew_relaxed, "w", unsigned short, "r", )
+build_mmio_write(raw_writel_relaxed, "l", unsigned int, "r", )
 
-#define readb readb
-#define readw readw
-#define readl readl
-#define readb_relaxed(a) __readb(a)
-#define readw_relaxed(a) __readw(a)
-#define readl_relaxed(a) __readl(a)
-#define __raw_readb __readb
-#define __raw_readw __readw
-#define __raw_readl __readl
+#ifdef CONFIG_PARAVIRT
+#define readb pv_readb
+#define readw pv_readw
+#define readl pv_readl
+#define __raw_readb pv_readb_relaxed
+#define __raw_readw pv_readw_relaxed
+#define __raw_readl pv_readl_relaxed
+#define writeb pv_writeb
+#define writew pv_writew
+#define writel pv_writel
+#define __raw_writeb pv_writeb_relaxed
+#define __raw_writew pv_writew_relaxed
+#define __raw_writel pv_writel_relaxed
+#else
+#define readb raw_readb
+#define readw raw_readw
+#define readl raw_readl
+#define __raw_readb raw_readb_relaxed
+#define __raw_readw raw_readw_relaxed
+#define __raw_readl raw_readl_relaxed
+#define writeb raw_writeb
+#define writew raw_writew
+#define writel raw_writel
+#define __raw_writeb raw_writeb_relaxed
+#define __raw_writew raw_writew_relaxed
+#define __raw_writel raw_writel_relaxed
+#endif
 
-#define writeb writeb
-#define writew writew
-#define writel writel
-#define writeb_relaxed(v, a) __writeb(v, a)
-#define writew_relaxed(v, a) __writew(v, a)
-#define writel_relaxed(v, a) __writel(v, a)
-#define __raw_writeb __writeb
-#define __raw_writew __writew
-#define __raw_writel __writel
+#define readb_relaxed(a) __raw_readb(a)
+#define readw_relaxed(a) __raw_readw(a)
+#define readl_relaxed(a) __raw_readl(a)
+
+#define writeb_relaxed(v, a) __raw_writeb(v, a)
+#define writew_relaxed(v, a) __raw_writew(v, a)
+#define writel_relaxed(v, a) __raw_writel(v, a)
 
 #ifdef CONFIG_X86_64
 
-build_mmio_read(readq, "q", u64, "=r", :"memory")
-build_mmio_read(__readq, "q", u64, "=r", )
-build_mmio_write(writeq, "q", u64, "r", :"memory")
-build_mmio_write(__writeq, "q", u64, "r", )
+build_mmio_read(raw_readq, "q", u64, "=r", : "memory")
+build_mmio_read(raw_readq_relaxed, "q", u64, "=r", )
+build_mmio_write(raw_writeq, "q", u64, "r", : "memory")
+build_mmio_write(raw_writeq_relaxed, "q", u64, "r", )
 
-#define readq_relaxed(a)	__readq(a)
-#define writeq_relaxed(v, a)	__writeq(v, a)
+#ifdef CONFIG_PARAVIRT
+#define readq			pv_readq
+#define __raw_readq		pv_readq_relaxed
+#define writeq			pv_writeq
+#define __raw_writeq		pv_writeq_relaxed
+#else
+#define readq			raw_readq
+#define __raw_readq		raw_readq_relaxed
+#define writeq			raw_writeq
+#define __raw_writeq		raw_writeq_relaxed
+#endif
 
-#define __raw_readq		__readq
-#define __raw_writeq		__writeq
-
-/* Let people know that we have them */
-#define readq			readq
-#define writeq			writeq
+#define readq_relaxed(a)	__raw_readq(a)
+#define writeq_relaxed(v, a)	__raw_writeq(v, a)
 
 #endif
 

arch/x86/include/asm/paravirt.h
@@ -681,6 +681,87 @@ static __always_inline unsigned long arch_local_irq_save(void)
 }
 #endif
 
+static __always_inline unsigned char pv_readb(const volatile void __iomem *addr)
+{
+	return PVOP_CALL1(unsigned char, mmio.raw_readb, addr);
+}
+
+static __always_inline unsigned short pv_readw(const volatile void __iomem *addr)
+{
+	return PVOP_CALL1(unsigned short, mmio.raw_readw, addr);
+}
+
+static __always_inline unsigned int pv_readl(const volatile void __iomem *addr)
+{
+	return PVOP_CALL1(unsigned int, mmio.raw_readl, addr);
+}
+
+static __always_inline unsigned char pv_readb_relaxed(const volatile void __iomem *addr)
+{
+	return PVOP_CALL1(unsigned char, mmio.raw_readb_relaxed, addr);
+}
+
+static __always_inline unsigned short pv_readw_relaxed(const volatile void __iomem *addr)
+{
+	return PVOP_CALL1(unsigned short, mmio.raw_readw_relaxed, addr);
+}
+
+static __always_inline unsigned int pv_readl_relaxed(const volatile void __iomem *addr)
+{
+	return PVOP_CALL1(unsigned int, mmio.raw_readl_relaxed, addr);
+}
+
+static __always_inline void pv_writeb(unsigned char val, volatile void __iomem *addr)
+{
+	PVOP_VCALL2(mmio.raw_writeb, val, addr);
+}
+
+static __always_inline void pv_writew(unsigned short val, volatile void __iomem *addr)
+{
+	PVOP_VCALL2(mmio.raw_writew, val, addr);
+}
+
+static __always_inline void pv_writel(unsigned int val, volatile void __iomem *addr)
+{
+	PVOP_VCALL2(mmio.raw_writel, val, addr);
+}
+
+static __always_inline void pv_writeb_relaxed(unsigned char val, volatile void __iomem *addr)
+{
+	PVOP_VCALL2(mmio.raw_writeb_relaxed, val, addr);
+}
+
+static __always_inline void pv_writew_relaxed(unsigned short val, volatile void __iomem *addr)
+{
+	PVOP_VCALL2(mmio.raw_writew_relaxed, val, addr);
+}
+
+static __always_inline void pv_writel_relaxed(unsigned int val, volatile void __iomem *addr)
+{
+	PVOP_VCALL2(mmio.raw_writel_relaxed, val, addr);
+}
+
+#ifdef CONFIG_X86_64
+static __always_inline u64 pv_readq(const volatile void __iomem *addr)
+{
+	return PVOP_CALL1(u64, mmio.raw_readq, addr);
+}
+
+static __always_inline u64 pv_readq_relaxed(const volatile void __iomem *addr)
+{
+	return PVOP_CALL1(u64, mmio.raw_readq_relaxed, addr);
+}
+
+static __always_inline void pv_writeq(u64 val, volatile void __iomem *addr)
+{
+	PVOP_VCALL2(mmio.raw_writeq, val, addr);
+}
+
+static __always_inline void pv_writeq_relaxed(u64 val, volatile void __iomem *addr)
+{
+	PVOP_VCALL2(mmio.raw_writeq_relaxed, val, addr);
+}
+#endif
 
 /* Make sure as little as possible of this mess escapes. */
 #undef PARAVIRT_CALL

arch/x86/kernel/paravirt.c
@@ -245,6 +245,24 @@ struct paravirt_patch_template pv_ops = {
 				PV_CALLEE_SAVE(__native_vcpu_is_preempted),
 #endif /* SMP */
 #endif
+	.mmio.raw_readb			= raw_readb,
+	.mmio.raw_readw			= raw_readw,
+	.mmio.raw_readl			= raw_readl,
+	.mmio.raw_readb_relaxed		= raw_readb_relaxed,
+	.mmio.raw_readw_relaxed		= raw_readw_relaxed,
+	.mmio.raw_readl_relaxed		= raw_readl_relaxed,
+	.mmio.raw_writeb		= raw_writeb,
+	.mmio.raw_writew		= raw_writew,
+	.mmio.raw_writel		= raw_writel,
+	.mmio.raw_writeb_relaxed	= raw_writeb_relaxed,
+	.mmio.raw_writew_relaxed	= raw_writew_relaxed,
+	.mmio.raw_writel_relaxed	= raw_writel_relaxed,
+#ifdef CONFIG_X86_64
+	.mmio.raw_readq			= raw_readq,
+	.mmio.raw_readq_relaxed		= raw_readq_relaxed,
+	.mmio.raw_writeq		= raw_writeq,
+	.mmio.raw_writeq_relaxed	= raw_writeq_relaxed,
+#endif
 };
 
 #ifdef CONFIG_PARAVIRT_XXL

arch/x86/include/asm/paravirt_types.h
@@ -222,6 +222,30 @@ struct pv_lock_ops {
 	struct paravirt_callee_save vcpu_is_preempted;
 } __no_randomize_layout;
 
+struct pv_mmio_ops {
+	unsigned char (*raw_readb)(const volatile void __iomem *addr);
+	unsigned short (*raw_readw)(const volatile void __iomem *addr);
+	unsigned int (*raw_readl)(const volatile void __iomem *addr);
+	unsigned char (*raw_readb_relaxed)(const volatile void __iomem *addr);
+	unsigned short (*raw_readw_relaxed)(const volatile void __iomem *addr);
+	unsigned int (*raw_readl_relaxed)(const volatile void __iomem *addr);
+
+	void (*raw_writeb)(unsigned char val, volatile void __iomem *addr);
+	void (*raw_writew)(unsigned short val, volatile void __iomem *addr);
+	void (*raw_writel)(unsigned int val, volatile void __iomem *addr);
+	void (*raw_writeb_relaxed)(unsigned char val, volatile void __iomem *addr);
+	void (*raw_writew_relaxed)(unsigned short val, volatile void __iomem *addr);
+	void (*raw_writel_relaxed)(unsigned int val, volatile void __iomem *addr);
+
+#ifdef CONFIG_X86_64
+	u64 (*raw_readq)(const volatile void __iomem *addr);
+	u64 (*raw_readq_relaxed)(const volatile void __iomem *addr);
+	void (*raw_writeq)(u64 val, volatile void __iomem *addr);
+	void (*raw_writeq_relaxed)(u64 val, volatile void __iomem *addr);
+#endif
+} __no_randomize_layout;
+
+
 /* This contains all the paravirt structures: we get a convenient
  * number for each function using the offset which we use to indicate
  * what to patch. */
@@ -230,6 +254,7 @@ struct paravirt_patch_template {
 	struct pv_irq_ops	irq;
 	struct pv_mmu_ops	mmu;
 	struct pv_lock_ops	lock;
+	struct pv_mmio_ops	mmio;
 } __no_randomize_layout;
 
 extern struct pv_info pv_info;

commit 8f214c1b268a68ceac5e5d5a4eddaebf42a55abe

x86/paravirt: Eliminate MMIO function call overhead on non-pKVM systems
Every MMIO accessor (readb/readw/readl/readq and write variants) goes
through a paravirt indirect-to-direct call dispatch (PVOP_CALL1/VCALL2)
when CONFIG_PARAVIRT=y. Even after boot-time ALTERNATIVE patching
converts the indirect call to a direct call, each MMIO access still
incurs a function call with full caller-saved register clobber. This
overhead affects all CONFIG_PARAVIRT=y systems (all distribution
kernels), not just pKVM guests that actually need the paravirt MMIO
path.

Use static_branch_unlikely(&pv_mmio) to dispatch between the native
raw_* inline functions and the pv_* PVOP functions. On non-pKVM systems
(the common case), the static branch defaults to the fall-through path
which inlines the native mov instruction directly at the call site with
zero function call overhead. On pKVM guests, the branch is patched to
jump to the PVOP dispatch path.

Fixes: 31a5126920c9 ("x86/paravirt: Add pv_mmio_ops")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/coco/pkvm/pkvm.c
@@ -163,5 +163,7 @@ __init void pkvm_guest_init_coco(void)
 	pv_ops.mmio.pci_mmcfg_writew = pkvm_mmio_writew;
 	pv_ops.mmio.pci_mmcfg_writel = pkvm_mmio_writel;
 
+	static_branch_enable(&pv_mmio);
+
 	apic_update_callback(wakeup_secondary_cpu, pkvm_wakeup_secondary_cpu);
 }

arch/x86/include/asm/io.h
@@ -43,7 +43,6 @@
 #include <asm/pgtable_types.h>
 #include <asm/shared/io.h>
 #include <asm/special_insns.h>
-#include <asm/paravirt.h>
 
 #define build_mmio_read(name, size, type, reg, barrier) \
 static inline type name(const volatile void __iomem *addr) \
@@ -72,18 +71,23 @@ build_mmio_write(raw_writew_relaxed, "w", unsigned short, "r", )
 build_mmio_write(raw_writel_relaxed, "l", unsigned int, "r", )
 
 #ifdef CONFIG_PARAVIRT
-#define readb pv_readb
-#define readw pv_readw
-#define readl pv_readl
-#define __raw_readb pv_readb_relaxed
-#define __raw_readw pv_readw_relaxed
-#define __raw_readl pv_readl_relaxed
-#define writeb pv_writeb
-#define writew pv_writew
-#define writel pv_writel
-#define __raw_writeb pv_writeb_relaxed
-#define __raw_writew pv_writew_relaxed
-#define __raw_writel pv_writel_relaxed
+#include <asm/paravirt.h>
+#define __pv_mmio_read(native, pv, addr)				\
+	(static_branch_unlikely(&pv_mmio) ? pv(addr) :	native(addr))
+#define __pv_mmio_write(native, pv, val, addr)				\
+	(static_branch_unlikely(&pv_mmio) ? pv(val, addr) : native(val, addr))
+#define readb(a)		__pv_mmio_read(raw_readb, pv_readb, a)
+#define readw(a)		__pv_mmio_read(raw_readw, pv_readw, a)
+#define readl(a)		__pv_mmio_read(raw_readl, pv_readl, a)
+#define __raw_readb(a)		__pv_mmio_read(raw_readb_relaxed, pv_readb_relaxed, a)
+#define __raw_readw(a)		__pv_mmio_read(raw_readw_relaxed, pv_readw_relaxed, a)
+#define __raw_readl(a)		__pv_mmio_read(raw_readl_relaxed, pv_readl_relaxed, a)
+#define writeb(v, a)		__pv_mmio_write(raw_writeb, pv_writeb, v, a)
+#define writew(v, a)		__pv_mmio_write(raw_writew, pv_writew, v, a)
+#define writel(v, a)		__pv_mmio_write(raw_writel, pv_writel, v, a)
+#define __raw_writeb(v, a)	__pv_mmio_write(raw_writeb_relaxed, pv_writeb_relaxed, v, a)
+#define __raw_writew(v, a)	__pv_mmio_write(raw_writew_relaxed, pv_writew_relaxed, v, a)
+#define __raw_writel(v, a)	__pv_mmio_write(raw_writel_relaxed, pv_writel_relaxed, v, a)
 #else
 #define readb raw_readb
 #define readw raw_readw
@@ -115,10 +119,10 @@ build_mmio_write(raw_writeq, "q", u64, "r", : "memory")
 build_mmio_write(raw_writeq_relaxed, "q", u64, "r", )
 
 #ifdef CONFIG_PARAVIRT
-#define readq			pv_readq
-#define __raw_readq		pv_readq_relaxed
-#define writeq			pv_writeq
-#define __raw_writeq		pv_writeq_relaxed
+#define readq(a)		__pv_mmio_read(raw_readq, pv_readq, a)
+#define __raw_readq(a)		__pv_mmio_read(raw_readq_relaxed, pv_readq_relaxed, a)
+#define writeq(v, a)		__pv_mmio_write(raw_writeq, pv_writeq, v, a)
+#define __raw_writeq(v, a)	__pv_mmio_write(raw_writeq_relaxed, pv_writeq_relaxed, v, a)
 #else
 #define readq			raw_readq
 #define __raw_readq		raw_readq_relaxed

arch/x86/include/asm/paravirt.h
@@ -681,6 +681,8 @@ static __always_inline unsigned long arch_local_irq_save(void)
 }
 #endif
 
+DECLARE_STATIC_KEY_FALSE(pv_mmio);
+
 static __always_inline unsigned char pv_readb(const volatile void __iomem *addr)
 {
 	return PVOP_CALL1(unsigned char, mmio.raw_readb, addr);

arch/x86/kernel/paravirt.c
@@ -55,6 +55,8 @@ DEFINE_ASM_FUNC(pv_native_read_cr2, "mov %cr2, %rax", .noinstr.text);
 
 DEFINE_STATIC_KEY_FALSE(virt_spin_lock_key);
 
+DEFINE_STATIC_KEY_FALSE(pv_mmio);
+
 void __init native_pv_lock_init(void)
 {
 	if (boot_cpu_has(X86_FEATURE_HYPERVISOR))
@@ -277,4 +279,5 @@ NOKPROBE_SYMBOL(native_load_idt);
 #endif
 
 EXPORT_SYMBOL(pv_ops);
+EXPORT_SYMBOL(pv_mmio);
 EXPORT_SYMBOL_GPL(pv_info);
fa40b27f46cffafd8efcd13d1ca4abc99a9912b2
Chuanxiao Dong <chuanxiao.dong@intel.com>
2026-09-18 10:59:41 +0800

    pKVM: x86: Add early memory reservation framework
    Once pKVM deprivileges the host, the host must not be able to access
    hypervisor memory. Such memory therefore needs to be set aside during
    early boot, before the buddy allocator can make it available to the
    host.
    
    Add the common x86 reservation framework. Provide machinery to snapshot
    and sort memblock regions and to allocate a contiguous, preferably
    PMD-aligned, memory region. Obtain the required number of pages through
    struct pkvm_reserve_ops so that sizing remains in the vendor backend.
    
    Invoke the reservation path from setup_arch(). If pKVM is disabled or no
    vendor backend provides reservation operations, return without reserving
    memory.
    
    The reservation logic is based on arm64 pKVM and may eventually be
    refactored into architecture-independent code.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

---
+++ arch/x86/include/asm/kvm_pkvm.h
@@ -0,0 +1,25 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_X86_KVM_PKVM_H
+#define _ASM_X86_KVM_PKVM_H
+
+#ifdef CONFIG_PKVM_X86
+
+struct pkvm_reserve_ops {
+	u64 (*total_pages)(void);
+};
+
+static inline int __init pkvm_vmx_init_reserve_ops(struct pkvm_reserve_ops *ops)
+{
+	return -EOPNOTSUPP;
+}
+
+#define PKVM_MEMBLOCK_REGIONS		128
+
+static inline unsigned long pkvm_data_pages(void)
+{
+	return 0;
+}
+
+#endif /* CONFIG_PKVM_X86 */
+
+#endif /* _ASM_X86_KVM_PKVM_H */

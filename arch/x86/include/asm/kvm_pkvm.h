3aed0bd020fc3b5628e29bbc17dad562c0c9a808
Chuanxiao Dong <chuanxiao.dong@intel.com>
2026-09-18 12:53:02 +0800

    pKVM: x86: Setup the VMCS host state fields with pKVM's own state
    The isolated pKVM hypervisor must enter with its own architectural state
    rather than state owned by the deprivileged host. Allocate and initialize
    pKVM-local GDT, IDT, and TSS objects for each CPU, and program the VMCS
    host-state fields to use them.
    
    pKVM runs with interrupts disabled and uses the IDT path for synchronous
    exceptions which should not happen if everything is fine, so disable
    FRED in the hypervisor host state for simplicity. An initial exception
    handler is installed for every architectural exception to halts the CPU.
    Annotate the assembly entries with UNWIND_HINT_IRET_REGS so objtool sees
    the exception-frame layout correctly.
    
    Squash changelog:
      cb855d4f8c63 ("pKVM: x86: Use UNWIND_HINT_IRET_REGS for exception entry handlers")
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/include/asm/kvm_pkvm.h
+++ arch/x86/include/asm/kvm_pkvm.h
@@ -6,6 +6,7 @@
 #include <linux/bug.h>
 #include <linux/kvm_host.h>
 #include <linux/mm.h>
+#include <asm/desc.h>
 #include <asm/pkvm_image.h>
 #include <asm/pkvm_redef.h>
 
@@ -27,9 +28,16 @@ static inline int __init pkvm_vmx_init_reserve_ops(struct pkvm_reserve_ops *ops)
 /* Size of reserved space for private parameter in pKVM stack */
 #define PKVM_STACK_TOP_RESV		16
 
+struct idt_page {
+	gate_desc idt[IDT_ENTRIES];
+} __aligned(PAGE_SIZE);
+
 struct pkvm_pcpu {
 	u8 stack[PKVM_STACK_SIZE] __aligned(16);
 	int cpu;
+	struct gdt_page gdt_page;
+	struct idt_page idt_page;
+	struct tss_struct tss;
 };
 
 struct pkvm_hyp {
@@ -52,6 +60,9 @@ PKVM_DECLARE(int, pkvm_setup_per_cpu, (int cpu, unsigned long base,
 				       unsigned long pcpu_pa, unsigned long vcpu_pa));
 PKVM_DECLARE(unsigned int, pkvm_per_cpu_nr_pages, (void));
 PKVM_DECLARE(unsigned long, pkvm_per_cpu_offset, (int cpu));
+#define GEN(x, ...) PKVM_DECLARE(void, handle_exception_##x, (void));
+#include <asm/GEN-for-each-exc.h>
+#undef GEN
 
 static inline unsigned long pkvm_data_pages(unsigned long extra_global,
 					    unsigned long extra_percpu)
